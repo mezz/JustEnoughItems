43d3b19e1f4524bcd86248bb081d2b5228cbb670
mezz <tehgeek@gmail.com>
2014-10-08 01:12:15 -0700

    Update forge, add more mod info

--- src/main/java/mezz/jei/JustEnoughItems.java
+++ src/main/java/mezz/jei/JustEnoughItems.java
@@ -6,17 +6,24 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import mezz.jei.gui.GuiWrapperManager;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = JustEnoughItems.MODID, version = JustEnoughItems.VERSION)
+@Mod(modid = JustEnoughItems.MODID,
+		name = JustEnoughItems.NAME,
+		version = JustEnoughItems.VERSION,
+		dependencies = "required-after:Forge@[10.13.0.1207,);")
 public class JustEnoughItems {
-    public static final String MODID = "JustEnoughItems";
-    public static final String VERSION = "1.0";
+	public static final String MODID = "JEI";
+	public static final String NAME = "JustEnoughItems";
+	public static final String VERSION = "1.0";
+
+	@Mod.Instance(JustEnoughItems.MODID)
+	public static JustEnoughItems instance;
 
 	public static ItemRegistry itemRegistry;
 	public static RecipeRegistry recipeRegistry;
 	public static GuiWrapperManager wrapperManager;
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(new GuiEventHandler());
 
 		itemRegistry = new ItemRegistry();
