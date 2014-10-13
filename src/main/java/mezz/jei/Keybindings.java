81d178120c53be1993b6308162f0a593de6298e8
mezz <tehgeek@gmail.com>
2014-10-12 22:35:31 -0700

    Add key bindings for recipes. #10

--- src/main/java/mezz/jei/Keybindings.java
+++ src/main/java/mezz/jei/Keybindings.java
@@ -7,11 +7,17 @@ import org.lwjgl.input.Keyboard;
 public class KeyBindings {
 
 	public static KeyBinding toggleOverlay;
+	public static KeyBinding showRecipe;
+	public static KeyBinding showUses;
 
 	public static void init() {
 
 		toggleOverlay = new KeyBinding("key.jei.toggleOverlay", Keyboard.KEY_O, "key.categories.jei");
+		showRecipe = new KeyBinding("key.jei.showRecipe", Keyboard.KEY_R, "key.categories.jei");
+		showUses = new KeyBinding("key.jei.showUses", Keyboard.KEY_U, "key.categories.jei");
 
 		ClientRegistry.registerKeyBinding(toggleOverlay);
+		ClientRegistry.registerKeyBinding(showRecipe);
+		ClientRegistry.registerKeyBinding(showUses);
 	}
 }
