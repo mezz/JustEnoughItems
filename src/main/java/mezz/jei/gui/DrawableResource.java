c5a929f2bb52ac6683279e8353db58f7064ae54b
mezz <tehgeek@gmail.com>
2014-12-04 22:12:31 -0800

    Use GuiUtils instead of extending Gui

--- src/main/java/mezz/jei/gui/DrawableResource.java
+++ src/main/java/mezz/jei/gui/DrawableResource.java
@@ -1,13 +1,13 @@
 package mezz.jei.gui;
 
+import cpw.mods.fml.client.config.GuiUtils;
 import mezz.jei.api.gui.IDrawable;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nonnull;
 
-public class DrawableResource extends Gui implements IDrawable {
+public class DrawableResource implements IDrawable {
 
 	@Nonnull
 	private final ResourceLocation resourceLocation;
@@ -37,7 +37,7 @@ public class DrawableResource extends Gui implements IDrawable {
 
 	public void draw(@Nonnull Minecraft minecraft) {
 		minecraft.getTextureManager().bindTexture(resourceLocation);
-		this.drawTexturedModalRect(0, 0, u, v, width, height);
+		GuiUtils.drawTexturedModalRect(0, 0, u, v, width, height, 0);
 	}
 
 }
