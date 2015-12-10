9de7b2276a5166258593ffe22c8a6b936ce95bca
mezz <tehgeek@gmail.com>
2015-12-10 00:45:27 -0800

    localize the new tooltips, limit tooltip line width

--- src/main/java/mezz/jei/config/Constants.java
+++ src/main/java/mezz/jei/config/Constants.java
@@ -13,4 +13,5 @@ public class Constants {
 	public static final String RESOURCE_DOMAIN = MOD_ID.toLowerCase(Locale.ENGLISH);
 	public static final String TEXTURE_GUI_PATH = "textures/gui/";
 
+	public static final int MAX_TOOLTIP_WIDTH = 125;
 }
