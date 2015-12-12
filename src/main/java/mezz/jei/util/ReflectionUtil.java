a3544e00966c64260952c961b9e4b4e17c9a7753
Shadowfacts <me@shadowfacts.net>
2015-12-11 21:57:23 -0500

    Fix obf fields & methods
    JavaDoc invokeMethod

--- src/main/java/mezz/jei/util/ReflectionUtil.java
+++ src/main/java/mezz/jei/util/ReflectionUtil.java
@@ -3,6 +3,7 @@ package mezz.jei.util;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 
@@ -20,9 +21,14 @@ public class ReflectionUtil {
 
 	private static Map<String, Field> fieldMap = new HashMap<>();
 	private static Map<String, Method> methodMap = new HashMap<>();
+	private static Map<String, String> obfFieldNameMap = new HashMap<>();
+	private static Map<String, String> obfMethodNameMap = new HashMap<>();
+
 
 	public static void initCommon() {
-
+		obfFieldNameMap.put("guiLeft", "field_147003_i");
+		obfFieldNameMap.put("xSize", "field_146999_f");
+		obfMethodNameMap.put("renderToolTip", "func_146285_a");
 	}
 
 	public static void initClient() {
@@ -39,15 +45,24 @@ public class ReflectionUtil {
 	}
 
 	private static void putField(String owner, String name) throws ClassNotFoundException, NoSuchFieldException {
-		Field f = Class.forName(owner).getDeclaredField(FMLDeobfuscatingRemapper.INSTANCE.mapFieldName(owner, name, null));
+		Field f = Class.forName(owner).getDeclaredField(getFieldName(name));
 		f.setAccessible(true);
 		fieldMap.put(owner + "." + name, f);
 	}
 
 	private static void putMethod(String owner, String name, Class... params) throws ClassNotFoundException, NoSuchMethodException {
-		Method m = Class.forName(owner).getDeclaredMethod(name, params);
+		Method m = Class.forName(owner).getDeclaredMethod(getMethodName(name), params);
 		m.setAccessible(true);
 		methodMap.put(owner + "." + name, m);
+	}
+
+	private static String getFieldName(String deobf) {
+		if ((Boolean)Launch.blackboard.get("fml.deobfuscatedEnvironment")) return deobf;
+		return obfFieldNameMap.get(deobf);
+	}
+	private static String getMethodName(String deobf) {
+		if ((Boolean)Launch.blackboard.get("fml.deobfuscatedEnvironment")) return deobf;
+		return obfMethodNameMap.get(deobf);
 	}
 
 	/**
@@ -78,6 +93,15 @@ public class ReflectionUtil {
 		return Integer.MIN_VALUE;
 	}
 
+	/**
+	 * Invokes a method
+	 * @param owner Full owner class name
+	 * @param name Unobfuscated method name
+	 * @param instance Instance to be invoked on, {@code null} if static method
+	 * @param args Arguments to be passed to the method
+	 * @return The return value of the method
+	 * @throws InvocationTargetException
+	 */
 	public static Object invokeMethod(String owner, String name, Object instance, Object... args) throws InvocationTargetException {
 		try {
 			return methodMap.get(owner + "." + name).invoke(instance, args);
