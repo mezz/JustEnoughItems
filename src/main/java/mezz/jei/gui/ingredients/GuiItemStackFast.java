53c551b3b292395a4433b077c21f791d39e01157
mezz <tehgeek@gmail.com>
2015-12-15 01:09:50 -0800

    Fix rendering for 1.8.8

--- src/main/java/mezz/jei/gui/ingredients/GuiItemStackFast.java
+++ src/main/java/mezz/jei/gui/ingredients/GuiItemStackFast.java
@@ -123,8 +123,7 @@ public class GuiItemStackFast {
 	private void renderModel(IBakedModel model, int color, ItemStack stack) {
 		Tessellator tessellator = Tessellator.getInstance();
 		WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-		worldrenderer.startDrawingQuads();
-		worldrenderer.setVertexFormat(DefaultVertexFormats.ITEM);
+		worldrenderer.func_181668_a(7, DefaultVertexFormats.ITEM);
 
 		for (EnumFacing enumfacing : EnumFacing.VALUES) {
 			this.renderQuads(worldrenderer, model.getFaceQuads(enumfacing), color, stack);
