a7359b6da3a40a4e920bda91a57a2554974f78b9
mezz <tehgeek@gmail.com>
2015-12-17 23:23:29 -0800

    Bump version for first Release

--- src/main/java/mezz/jei/api/package-info.java
+++ src/main/java/mezz/jei/api/package-info.java
@@ -1,4 +1,4 @@
-@API(apiVersion = "0.34.1", owner = "JEI", provides = "JustEnoughItemsAPI")
+@API(apiVersion = "1.0.0", owner = "JEI", provides = "JustEnoughItemsAPI")
 package mezz.jei.api;
 
 import net.minecraftforge.fml.common.API;
