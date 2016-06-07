39976e1c9da8fca880cbdc29c77b3a65cac562e1
mezz <tehgeek@gmail.com>
2016-06-07 13:52:33 -0700

    Fix #306 update required Forge version

--- src/main/java/mezz/jei/JustEnoughItems.java
+++ src/main/java/mezz/jei/JustEnoughItems.java
@@ -22,7 +22,7 @@ import java.util.Map;
 		version = Constants.VERSION,
 		guiFactory = "mezz.jei.config.JEIModGuiFactory",
 		acceptedMinecraftVersions = "[1.9,1.9.4]",
-		dependencies = "required-after:Forge@[12.16.0.1865,);")
+		dependencies = "required-after:Forge@[12.17.0.1946,);")
 public class JustEnoughItems {
 
 	@SidedProxy(clientSide = "mezz.jei.ProxyCommonClient", serverSide = "mezz.jei.ProxyCommon")
