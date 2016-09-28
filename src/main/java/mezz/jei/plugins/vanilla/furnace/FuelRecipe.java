532dedfeefa37e323e7dccf330963b7c0835ece3
mezz <tehgeek@gmail.com>
2016-09-27 23:42:43 -0700

    Close #451 Show "Smelts # items" for furnace fuels

--- src/main/java/mezz/jei/plugins/vanilla/furnace/FuelRecipe.java
+++ src/main/java/mezz/jei/plugins/vanilla/furnace/FuelRecipe.java
@@ -1,6 +1,7 @@
 package mezz.jei.plugins.vanilla.furnace;
 
 import java.awt.Color;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -18,12 +19,23 @@ import net.minecraft.util.ResourceLocation;
 
 public class FuelRecipe extends BlankRecipeWrapper {
 	private final List<List<ItemStack>> inputs;
+	private final String smeltCountString;
 	private final String burnTimeString;
 	private final IDrawableAnimated flame;
 
 	public FuelRecipe(IGuiHelper guiHelper, Collection<ItemStack> input, int burnTime) {
 		List<ItemStack> inputList = new ArrayList<ItemStack>(input);
 		this.inputs = Collections.singletonList(inputList);
+
+		if (burnTime == 200) {
+			this.smeltCountString = Translator.translateToLocal("gui.jei.category.fuel.smeltCount.single");
+		} else {
+			NumberFormat numberInstance = NumberFormat.getNumberInstance();
+			numberInstance.setMaximumFractionDigits(2);
+			String smeltCount = numberInstance.format(burnTime / 200f);
+			this.smeltCountString = Translator.translateToLocalFormatted("gui.jei.category.fuel.smeltCount", smeltCount);
+		}
+
 		this.burnTimeString = Translator.translateToLocalFormatted("gui.jei.category.fuel.burnTime", burnTime);
 
 		ResourceLocation furnaceBackgroundLocation = new ResourceLocation("minecraft", "textures/gui/container/furnace.png");
@@ -43,7 +55,8 @@ public class FuelRecipe extends BlankRecipeWrapper {
 
 	@Override
 	public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
-		minecraft.fontRendererObj.drawString(burnTimeString, 24, 12, Color.gray.getRGB());
+		minecraft.fontRendererObj.drawString(smeltCountString, 24, 8, Color.gray.getRGB());
+		minecraft.fontRendererObj.drawString(burnTimeString, 24, 18, Color.gray.getRGB());
 	}
 
 	@Override
