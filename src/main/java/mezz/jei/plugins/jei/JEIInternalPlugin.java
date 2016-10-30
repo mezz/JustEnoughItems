4da735e65b0277dbae8d65bbbf2106ea6682eab5
mezz <tehgeek@gmail.com>
2016-10-30 15:47:07 -0700

    Remove DebugRecipeRegistryPlugin

--- src/main/java/mezz/jei/plugins/jei/JEIInternalPlugin.java
+++ src/main/java/mezz/jei/plugins/jei/JEIInternalPlugin.java
@@ -20,7 +20,6 @@ import mezz.jei.config.Config;
 import mezz.jei.plugins.jei.debug.DebugRecipe;
 import mezz.jei.plugins.jei.debug.DebugRecipeCategory;
 import mezz.jei.plugins.jei.debug.DebugRecipeHandler;
-import mezz.jei.plugins.jei.debug.DebugRecipeRegistryPlugin;
 import mezz.jei.plugins.jei.description.ItemDescriptionRecipeCategory;
 import mezz.jei.plugins.jei.description.ItemDescriptionRecipeHandler;
 import mezz.jei.plugins.jei.ingredients.DebugIngredient;
@@ -96,8 +95,6 @@ public class JEIInternalPlugin extends BlankModPlugin {
 					);
 				}
 			});
-
-			registry.addRecipeRegistryPlugin(new DebugRecipeRegistryPlugin());
 		}
 	}
 
