75c026f45bdd324fa78e0c8b519920e035c0a243
gigaherz <gigaherz@gmail.com>
2017-02-13 21:30:45 +0100

    Use the right Pair implementation.

--- src/main/java/mezz/jei/plugins/vanilla/anvil/AnvilRecipeMaker.java
+++ src/main/java/mezz/jei/plugins/vanilla/anvil/AnvilRecipeMaker.java
@@ -2,7 +2,6 @@ package mezz.jei.plugins.vanilla.anvil;
 
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
-import javafx.util.Pair;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.util.Log;
 import net.minecraft.enchantment.Enchantment;
@@ -12,6 +11,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.oredict.OreDictionary;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Collections;
 import java.util.List;
@@ -52,8 +52,6 @@ public class AnvilRecipeMaker {
 		}
 	}
 
-	private static <T1, T2> Pair<T1, T2> pairOf(T1 left, T2 right) { return new Pair<T1, T2>(left, right); }
-
 	@SuppressWarnings("unchecked")
 	private static void registerRepairRecipes(IModRegistry registry) {
 		ItemStack repairWood = new ItemStack(Blocks.PLANKS, 1, OreDictionary.WILDCARD_VALUE);
@@ -63,62 +61,62 @@ public class AnvilRecipeMaker {
 		ItemStack repairDiamond = new ItemStack(Items.DIAMOND);
 		ItemStack repairLeather = new ItemStack(Items.LEATHER);
 		List<Pair<ItemStack,ItemStack>> items = Lists.newArrayList(
-				pairOf(new ItemStack(Items.WOODEN_SWORD), repairWood),
-				pairOf(new ItemStack(Items.WOODEN_PICKAXE), repairWood),
-				pairOf(new ItemStack(Items.WOODEN_AXE), repairWood),
-				pairOf(new ItemStack(Items.WOODEN_SHOVEL), repairWood),
-				pairOf(new ItemStack(Items.WOODEN_HOE), repairWood),
-				pairOf(new ItemStack(Items.STONE_SWORD), repairStone),
-				pairOf(new ItemStack(Items.STONE_PICKAXE), repairStone),
-				pairOf(new ItemStack(Items.STONE_AXE), repairStone),
-				pairOf(new ItemStack(Items.STONE_SHOVEL), repairStone),
-				pairOf(new ItemStack(Items.STONE_HOE), repairStone),
-				pairOf(new ItemStack(Items.LEATHER_HELMET), repairLeather),
-				pairOf(new ItemStack(Items.LEATHER_CHESTPLATE), repairLeather),
-				pairOf(new ItemStack(Items.LEATHER_LEGGINGS), repairLeather),
-				pairOf(new ItemStack(Items.LEATHER_BOOTS), repairLeather),
-				pairOf(new ItemStack(Items.IRON_SWORD), repairIron),
-				pairOf(new ItemStack(Items.IRON_PICKAXE), repairIron),
-				pairOf(new ItemStack(Items.IRON_AXE), repairIron),
-				pairOf(new ItemStack(Items.IRON_SHOVEL), repairIron),
-				pairOf(new ItemStack(Items.IRON_HOE), repairIron),
-				pairOf(new ItemStack(Items.IRON_HELMET), repairIron),
-				pairOf(new ItemStack(Items.IRON_CHESTPLATE), repairIron),
-				pairOf(new ItemStack(Items.IRON_LEGGINGS), repairIron),
-				pairOf(new ItemStack(Items.IRON_BOOTS), repairIron),
-				pairOf(new ItemStack(Items.CHAINMAIL_HELMET), repairIron),
-				pairOf(new ItemStack(Items.CHAINMAIL_CHESTPLATE), repairIron),
-				pairOf(new ItemStack(Items.CHAINMAIL_LEGGINGS), repairIron),
-				pairOf(new ItemStack(Items.CHAINMAIL_BOOTS), repairIron),
-				pairOf(new ItemStack(Items.GOLDEN_SWORD), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_PICKAXE), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_AXE), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_SHOVEL), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_HOE), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_HELMET), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_CHESTPLATE), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_LEGGINGS), repairGold),
-				pairOf(new ItemStack(Items.GOLDEN_BOOTS), repairGold),
-				pairOf(new ItemStack(Items.DIAMOND_SWORD), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_PICKAXE), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_AXE), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_SHOVEL), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_HOE), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_HELMET), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_CHESTPLATE), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_LEGGINGS), repairDiamond),
-				pairOf(new ItemStack(Items.DIAMOND_BOOTS), repairDiamond),
-				pairOf(new ItemStack(Items.SHIELD), repairWood),
-				pairOf(new ItemStack(Items.ELYTRA), repairLeather)
+				Pair.of(new ItemStack(Items.WOODEN_SWORD), repairWood),
+				Pair.of(new ItemStack(Items.WOODEN_PICKAXE), repairWood),
+				Pair.of(new ItemStack(Items.WOODEN_AXE), repairWood),
+				Pair.of(new ItemStack(Items.WOODEN_SHOVEL), repairWood),
+				Pair.of(new ItemStack(Items.WOODEN_HOE), repairWood),
+				Pair.of(new ItemStack(Items.STONE_SWORD), repairStone),
+				Pair.of(new ItemStack(Items.STONE_PICKAXE), repairStone),
+				Pair.of(new ItemStack(Items.STONE_AXE), repairStone),
+				Pair.of(new ItemStack(Items.STONE_SHOVEL), repairStone),
+				Pair.of(new ItemStack(Items.STONE_HOE), repairStone),
+				Pair.of(new ItemStack(Items.LEATHER_HELMET), repairLeather),
+				Pair.of(new ItemStack(Items.LEATHER_CHESTPLATE), repairLeather),
+				Pair.of(new ItemStack(Items.LEATHER_LEGGINGS), repairLeather),
+				Pair.of(new ItemStack(Items.LEATHER_BOOTS), repairLeather),
+				Pair.of(new ItemStack(Items.IRON_SWORD), repairIron),
+				Pair.of(new ItemStack(Items.IRON_PICKAXE), repairIron),
+				Pair.of(new ItemStack(Items.IRON_AXE), repairIron),
+				Pair.of(new ItemStack(Items.IRON_SHOVEL), repairIron),
+				Pair.of(new ItemStack(Items.IRON_HOE), repairIron),
+				Pair.of(new ItemStack(Items.IRON_HELMET), repairIron),
+				Pair.of(new ItemStack(Items.IRON_CHESTPLATE), repairIron),
+				Pair.of(new ItemStack(Items.IRON_LEGGINGS), repairIron),
+				Pair.of(new ItemStack(Items.IRON_BOOTS), repairIron),
+				Pair.of(new ItemStack(Items.CHAINMAIL_HELMET), repairIron),
+				Pair.of(new ItemStack(Items.CHAINMAIL_CHESTPLATE), repairIron),
+				Pair.of(new ItemStack(Items.CHAINMAIL_LEGGINGS), repairIron),
+				Pair.of(new ItemStack(Items.CHAINMAIL_BOOTS), repairIron),
+				Pair.of(new ItemStack(Items.GOLDEN_SWORD), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_PICKAXE), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_AXE), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_SHOVEL), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_HOE), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_HELMET), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_CHESTPLATE), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_LEGGINGS), repairGold),
+				Pair.of(new ItemStack(Items.GOLDEN_BOOTS), repairGold),
+				Pair.of(new ItemStack(Items.DIAMOND_SWORD), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_PICKAXE), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_AXE), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_SHOVEL), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_HOE), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_HELMET), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_CHESTPLATE), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_LEGGINGS), repairDiamond),
+				Pair.of(new ItemStack(Items.DIAMOND_BOOTS), repairDiamond),
+				Pair.of(new ItemStack(Items.SHIELD), repairWood),
+				Pair.of(new ItemStack(Items.ELYTRA), repairLeather)
 				);
 		for (Pair<ItemStack,ItemStack> entry : items) {
-			ItemStack damaged1 = entry.getKey().copy();
+			ItemStack damaged1 = entry.getLeft().copy();
 			damaged1.setItemDamage(damaged1.getMaxDamage() * 3 / 4);
-			ItemStack damaged2 = entry.getKey().copy();
+			ItemStack damaged2 = entry.getLeft().copy();
 			damaged2.setItemDamage(damaged2.getMaxDamage() / 2);
-			ItemStack damaged3 = entry.getKey().copy();
+			ItemStack damaged3 = entry.getLeft().copy();
 			damaged3.setItemDamage(damaged2.getMaxDamage() / 4);
-			registry.addAnvilRecipe(damaged2, entry.getValue(), damaged3);
+			registry.addAnvilRecipe(damaged2, entry.getRight(), damaged3);
 			registry.addAnvilRecipe(damaged1, damaged2, damaged3);
 		}
 	}
