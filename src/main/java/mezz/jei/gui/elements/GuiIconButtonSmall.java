f1b53f107b527e31622938f76862d2b897b78ab8
mezz <tehgeek@gmail.com>
2017-04-04 04:26:29 -0700

    Color small button icons to match the ones on large buttons

--- src/main/java/mezz/jei/gui/elements/GuiIconButtonSmall.java
+++ src/main/java/mezz/jei/gui/elements/GuiIconButtonSmall.java
@@ -4,6 +4,7 @@ import mezz.jei.api.gui.IDrawable;
 import mezz.jei.config.Constants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.config.GuiUtils;
 
@@ -28,6 +29,22 @@ public class GuiIconButtonSmall extends GuiButton {
 			int k = this.getHoverState(this.hovered);
 			GuiUtils.drawContinuousTexturedBox(BUTTON_RESOURCE, this.xPosition, this.yPosition, 0, 182 + k * 20, this.width, this.height, 95, 20, 2, 2, 2, 2, this.zLevel);
 			this.mouseDragged(mc, mouseX, mouseY);
+
+			int color = 14737632;
+			if (!this.enabled) {
+				color = 10526880;
+			} else if (this.hovered) {
+				color = 16777120;
+			}
+			if ((color & -67108864) == 0) {
+				color |= -16777216;
+			}
+
+			float red = (float)(color >> 16 & 255) / 255.0F;
+			float blue = (float)(color >> 8 & 255) / 255.0F;
+			float green = (float)(color & 255) / 255.0F;
+			float alpha = (float)(color >> 24 & 255) / 255.0F;
+			GlStateManager.color(red, blue, green, alpha);
 
 			int xOffset = xPosition + (height - this.icon.getWidth()) / 2;
 			int yOffset = yPosition + (width - this.icon.getHeight()) / 2;
