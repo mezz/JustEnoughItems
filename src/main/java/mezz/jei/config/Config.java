687f9efcb52484731b737e593a57e1204624b6d0
mezz <tehgeek@gmail.com>
2017-06-17 12:39:39 -0700

    Increase the default value for maxSubtypes from 100 to 300

--- src/main/java/mezz/jei/config/Config.java
+++ src/main/java/mezz/jei/config/Config.java
@@ -25,6 +25,7 @@ import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.versioning.ArtifactVersion;
 
 public final class Config {
 	private static final String configKeyPrefix = "config.jei";
@@ -219,7 +220,7 @@ public final class Config {
 		final File searchColorsConfigFile = new File(jeiConfigurationDir, "searchColors.cfg");
 		final File worldConfigFile = new File(jeiConfigurationDir, "worldSettings.cfg");
 		worldConfig = new Configuration(worldConfigFile, "0.1.0");
-		config = new LocalizedConfiguration(configKeyPrefix, configFile, "0.3.0");
+		config = new LocalizedConfiguration(configKeyPrefix, configFile, "0.4.0");
 		itemBlacklistConfig = new LocalizedConfiguration(configKeyPrefix, itemBlacklistConfigFile, "0.1.0");
 		searchColorsConfig = new LocalizedConfiguration(configKeyPrefix, searchColorsConfigFile, "0.1.0");
 
@@ -282,10 +283,16 @@ public final class Config {
 
 		SearchMode[] searchModes = SearchMode.values();
 
-		// set new defaults moving from config version 0.2.0 to 0.3.0
-		if ("0.2.0".equals(config.getLoadedConfigVersion())) {
+		String loadedConfigVersion = config.getLoadedConfigVersion();
+		// set new defaults moving to config version 0.3.0
+		if (loadedConfigVersion != null && versionCompare(loadedConfigVersion, "0.3.0") < 0) {
 			config.setEnum("creativeTabSearchMode", CATEGORY_SEARCH, defaultValues.creativeTabSearchMode, searchModes);
 			config.setEnum("oreDictSearchMode", CATEGORY_SEARCH, defaultValues.oreDictSearchMode, searchModes);
+		}
+
+		// set new defaults moving to config version 0.4.0
+		if (loadedConfigVersion != null && versionCompare(loadedConfigVersion, "0.4.0") < 0) {
+			config.setInt("maxSubtypes", CATEGORY_ADVANCED, defaultValues.maxSubtypes, 10, 10000);
 		}
 
 		values.modNameSearchMode = config.getEnum("modNameSearchMode", CATEGORY_SEARCH, defaultValues.modNameSearchMode, searchModes);
@@ -508,5 +515,38 @@ public final class Config {
 		ITEM, WILDCARD, MOD_ID;
 
 		public static final IngredientBlacklistType[] VALUES = values();
+	}
+
+	/**
+	 * https://stackoverflow.com/questions/6701948/efficient-way-to-compare-version-strings-in-java
+	 * Compares two version strings.
+	 *
+	 * Use this instead of String.compareTo() for a non-lexicographical
+	 * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
+	 *
+	 * note It does not work if "1.10" is supposed to be equal to "1.10.0".
+	 *
+	 * @param str1 a string of ordinal numbers separated by decimal points.
+	 * @param str2 a string of ordinal numbers separated by decimal points.
+	 * @return The result is a negative integer if str1 is _numerically_ less than str2.
+	 *         The result is a positive integer if str1 is _numerically_ greater than str2.
+	 *         The result is zero if the strings are _numerically_ equal.
+	 */
+	private static int versionCompare(String str1, String str2) {
+		String[] vals1 = str1.split("\\.");
+		String[] vals2 = str2.split("\\.");
+		int i = 0;
+		// set index to first non-equal ordinal or length of shortest version string
+		while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
+			i++;
+		}
+		// compare first non-equal ordinal number
+		if (i < vals1.length && i < vals2.length) {
+			int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+			return Integer.signum(diff);
+		}
+		// the strings are equal or one string is a substring of the other
+		// e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+		return Integer.signum(vals1.length - vals2.length);
 	}
 }
