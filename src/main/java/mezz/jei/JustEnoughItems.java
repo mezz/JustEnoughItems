e1a1524c9cd9ce17f847be468dcd58950fa10f9f
mezz <tehgeek@gmail.com>
2017-08-11 21:45:18 -0700

    Update dependencies, add explicit support for 1.12.1

--- src/main/java/mezz/jei/JustEnoughItems.java
+++ src/main/java/mezz/jei/JustEnoughItems.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.relauncher.Side;
 		name = Constants.NAME,
 		version = Constants.VERSION,
 		guiFactory = "mezz.jei.config.JEIModGuiFactory",
-		acceptedMinecraftVersions = "[1.12]",
+		acceptedMinecraftVersions = "[1.12,1.13)",
 		dependencies = "required-after:forge@[14.21.0.2348,);")
 public class JustEnoughItems {
 
