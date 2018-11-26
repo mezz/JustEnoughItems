41d7f9e508fd0ac652f06a9f3a3e579c086071c8
mezz <tehgeek@gmail.com>
2018-11-25 23:01:37 -0800

    add missing package-info

--- src/main/java/mezz/jei/bookmarks/package-info.java
+++ src/main/java/mezz/jei/bookmarks/package-info.java
@@ -1,1 +1,9 @@
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 package mezz.jei.bookmarks;
+
+import mcp.MethodsReturnNonnullByDefault;
+import mezz.jei.util.FieldsAreNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
