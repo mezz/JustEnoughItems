7304af4c4ccede849333a6207d6ae092ae0f644e
mezz <tehgeek@gmail.com>
2018-12-01 14:29:59 -0800

    Set keybind back to A

--- src/main/java/mezz/jei/config/KeyBindings.java
+++ src/main/java/mezz/jei/config/KeyBindings.java
@@ -35,7 +35,7 @@ public final class KeyBindings {
 			recipeBack = new KeyBinding("key.jei.recipeBack", KeyConflictContext.GUI, Keyboard.KEY_BACK, categoryName),
 			previousPage = new KeyBinding("key.jei.previousPage", KeyConflictContext.GUI, Keyboard.KEY_PRIOR, categoryName),
 			nextPage = new KeyBinding("key.jei.nextPage", KeyConflictContext.GUI, Keyboard.KEY_NEXT, categoryName),
-			bookmark = new KeyBinding("key.jei.bookmark", KeyConflictContext.GUI, Keyboard.KEY_B, categoryName),
+			bookmark = new KeyBinding("key.jei.bookmark", KeyConflictContext.GUI, Keyboard.KEY_A, categoryName),
 			toggleBookmarkOverlay = new KeyBinding("key.jei.toggleBookmarkOverlay", KeyConflictContext.GUI, Keyboard.KEY_NONE, categoryName)
 		);
 	}
