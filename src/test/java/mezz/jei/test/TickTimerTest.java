a1e01f5817007b4ab8f1222a7e8f7e237550e13c
mezz <tehgeek@gmail.com>
2020-06-19 16:51:43 -0700

    WIP update to FG3

--- src/test/java/mezz/jei/test/TickTimerTest.java
+++ src/test/java/mezz/jei/test/TickTimerTest.java
@@ -1,8 +1,8 @@
 package mezz.jei.test;
 
 import mezz.jei.gui.TickTimer;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class TickTimerTest {
 	@Test
@@ -12,11 +12,11 @@ public class TickTimerTest {
 		for (int i = 0; i < 1000; i++) {
 			int expectedValue = (i % msPerCycle) * 50;
 			int value = TickTimer.getValue(0, i, maxValue, msPerCycle, false);
-			Assert.assertEquals(expectedValue, value);
+			Assertions.assertEquals(expectedValue, value);
 
 			int expectedDownValue = maxValue - expectedValue;
 			int downValue = TickTimer.getValue(0, i, maxValue, msPerCycle, true);
-			Assert.assertEquals(expectedDownValue, downValue);
+			Assertions.assertEquals(expectedDownValue, downValue);
 		}
 	}
 
@@ -24,15 +24,15 @@ public class TickTimerTest {
 	public void testMoreTicksThanValuesMath() {
 		int maxValue = 4;
 		int msPerCycle = 20;
-		int[] expectedValues = new int[] {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
+		int[] expectedValues = new int[]{0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
 		for (int i = 0; i < 1000; i++) {
 			int expectedValue = expectedValues[i % msPerCycle];
 			int value = TickTimer.getValue(0, i, maxValue, msPerCycle, false);
-			Assert.assertEquals(expectedValue, value);
+			Assertions.assertEquals(expectedValue, value);
 
 			int expectedDownValue = maxValue - expectedValue;
 			int downValue = TickTimer.getValue(0, i, maxValue, msPerCycle, true);
-			Assert.assertEquals(expectedDownValue, downValue);
+			Assertions.assertEquals(expectedDownValue, downValue);
 		}
 	}
 
@@ -40,15 +40,15 @@ public class TickTimerTest {
 	public void testIndivisibleTicking() {
 		int maxValue = 3;
 		int msPerCycle = 10;
-		int[] expectedValues = new int[] {0, 0, 0, 1, 1, 2, 2, 2, 3, 3};
+		int[] expectedValues = new int[]{0, 0, 0, 1, 1, 2, 2, 2, 3, 3};
 		for (int i = 0; i < 1000; i++) {
 			int expectedValue = expectedValues[i % msPerCycle];
 			int value = TickTimer.getValue(0, i, maxValue, msPerCycle, false);
-			Assert.assertEquals(expectedValue, value);
+			Assertions.assertEquals(expectedValue, value);
 
 			int expectedDownValue = maxValue - expectedValue;
 			int downValue = TickTimer.getValue(0, i, maxValue, msPerCycle, true);
-			Assert.assertEquals(expectedDownValue, downValue);
+			Assertions.assertEquals(expectedDownValue, downValue);
 		}
 	}
 }
