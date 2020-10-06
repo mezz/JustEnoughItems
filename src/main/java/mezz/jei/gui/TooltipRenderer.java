f1a3b34847267dbe8ab08cb97d499d136a9b67a6
Richard Freimer <richard@freimer.com>
2020-10-06 16:09:03 -0400

    Switch back to using GuiUtils.drawHoveringText so that the tooltip has wrapping again

--- src/main/java/mezz/jei/gui/TooltipRenderer.java
+++ src/main/java/mezz/jei/gui/TooltipRenderer.java
@@ -40,12 +40,6 @@ public final class TooltipRenderer {
 		int scaledWidth = minecraft.getMainWindow().getScaledWidth();
 		int scaledHeight = minecraft.getMainWindow().getScaledHeight();
 		ItemStack itemStack = ingredient instanceof ItemStack ? (ItemStack) ingredient : ItemStack.EMPTY;
-		
-		// Vanilla workaround for tool tips. Use the below when forge adds it back again
-		if(minecraft.currentScreen != null)
-			minecraft.currentScreen.renderToolTip(matrixStack, textLines.stream().map(LanguageMap.getInstance()::func_241870_a).collect(ImmutableList.toImmutableList()), x, y, font);
-		
-		// TODO use again when available 
-		//GuiUtils.drawHoveringText(itemStack, matrixStack, textLines, x, y, scaledWidth, scaledHeight, maxWidth, font);
+		GuiUtils.drawHoveringText(itemStack, matrixStack, textLines, x, y, scaledWidth, scaledHeight, maxWidth, font);
 	}
 }
