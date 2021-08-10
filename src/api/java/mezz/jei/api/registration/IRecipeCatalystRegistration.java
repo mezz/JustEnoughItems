ff3b7ffa6be816e4e78e8256bd7f1f878ce808e1
mezz <tehgeek@gmail.com>
2021-08-09 18:40:34 -1000

    clarify javadocs for addRecipeCatalyst

--- src/api/java/mezz/jei/api/registration/IRecipeCatalystRegistration.java
+++ src/api/java/mezz/jei/api/registration/IRecipeCatalystRegistration.java
@@ -7,11 +7,13 @@ import net.minecraft.world.item.ItemStack;
 
 public interface IRecipeCatalystRegistration {
 	/**
-	 * Add an association between an ingredient and what it can craft. (i.e. Furnace ItemStack -> Smelting and Fuel Recipes)
+	 * Add an association between an {@link ItemStack} and what it can craft.
+	 * (i.e. Furnace ItemStack can craft Smelting and Fuel Recipes)
 	 * Allows players to see what ingredient they need to craft in order to make recipes from a recipe category.
 	 *
-	 * @param catalystIngredient the ingredient that can craft recipes (like a furnace or crafting table)
-	 * @param recipeCategoryUids the recipe categories handled by the ingredient
+	 * @param catalystIngredient the {@link ItemStack} that can craft recipes (like a furnace or crafting table)
+	 * @param recipeCategoryUids the recipe categories handled by the catalyst
+	 * @see #addRecipeCatalyst(IIngredientType, Object, ResourceLocation...) to add non-{@link ItemStack} catalysts.
 	 */
 	default void addRecipeCatalyst(ItemStack catalystIngredient, ResourceLocation... recipeCategoryUids) {
 		addRecipeCatalyst(VanillaTypes.ITEM, catalystIngredient, recipeCategoryUids);
