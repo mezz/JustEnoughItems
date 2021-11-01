5f4bbc0b281a260d21714874bbe64e4f14e5bfa9
Sintinium <sintinium@gmail.com>
2021-10-31 21:31:39 -0500

    Add support to open the config screen with Configured mod (#2543)

--- src/main/java/mezz/jei/config/JEIClientConfig.java
+++ src/main/java/mezz/jei/config/JEIClientConfig.java
@@ -1,15 +1,23 @@
 package mezz.jei.config;
 
+import mezz.jei.api.constants.ModIds;
 import mezz.jei.events.EventBusHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
+
+import java.util.Optional;
+import java.util.function.BiFunction;
 
 public class JEIClientConfig {
 	private static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
@@ -43,12 +51,18 @@ public class JEIClientConfig {
 			return;
 		}
 
-		ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.curseforge.com/minecraft/mc-mods/configured");
-		Style style = Style.EMPTY
-				.setUnderlined(true)
-				.withClickEvent(clickEvent);
-		TranslationTextComponent textComponent = new TranslationTextComponent("jei.message.configured");
-		ITextComponent message = textComponent.setStyle(style);
-		mc.player.displayClientMessage(message, false);
+        ModContainer jeiContainer = ModList.get().getModContainerById(ModIds.JEI_ID).get();
+        Optional<BiFunction<Minecraft, Screen, Screen>> configGuiFactory = jeiContainer.getCustomExtension(ExtensionPoint.CONFIGGUIFACTORY);
+        if (configGuiFactory.isPresent()) {
+            mc.setScreen(configGuiFactory.get().apply(mc, mc.screen));
+        } else {
+            ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.curseforge.com/minecraft/mc-mods/configured");
+            Style style = Style.EMPTY
+                    .setUnderlined(true)
+                    .withClickEvent(clickEvent);
+            TranslationTextComponent textComponent = new TranslationTextComponent("jei.message.configured");
+            ITextComponent message = textComponent.setStyle(style);
+            mc.player.displayClientMessage(message, false);
+        }
 	}
 }
