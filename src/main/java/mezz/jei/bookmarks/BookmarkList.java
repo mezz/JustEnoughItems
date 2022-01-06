2746a549c89ca7e505b76778702130131a92db51
mezz <tehgeek@gmail.com>
2022-01-06 16:23:04 +0100

    Fix #1940 Allow bookmarking the same item with different NBT

--- src/main/java/mezz/jei/bookmarks/BookmarkList.java
+++ src/main/java/mezz/jei/bookmarks/BookmarkList.java
@@ -11,6 +11,7 @@ import mezz.jei.gui.ingredients.IIngredientListElement;
 import mezz.jei.gui.overlay.IIngredientGridSource;
 import mezz.jei.ingredients.IngredientListElementFactory;
 import mezz.jei.ingredients.IngredientManager;
+import net.minecraft.world.item.ItemStack;
 
 public class BookmarkList implements IIngredientGridSource {
 	private final List<Object> list = new LinkedList<>();
@@ -37,15 +38,20 @@ public class BookmarkList implements IIngredientGridSource {
 		return false;
 	}
 
-	private boolean contains(Object ingredient) {
+	private <T> boolean contains(T ingredient) {
 		// We cannot assume that ingredients have a working equals() implementation. Even ItemStack doesn't have one...
-		IIngredientHelper<Object> ingredientHelper = ingredientManager.getIngredientHelper(ingredient);
+		IIngredientHelper<T> ingredientHelper = ingredientManager.getIngredientHelper(ingredient);
 		for (Object existing : list) {
 			if (ingredient == existing) {
 				return true;
 			}
-			if (existing != null && existing.getClass() == ingredient.getClass()) {
-				if (equalUids(ingredientHelper, existing, ingredient)) {
+			if (ingredient.getClass().isInstance(existing)) {
+				@SuppressWarnings("unchecked")
+				T castExisting = (T) existing;
+				if (ingredient instanceof ItemStack) {
+					return ItemStack.matches((ItemStack) ingredient, (ItemStack) castExisting);
+				}
+				if (equalUids(ingredientHelper, castExisting, ingredient)) {
 					return true;
 				}
 			}
@@ -53,7 +59,7 @@ public class BookmarkList implements IIngredientGridSource {
 		return false;
 	}
 
-	private static boolean equalUids(IIngredientHelper<Object> ingredientHelper, Object a, Object b) {
+	private static <T> boolean equalUids(IIngredientHelper<T> ingredientHelper, T a, T b) {
 		String uidA = ingredientHelper.getUniqueId(a, UidContext.Ingredient);
 		String uidB = ingredientHelper.getUniqueId(b, UidContext.Ingredient);
 		return uidA.equals(uidB);
