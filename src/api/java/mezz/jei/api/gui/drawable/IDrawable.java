a473348b4aed26ab2ef929a1d502ff5eaabbe1a8
mezz <tehgeek@gmail.com>
2022-02-08 10:44:14 -0800

    improve IDrawable doc links

--- src/api/java/mezz/jei/api/gui/drawable/IDrawable.java
+++ src/api/java/mezz/jei/api/gui/drawable/IDrawable.java
@@ -1,15 +1,24 @@
 package mezz.jei.api.gui.drawable;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 
 /**
  * Represents something to be drawn on screen.
- * Useful for drawing miscellaneous things in
- * {@link IRecipeCategory#draw(Object, IRecipeLayoutView, PoseStack, double, double)}.
+ *
+ * Useful for drawing miscellaneous things like in
+ * {@link IRecipeCategory#draw(Object, IRecipeSlotsView, PoseStack, double, double)}.
+ * {@link IRecipeCategory#getBackground()}
+ * {@link IRecipeSlotBuilder#setBackground(IDrawable)}
+ * {@link IRecipeSlotBuilder#setOverlay(IDrawable)}]
+ * and anywhere else things are drawn on the screen.
  *
  * @see IGuiHelper for many functions to create IDrawables.
+ * @see IGuiHelper#createDrawableIngredient(IIngredientType, Object) to draw an ingredient.
  * @see IDrawableAnimated
  * @see IDrawableStatic
  */
