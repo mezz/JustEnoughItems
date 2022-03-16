aeb8eca44ce9ade21a231c46be121b81216c712f
mezz <tehgeek@gmail.com>
2022-03-15 18:40:15 -1000

    use tabs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,26 @@
 pluginManagement {
-    repositories {
+	repositories {
 		maven("https://maven.minecraftforge.net")
 		maven("https://maven.parchmentmc.org")
-        maven("https://maven.blamejared.com")
-        gradlePluginPortal()
-        maven("https://maven.fabricmc.net/") {
-            name = "Fabric"
-        }
-        maven("https://repo.spongepowered.org/repository/maven-public/") {
-            name = "Sponge Snapshots"
-        }
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "net.minecraftforge.gradle") {
-                useModule("${requested.id}:ForgeGradle:${requested.version}")
-            }
-            if (requested.id.id == "org.spongepowered.mixin") {
-                useModule("org.spongepowered:mixingradle:${requested.version}")
-            }
-        }
-    }
+		maven("https://maven.blamejared.com")
+		gradlePluginPortal()
+		maven("https://maven.fabricmc.net/") {
+			name = "Fabric"
+		}
+		maven("https://repo.spongepowered.org/repository/maven-public/") {
+			name = "Sponge Snapshots"
+		}
+	}
+	resolutionStrategy {
+		eachPlugin {
+			if (requested.id.id == "net.minecraftforge.gradle") {
+				useModule("${requested.id}:ForgeGradle:${requested.version}")
+			}
+			if (requested.id.id == "org.spongepowered.mixin") {
+				useModule("org.spongepowered:mixingradle:${requested.version}")
+			}
+		}
+	}
 }
 
 val minecraftVersion: String by settings
