ac9cbc0f58c86c85cc14370db4156922e861c982
mezz <tehgeek@gmail.com>
2022-03-17 09:13:00 -1000

    Migrate build.gradle to build.gradle.kts (#2761)
    Co-authored-by: Jared <jaredlll08@gmail.com>

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+	repositories {
+		maven("https://maven.minecraftforge.net")
+		maven("https://maven.parchmentmc.org")
+		maven("https://maven.blamejared.com")
+		gradlePluginPortal()
+		maven("https://maven.fabricmc.net/") {
+			name = "Fabric"
+		}
+		maven("https://repo.spongepowered.org/repository/maven-public/") {
+			name = "Sponge Snapshots"
+		}
+	}
+	resolutionStrategy {
+		eachPlugin {
+			if (requested.id.id == "net.minecraftforge.gradle") {
+				useModule("${requested.id}:ForgeGradle:${requested.version}")
+			}
+			if (requested.id.id == "org.spongepowered.mixin") {
+				useModule("org.spongepowered:mixingradle:${requested.version}")
+			}
+		}
+	}
+}
+
+val minecraftVersion: String by settings
+
+rootProject.name = "jei-${minecraftVersion}"
