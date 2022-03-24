6ef09b41d504104cfa403f59e2b14a21ea7d4986
mezz <tehgeek@gmail.com>
2022-03-24 11:52:30 -1000

    Fix #2756 Fuel recipes with high values can overflow the recipe background
    (cherry picked from commit 8edf068e17646debaf3f095d32a736b172448614)

--- src/main/java/mezz/jei/plugins/vanilla/cooking/fuel/FurnaceFuelCategory.java
+++ src/main/java/mezz/jei/plugins/vanilla/cooking/fuel/FurnaceFuelCategory.java
@@ -3,6 +3,7 @@ package mezz.jei.plugins.vanilla.cooking.fuel;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.Rectangle2d;
 import net.minecraft.util.ResourceLocation;
 
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
@@ -17,6 +18,7 @@ import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.config.Constants;
 import mezz.jei.gui.textures.Textures;
 import mezz.jei.plugins.vanilla.cooking.FurnaceVariantCategory;
+import mezz.jei.util.MathUtil;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
@@ -24,6 +26,7 @@ public class FurnaceFuelCategory extends FurnaceVariantCategory<FuelRecipe> {
 	private final IDrawableStatic background;
 	private final IDrawableStatic flameTransparentBackground;
 	private final ITextComponent localizedName;
+	private final Rectangle2d textArea;
 
 	public FurnaceFuelCategory(IGuiHelper guiHelper, Textures textures) {
 		super(guiHelper);
@@ -31,12 +34,15 @@ public class FurnaceFuelCategory extends FurnaceVariantCategory<FuelRecipe> {
 		// width of the recipe depends on the text, which is different in each language
 		Minecraft minecraft = Minecraft.getInstance();
 		FontRenderer fontRenderer = minecraft.font;
-		ITextComponent smeltCountText = FuelRecipe.createSmeltCountText(100000);
-		int stringWidth = fontRenderer.width(smeltCountText.getString());
+		ITextComponent maxSmeltCountText = FuelRecipe.createSmeltCountText(10000000 * 200);
+		int maxStringWidth = fontRenderer.width(maxSmeltCountText.getString());
+		int backgroundHeight = 34;
+		int textPadding = 20;
 
-		background = guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 0, 134, 18, 34)
-			.addPadding(0, 0, 0, stringWidth + 20)
+		background = guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 0, 134, 18, backgroundHeight)
+			.addPadding(0, 0, 0, textPadding + maxStringWidth)
 			.build();
+		textArea = new Rectangle2d(20, 0, textPadding + maxStringWidth, backgroundHeight);
 
 		flameTransparentBackground = textures.getFlameIcon();
 		localizedName = new TranslationTextComponent("gui.jei.category.fuel");
@@ -92,6 +98,7 @@ public class FurnaceFuelCategory extends FurnaceVariantCategory<FuelRecipe> {
 		flame.draw(matrixStack, 1, 0);
 		Minecraft minecraft = Minecraft.getInstance();
 		ITextComponent smeltCountText = recipe.getSmeltCountText();
-		minecraft.font.draw(matrixStack, smeltCountText, 24, 13, 0xFF808080);
+		Rectangle2d centerArea = MathUtil.centerTextArea(this.textArea, minecraft.font, smeltCountText);
+		minecraft.font.draw(matrixStack, smeltCountText, centerArea.getX(), centerArea.getY(), 0xFF808080);
 	}
 }
