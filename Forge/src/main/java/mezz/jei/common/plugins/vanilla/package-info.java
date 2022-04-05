c0cdd31befad9ba670dafd7f8aec16348261cc9e
mezz <tehgeek@gmail.com>
2022-04-05 09:54:29 -1000

    fix anvil source root

--- Forge/src/main/java/mezz/jei/common/plugins/vanilla/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@FieldsAndMethodsAreNonnullByDefault
-package mezz.jei.common.plugins.vanilla;
-
-import mezz.jei.core.FieldsAndMethodsAreNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
