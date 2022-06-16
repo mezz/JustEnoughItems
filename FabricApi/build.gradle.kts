06c539c202e3f8d040e0b27b84fb0823a05bbab7
sciwhiz12 <arnoldnunag12@gmail.com>
2022-06-17 00:01:30 +0800

    Fix improper property use causing invalid dependency POM
    The archivesName property of BasePluginExtension is a Property<String>.
    The default implementation of toString() for a Property is to print out
    their name and owning object, and not the toString() of their contents.
    
    This meant that passing archivesName to Node#appendNode as a value,
    which calls toString() on it to make it into a String, results in e.g.
    `extension 'base' property 'archivesName'` instead of the expected
    value of e.g. `jei-1.19-common-api`.
    
    The simple fix is to call Property#get() on archivesName to get its
    actual value and pass that on as the node value.

--- FabricApi/build.gradle.kts
+++ FabricApi/build.gradle.kts
@@ -103,7 +103,7 @@ publishing {
                 dependencyProjects.forEach {
                     val dependencyNode = dependenciesNode.appendNode("dependency")
                     dependencyNode.appendNode("groupId", it.group)
-                    dependencyNode.appendNode("artifactId", it.base.archivesName)
+                    dependencyNode.appendNode("artifactId", it.base.archivesName.get())
                     dependencyNode.appendNode("version", it.version)
                 }
             }
