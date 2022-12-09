9b0f12d431971f7d8932923e731af31a9cf1dc41
mezz <tehgeek@gmail.com>
2022-12-08 23:14:26 -0800

    fix test

--- Forge/src/test/java/mezz/jei/test/lib/TestModIdHelper.java
+++ Forge/src/test/java/mezz/jei/test/lib/TestModIdHelper.java
@@ -2,6 +2,7 @@ package mezz.jei.test.lib;
 
 import mezz.jei.api.helpers.IModIdHelper;
 import mezz.jei.api.ingredients.IIngredientHelper;
+import mezz.jei.api.ingredients.ITypedIngredient;
 import net.minecraft.network.chat.Component;
 
 import java.util.List;
@@ -19,6 +20,11 @@ public class TestModIdHelper implements IModIdHelper {
 
 	@Override
 	public <T> List<Component> addModNameToIngredientTooltip(List<Component> tooltip, T ingredient, IIngredientHelper<T> ingredientHelper) {
+		return tooltip;
+	}
+
+	@Override
+	public <T> List<Component> addModNameToIngredientTooltip(List<Component> tooltip, ITypedIngredient<T> typedIngredient) {
 		return tooltip;
 	}
 
