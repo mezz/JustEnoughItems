Mysticpasta1/JEI-Async

Improve performance and memory size of the ingredient filter (#2734)

src/test/java/mezz/jei/test/search/suffixtree/GeneralizedSuffixTreeTest.java
@@ -1,0 +1,154 @@
+package mezz.jei.test.search.suffixtree;
+
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+import mezz.jei.search.suffixtree.GeneralizedSuffixTree;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+public class GeneralizedSuffixTreeTest {
+
+	private static Set<Integer> search(GeneralizedSuffixTree tree, String word) {
+		IntSet results = new IntOpenHashSet();
+		tree.getSearchResults(word, results);
+		return results;
+	}
+
+	@Test
+	public void testSearch() {
+		GeneralizedSuffixTree tree = new GeneralizedSuffixTree();
+
+		tree.put("a", 0);
+		Assertions.assertEquals(Set.of(0), search(tree, "a"));
+
+		tree.put("ab", 1);
+		Assertions.assertEquals(Set.of(1), search(tree, "ab"));
+		Assertions.assertEquals(Set.of(1), search(tree, "b"));
+		Assertions.assertEquals(Set.of(0, 1), search(tree, "a"));
+
+		tree.put("cab", 2);
+		Assertions.assertEquals(Set.of(2), search(tree, "cab"));
+		Assertions.assertEquals(Set.of(2), search(tree, "ca"));
+		Assertions.assertEquals(Set.of(2), search(tree, "c"));
+		Assertions.assertEquals(Set.of(1, 2), search(tree, "ab"));
+		Assertions.assertEquals(Set.of(1, 2), search(tree, "b"));
+		Assertions.assertEquals(Set.of(0, 1, 2), search(tree, "a"));
+
+		tree.put("abcabxabcd", 3);
+		Assertions.assertEquals(Set.of(3), search(tree, "abcabxabcd"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abcabxabc"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abcabxab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abcabxa"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abcabx"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abcab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abca"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abc"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "bcabxabcd"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bcabxabc"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bcabxab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bcabxa"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bcabx"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bcab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bca"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bc"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "cabxabcd"));
+		Assertions.assertEquals(Set.of(3), search(tree, "cabxabc"));
+		Assertions.assertEquals(Set.of(3), search(tree, "cabxab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "cabxa"));
+		Assertions.assertEquals(Set.of(3), search(tree, "cabx"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "abxabcd"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abxabc"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abxab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abxa"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abx"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "bxabcd"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bxabc"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bxab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bxa"));
+		Assertions.assertEquals(Set.of(3), search(tree, "bx"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "xabcd"));
+		Assertions.assertEquals(Set.of(3), search(tree, "xabc"));
+		Assertions.assertEquals(Set.of(3), search(tree, "xab"));
+		Assertions.assertEquals(Set.of(3), search(tree, "xa"));
+		Assertions.assertEquals(Set.of(3), search(tree, "x"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "abcd"));
+		Assertions.assertEquals(Set.of(3), search(tree, "abc"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "bcd"));
+
+		Assertions.assertEquals(Set.of(3), search(tree, "d"));
+
+		Assertions.assertEquals(Set.of(2, 3), search(tree, "cab"));
+		Assertions.assertEquals(Set.of(2, 3), search(tree, "ca"));
+		Assertions.assertEquals(Set.of(2, 3), search(tree, "c"));
+
+		Assertions.assertEquals(Set.of(1, 2, 3), search(tree, "ab"));
+		Assertions.assertEquals(Set.of(1, 2, 3), search(tree, "b"));
+		Assertions.assertEquals(Set.of(0, 1, 2, 3), search(tree, "a"));
+	}
+
+	@Test
+	public void testPuttingSameString() {
+		GeneralizedSuffixTree tree = new GeneralizedSuffixTree();
+
+		tree.put("ab", 0);
+		Assertions.assertEquals(Set.of(0), search(tree, "a"));
+		Assertions.assertEquals(Set.of(0), search(tree, "b"));
+		Assertions.assertEquals(Set.of(0), search(tree, "ab"));
+
+		tree.put("ab", 1);
+		Assertions.assertEquals(Set.of(0, 1), search(tree, "a"));
+		Assertions.assertEquals(Set.of(0, 1), search(tree, "b"));
+		Assertions.assertEquals(Set.of(0, 1), search(tree, "ab"));
+	}
+
+	@Test
+	public void testPuttingShorterString() {
+		GeneralizedSuffixTree tree = new GeneralizedSuffixTree();
+
+		tree.put("ab", 0);
+		Assertions.assertEquals(Set.of(0), search(tree, "a"));
+		Assertions.assertEquals(Set.of(0), search(tree, "b"));
+		Assertions.assertEquals(Set.of(0), search(tree, "ab"));
+
+		tree.put("a", 1);
+		Assertions.assertEquals(Set.of(0, 1), search(tree, "a"));
+		Assertions.assertEquals(Set.of(0), search(tree, "b"));
+		Assertions.assertEquals(Set.of(0), search(tree, "ab"));
+	}
+
+	@Test
+	public void testNonMatchingSearches() {
+		GeneralizedSuffixTree tree = new GeneralizedSuffixTree();
+
+		tree.put("ab", 0);
+		Assertions.assertEquals(Set.of(), search(tree, ""));
+		Assertions.assertEquals(Set.of(), search(tree, "abc"));
+		Assertions.assertEquals(Set.of(), search(tree, "ac"));
+		Assertions.assertEquals(Set.of(), search(tree, "ba"));
+		Assertions.assertEquals(Set.of(), search(tree, "c"));
+	}
+
+	@Test
+	public void testIndexWorksOutOfOrder() {
+		GeneralizedSuffixTree tree = new GeneralizedSuffixTree();
+
+		tree.put("ab", 10);
+		Assertions.assertEquals(Set.of(10), search(tree, "a"));
+		Assertions.assertEquals(Set.of(10), search(tree, "b"));
+		Assertions.assertEquals(Set.of(10), search(tree, "ab"));
+
+		tree.put("a", 5);
+		Assertions.assertEquals(Set.of(10, 5), search(tree, "a"));
+		Assertions.assertEquals(Set.of(10), search(tree, "b"));
+		Assertions.assertEquals(Set.of(10), search(tree, "ab"));
+	}
+}

src/test/java/mezz/jei/test/util/SubStringTest.java
@@ -1,0 +1,233 @@
+package mezz.jei.test.util;
+
+import mezz.jei.util.SubString;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class SubStringTest {
+	@Test
+	public void testEmptyString() {
+		SubString subString = new SubString("");
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testEmptyBySubStringOffset() {
+		SubString subString = new SubString("a", 1);
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testEmptyBySubStringLength() {
+		SubString subString = new SubString("a", 0, 0);
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testCharAt() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		for (int i = 0; i < string.length(); i++) {
+			Assertions.assertEquals(string.charAt(i), subString.charAt(i));
+		}
+	}
+
+	@Test
+	public void testRegionMatchesSameString() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		for (int start = 0; start < string.length(); start++) {
+			for (int end = string.length() - start; end >= 0; end--) {
+				Assertions.assertTrue(subString.regionMatches(start, string, start, end));
+			}
+		}
+	}
+
+	@Test
+	public void testRegionMatchesWithZeroLength() {
+		String string = "abcdefg";
+
+		// make sure we follow the same rules as regular strings
+		Assertions.assertTrue(string.regionMatches(0, "", 0, 0));
+		Assertions.assertTrue("".regionMatches(0, string, 0, 0));
+
+		SubString subString = new SubString(string);
+		for (int start = 0; start < string.length(); start++) {
+			Assertions.assertTrue(subString.regionMatches(start, string, start, 0));
+			Assertions.assertTrue(subString.regionMatches(start, "", 0, 0));
+		}
+
+		SubString emptySubString = new SubString(string, 0, 0);
+		for (int start = 0; start < string.length(); start++) {
+			Assertions.assertTrue(emptySubString.regionMatches(start, string, start, 0));
+		}
+	}
+
+	@Test
+	public void testRegionMatchesSameSubString() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		SubString copy = new SubString(subString);
+		for (int end = string.length(); end >= 0; end--) {
+			Assertions.assertTrue(subString.regionMatches(copy, end));
+		}
+	}
+
+	@Test
+	public void testRegionMatchesDifferentString() {
+		String string = "abcdefg";
+		SubString subString = new SubString("123abcdefg456");
+		for (int start = 0; start < string.length(); start++) {
+			for (int end = string.length() - start; end >= 0; end--) {
+				Assertions.assertTrue(subString.regionMatches(start + 3, string, start, end));
+			}
+		}
+	}
+
+	@Test
+	public void testRegionMatchesWithOffset() {
+		String string = "abcdefg";
+		SubString subString = new SubString("123abcdefg", 3);
+		for (int start = 0; start < string.length(); start++) {
+			for (int end = string.length() - start; end >= 0; end--) {
+				Assertions.assertTrue(subString.regionMatches(start, string, start, end));
+			}
+		}
+	}
+
+	@Test
+	public void testRegionMatchesFails() {
+		String string = "abcdefg";
+		SubString subString = new SubString("abcdef3");
+		Assertions.assertFalse(subString.regionMatches(0, string, 0, string.length()));
+	}
+
+	@Test
+	public void testSubstringOffset() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		for (int offsetAmount = 0; offsetAmount < string.length(); offsetAmount++) {
+			SubString offsetString = subString.substring(offsetAmount);
+			Assertions.assertEquals(offsetString.length(), subString.length() - offsetAmount);
+
+			for (int i = 0; i < offsetString.length(); i++) {
+				Assertions.assertEquals(string.charAt(i + offsetAmount), offsetString.charAt(i));
+			}
+		}
+	}
+
+	@Test
+	public void testInvalidSubstringOffset() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		Assertions.assertThrows(AssertionError.class, () -> subString.substring(string.length() + 1));
+	}
+
+	@Test
+	public void testAppend() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string, 0, 0);
+		for (int i = 0; i < string.length(); i++) {
+			char c = string.charAt(i);
+			subString = subString.append(c);
+			Assertions.assertEquals(c, subString.charAt(i));
+		}
+	}
+
+	@Test
+	public void testInvalidAppend() {
+		SubString subString = new SubString("abc");
+		Assertions.assertThrows(AssertionError.class, () -> subString.append('c'));
+	}
+
+	@Test
+	public void testShorten() {
+		String string = "abc";
+		SubString subString = new SubString(string);
+		Assertions.assertEquals(3, subString.length());
+		Assertions.assertTrue(subString.regionMatches(0, string, 0, 3));
+
+		subString = subString.shorten(1);
+		Assertions.assertEquals(2, subString.length());
+		Assertions.assertTrue(subString.regionMatches(0, string, 0, 2));
+
+		subString = subString.shorten(2);
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testIsPrefixAndStartsWith() {
+		List<SubString> subStrings = List.of(
+			new SubString("abcdefg"),
+			new SubString("1abcdefg", 1),
+			new SubString("1abcdefg2", 1, 7),
+			new SubString("abcdefg12", 0, 7)
+		);
+		List<SubString> validPrefixes = List.of(
+			new SubString("abcd123", 0, 4),
+			new SubString("abcd"),
+			new SubString("abcdefg"),
+			new SubString("1abcd23", 1, 4)
+		);
+		for (SubString subString : subStrings) {
+			for (SubString validPrefix : validPrefixes) {
+				Assertions.assertTrue(subString.startsWith(validPrefix), subString + "\n" + validPrefix);
+				Assertions.assertTrue(validPrefix.isPrefix(subString), subString + "\n" + validPrefix);
+			}
+		}
+	}
+
+	@Test
+	public void testNotIsPrefixAndStartsWith() {
+		List<SubString> subStrings = List.of(
+			new SubString("abcdefg"),
+			new SubString("1abcdefg", 1),
+			new SubString("1abcdefg2", 1, 7),
+			new SubString("abcdefg12", 0, 7)
+		);
+		List<SubString> invalidPrefixes = List.of(
+			new SubString("abcdefgh"),
+			new SubString("abcd123", 0, 5),
+			new SubString("abcdf"),
+			new SubString("1abcd23", 0, 4)
+		);
+		for (SubString subString : subStrings) {
+			for (SubString invalidPrefix : invalidPrefixes) {
+				Assertions.assertFalse(subString.startsWith(invalidPrefix), subString + "\n" + invalidPrefix);
+				Assertions.assertFalse(invalidPrefix.isPrefix(subString), subString + "\n" + invalidPrefix);
+			}
+		}
+	}
+
+	@Test
+	public void testEmptyPrefix() {
+		// make sure we follow the same rules as regular strings
+		//noinspection ConstantConditions
+		Assertions.assertTrue("abcdefg".startsWith(""));
+		//noinspection ConstantConditions,MismatchedStringCase
+		Assertions.assertFalse("".startsWith("abcdefg"));
+
+		List<SubString> subStrings = List.of(
+			new SubString("abcdefg"),
+			new SubString("1abcdefg", 1),
+			new SubString("1abcdefg2", 1, 7),
+			new SubString("abcdefg12", 0, 7)
+		);
+		List<SubString> emptyPrefixes = List.of(
+			new SubString(""),
+			new SubString("abcd", 0, 0),
+			new SubString("abcd", 1, 0)
+		);
+		for (SubString subString : subStrings){
+			for (SubString emptyPrefix : emptyPrefixes) {
+				Assertions.assertTrue(subString.startsWith(emptyPrefix), subString + "\n" + emptyPrefix);
+				Assertions.assertFalse(emptyPrefix.startsWith(subString), subString + "\n" + emptyPrefix);
+
+				Assertions.assertFalse(subString.isPrefix(emptyPrefix), subString + "\n" + emptyPrefix);
+				Assertions.assertTrue(emptyPrefix.isPrefix(subString), subString + "\n" + emptyPrefix);
+			}
+		}
+	}
+}

mezz/JustEnoughItems

src/test/java/mezz/jei/test/search/suffixtree/GeneralizedSuffixTreeTest.java
@@ -1,0 +1,154 @@
+package mezz.jei.test.search.suffixtree;
+
+import com.google.common.collect.ImmutableSet;
+import mezz.jei.search.suffixtree.GeneralizedSuffixTree;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GeneralizedSuffixTreeTest {
+
+	private static <T> Set<T> search(GeneralizedSuffixTree<T> tree, String word) {
+		Set<T> results = new HashSet<>();
+		tree.getSearchResults(word, results);
+		return results;
+	}
+
+	@Test
+	public void testSearch() {
+		GeneralizedSuffixTree<Integer> tree = new GeneralizedSuffixTree<>();
+
+		tree.put("a", 0);
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "a"));
+
+		tree.put("ab", 1);
+		Assertions.assertEquals(ImmutableSet.of(1), search(tree, "ab"));
+		Assertions.assertEquals(ImmutableSet.of(1), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(0, 1), search(tree, "a"));
+
+		tree.put("cab", 2);
+		Assertions.assertEquals(ImmutableSet.of(2), search(tree, "cab"));
+		Assertions.assertEquals(ImmutableSet.of(2), search(tree, "ca"));
+		Assertions.assertEquals(ImmutableSet.of(2), search(tree, "c"));
+		Assertions.assertEquals(ImmutableSet.of(1, 2), search(tree, "ab"));
+		Assertions.assertEquals(ImmutableSet.of(1, 2), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(0, 1, 2), search(tree, "a"));
+
+		tree.put("abcabxabcd", 3);
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abcabxabcd"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abcabxabc"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abcabxab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abcabxa"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abcabx"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abcab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abca"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abc"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bcabxabcd"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bcabxabc"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bcabxab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bcabxa"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bcabx"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bcab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bca"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bc"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "cabxabcd"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "cabxabc"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "cabxab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "cabxa"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "cabx"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abxabcd"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abxabc"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abxab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abxa"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abx"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bxabcd"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bxabc"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bxab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bxa"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bx"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "xabcd"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "xabc"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "xab"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "xa"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "x"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abcd"));
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "abc"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "bcd"));
+
+		Assertions.assertEquals(ImmutableSet.of(3), search(tree, "d"));
+
+		Assertions.assertEquals(ImmutableSet.of(2, 3), search(tree, "cab"));
+		Assertions.assertEquals(ImmutableSet.of(2, 3), search(tree, "ca"));
+		Assertions.assertEquals(ImmutableSet.of(2, 3), search(tree, "c"));
+
+		Assertions.assertEquals(ImmutableSet.of(1, 2, 3), search(tree, "ab"));
+		Assertions.assertEquals(ImmutableSet.of(1, 2, 3), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(0, 1, 2, 3), search(tree, "a"));
+	}
+
+	@Test
+	public void testPuttingSameString() {
+		GeneralizedSuffixTree<Integer> tree = new GeneralizedSuffixTree<>();
+
+		tree.put("ab", 0);
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "a"));
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "ab"));
+
+		tree.put("ab", 1);
+		Assertions.assertEquals(ImmutableSet.of(0, 1), search(tree, "a"));
+		Assertions.assertEquals(ImmutableSet.of(0, 1), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(0, 1), search(tree, "ab"));
+	}
+
+	@Test
+	public void testPuttingShorterString() {
+		GeneralizedSuffixTree<Integer> tree = new GeneralizedSuffixTree<>();
+
+		tree.put("ab", 0);
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "a"));
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "ab"));
+
+		tree.put("a", 1);
+		Assertions.assertEquals(ImmutableSet.of(0, 1), search(tree, "a"));
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(0), search(tree, "ab"));
+	}
+
+	@Test
+	public void testNonMatchingSearches() {
+		GeneralizedSuffixTree<Integer> tree = new GeneralizedSuffixTree<>();
+
+		tree.put("ab", 0);
+		Assertions.assertEquals(ImmutableSet.of(), search(tree, ""));
+		Assertions.assertEquals(ImmutableSet.of(), search(tree, "abc"));
+		Assertions.assertEquals(ImmutableSet.of(), search(tree, "ac"));
+		Assertions.assertEquals(ImmutableSet.of(), search(tree, "ba"));
+		Assertions.assertEquals(ImmutableSet.of(), search(tree, "c"));
+	}
+
+	@Test
+	public void testIndexWorksOutOfOrder() {
+		GeneralizedSuffixTree<Integer> tree = new GeneralizedSuffixTree<>();
+
+		tree.put("ab", 10);
+		Assertions.assertEquals(ImmutableSet.of(10), search(tree, "a"));
+		Assertions.assertEquals(ImmutableSet.of(10), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(10), search(tree, "ab"));
+
+		tree.put("a", 5);
+		Assertions.assertEquals(ImmutableSet.of(10, 5), search(tree, "a"));
+		Assertions.assertEquals(ImmutableSet.of(10), search(tree, "b"));
+		Assertions.assertEquals(ImmutableSet.of(10), search(tree, "ab"));
+	}
+}

src/test/java/mezz/jei/test/util/SubStringTest.java
@@ -1,0 +1,234 @@
+package mezz.jei.test.util;
+
+import com.google.common.collect.ImmutableList;
+import mezz.jei.util.SubString;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class SubStringTest {
+	@Test
+	public void testEmptyString() {
+		SubString subString = new SubString("");
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testEmptyBySubStringOffset() {
+		SubString subString = new SubString("a", 1);
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testEmptyBySubStringLength() {
+		SubString subString = new SubString("a", 0, 0);
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testCharAt() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		for (int i = 0; i < string.length(); i++) {
+			Assertions.assertEquals(string.charAt(i), subString.charAt(i));
+		}
+	}
+
+	@Test
+	public void testRegionMatchesSameString() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		for (int start = 0; start < string.length(); start++) {
+			for (int end = string.length() - start; end >= 0; end--) {
+				Assertions.assertTrue(subString.regionMatches(start, string, start, end));
+			}
+		}
+	}
+
+	@Test
+	public void testRegionMatchesWithZeroLength() {
+		String string = "abcdefg";
+
+		// make sure we follow the same rules as regular strings
+		Assertions.assertTrue(string.regionMatches(0, "", 0, 0));
+		Assertions.assertTrue("".regionMatches(0, string, 0, 0));
+
+		SubString subString = new SubString(string);
+		for (int start = 0; start < string.length(); start++) {
+			Assertions.assertTrue(subString.regionMatches(start, string, start, 0));
+			Assertions.assertTrue(subString.regionMatches(start, "", 0, 0));
+		}
+
+		SubString emptySubString = new SubString(string, 0, 0);
+		for (int start = 0; start < string.length(); start++) {
+			Assertions.assertTrue(emptySubString.regionMatches(start, string, start, 0));
+		}
+	}
+
+	@Test
+	public void testRegionMatchesSameSubString() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		SubString copy = new SubString(subString);
+		for (int end = string.length(); end >= 0; end--) {
+			Assertions.assertTrue(subString.regionMatches(copy, end));
+		}
+	}
+
+	@Test
+	public void testRegionMatchesDifferentString() {
+		String string = "abcdefg";
+		SubString subString = new SubString("123abcdefg456");
+		for (int start = 0; start < string.length(); start++) {
+			for (int end = string.length() - start; end >= 0; end--) {
+				Assertions.assertTrue(subString.regionMatches(start + 3, string, start, end));
+			}
+		}
+	}
+
+	@Test
+	public void testRegionMatchesWithOffset() {
+		String string = "abcdefg";
+		SubString subString = new SubString("123abcdefg", 3);
+		for (int start = 0; start < string.length(); start++) {
+			for (int end = string.length() - start; end >= 0; end--) {
+				Assertions.assertTrue(subString.regionMatches(start, string, start, end));
+			}
+		}
+	}
+
+	@Test
+	public void testRegionMatchesFails() {
+		String string = "abcdefg";
+		SubString subString = new SubString("abcdef3");
+		Assertions.assertFalse(subString.regionMatches(0, string, 0, string.length()));
+	}
+
+	@Test
+	public void testSubstringOffset() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		for (int offsetAmount = 0; offsetAmount < string.length(); offsetAmount++) {
+			SubString offsetString = subString.substring(offsetAmount);
+			Assertions.assertEquals(offsetString.length(), subString.length() - offsetAmount);
+
+			for (int i = 0; i < offsetString.length(); i++) {
+				Assertions.assertEquals(string.charAt(i + offsetAmount), offsetString.charAt(i));
+			}
+		}
+	}
+
+	@Test
+	public void testInvalidSubstringOffset() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string);
+		Assertions.assertThrows(AssertionError.class, () -> subString.substring(string.length() + 1));
+	}
+
+	@Test
+	public void testAppend() {
+		String string = "abcdefg";
+		SubString subString = new SubString(string, 0, 0);
+		for (int i = 0; i < string.length(); i++) {
+			char c = string.charAt(i);
+			subString = subString.append(c);
+			Assertions.assertEquals(c, subString.charAt(i));
+		}
+	}
+
+	@Test
+	public void testInvalidAppend() {
+		SubString subString = new SubString("abc");
+		Assertions.assertThrows(AssertionError.class, () -> subString.append('c'));
+	}
+
+	@Test
+	public void testShorten() {
+		String string = "abc";
+		SubString subString = new SubString(string);
+		Assertions.assertEquals(3, subString.length());
+		Assertions.assertTrue(subString.regionMatches(0, string, 0, 3));
+
+		subString = subString.shorten(1);
+		Assertions.assertEquals(2, subString.length());
+		Assertions.assertTrue(subString.regionMatches(0, string, 0, 2));
+
+		subString = subString.shorten(2);
+		Assertions.assertTrue(subString.isEmpty());
+	}
+
+	@Test
+	public void testIsPrefixAndStartsWith() {
+		List<SubString> subStrings = ImmutableList.of(
+			new SubString("abcdefg"),
+			new SubString("1abcdefg", 1),
+			new SubString("1abcdefg2", 1, 7),
+			new SubString("abcdefg12", 0, 7)
+		);
+		List<SubString> validPrefixes = ImmutableList.of(
+			new SubString("abcd123", 0, 4),
+			new SubString("abcd"),
+			new SubString("abcdefg"),
+			new SubString("1abcd23", 1, 4)
+		);
+		for (SubString subString : subStrings) {
+			for (SubString validPrefix : validPrefixes) {
+				Assertions.assertTrue(subString.startsWith(validPrefix), subString + "\n" + validPrefix);
+				Assertions.assertTrue(validPrefix.isPrefix(subString), subString + "\n" + validPrefix);
+			}
+		}
+	}
+
+	@Test
+	public void testNotIsPrefixAndStartsWith() {
+		List<SubString> subStrings = ImmutableList.of(
+			new SubString("abcdefg"),
+			new SubString("1abcdefg", 1),
+			new SubString("1abcdefg2", 1, 7),
+			new SubString("abcdefg12", 0, 7)
+		);
+		List<SubString> invalidPrefixes = ImmutableList.of(
+			new SubString("abcdefgh"),
+			new SubString("abcd123", 0, 5),
+			new SubString("abcdf"),
+			new SubString("1abcd23", 0, 4)
+		);
+		for (SubString subString : subStrings) {
+			for (SubString invalidPrefix : invalidPrefixes) {
+				Assertions.assertFalse(subString.startsWith(invalidPrefix), subString + "\n" + invalidPrefix);
+				Assertions.assertFalse(invalidPrefix.isPrefix(subString), subString + "\n" + invalidPrefix);
+			}
+		}
+	}
+
+	@Test
+	public void testEmptyPrefix() {
+		// make sure we follow the same rules as regular strings
+		//noinspection ConstantConditions
+		Assertions.assertTrue("abcdefg".startsWith(""));
+		//noinspection ConstantConditions,MismatchedStringCase
+		Assertions.assertFalse("".startsWith("abcdefg"));
+
+		List<SubString> subStrings = ImmutableList.of(
+			new SubString("abcdefg"),
+			new SubString("1abcdefg", 1),
+			new SubString("1abcdefg2", 1, 7),
+			new SubString("abcdefg12", 0, 7)
+		);
+		List<SubString> emptyPrefixes = ImmutableList.of(
+			new SubString(""),
+			new SubString("abcd", 0, 0),
+			new SubString("abcd", 1, 0)
+		);
+		for (SubString subString : subStrings){
+			for (SubString emptyPrefix : emptyPrefixes) {
+				Assertions.assertTrue(subString.startsWith(emptyPrefix), subString + "\n" + emptyPrefix);
+				Assertions.assertFalse(emptyPrefix.startsWith(subString), subString + "\n" + emptyPrefix);
+
+				Assertions.assertFalse(subString.isPrefix(emptyPrefix), subString + "\n" + emptyPrefix);
+				Assertions.assertTrue(emptyPrefix.isPrefix(subString), subString + "\n" + emptyPrefix);
+			}
+		}
+	}
+}