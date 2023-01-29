3c680031ecf60443ea6beb47e6843247536e0f82
mezz <tehgeek@gmail.com>
2023-01-29 14:48:17 -0800

    List fewer changes in the changelog.md
    (cherry picked from commit acf218192a552349d80907ba96eeade15f76166e)

--- Changelog/build.gradle.kts
+++ Changelog/build.gradle.kts
@@ -7,15 +7,13 @@ plugins {
 // gradle.properties
 val specificationVersion: String by extra
 val changelogUntaggedName = "Current release $specificationVersion"
-val firstChangelogCommit = "e72e49fa7a072755e7f96cad65388205f6a010dc"
-val lastChangelogCommit = "HEAD"
 
 tasks.register<GitChangelogTask>("makeChangelog") {
 	fromRepo.set(projectDir.absolutePath.toString())
 	file.set(file("changelog.html"))
 	untaggedName.set(changelogUntaggedName)
-	fromRevision.set(firstChangelogCommit)
-	toRevision.set(lastChangelogCommit)
+	fromRevision.set("e72e49fa7a072755e7f96cad65388205f6a010dc")
+	toRevision.set("HEAD")
 	templateContent.set(file("changelog.mustache").readText())
 }
 
@@ -23,7 +21,7 @@ tasks.register<GitChangelogTask>("makeMarkdownChangelog") {
 	fromRepo.set(projectDir.absolutePath.toString())
 	file.set(file("changelog.md"))
 	untaggedName.set(changelogUntaggedName)
-	fromRevision.set(firstChangelogCommit)
-	toRevision.set(lastChangelogCommit)
+	fromRevision.set(System.getenv("GIT_PREVIOUS_SUCCESSFUL_COMMIT") ?: "HEAD~10")
+	toRevision.set("HEAD")
 	templateContent.set(file("changelog-markdown.mustache").readText())
 }
