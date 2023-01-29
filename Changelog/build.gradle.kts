1613aed71b77fc1c8fb0493046b5ce15a497b838
mezz <tehgeek@gmail.com>
2023-01-28 17:31:50 -0800

    Add modrinth upload task

--- Changelog/build.gradle.kts
+++ Changelog/build.gradle.kts
@@ -1,17 +1,29 @@
 import se.bjurr.gitchangelog.plugin.gradle.GitChangelogTask
 
 plugins {
-	id("se.bjurr.gitchangelog.git-changelog-gradle-plugin") version("1.71.4")
+	id("se.bjurr.gitchangelog.git-changelog-gradle-plugin") version("1.77.2")
 }
 
 // gradle.properties
 val specificationVersion: String by extra
+val changelogUntaggedName = "Current release $specificationVersion"
+val firstChangelogCommit = "e72e49fa7a072755e7f96cad65388205f6a010dc"
+val lastChangelogCommit = "HEAD"
 
 tasks.register<GitChangelogTask>("makeChangelog") {
 	fromRepo = projectDir.absolutePath.toString()
 	file = file("changelog.html")
-	untaggedName = "Current release $specificationVersion"
-	fromCommit = "e72e49fa7a072755e7f96cad65388205f6a010dc"
-	toRef = "HEAD"
+	untaggedName = changelogUntaggedName
+	fromCommit = firstChangelogCommit
+	toRef = lastChangelogCommit
 	templateContent = file("changelog.mustache").readText()
+}
+
+tasks.register<GitChangelogTask>("makeMarkdownChangelog") {
+	fromRepo = projectDir.absolutePath.toString()
+	file = file("changelog.md")
+	untaggedName = changelogUntaggedName
+	fromCommit = firstChangelogCommit
+	toRef = lastChangelogCommit
+	templateContent = file("changelog-markdown.mustache").readText()
 }
