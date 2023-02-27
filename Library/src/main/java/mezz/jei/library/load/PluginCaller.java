71229ebe8e605ed84300ed95fcfd8bc559f22820
mezz <tehgeek@gmail.com>
2023-02-26 18:10:30 -0800

    Improve logging of slow plugins, reduce info log spam

--- Library/src/main/java/mezz/jei/library/load/PluginCaller.java
+++ Library/src/main/java/mezz/jei/library/load/PluginCaller.java
@@ -1,34 +1,39 @@
 package mezz.jei.library.load;
 
+import com.google.common.base.Stopwatch;
+import mezz.jei.api.IModPlugin;
+import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-
-import net.minecraft.resources.ResourceLocation;
-
-import com.google.common.base.Stopwatch;
-import mezz.jei.api.IModPlugin;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class PluginCaller {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	public static void callOnPlugins(String title, List<IModPlugin> plugins, Consumer<IModPlugin> func) {
-		List<IModPlugin> erroredPlugins = new ArrayList<>();
+		LOGGER.info("{}...", title);
+		Stopwatch stopwatch = Stopwatch.createStarted();
 
-		for (IModPlugin plugin : plugins) {
-			try {
-				ResourceLocation pluginUid = plugin.getPluginUid();
-				LOGGER.info("{}: {} ...", title, pluginUid);
-				Stopwatch stopwatch = Stopwatch.createStarted();
-				func.accept(plugin);
-				LOGGER.info("{}: {} took {}", title, pluginUid, stopwatch);
-			} catch (RuntimeException | LinkageError e) {
-				LOGGER.error("Caught an error from mod plugin: {} {}", plugin.getClass(), plugin.getPluginUid(), e);
-				erroredPlugins.add(plugin);
+		try (PluginCallerTimer timer = new PluginCallerTimer()) {
+			List<IModPlugin> erroredPlugins = new ArrayList<>();
+
+			for (IModPlugin plugin : plugins) {
+				try {
+					ResourceLocation pluginUid = plugin.getPluginUid();
+					timer.begin(title, pluginUid);
+					func.accept(plugin);
+					timer.end();
+				} catch (RuntimeException | LinkageError e) {
+					LOGGER.error("Caught an error from mod plugin: {} {}", plugin.getClass(), plugin.getPluginUid(), e);
+					erroredPlugins.add(plugin);
+				}
 			}
+			plugins.removeAll(erroredPlugins);
 		}
-		plugins.removeAll(erroredPlugins);
+
+		LOGGER.info("{} took {}", title, stopwatch);
 	}
 }
