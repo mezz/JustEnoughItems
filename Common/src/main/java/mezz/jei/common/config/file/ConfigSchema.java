570a3054aeed76362b2116c9bf19228e046a57e0
mezz <tehgeek@gmail.com>
2023-04-30 20:47:11 -0700

    Fix #3194 Make the save config thread short-lived

--- Common/src/main/java/mezz/jei/common/config/file/ConfigSchema.java
+++ Common/src/main/java/mezz/jei/common/config/file/ConfigSchema.java
@@ -1,9 +1,9 @@
 package mezz.jei.common.config.file;
 
 import mezz.jei.common.config.ConfigManager;
+import mezz.jei.common.util.DeduplicatingRunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
 
 import java.io.IOException;
@@ -11,20 +11,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ConfigSchema implements IConfigSchema {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final Duration SAVE_DELAY_TIME = Duration.ofSeconds(2);
-    private static final Timer SAVE_DELAY_TIMER = new Timer("JEI Config Save Delay");
 
     private final Path path;
     @Unmodifiable
     private final List<ConfigCategory> categories;
     private final AtomicBoolean needsLoad = new AtomicBoolean(true);
-    private @Nullable TimerTask saveTask;
+    private final DeduplicatingRunner delayedSave = new DeduplicatingRunner(this::save, SAVE_DELAY_TIME, "JEI Config Save Delay");
 
     public ConfigSchema(Path path, List<ConfigCategoryBuilder> categoryBuilders) {
         this.path = path;
@@ -72,17 +69,7 @@ public class ConfigSchema implements IConfigSchema {
 
     @Override
     public void markDirty() {
-        if (this.saveTask != null) {
-            this.saveTask.cancel();
-        }
-        this.saveTask = new TimerTask() {
-            @Override
-            public void run() {
-                save();
-                saveTask = null;
-            }
-        };
-        SAVE_DELAY_TIMER.schedule(saveTask, SAVE_DELAY_TIME.toMillis());
+        delayedSave.run();
     }
 
     @Override
