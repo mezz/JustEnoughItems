4c973aed663bf6855349263a92faf405c662b9c8
Davide Albiero <davide2910@live.it>
2023-06-13 13:53:45 +0200

    Port to 1.20.1
    Update ForgeGradle 6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,6 +32,10 @@ pluginManagement {
 	}
 }
 
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
 val minecraftVersion: String by settings
 
 rootProject.name = "jei-${minecraftVersion}"
