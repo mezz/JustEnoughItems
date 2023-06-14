491dcbf8ff9305ba07fbb6af591521cbc443adb6
Davide Albiero <davide2910@live.it>
2023-06-14 12:23:28 +0200

    Remove foojay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,10 +32,6 @@ pluginManagement {
 	}
 }
 
-plugins {
-	id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
-
 val minecraftVersion: String by settings
 
 rootProject.name = "jei-${minecraftVersion}"
