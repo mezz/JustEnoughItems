1138ce712c0c6709327a58aec0e5ea6ae3509393
mezz <tehgeek@gmail.com>
2023-08-08 22:02:55 -0700

    try enabling reproducible builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,4 +96,11 @@ subprojects {
             ))
         }
     }
+
+    // Activate reproducible builds
+    // https://docs.gradle.org/current/userguide/working_with_files.html#sec:reproducible_archives
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
 }
