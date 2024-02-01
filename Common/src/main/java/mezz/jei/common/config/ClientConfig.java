Mysticpasta1/JEI-Async

Close #3320 Search default block tags for items with blocks in them

Common/src/main/java/mezz/jei/common/config/ClientConfig.java
@@ -19,6 +19,7 @@ public final class ClientConfig implements IClientConfig {
 	private final Supplier<Boolean> cheatToHotbarUsingHotkeysEnabled;
 	private final Supplier<Boolean> addBookmarksToFront;
 	private final Supplier<Boolean> lookupFluidContents;
+	private final Supplier<Boolean> lookupBlockTags;
 	private final Supplier<GiveMode> giveMode;
 	private final Supplier<Integer> maxRecipeGuiHeight;
 	private final Supplier<List<IngredientSortStage>> ingredientSorterStages;
@@ -51,6 +52,11 @@ public final class ClientConfig implements IClientConfig {
 			"LookupFluidContents",
 			false,
 			"When looking up recipes with items that contain fluids, also look up recipes for the fluids."
+		);
+		lookupBlockTags = advanced.addBoolean(
+			"LookupBlockTags",
+			true,
+			"When searching for item tags, also include tags for the default blocks contained in the items."
 		);
 		giveMode = advanced.addEnum(
 			"GiveMode",
@@ -106,6 +112,11 @@ public final class ClientConfig implements IClientConfig {
 	@Override
 	public boolean isLookupFluidContents() {
 		return lookupFluidContents.get();
+	}
+
+	@Override
+	public boolean isLookupBlockTags() {
+		return lookupBlockTags.get();
 	}
 
 	@Override

Common/src/main/java/mezz/jei/common/config/IClientConfig.java
@@ -17,7 +17,9 @@ public interface IClientConfig {
 
     boolean isLookupFluidContents();
 
-    GiveMode getGiveMode();
+	boolean isLookupBlockTags();
+
+	GiveMode getGiveMode();
 
 	int getMaxRecipeGuiHeight();
 

Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/ItemStackHelper.java
@@ -1,10 +1,14 @@
 package mezz.jei.library.plugins.vanilla.ingredients;
 
+import com.google.common.collect.Streams;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.helpers.IColorHelper;
 import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.subtypes.UidContext;
+import mezz.jei.common.Internal;
+import mezz.jei.common.config.IClientConfig;
+import mezz.jei.common.config.IJeiClientConfigs;
 import mezz.jei.common.platform.IPlatformItemStackHelper;
 import mezz.jei.common.platform.IPlatformRegistry;
 import mezz.jei.common.platform.Services;
@@ -16,6 +20,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
@@ -126,8 +131,21 @@ public class ItemStackHelper implements IIngredientHelper<ItemStack> {
 
 	@Override
 	public Stream<ResourceLocation> getTagStream(ItemStack ingredient) {
-		return ingredient.getTags()
+		Stream<ResourceLocation> itemTagStream = ingredient.getTags()
 			.map(TagKey::location);
+
+		if (ingredient.getItem() instanceof BlockItem blockItem) {
+			IJeiClientConfigs jeiClientConfigs = Internal.getJeiClientConfigs();
+			IClientConfig clientConfig = jeiClientConfigs.getClientConfig();
+			if (clientConfig.isLookupBlockTags()) {
+				Stream<ResourceLocation> blockTagStream = blockItem.getBlock()
+					.defaultBlockState()
+					.getTags()
+					.map(TagKey::location);
+				return Streams.concat(itemTagStream, blockTagStream);
+			}
+		}
+		return itemTagStream;
 	}
 
 	@Override

mezz/JustEnoughItems

Common/src/main/java/mezz/jei/common/config/ClientConfig.java
@@ -17,8 +17,9 @@ public final class ClientConfig implements IClientConfig {
 	private final Supplier<Boolean> centerSearchBarEnabled;
 	private final Supplier<Boolean> lowMemorySlowSearchEnabled;
 	private final Supplier<Boolean> cheatToHotbarUsingHotkeysEnabled;
-	private final Supplier<Boolean> addBookmarksToFront;
-	private final Supplier<Boolean> lookupFluidContents;
+	private final Supplier<Boolean> addBookmarksToFrontEnabled;
+	private final Supplier<Boolean> lookupFluidContentsEnabled;
+	private final Supplier<Boolean> lookupBlockTagsEnabled;
 	private final Supplier<GiveMode> giveMode;
 	private final Supplier<Integer> maxRecipeGuiHeight;
 	private final Supplier<List<IngredientSortStage>> ingredientSorterStages;
@@ -42,15 +43,20 @@ public final class ClientConfig implements IClientConfig {
 			false,
 			"Enable cheating items into the hotbar by using the shift+number keys."
 		);
-		addBookmarksToFront = advanced.addBoolean(
+		addBookmarksToFrontEnabled = advanced.addBoolean(
 			"AddBookmarksToFrontEnabled",
 			true,
 			"Enable adding new bookmarks to the front of the bookmark list."
 		);
-		lookupFluidContents = advanced.addBoolean(
-			"LookupFluidContents",
+		lookupFluidContentsEnabled = advanced.addBoolean(
+			"lookupFluidContentsEnabled",
 			false,
 			"When looking up recipes with items that contain fluids, also look up recipes for the fluids."
+		);
+		lookupBlockTagsEnabled = advanced.addBoolean(
+			"lookupBlockTagsEnabled",
+			true,
+			"When searching for item tags, also include tags for the default blocks contained in the items."
 		);
 		giveMode = advanced.addEnum(
 			"GiveMode",
@@ -99,13 +105,18 @@ public final class ClientConfig implements IClientConfig {
 	}
 
 	@Override
-	public boolean isAddingBookmarksToFront() {
-		return addBookmarksToFront.get();
+	public boolean isAddingBookmarksToFrontEnabled() {
+		return addBookmarksToFrontEnabled.get();
 	}
 
 	@Override
-	public boolean isLookupFluidContents() {
-		return lookupFluidContents.get();
+	public boolean isLookupFluidContentsEnabled() {
+		return lookupFluidContentsEnabled.get();
+	}
+
+	@Override
+	public boolean isLookupBlockTagsEnabled() {
+		return lookupBlockTagsEnabled.get();
 	}
 
 	@Override

Common/src/main/java/mezz/jei/common/config/IClientConfig.java
@@ -13,11 +13,13 @@ public interface IClientConfig {
 
 	boolean isCheatToHotbarUsingHotkeysEnabled();
 
-	boolean isAddingBookmarksToFront();
+	boolean isAddingBookmarksToFrontEnabled();
 
-    boolean isLookupFluidContents();
+    boolean isLookupFluidContentsEnabled();
 
-    GiveMode getGiveMode();
+	boolean isLookupBlockTagsEnabled();
+
+	GiveMode getGiveMode();
 
 	int getMaxRecipeGuiHeight();
 

Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/ItemStackHelper.java
@@ -1,10 +1,14 @@
 package mezz.jei.library.plugins.vanilla.ingredients;
 
+import com.google.common.collect.Streams;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.helpers.IColorHelper;
 import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.subtypes.UidContext;
+import mezz.jei.common.Internal;
+import mezz.jei.common.config.IClientConfig;
+import mezz.jei.common.config.IJeiClientConfigs;
 import mezz.jei.common.platform.IPlatformItemStackHelper;
 import mezz.jei.common.platform.IPlatformRegistry;
 import mezz.jei.common.platform.Services;
@@ -16,6 +20,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
@@ -126,8 +131,21 @@ public class ItemStackHelper implements IIngredientHelper<ItemStack> {
 
 	@Override
 	public Stream<ResourceLocation> getTagStream(ItemStack ingredient) {
-		return ingredient.getTags()
+		Stream<ResourceLocation> itemTagStream = ingredient.getTags()
 			.map(TagKey::location);
+
+		if (ingredient.getItem() instanceof BlockItem blockItem) {
+			IJeiClientConfigs jeiClientConfigs = Internal.getJeiClientConfigs();
+			IClientConfig clientConfig = jeiClientConfigs.getClientConfig();
+			if (clientConfig.isLookupBlockTagsEnabled()) {
+				Stream<ResourceLocation> blockTagStream = blockItem.getBlock()
+					.defaultBlockState()
+					.getTags()
+					.map(TagKey::location);
+				return Streams.concat(itemTagStream, blockTagStream);
+			}
+		}
+		return itemTagStream;
 	}
 
 	@Override