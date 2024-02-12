915c25a069875608d2ea38b869c13bc041548212
mezz <tehgeek@gmail.com>
2024-02-11 16:34:45 -0800

    fix buildscript type warning

--- Fabric/build.gradle.kts
+++ Fabric/build.gradle.kts
@@ -104,10 +104,10 @@ loom {
             it.dependencyProject.tasks.jar.get().archiveFile.get().asFile
         }
         val classPaths = sourceSets.main.get().output.classesDirs
-        val resourcesPaths = listOf(
+        val resourcesPaths = listOfNotNull(
             sourceSets.main.get().output.resourcesDir
         )
-        val classPathGroups = listOf(dependencyJarPaths, classPaths, resourcesPaths).flatten().filterNotNull()
+        val classPathGroups = listOf(dependencyJarPaths, classPaths, resourcesPaths).flatten()
         val classPathGroupsString = classPathGroups.joinToString(separator = File.pathSeparator) {
             it.absoluteFile.toString()
         }
