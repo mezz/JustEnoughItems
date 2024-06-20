5c9e1cd813d2f3107a2ba547a651444a0aed1702
mezz <tehgeek@gmail.com>
2024-06-20 10:00:06 +0900

    Close #3552 allow incomplete smithing recipes to be displayed
    (cherry picked from commit 5a01a96cf4c76da34254c51e41e320e883163ba5)

--- Debug/src/main/java/mezz/jei/debug/JeiDebugPlugin.java
+++ Debug/src/main/java/mezz/jei/debug/JeiDebugPlugin.java
@@ -5,6 +5,7 @@ import net.minecraft.network.chat.TranslatableComponent;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.ModIds;
+import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IClickableIngredientFactory;
 import mezz.jei.api.gui.handlers.IGuiContainerHandler;
@@ -42,6 +43,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.UpgradeRecipe;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
@@ -192,6 +195,16 @@ public class JeiDebugPlugin implements IModPlugin {
 
 		registration.addRecipes(DebugFocusRecipeCategory.TYPE, List.of(
 			new DebugRecipe()
+		));
+
+		UpgradeRecipe testRecipeWithoutBase = new UpgradeRecipe(
+			new ResourceLocation(ModIds.JEI_ID, "test_recipe_without_base"),
+			Ingredient.EMPTY,
+			Ingredient.of(new ItemStack(Items.APPLE)),
+			new ItemStack(Items.BAKED_POTATO)
+		);
+		registration.addRecipes(RecipeTypes.SMITHING, List.of(
+			testRecipeWithoutBase
 		));
 
 		if (DebugConfig.isCrashingTestRecipesEnabled()) {
