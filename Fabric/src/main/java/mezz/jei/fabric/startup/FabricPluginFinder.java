eea9a9eda0d421b9da9cf4dad1f01b809760290f
mezz <tehgeek@gmail.com>
2024-07-05 11:27:19 +0900

    Fix #3577 Prevent crash when mods specify an invalid plugin entrypoint

--- Fabric/src/main/java/mezz/jei/fabric/startup/FabricPluginFinder.java
+++ Fabric/src/main/java/mezz/jei/fabric/startup/FabricPluginFinder.java
@@ -1,13 +1,20 @@
 package mezz.jei.fabric.startup;
 
 import mezz.jei.api.IModPlugin;
+import net.fabricmc.loader.api.EntrypointException;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
+import net.fabricmc.loader.api.metadata.ModMetadata;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public final class FabricPluginFinder {
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	private FabricPluginFinder() {
 
 	}
@@ -21,7 +28,22 @@ public final class FabricPluginFinder {
 		FabricLoader fabricLoader = FabricLoader.getInstance();
 		List<EntrypointContainer<T>> pluginContainers = fabricLoader.getEntrypointContainers(entrypointContainerKey, instanceClass);
 		return pluginContainers.stream()
-			.map(EntrypointContainer::getEntrypoint)
+			.<T>mapMulti((entrypointContainer, consumer) -> {
+				try {
+					T entrypoint = entrypointContainer.getEntrypoint();
+					consumer.accept(entrypoint);
+				} catch (EntrypointException e) {
+					String modName;
+					try {
+						ModContainer provider = entrypointContainer.getProvider();
+						ModMetadata metadata = provider.getMetadata();
+						modName = metadata.getName();
+					} catch (RuntimeException ignored) {
+						modName = "unknown";
+					}
+					LOGGER.error("{} specified an invalid entrypoint for its JEI plugin", modName, e);
+				}
+			})
 			.collect(Collectors.toList());
 	}
 }
