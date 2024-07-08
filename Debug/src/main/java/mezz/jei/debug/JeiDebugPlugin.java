cc34696c84005099d15d5e5af271496682b83eb2
mezz <tehgeek@gmail.com>
2024-07-08 19:31:57 +0900

    Fix edge cases in recipe layout positioning
    (cherry picked from commit 171a0024c054bf5a702c0172f49490f00fde7000)

--- Debug/src/main/java/mezz/jei/debug/JeiDebugPlugin.java
+++ Debug/src/main/java/mezz/jei/debug/JeiDebugPlugin.java
@@ -135,7 +135,8 @@ public class JeiDebugPlugin implements IModPlugin {
 		registration.addRecipeCategories(
 			debugRecipeCategory,
 			new DebugFocusRecipeCategory<>(guiHelper, platformFluidHelper),
-			new ErrorRecipeCategory(guiHelper)
+			new ErrorRecipeCategory(guiHelper),
+			new ObnoxiouslyLargeCategory(guiHelper, ingredientManager)
 		);
 	}
 
@@ -210,6 +211,8 @@ public class JeiDebugPlugin implements IModPlugin {
 		if (DebugConfig.isCrashingTestRecipesEnabled()) {
 			registration.addRecipes(ErrorRecipeCategory.TYPE, Arrays.stream(ErrorRecipe.CrashType.values()).map(ErrorRecipe::new).toList());
 		}
+
+		registration.addRecipes(ObnoxiouslyLargeCategory.TYPE, List.of(new ObnoxiouslyLargeRecipe()));
 	}
 
 	private <T> void registerFluidRecipes(IRecipeRegistration registration, IPlatformFluidHelper<T> platformFluidHelper) {
