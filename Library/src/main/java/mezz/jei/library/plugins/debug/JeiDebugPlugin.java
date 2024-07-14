Mysticpasta1/JEI-Async

Normalize ingredients used in Info recipes and as Catalysts

Library/src/main/java/mezz/jei/library/plugins/debug/JeiDebugPlugin.java
@@ -163,6 +163,9 @@ public class JeiDebugPlugin implements IModPlugin {
 		long bucketVolume = platformFluidHelper.bucketVolume();
 		T fluidIngredient = platformFluidHelper.create(Fluids.WATER.defaultFluidState().holder(), bucketVolume);
 		registration.addIngredientInfo(fluidIngredient, platformFluidHelper.getFluidIngredientType(), Component.literal("water"));
+
+		fluidIngredient = platformFluidHelper.create(Fluids.LAVA.defaultFluidState().holder(), 1);
+		registration.addIngredientInfo(fluidIngredient, platformFluidHelper.getFluidIngredientType(), Component.literal("small amount of lava that should still show as 1 bucket"));
 	}
 
 	@Override

mezz/JustEnoughItems

Library/src/main/java/mezz/jei/library/plugins/debug/JeiDebugPlugin.java
@@ -156,6 +156,9 @@ public class JeiDebugPlugin implements IModPlugin {
 		long bucketVolume = platformFluidHelper.bucketVolume();
 		T fluidIngredient = platformFluidHelper.create(Fluids.WATER, bucketVolume, null);
 		registration.addIngredientInfo(fluidIngredient, platformFluidHelper.getFluidIngredientType(), Component.literal("water"));
+
+		fluidIngredient = platformFluidHelper.create(Fluids.LAVA.defaultFluidState().getType(), 1);
+		registration.addIngredientInfo(fluidIngredient, platformFluidHelper.getFluidIngredientType(), Component.literal("small amount of lava that should still show as 1 bucket"));
 	}
 
 	@Override