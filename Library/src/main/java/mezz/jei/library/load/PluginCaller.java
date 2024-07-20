6d6d712ea7afcf0e599f02ae83fdd2db2db7ecaa
mezz <tehgeek@gmail.com>
2024-07-20 16:51:32 +0900

    Do not remove broken plugins if they crash, just keep trying to use them

--- Library/src/main/java/mezz/jei/library/load/PluginCaller.java
+++ Library/src/main/java/mezz/jei/library/load/PluginCaller.java
@@ -6,7 +6,6 @@ import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -18,8 +17,6 @@ public class PluginCaller {
 		Stopwatch stopwatch = Stopwatch.createStarted();
 
 		try (PluginCallerTimer timer = new PluginCallerTimer()) {
-			List<IModPlugin> erroredPlugins = new ArrayList<>();
-
 			for (IModPlugin plugin : plugins) {
 				try {
 					ResourceLocation pluginUid = plugin.getPluginUid();
@@ -28,10 +25,8 @@ public class PluginCaller {
 					timer.end();
 				} catch (RuntimeException | LinkageError e) {
 					LOGGER.error("Caught an error from mod plugin: {} {}", plugin.getClass(), plugin.getPluginUid(), e);
-					erroredPlugins.add(plugin);
 				}
 			}
-			plugins.removeAll(erroredPlugins);
 		}
 
 		LOGGER.info("{} took {}", title, stopwatch);
