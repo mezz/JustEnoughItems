513947ba504a537381c00710ec25738171db569f
mezz <tehgeek@gmail.com>
2024-07-22 22:26:27 +0900

    Update for breaking change in NeoForge for config guis
    See https://github.com/neoforged/NeoForge/pull/1199

--- NeoForge/src/main/java/mezz/jei/neoforge/platform/ConfigHelper.java
+++ NeoForge/src/main/java/mezz/jei/neoforge/platform/ConfigHelper.java
@@ -5,10 +5,10 @@ import mezz.jei.common.platform.IPlatformConfigHelper;
 import mezz.jei.common.util.ErrorUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforgespi.language.IModInfo;
 
 import java.nio.file.Path;
 import java.util.Optional;
@@ -26,8 +26,10 @@ public class ConfigHelper implements IPlatformConfigHelper {
 		ErrorUtil.checkNotNull(minecraft.screen, "minecraft.screen");
 		return ModList.get()
 			.getModContainerById(ModIds.JEI_ID)
-			.map(ModContainer::getModInfo)
-			.flatMap(IConfigScreenFactory::getForMod)
-			.map(f -> f.createScreen(minecraft, minecraft.screen));
+			.flatMap(m -> {
+				IModInfo modInfo = m.getModInfo();
+				return IConfigScreenFactory.getForMod(modInfo)
+					.map(f -> f.createScreen(m, minecraft.screen));
+			});
 	}
 }
