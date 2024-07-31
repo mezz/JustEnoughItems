77658ea2e08e11ac779e33b16b6f8f2d0d8b54d9
mezz <tehgeek@gmail.com>
2024-07-31 18:08:03 +0900

    clean unused imports

--- Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/subtypes/SuspiciousStewSubtypeInterpreter.java
+++ Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/subtypes/SuspiciousStewSubtypeInterpreter.java
@@ -2,17 +2,12 @@ package mezz.jei.library.plugins.vanilla.ingredients.subtypes;
 
 import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
 import mezz.jei.api.ingredients.subtypes.UidContext;
-import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.Instrument;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.SuspiciousStewEffects;
-import net.minecraft.world.item.enchantment.Enchantment;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.StringJoiner;
 
 public class SuspiciousStewSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
