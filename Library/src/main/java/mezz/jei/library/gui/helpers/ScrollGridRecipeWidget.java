Mysticpasta1/JEI-Async

Add support for scrollable and draggable widgets in recipe layouts (#3631)

Library/src/main/java/mezz/jei/library/gui/helpers/ScrollGridRecipeWidget.java
@@ -1,0 +1,219 @@
+package mezz.jei.library.gui.helpers;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
+import mezz.jei.api.gui.inputs.IJeiInputHandler;
+import mezz.jei.api.gui.inputs.IJeiUserInput;
+import mezz.jei.api.gui.inputs.RecipeSlotUnderMouse;
+import mezz.jei.api.gui.widgets.ISlottedRecipeWidget;
+import mezz.jei.common.Internal;
+import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
+import mezz.jei.common.gui.textures.Textures;
+import mezz.jei.common.util.ImmutableRect2i;
+import mezz.jei.common.util.ImmutableSize2i;
+import mezz.jei.common.util.MathUtil;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
+import net.minecraft.client.gui.navigation.ScreenRectangle;
+import net.minecraft.util.Mth;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ScrollGridRecipeWidget implements ISlottedRecipeWidget, IJeiInputHandler {
+	public static final int SCROLLBAR_PADDING = 2;
+	public static final int SCROLLBAR_WIDTH = 14;
+	private static final int MIN_SCROLL_MARKER_HEIGHT = 14;
+
+	private final DrawableNineSliceTexture scrollbarMarker;
+	private final DrawableNineSliceTexture scrollbarBackground;
+	private final IDrawable slotBackground;
+	private final int columns;
+	private final int visibleRows;
+	private final int hiddenRows;
+	private final List<IRecipeSlotDrawable> slots;
+	private final ScreenRectangle area;
+	private final ImmutableRect2i scrollArea;
+	/**
+	 * Position of the mouse on the scroll marker when dragging.
+	 */
+	private double dragOriginY = -1;
+	/**
+	 * Amount scrolled in percent, (0 = top, 1 = bottom)
+	 */
+	private float scrollOffsetY = 0;
+
+	public static ImmutableSize2i calculateSize(IDrawable slotBackground, int columns, int visibleRows) {
+		return new ImmutableSize2i(
+			columns * slotBackground.getWidth() + SCROLLBAR_PADDING + SCROLLBAR_WIDTH,
+			visibleRows * slotBackground.getHeight()
+		);
+	}
+
+	public ScrollGridRecipeWidget(ScreenRectangle area, int columns, int visibleRows, List<IRecipeSlotDrawable> slots) {
+		this.slots = slots;
+		Textures textures = Internal.getTextures();
+		this.scrollbarMarker = textures.getScrollbarMarker();
+		this.scrollbarBackground = textures.getScrollbarBackground();
+		this.slotBackground = textures.getSlotDrawable();
+
+		this.columns = columns;
+		this.visibleRows = visibleRows;
+		this.area = area;
+		this.scrollArea = calculateScrollArea(area.width(), area.height());
+		int totalRows = MathUtil.divideCeil(slots.size(), columns);
+		this.hiddenRows = Math.max(totalRows - visibleRows, 0);
+	}
+
+	@Override
+	public ScreenPosition getPosition() {
+		return area.position();
+	}
+
+	@Override
+	public ScreenRectangle getArea() {
+		return area;
+	}
+
+	private static ImmutableRect2i calculateScrollArea(int width, int height) {
+		return new ImmutableRect2i(
+			width - SCROLLBAR_WIDTH,
+			0,
+			SCROLLBAR_WIDTH,
+			height
+		);
+	}
+
+	private ImmutableRect2i calculateScrollbarMarkerArea(int hiddenRows) {
+		int totalSpace = scrollArea.height() - 2;
+		int scrollMarkerWidth = scrollArea.width() - 2;
+		int scrollMarkerHeight = Math.round(totalSpace * (visibleRows / (float) (visibleRows + hiddenRows)));
+		scrollMarkerHeight = Math.max(scrollMarkerHeight, MIN_SCROLL_MARKER_HEIGHT);
+		int scrollbarMarkerY = Math.round((totalSpace - scrollMarkerHeight) * scrollOffsetY);
+		return new ImmutableRect2i(
+			scrollArea.getX() + 1,
+			scrollArea.getY() + 1 + scrollbarMarkerY,
+			scrollMarkerWidth,
+			scrollMarkerHeight
+		);
+	}
+
+	@Override
+	public void draw(GuiGraphics guiGraphics, double mouseX, double mouseY) {
+		scrollbarBackground.draw(guiGraphics, scrollArea);
+
+		ImmutableRect2i scrollbarMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+		scrollbarMarker.draw(guiGraphics, scrollbarMarkerArea);
+
+		final int totalSlots = slots.size();
+		final int firstRow = getRowIndexForScroll(hiddenRows, scrollOffsetY);
+		final int firstIndex = columns * firstRow;
+
+		final int slotWidth = slotBackground.getWidth();
+		final int slotHeight = slotBackground.getHeight();
+
+		for (int row = 0; row < visibleRows; row++) {
+			final int y = row * slotHeight;
+			for (int column = 0; column < columns; column++) {
+				final int x = column * slotWidth;
+				final int slotIndex = firstIndex + (row * columns) + column;
+				slotBackground.draw(guiGraphics, x, y);
+				if (slotIndex < totalSlots) {
+					IRecipeSlotDrawable slot = slots.get(slotIndex);
+					slot.setPosition(x + 1, y + 1);
+					slot.draw(guiGraphics);
+				}
+			}
+		}
+	}
+
+	@Override
+	public Optional<RecipeSlotUnderMouse> getSlotUnderMouse(double mouseX, double mouseY) {
+		final int firstRow = getRowIndexForScroll(hiddenRows, scrollOffsetY);
+		final int startIndex = firstRow * columns;
+		final int endIndex = Math.min(startIndex + (visibleRows * columns), slots.size());
+		for (int i = startIndex; i < endIndex; i++) {
+			IRecipeSlotDrawable slot = slots.get(i);
+			if (slot.isMouseOver(mouseX, mouseY)) {
+				return Optional.of(new RecipeSlotUnderMouse(slot, getPosition()));
+			}
+		}
+		return Optional.empty();
+	}
+
+	private static int getRowIndexForScroll(int hiddenRows, float scrollOffset) {
+		int rowIndex = (int) ((double) (scrollOffset * (float) hiddenRows) + 0.5D);
+		return Math.max(rowIndex, 0);
+	}
+
+	private static float subtractInputFromScroll(int hiddenRows, float scrollOffset, double scrollDeltaY) {
+		float newScrollOffset = scrollOffset - (float) (scrollDeltaY / (double) hiddenRows);
+		return Mth.clamp(newScrollOffset, 0.0F, 1.0F);
+	}
+
+	@Override
+	public boolean handleInput(double mouseX, double mouseY, IJeiUserInput userInput) {
+		if (!userInput.is(Internal.getKeyMappings().getLeftClick())) {
+			return false;
+		}
+		if (!userInput.isSimulate()) {
+			dragOriginY = -1;
+		}
+
+		if (scrollArea.contains(mouseX, mouseY)) {
+			if (hiddenRows == 0) {
+				return false;
+			}
+
+			if (userInput.isSimulate()) {
+				ImmutableRect2i scrollMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+				if (!scrollMarkerArea.contains(mouseX, mouseY)) {
+					moveScrollbarCenterTo(scrollMarkerArea, mouseY);
+					scrollMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+				}
+				dragOriginY = mouseY - scrollMarkerArea.y();
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private void moveScrollbarCenterTo(ImmutableRect2i scrollMarkerArea, double centerY) {
+		double topY = centerY - (scrollMarkerArea.height() / 2.0);
+		moveScrollbarTo(scrollMarkerArea, topY);
+	}
+
+	private void moveScrollbarTo(ImmutableRect2i scrollMarkerArea, double topY) {
+		int minY = scrollArea.y();
+		int maxY = scrollArea.y() + scrollArea.height() - scrollMarkerArea.height();
+		double relativeY = topY - minY;
+		int totalSpace = maxY - minY;
+		scrollOffsetY = (float) (relativeY / (float) totalSpace);
+		scrollOffsetY = Mth.clamp(scrollOffsetY, 0.0F, 1.0F);
+	}
+
+	@Override
+	public boolean handleMouseScrolled(double mouseX, double mouseY, double scrollDeltaX, double scrollDeltaY) {
+		int totalSlots = slots.size();
+		if (hiddenRows > 0) {
+			scrollOffsetY = subtractInputFromScroll(totalSlots, scrollOffsetY, scrollDeltaY);
+		} else {
+			scrollOffsetY = 0.0f;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean handleMouseDragged(double mouseX, double mouseY, InputConstants.Key mouseKey, double dragX, double dragY) {
+		if (dragOriginY < 0 || mouseKey.getValue() != InputConstants.MOUSE_BUTTON_LEFT) {
+			return false;
+		}
+
+		ImmutableRect2i scrollbarMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+
+		double topY = mouseY - dragOriginY;
+		moveScrollbarTo(scrollbarMarkerArea, topY);
+		return true;
+	}
+}

Library/src/main/java/mezz/jei/library/plugins/vanilla/cooking/AbstractCookingCategory.java
@@ -1,19 +1,19 @@
 package mezz.jei.library.plugins.vanilla.cooking;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import net.minecraft.client.gui.GuiGraphics;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.widgets.IRecipeWidget;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.common.Constants;
 import mezz.jei.library.util.RecipeUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -26,10 +26,10 @@ import static mezz.jei.api.recipe.RecipeIngredientRole.OUTPUT;
 
 public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> extends FurnaceVariantCategory<RecipeHolder<T>> {
 	private final IDrawable background;
-	private final int regularCookTime;
 	private final IDrawable icon;
 	private final Component localizedName;
-	private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+	protected final IGuiHelper guiHelper;
+	protected final int regularCookTime;
 
 	public AbstractCookingCategory(IGuiHelper guiHelper, Block icon, String translationKey, int regularCookTime) {
 		super(guiHelper);
@@ -37,24 +37,7 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 		this.regularCookTime = regularCookTime;
 		this.icon = guiHelper.createDrawableItemStack(new ItemStack(icon));
 		this.localizedName = Component.translatable(translationKey);
-		this.cachedArrows = CacheBuilder.newBuilder()
-			.maximumSize(25)
-			.build(new CacheLoader<>() {
-				@Override
-				public IDrawableAnimated load(Integer cookTime) {
-					return guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
-						.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
-				}
-			});
-	}
-
-	protected IDrawableAnimated getArrow(RecipeHolder<T> recipeHolder) {
-		T recipe = recipeHolder.value();
-		int cookTime = recipe.getCookingTime();
-		if (cookTime <= 0) {
-			cookTime = regularCookTime;
-		}
-		return this.cachedArrows.getUnchecked(cookTime);
+		this.guiHelper = guiHelper;
 	}
 
 	@Override
@@ -70,9 +53,6 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	@Override
 	public void draw(RecipeHolder<T> recipeHolder, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
 		animatedFlame.draw(guiGraphics, 1, 20);
-
-		IDrawableAnimated arrow = getArrow(recipeHolder);
-		arrow.draw(guiGraphics, 24, 18);
 
 		drawExperience(recipeHolder, guiGraphics, 0);
 		drawCookTime(recipeHolder, guiGraphics, 45);
@@ -120,6 +100,11 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	}
 
 	@Override
+	public void createRecipeExtras(IRecipeExtrasBuilder acceptor, RecipeHolder<T> recipeHolder, IFocusGroup focuses) {
+		acceptor.addWidget(createCookingArrowWidget(recipeHolder, new ScreenPosition(24, 18)));
+	}
+
+	@Override
 	public boolean isHandled(RecipeHolder<T> recipeHolder) {
 		T recipe = recipeHolder.value();
 		return !recipe.isSpecial();
@@ -128,5 +113,35 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	@Override
 	public ResourceLocation getRegistryName(RecipeHolder<T> recipe) {
 		return recipe.id();
+	}
+
+	protected IRecipeWidget createCookingArrowWidget(RecipeHolder<T> recipeHolder, ScreenPosition position) {
+		return new CookingArrowRecipeWidget<>(guiHelper, recipeHolder, regularCookTime, position);
+	}
+
+	private static class CookingArrowRecipeWidget<T extends AbstractCookingRecipe> implements IRecipeWidget {
+		private final IDrawableAnimated arrow;
+		private final ScreenPosition position;
+
+		public CookingArrowRecipeWidget(IGuiHelper guiHelper, RecipeHolder<T> recipeHolder, int regularCookTime, ScreenPosition position) {
+			T recipe = recipeHolder.value();
+			int cookTime = recipe.getCookingTime();
+			if (cookTime <= 0) {
+				cookTime = regularCookTime;
+			}
+			this.arrow = guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
+				.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
+			this.position = position;
+		}
+
+		@Override
+		public ScreenPosition getPosition() {
+			return position;
+		}
+
+		@Override
+		public void draw(GuiGraphics guiGraphics, double mouseX, double mouseY) {
+			arrow.draw(guiGraphics);
+		}
 	}
 }

Library/src/main/java/mezz/jei/library/plugins/vanilla/cooking/CampfireCookingCategory.java
@@ -1,17 +1,18 @@
 package mezz.jei.library.plugins.vanilla.cooking;
 
-import net.minecraft.client.gui.GuiGraphics;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.common.Constants;
 import mezz.jei.library.util.RecipeUtil;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
 import net.minecraft.world.item.crafting.CampfireCookingRecipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.level.block.Blocks;
@@ -39,8 +40,6 @@ public class CampfireCookingCategory extends AbstractCookingCategory<CampfireCoo
 	@Override
 	public void draw(RecipeHolder<CampfireCookingRecipe> recipeHolder, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
 		animatedFlame.draw(guiGraphics, 1, 20);
-		IDrawableAnimated arrow = getArrow(recipeHolder);
-		arrow.draw(guiGraphics, 24, 8);
 		drawCookTime(recipeHolder, guiGraphics, 35);
 	}
 
@@ -52,5 +51,10 @@ public class CampfireCookingCategory extends AbstractCookingCategory<CampfireCoo
 
 		builder.addSlot(RecipeIngredientRole.OUTPUT, 61, 9)
 			.addItemStack(RecipeUtil.getResultItem(recipe));
+	}
+
+	@Override
+	public void createRecipeExtras(IRecipeExtrasBuilder acceptor, RecipeHolder<CampfireCookingRecipe> recipeHolder, IFocusGroup focuses) {
+		acceptor.addWidget(createCookingArrowWidget(recipeHolder, new ScreenPosition(24, 8)));
 	}
 }

mezz/JustEnoughItems

Library/src/main/java/mezz/jei/library/gui/helpers/ScrollGridRecipeWidget.java
@@ -1,0 +1,219 @@
+package mezz.jei.library.gui.helpers;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
+import mezz.jei.api.gui.inputs.IJeiInputHandler;
+import mezz.jei.api.gui.inputs.IJeiUserInput;
+import mezz.jei.api.gui.inputs.RecipeSlotUnderMouse;
+import mezz.jei.api.gui.widgets.ISlottedRecipeWidget;
+import mezz.jei.common.Internal;
+import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
+import mezz.jei.common.gui.textures.Textures;
+import mezz.jei.common.util.ImmutableRect2i;
+import mezz.jei.common.util.ImmutableSize2i;
+import mezz.jei.common.util.MathUtil;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
+import net.minecraft.client.gui.navigation.ScreenRectangle;
+import net.minecraft.util.Mth;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ScrollGridRecipeWidget implements ISlottedRecipeWidget, IJeiInputHandler {
+	public static final int SCROLLBAR_PADDING = 2;
+	public static final int SCROLLBAR_WIDTH = 14;
+	private static final int MIN_SCROLL_MARKER_HEIGHT = 14;
+
+	private final DrawableNineSliceTexture scrollbarMarker;
+	private final DrawableNineSliceTexture scrollbarBackground;
+	private final IDrawable slotBackground;
+	private final int columns;
+	private final int visibleRows;
+	private final int hiddenRows;
+	private final List<IRecipeSlotDrawable> slots;
+	private final ScreenRectangle area;
+	private final ImmutableRect2i scrollArea;
+	/**
+	 * Position of the mouse on the scroll marker when dragging.
+	 */
+	private double dragOriginY = -1;
+	/**
+	 * Amount scrolled in percent, (0 = top, 1 = bottom)
+	 */
+	private float scrollOffsetY = 0;
+
+	public static ImmutableSize2i calculateSize(IDrawable slotBackground, int columns, int visibleRows) {
+		return new ImmutableSize2i(
+			columns * slotBackground.getWidth() + SCROLLBAR_PADDING + SCROLLBAR_WIDTH,
+			visibleRows * slotBackground.getHeight()
+		);
+	}
+
+	public ScrollGridRecipeWidget(ScreenRectangle area, int columns, int visibleRows, List<IRecipeSlotDrawable> slots) {
+		this.slots = slots;
+		Textures textures = Internal.getTextures();
+		this.scrollbarMarker = textures.getScrollbarMarker();
+		this.scrollbarBackground = textures.getScrollbarBackground();
+		this.slotBackground = textures.getSlotDrawable();
+
+		this.columns = columns;
+		this.visibleRows = visibleRows;
+		this.area = area;
+		this.scrollArea = calculateScrollArea(area.width(), area.height());
+		int totalRows = MathUtil.divideCeil(slots.size(), columns);
+		this.hiddenRows = Math.max(totalRows - visibleRows, 0);
+	}
+
+	@Override
+	public ScreenPosition getPosition() {
+		return area.position();
+	}
+
+	@Override
+	public ScreenRectangle getArea() {
+		return area;
+	}
+
+	private static ImmutableRect2i calculateScrollArea(int width, int height) {
+		return new ImmutableRect2i(
+			width - SCROLLBAR_WIDTH,
+			0,
+			SCROLLBAR_WIDTH,
+			height
+		);
+	}
+
+	private ImmutableRect2i calculateScrollbarMarkerArea(int hiddenRows) {
+		int totalSpace = scrollArea.height() - 2;
+		int scrollMarkerWidth = scrollArea.width() - 2;
+		int scrollMarkerHeight = Math.round(totalSpace * (visibleRows / (float) (visibleRows + hiddenRows)));
+		scrollMarkerHeight = Math.max(scrollMarkerHeight, MIN_SCROLL_MARKER_HEIGHT);
+		int scrollbarMarkerY = Math.round((totalSpace - scrollMarkerHeight) * scrollOffsetY);
+		return new ImmutableRect2i(
+			scrollArea.getX() + 1,
+			scrollArea.getY() + 1 + scrollbarMarkerY,
+			scrollMarkerWidth,
+			scrollMarkerHeight
+		);
+	}
+
+	@Override
+	public void draw(GuiGraphics guiGraphics, double mouseX, double mouseY) {
+		scrollbarBackground.draw(guiGraphics, scrollArea);
+
+		ImmutableRect2i scrollbarMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+		scrollbarMarker.draw(guiGraphics, scrollbarMarkerArea);
+
+		final int totalSlots = slots.size();
+		final int firstRow = getRowIndexForScroll(hiddenRows, scrollOffsetY);
+		final int firstIndex = columns * firstRow;
+
+		final int slotWidth = slotBackground.getWidth();
+		final int slotHeight = slotBackground.getHeight();
+
+		for (int row = 0; row < visibleRows; row++) {
+			final int y = row * slotHeight;
+			for (int column = 0; column < columns; column++) {
+				final int x = column * slotWidth;
+				final int slotIndex = firstIndex + (row * columns) + column;
+				slotBackground.draw(guiGraphics, x, y);
+				if (slotIndex < totalSlots) {
+					IRecipeSlotDrawable slot = slots.get(slotIndex);
+					slot.setPosition(x + 1, y + 1);
+					slot.draw(guiGraphics);
+				}
+			}
+		}
+	}
+
+	@Override
+	public Optional<RecipeSlotUnderMouse> getSlotUnderMouse(double mouseX, double mouseY) {
+		final int firstRow = getRowIndexForScroll(hiddenRows, scrollOffsetY);
+		final int startIndex = firstRow * columns;
+		final int endIndex = Math.min(startIndex + (visibleRows * columns), slots.size());
+		for (int i = startIndex; i < endIndex; i++) {
+			IRecipeSlotDrawable slot = slots.get(i);
+			if (slot.isMouseOver(mouseX, mouseY)) {
+				return Optional.of(new RecipeSlotUnderMouse(slot, getPosition()));
+			}
+		}
+		return Optional.empty();
+	}
+
+	private static int getRowIndexForScroll(int hiddenRows, float scrollOffset) {
+		int rowIndex = (int) ((double) (scrollOffset * (float) hiddenRows) + 0.5D);
+		return Math.max(rowIndex, 0);
+	}
+
+	private static float subtractInputFromScroll(int hiddenRows, float scrollOffset, double scrollDeltaY) {
+		float newScrollOffset = scrollOffset - (float) (scrollDeltaY / (double) hiddenRows);
+		return Mth.clamp(newScrollOffset, 0.0F, 1.0F);
+	}
+
+	@Override
+	public boolean handleInput(double mouseX, double mouseY, IJeiUserInput userInput) {
+		if (!userInput.is(Internal.getKeyMappings().getLeftClick())) {
+			return false;
+		}
+		if (!userInput.isSimulate()) {
+			dragOriginY = -1;
+		}
+
+		if (scrollArea.contains(mouseX, mouseY)) {
+			if (hiddenRows == 0) {
+				return false;
+			}
+
+			if (userInput.isSimulate()) {
+				ImmutableRect2i scrollMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+				if (!scrollMarkerArea.contains(mouseX, mouseY)) {
+					moveScrollbarCenterTo(scrollMarkerArea, mouseY);
+					scrollMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+				}
+				dragOriginY = mouseY - scrollMarkerArea.y();
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private void moveScrollbarCenterTo(ImmutableRect2i scrollMarkerArea, double centerY) {
+		double topY = centerY - (scrollMarkerArea.height() / 2.0);
+		moveScrollbarTo(scrollMarkerArea, topY);
+	}
+
+	private void moveScrollbarTo(ImmutableRect2i scrollMarkerArea, double topY) {
+		int minY = scrollArea.y();
+		int maxY = scrollArea.y() + scrollArea.height() - scrollMarkerArea.height();
+		double relativeY = topY - minY;
+		int totalSpace = maxY - minY;
+		scrollOffsetY = (float) (relativeY / (float) totalSpace);
+		scrollOffsetY = Mth.clamp(scrollOffsetY, 0.0F, 1.0F);
+	}
+
+	@Override
+	public boolean handleMouseScrolled(double mouseX, double mouseY, double scrollDelta) {
+		int totalSlots = slots.size();
+		if (hiddenRows > 0) {
+			scrollOffsetY = subtractInputFromScroll(totalSlots, scrollOffsetY, scrollDelta);
+		} else {
+			scrollOffsetY = 0.0f;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean handleMouseDragged(double mouseX, double mouseY, InputConstants.Key mouseKey, double dragX, double dragY) {
+		if (dragOriginY < 0 || mouseKey.getValue() != InputConstants.MOUSE_BUTTON_LEFT) {
+			return false;
+		}
+
+		ImmutableRect2i scrollbarMarkerArea = calculateScrollbarMarkerArea(hiddenRows);
+
+		double topY = mouseY - dragOriginY;
+		moveScrollbarTo(scrollbarMarkerArea, topY);
+		return true;
+	}
+}

Library/src/main/java/mezz/jei/library/plugins/vanilla/cooking/AbstractCookingCategory.java
@@ -1,12 +1,11 @@
 package mezz.jei.library.plugins.vanilla.cooking;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.widgets.IRecipeWidget;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.common.Constants;
@@ -14,6 +13,7 @@ import mezz.jei.library.util.RecipeUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -25,10 +25,10 @@ import static mezz.jei.api.recipe.RecipeIngredientRole.OUTPUT;
 
 public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> extends FurnaceVariantCategory<T> {
 	private final IDrawable background;
-	private final int regularCookTime;
 	private final IDrawable icon;
 	private final Component localizedName;
-	private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+	protected final IGuiHelper guiHelper;
+	protected final int regularCookTime;
 
 	public AbstractCookingCategory(IGuiHelper guiHelper, Block icon, String translationKey, int regularCookTime) {
 		super(guiHelper);
@@ -36,23 +36,7 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 		this.regularCookTime = regularCookTime;
 		this.icon = guiHelper.createDrawableItemStack(new ItemStack(icon));
 		this.localizedName = Component.translatable(translationKey);
-		this.cachedArrows = CacheBuilder.newBuilder()
-			.maximumSize(25)
-			.build(new CacheLoader<>() {
-				@Override
-				public IDrawableAnimated load(Integer cookTime) {
-					return guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
-						.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
-				}
-			});
-	}
-
-	protected IDrawableAnimated getArrow(T recipe) {
-		int cookTime = recipe.getCookingTime();
-		if (cookTime <= 0) {
-			cookTime = regularCookTime;
-		}
-		return this.cachedArrows.getUnchecked(cookTime);
+		this.guiHelper = guiHelper;
 	}
 
 	@Override
@@ -68,9 +52,6 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	@Override
 	public void draw(T recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
 		animatedFlame.draw(guiGraphics, 1, 20);
-
-		IDrawableAnimated arrow = getArrow(recipe);
-		arrow.draw(guiGraphics, 24, 18);
 
 		drawExperience(recipe, guiGraphics, 0);
 		drawCookTime(recipe, guiGraphics, 45);
@@ -119,7 +100,41 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	}
 
 	@Override
+	public void createRecipeExtras(IRecipeExtrasBuilder acceptor, T recipe, IFocusGroup focuses) {
+		acceptor.addWidget(createCookingArrowWidget(recipe, new ScreenPosition(24, 18)));
+	}
+
+	@Override
 	public ResourceLocation getRegistryName(T recipe) {
 		return recipe.getId();
+	}
+
+	protected IRecipeWidget createCookingArrowWidget(T recipe, ScreenPosition position) {
+		return new CookingArrowRecipeWidget<>(guiHelper, recipe, regularCookTime, position);
+	}
+
+	private static class CookingArrowRecipeWidget<T extends AbstractCookingRecipe> implements IRecipeWidget {
+		private final IDrawableAnimated arrow;
+		private final ScreenPosition position;
+
+		public CookingArrowRecipeWidget(IGuiHelper guiHelper, T recipe, int regularCookTime, ScreenPosition position) {
+			int cookTime = recipe.getCookingTime();
+			if (cookTime <= 0) {
+				cookTime = regularCookTime;
+			}
+			this.arrow = guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
+				.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
+			this.position = position;
+		}
+
+		@Override
+		public ScreenPosition getPosition() {
+			return position;
+		}
+
+		@Override
+		public void draw(GuiGraphics guiGraphics, double mouseX, double mouseY) {
+			arrow.draw(guiGraphics);
+		}
 	}
 }

Library/src/main/java/mezz/jei/library/plugins/vanilla/cooking/CampfireCookingCategory.java
@@ -1,17 +1,18 @@
 package mezz.jei.library.plugins.vanilla.cooking;
 
-import net.minecraft.client.gui.GuiGraphics;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.common.Constants;
 import mezz.jei.library.util.RecipeUtil;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
 import net.minecraft.world.item.crafting.CampfireCookingRecipe;
 import net.minecraft.world.level.block.Blocks;
 
@@ -38,8 +39,6 @@ public class CampfireCookingCategory extends AbstractCookingCategory<CampfireCoo
 	@Override
 	public void draw(CampfireCookingRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
 		animatedFlame.draw(guiGraphics, 1, 20);
-		IDrawableAnimated arrow = getArrow(recipe);
-		arrow.draw(guiGraphics, 24, 8);
 		drawCookTime(recipe, guiGraphics, 35);
 	}
 
@@ -50,5 +49,10 @@ public class CampfireCookingCategory extends AbstractCookingCategory<CampfireCoo
 
 		builder.addSlot(RecipeIngredientRole.OUTPUT, 61, 9)
 			.addItemStack(RecipeUtil.getResultItem(recipe));
+	}
+
+	@Override
+	public void createRecipeExtras(IRecipeExtrasBuilder acceptor, CampfireCookingRecipe recipe, IFocusGroup focuses) {
+		acceptor.addWidget(createCookingArrowWidget(recipe, new ScreenPosition(24, 8)));
 	}
 }