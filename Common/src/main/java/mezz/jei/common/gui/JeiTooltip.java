a8abd83b3350fc0552e1b385793a24d148878d97
mezz <tehgeek@gmail.com>
2024-08-06 13:36:30 +0900

    (cherry picked from commit 774bc6d3a0abb3e80b7caace671bc932f2d717d6)

--- Common/src/main/java/mezz/jei/common/gui/JeiTooltip.java
+++ Common/src/main/java/mezz/jei/common/gui/JeiTooltip.java
@@ -68,11 +68,23 @@ public class JeiTooltip implements ITooltipBuilder {
 	}
 
 	@Override
-	public void add(TooltipComponent component) {
+	public void add(@Nullable TooltipComponent component) {
+		if (component == null) {
+			if (Services.PLATFORM.getModHelper().isInDev()) {
+				throw new NullPointerException("Tried to add null tooltip component");
+			}
+			return;
+		}
 		elements.add(Either.right(component));
 	}
 
-	public void addClientTooltipComponent(ClientTooltipComponent component) {
+	public void addClientTooltipComponent(@Nullable ClientTooltipComponent component) {
+		if (component == null) {
+			if (Services.PLATFORM.getModHelper().isInDev()) {
+				throw new NullPointerException("Tried to add null tooltip component");
+			}
+			return;
+		}
 		if (component instanceof TooltipComponent tooltipComponent) {
 			add(tooltipComponent);
 			return;
