c1e57c30866f918b6c3e9eebb55d6bea2a6e2a32
mezz <tehgeek@gmail.com>
2024-08-08 13:44:01 +0900

    #3665 Correctly handle recipe transfer and bookmark buttons when they overlap the recipe
    (cherry picked from commit 7b4d2b49367558d3b559372486eb2249ec1f51c8)

--- Gui/src/main/java/mezz/jei/gui/recipes/RecipeLayoutWithButtons.java
+++ Gui/src/main/java/mezz/jei/gui/recipes/RecipeLayoutWithButtons.java
@@ -93,8 +93,8 @@ public record RecipeLayoutWithButtons<R>(
 		}
 		return new CombinedInputHandler(
 			"RecipeLayoutWithButtons",
-			transferButton.createInputHandler(),
 			bookmarkButton.createInputHandler(),
+			transferButton.createInputHandler(),
 			recipeLayoutInputHandler
 		);
 	}
