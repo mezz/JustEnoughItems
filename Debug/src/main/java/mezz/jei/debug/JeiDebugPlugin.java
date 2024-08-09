4db4461b509b1004956374ecdb99309117206fc8
mezz <tehgeek@gmail.com>
2024-08-09 09:48:45 +0900

    Add support for multiple Ghost Ingredient Handlers on a single screen
    (cherry picked from commit 52038ff77b612c65dfb27ea9697602a0db3b4ee7)

--- Debug/src/main/java/mezz/jei/debug/JeiDebugPlugin.java
+++ Debug/src/main/java/mezz/jei/debug/JeiDebugPlugin.java
@@ -260,6 +260,7 @@ public class JeiDebugPlugin implements IModPlugin {
 		});
 
 		registration.addGhostIngredientHandler(BrewingStandScreen.class, new DebugGhostIngredientHandler<>(ingredientManager));
+		registration.addGhostIngredientHandler(BrewingStandScreen.class, new DebugGhostIngredientHandlerTwo<>(ingredientManager));
 	}
 
 	@Override
