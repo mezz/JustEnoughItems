341ec82621605cfcff23370a7f87f61744ea9d28
mezz <tehgeek@gmail.com>
2024-08-12 22:09:39 +0900

    Optimize adding ingredients to recipes for lookups
    (cherry picked from commit f4e0b3cd0f895df7fdafb874ccf1ddbf4bfa29a0)

--- Library/src/main/java/mezz/jei/library/gui/recipes/RecipeLayoutIngredientSupplier.java
+++ Library/src/main/java/mezz/jei/library/gui/recipes/RecipeLayoutIngredientSupplier.java
@@ -1,52 +1,27 @@
 package mezz.jei.library.gui.recipes;
 
-import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.RecipeIngredientRole;
-import mezz.jei.library.gui.recipes.layout.builder.RecipeSlotIngredients;
+import mezz.jei.library.gui.recipes.supplier.builder.IngredientSlotBuilder;
 import mezz.jei.library.ingredients.IIngredientSupplier;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.Map;
 
 public class RecipeLayoutIngredientSupplier implements IIngredientSupplier {
-	private final List<RecipeSlotIngredients> slots;
+	private final Map<RecipeIngredientRole, IngredientSlotBuilder> ingredientSlotBuilders;
 
-	public RecipeLayoutIngredientSupplier(List<RecipeSlotIngredients> slots) {
-		this.slots = slots;
+	public RecipeLayoutIngredientSupplier(Map<RecipeIngredientRole, IngredientSlotBuilder> ingredientSlotBuilders) {
+		this.ingredientSlotBuilders = ingredientSlotBuilders;
 	}
 
 	@Override
-	public Stream<? extends IIngredientType<?>> getIngredientTypes(RecipeIngredientRole role) {
-		return slots.stream()
-			.filter(slot -> slot.role() == role)
-			.map(RecipeSlotIngredients::types)
-			.flatMap(Collection::stream)
-			.distinct();
-	}
-
-	@Override
-	public <T> Stream<T> getIngredientStream(IIngredientType<T> ingredientType, RecipeIngredientRole role) {
-		return slots.stream()
-			.filter(slot -> slot.role() == role)
-			.flatMap(slot -> slot.ingredients().stream())
-			.map(ingredientType::castIngredient)
-			.flatMap(Optional::stream);
-	}
-
-	@Override
-	public Collection<Optional<ITypedIngredient<?>>> getIngredients(RecipeIngredientRole role) {
-		List<Optional<ITypedIngredient<?>>> ingredients = new ArrayList<>();
-
-		for (RecipeSlotIngredients slot : slots) {
-			if (slot.role() == role) {
-				ingredients.addAll(slot.ingredients());
-			}
+	public Collection<ITypedIngredient<?>> getIngredients(RecipeIngredientRole role) {
+		IngredientSlotBuilder ingredientSlotBuilder = ingredientSlotBuilders.get(role);
+		if (ingredientSlotBuilder == null) {
+			return List.of();
 		}
-
-		return ingredients;
+		return ingredientSlotBuilder.getAllIngredients();
 	}
 }
