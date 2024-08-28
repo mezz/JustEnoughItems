43cec0a7a782dd0b3b064c2471f27503d6ae581e
mezz <tehgeek@gmail.com>
2024-08-28 23:25:48 +0900

    fix tests

--- NeoForge/src/test/java/mezz/jei/test/lib/TestIngredientFilterConfig.java
+++ NeoForge/src/test/java/mezz/jei/test/lib/TestIngredientFilterConfig.java
@@ -31,6 +31,11 @@ public class TestIngredientFilterConfig implements IIngredientFilterConfig {
 	}
 
 	@Override
+	public SearchMode getCreativeTabSearchMode() {
+		return SearchMode.DISABLED;
+	}
+
+	@Override
 	public boolean getSearchAdvancedTooltips() {
 		return false;
 	}
