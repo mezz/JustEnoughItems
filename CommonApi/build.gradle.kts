29462490f3e0204e00b9330f613e5084a1b344e3
mezz <tehgeek@gmail.com>
2024-09-03 22:33:25 +0900

    add Fabric-Loom-Remap to common-api jar
    (cherry picked from commit da426b88557964927865d391939d79579bd27f15)

--- CommonApi/build.gradle.kts
+++ CommonApi/build.gradle.kts
@@ -59,6 +59,12 @@ tasks.withType<JavaCompile> {
     }
 }
 
+tasks.withType<Jar> {
+    manifest {
+        attributes["Fabric-Loom-Remap"] = true
+    }
+}
+
 publishing {
     publications {
         register<MavenPublication>("commonApiJar") {
