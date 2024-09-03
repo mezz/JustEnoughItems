68e7b6acd7c60169df3ff81e9c56b01a0ff1834a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-09-03 04:18:18 -0400

    Use reference set in Node instead of wrapping map
    Adapted to the 1.16 source layout.
    
    (cherry picked from commit 92978a79a83527c52912c417b7d00a3d23826174)

--- src/main/java/mezz/jei/search/suffixtree/Node.java
+++ src/main/java/mezz/jei/search/suffixtree/Node.java
@@ -22,12 +22,12 @@ import com.google.common.collect.ImmutableList;
 import it.unimi.dsi.fastutil.chars.Char2ObjectMap;
 import it.unimi.dsi.fastutil.chars.Char2ObjectMaps;
 import it.unimi.dsi.fastutil.chars.Char2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 import mezz.jei.util.SubString;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.IdentityHashMap;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Set;
@@ -172,7 +172,7 @@ class Node<T> {
 		} else if (data.size() == 16) {
 			// "upgrade" data to a Set once it's getting bigger,
 			// to improve its `contains` performance.
-			Collection<T> newData = Collections.newSetFromMap(new IdentityHashMap<>());
+			Collection<T> newData = new ReferenceOpenHashSet<>(17);
 			newData.addAll(data);
 			newData.add(value);
 			data = newData;
