7f66ad0d5ed85b8404bc4e7e375aac2b51a1b172
mezz <tehgeek@gmail.com>
2024-09-12 16:44:49 +0900

    Revert "Include flowing fluids in the ingredient list"
    This reverts commit 879f116bf4fe346202e5c294fdf276e2ebb4b954.
    
    (cherry picked from commit 8f1cd5e3aeeee6eefd573bebeb6950bf4d627f2d)

--- Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/fluid/FluidStackListFactory.java
+++ Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/fluid/FluidStackListFactory.java
@@ -13,6 +13,7 @@ public final class FluidStackListFactory {
 
 	public static <T> List<T> create(IPlatformRegistry<Fluid> registry, IPlatformFluidHelper<T> helper) {
 		return registry.getValues()
+			.filter(fluid -> fluid.isSource(fluid.defaultFluidState()))
 			.map(fluid -> helper.create(fluid, helper.bucketVolume()))
 			.toList();
 	}
