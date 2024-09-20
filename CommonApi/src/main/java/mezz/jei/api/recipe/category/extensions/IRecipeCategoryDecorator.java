Mysticpasta1/JEI-Async

Add support for Recipe Category Decorators (#3255)
Co-authored-by: Relentless <relentless@rlnt.dev>
Co-authored-by: LLytho <main@lytho.dev>
Co-authored-by: mezz <tehgeek@gmail.com>

CommonApi/src/main/java/mezz/jei/api/recipe/category/extensions/IRecipeCategoryDecorator.java
@@ -1,0 +1,60 @@
+package mezz.jei.api.recipe.category.extensions;
+
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.registration.IAdvancedRegistration;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
+
+/**
+ * The {@link IRecipeCategoryDecorator} allows further customization of recipe categories.
+ * It can be used to draw additional elements or tooltips on recipes, even of other mods.
+ * <p>
+ * Register it with {@link IAdvancedRegistration#addRecipeCategoryDecorator(RecipeType, IRecipeCategoryDecorator)}.
+ *
+ * @since 15.1.0
+ */
+public interface IRecipeCategoryDecorator<T> {
+	/**
+	 * Draw extras or additional info about the recipe after the {@link IRecipeCategory} that
+	 * this decorator is registered to has drawn.
+	 * Tooltips are handled by {@link #decorateExistingTooltips(List, Object, IRecipeCategory, IRecipeSlotsView, double, double)}.
+	 *
+	 * @param recipe          the current recipe being drawn.
+	 * @param recipeCategory  the recipe category of the recipe.
+	 * @param recipeSlotsView a view of the current recipe slots being drawn.
+	 * @param guiGraphics     the current {@link GuiGraphics} for rendering.
+	 * @param mouseX          the X position of the mouse, relative to the recipe.
+	 * @param mouseY          the Y position of the mouse, relative to the recipe.
+	 *
+	 * @see IDrawable for a simple class for drawing things.
+	 * @see IGuiHelper for useful functions.
+	 * @see IRecipeSlotsView for information about the ingredients that are currently being drawn.
+	 */
+	default void draw(T recipe, IRecipeCategory<T> recipeCategory, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+
+	}
+
+	/**
+	 * Allows modifying of and adding to existing tooltips added by the {@link IRecipeCategory} that
+	 * this decorator is registered to.
+	 * To avoid removing tooltips from the category itself, make sure to return original list with your
+	 * edits and additions.
+	 *
+	 * @param tooltips        the existing tooltip strings.
+	 * @param recipe          the current recipe being drawn.
+	 * @param recipeCategory  the recipe category of the recipe.
+	 * @param recipeSlotsView a view of the current recipe slots being drawn.
+	 * @param mouseX          the X position of the mouse, relative to the recipe.
+	 * @param mouseY          the Y position of the mouse, relative to the recipe.
+	 * @return tooltip strings. If there is no tooltip at this position, return an empty list.
+	 */
+	default List<Component> decorateExistingTooltips(List<Component> tooltips, T recipe, IRecipeCategory<T> recipeCategory, IRecipeSlotsView recipeSlotsView, double mouseX, double mouseY) {
+		return tooltips;
+	}
+}

mezz/JustEnoughItems

CommonApi/src/main/java/mezz/jei/api/recipe/category/extensions/IRecipeCategoryDecorator.java
@@ -1,0 +1,64 @@
+package mezz.jei.api.recipe.category.extensions;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.registration.IAdvancedRegistration;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
+
+/**
+ * The {@link IRecipeCategoryDecorator} allows further customization of recipe categories.
+ * It can be used to draw additional elements or tooltips on recipes, even of other mods.
+ * <p>
+ * Register it with {@link IAdvancedRegistration#addRecipeCategoryDecorator(RecipeType, IRecipeCategoryDecorator)}.
+ *
+ * @since 11.8.0
+ */
+public interface IRecipeCategoryDecorator<T> {
+	/**
+	 * Draw extras or additional info about the recipe after the {@link IRecipeCategory} that
+	 * this decorator is registered to has drawn.
+	 * Tooltips are handled by {@link #decorateExistingTooltips(List, Object, IRecipeCategory, IRecipeSlotsView, double, double)}.
+	 *
+	 * @param recipe          the current recipe being drawn.
+	 * @param recipeCategory  the recipe category of the recipe.
+	 * @param recipeSlotsView a view of the current recipe slots being drawn.
+	 * @param poseStack     the current {@link PoseStack} for rendering.
+	 * @param mouseX          the X position of the mouse, relative to the recipe.
+	 * @param mouseY          the Y position of the mouse, relative to the recipe.
+	 *
+	 * @see IDrawable for a simple class for drawing things.
+	 * @see IGuiHelper for useful functions.
+	 * @see IRecipeSlotsView for information about the ingredients that are currently being drawn.
+	 *
+	 * @since 11.8.0
+	 */
+	default void draw(T recipe, IRecipeCategory<T> recipeCategory, IRecipeSlotsView recipeSlotsView, PoseStack poseStack, double mouseX, double mouseY) {
+
+	}
+
+	/**
+	 * Allows modifying of and adding to existing tooltips added by the {@link IRecipeCategory} that
+	 * this decorator is registered to.
+	 * To avoid removing tooltips from the category itself, make sure to return original list with your
+	 * edits and additions.
+	 *
+	 * @param tooltips        the existing tooltip strings.
+	 * @param recipe          the current recipe being drawn.
+	 * @param recipeCategory  the recipe category of the recipe.
+	 * @param recipeSlotsView a view of the current recipe slots being drawn.
+	 * @param mouseX          the X position of the mouse, relative to the recipe.
+	 * @param mouseY          the Y position of the mouse, relative to the recipe.
+	 * @return tooltip strings. If there is no tooltip at this position, return an empty list.
+	 *
+	 * @since 11.8.0
+	 */
+	default List<Component> decorateExistingTooltips(List<Component> tooltips, T recipe, IRecipeCategory<T> recipeCategory, IRecipeSlotsView recipeSlotsView, double mouseX, double mouseY) {
+		return tooltips;
+	}
+}