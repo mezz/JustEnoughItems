79573b7f6d4b31d155fa14c40ecd77362f8f1b5e
mezz <tehgeek@gmail.com>
2024-10-02 12:09:14 +0900

    Cache registry access during JEI runtime
    (cherry picked from commit b033bbec3b65ca80a96152b44c2af69614429ed8)

--- Library/src/main/java/mezz/jei/library/startup/JeiStarter.java
+++ Library/src/main/java/mezz/jei/library/startup/JeiStarter.java
@@ -18,6 +18,7 @@ import mezz.jei.common.platform.Services;
 import mezz.jei.common.recipes.VanillaClientRecipeLoader;
 import mezz.jei.common.util.ChatUtil;
 import mezz.jei.common.util.ErrorUtil;
+import mezz.jei.common.util.RegistryUtil;
 import mezz.jei.common.util.Translator;
 import mezz.jei.common.config.DebugConfig;
 import mezz.jei.core.config.IWorldConfig;
@@ -129,6 +130,7 @@ public final class JeiStarter {
 				level.getRecipeManager().replaceRecipes(vanillaRecipes);
 			}
 		}
+		RegistryUtil.setRegistryAccess(level.registryAccess());
 
 		LoggedTimer totalTime = new LoggedTimer();
 		totalTime.start("Starting JEI");
@@ -275,5 +277,6 @@ public final class JeiStarter {
 			stopCallback.onRuntimeStopped();
 		}
 		stopCallbacks.clear();
+		RegistryUtil.setRegistryAccess(null);
 	}
 }
