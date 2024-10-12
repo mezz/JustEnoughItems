373bc0d8783c3be549f4e37bc4b95a954b7fede7
Greg-21 <34160838+Greg-21@users.noreply.github.com>
2024-10-12 23:30:33 +0200

    Revamped configuration file comments

--- Library/src/main/java/mezz/jei/library/config/ModIdFormatConfig.java
+++ Library/src/main/java/mezz/jei/library/config/ModIdFormatConfig.java
@@ -35,7 +35,7 @@ public class ModIdFormatConfig implements IModIdFormatConfig {
 			"ModNameFormat",
 			defaultModNameFormat,
 			ChatFormattingSerializer.INSTANCE,
-			"Formatting for mod name tooltip"
+			"Formatting for the mod names in tooltips for JEI GUIs. Leave blank to disable."
 		);
 		this.modNameFormat = new CachedSupplierTransformer<>(configValue, ModIdFormatConfig::toFormatString);
 	}
