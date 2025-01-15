0cf719766555d7b51c072aa87becb4133333e393
Mysticpasta1 <ethana5tc@outlook.com>
2025-01-15 07:44:39 -0600

    use new forge method PR'd by mezz

--- Forge/src/main/java/mezz/jei/forge/platform/RenderHelper.java
+++ Forge/src/main/java/mezz/jei/forge/platform/RenderHelper.java
@@ -17,6 +17,7 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -32,90 +33,79 @@ import java.util.List;
 import java.util.Optional;
 
 public class RenderHelper implements IPlatformRenderHelper {
-	@Override
-	public Font getFontRenderer(Minecraft minecraft, ItemStack itemStack) {
-		IClientItemExtensions renderProperties = IClientItemExtensions.of(itemStack);
-		Font fontRenderer = renderProperties.getFont(itemStack, IClientItemExtensions.FontContext.TOOLTIP);
-		if (fontRenderer != null) {
-			return fontRenderer;
-		}
-		return minecraft.font;
-	}
+    @Override
+    public Font getFontRenderer(Minecraft minecraft, ItemStack itemStack) {
+        IClientItemExtensions renderProperties = IClientItemExtensions.of(itemStack);
+        Font fontRenderer = renderProperties.getFont(itemStack, IClientItemExtensions.FontContext.TOOLTIP);
+        if (fontRenderer != null) {
+            return fontRenderer;
+        }
+        return minecraft.font;
+    }
 
-	@Override
-	public boolean shouldRender(MobEffectInstance potionEffect) {
-		IClientMobEffectExtensions effectRenderer = IClientMobEffectExtensions.of(potionEffect);
-		return effectRenderer.isVisibleInInventory(potionEffect);
-	}
+    @Override
+    public boolean shouldRender(MobEffectInstance potionEffect) {
+        IClientMobEffectExtensions effectRenderer = IClientMobEffectExtensions.of(potionEffect);
+        return effectRenderer.isVisibleInInventory(potionEffect);
+    }
 
-	@Override
-	public TextureAtlasSprite getParticleIcon(BakedModel bakedModel) {
-		return bakedModel.getParticleIcon(ModelData.EMPTY);
-	}
+    @Override
+    public TextureAtlasSprite getParticleIcon(BakedModel bakedModel) {
+        return bakedModel.getParticleIcon(ModelData.EMPTY);
+    }
 
-	@Override
-	public ItemColors getItemColors() {
-		return Minecraft.getInstance().getItemColors();
-	}
+    @Override
+    public ItemColors getItemColors() {
+        return Minecraft.getInstance().getItemColors();
+    }
 
-	@Override
-	public Optional<NativeImage> getMainImage(TextureAtlasSprite sprite) {
-		SpriteContents contents = sprite.contents();
-		NativeImage[] frames = contents.byMipLevel;
-		if (frames.length == 0) {
-			return Optional.empty();
-		}
-		NativeImage frame = frames[0];
-		return Optional.ofNullable(frame);
-	}
+    @Override
+    public Optional<NativeImage> getMainImage(TextureAtlasSprite sprite) {
+        SpriteContents contents = sprite.contents();
+        NativeImage[] frames = contents.byMipLevel;
+        if (frames.length == 0) {
+            return Optional.empty();
+        }
+        NativeImage frame = frames[0];
+        return Optional.ofNullable(frame);
+    }
 
-	@Override
-	public void renderTooltip(GuiGraphics guiGraphics, List<Either<FormattedText, TooltipComponent>> elements, int x, int y, Font font, ItemStack stack) {
-		// temporary hack while we wait for https://github.com/MinecraftForge/MinecraftForge/pull/10055
+    @Override
+    public void renderTooltip(GuiGraphics guiGraphics, List<Either<FormattedText, TooltipComponent>> elements, int x, int y, Font font, ItemStack stack) {
+        Screen screen = Minecraft.getInstance().screen;
+        if (screen == null) {
+            return;
+        }
+        guiGraphics.tooltipStack = stack;
 
-		Screen screen = Minecraft.getInstance().screen;
-		if (screen == null) {
-			return;
-		}
-		guiGraphics.tooltipStack = stack;
-		List<Component> componentList = new ArrayList<>();
-		for(var element : elements) {
-			if (Either.left(element) instanceof Component component) {
-				componentList.add(component);
-			}
-		}
-		if (componentList.isEmpty()) {
-			return;
-		}
+        List<ClientTooltipComponent> components = ForgeHooksClient.gatherTooltipComponentsFromElements(stack, elements, x, screen.width, screen.height, font);
+        guiGraphics.renderTooltipInternal(font, components, x, y, DefaultTooltipPositioner.INSTANCE);
+        guiGraphics.tooltipStack = ItemStack.EMPTY;
+    }
 
-		List<ClientTooltipComponent> components = ForgeHooksClient.gatherTooltipComponents(stack, componentList, x, screen.width, screen.height, font);
-		guiGraphics.renderTooltipInternal(font, components, x, y, DefaultTooltipPositioner.INSTANCE);
-		guiGraphics.tooltipStack = ItemStack.EMPTY;
-	}
+    @Override
+    public Component getName(TagKey<?> tagKey) {
+        String tagTranslationKey = getTagTranslationKey(tagKey);
+        return Component.translatableWithFallback(tagTranslationKey, "#" + tagKey.location());
+    }
 
-	@Override
-	public Component getName(TagKey<?> tagKey) {
-		String tagTranslationKey = getTagTranslationKey(tagKey);
-		return Component.translatableWithFallback(tagTranslationKey, "#" + tagKey.location());
-	}
+    private static String getTagTranslationKey(TagKey<?> tagKey) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("tag.");
+        ResourceLocation registryIdentifier = tagKey.registry().location();
+        ResourceLocation tagIdentifier = tagKey.location();
+        if (!registryIdentifier.getNamespace().equals(ModIds.MINECRAFT_ID)) {
+            stringBuilder.append(registryIdentifier.getNamespace()).append(".");
+        }
 
-	private static String getTagTranslationKey(TagKey<?> tagKey) {
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("tag.");
-		ResourceLocation registryIdentifier = tagKey.registry().location();
-		ResourceLocation tagIdentifier = tagKey.location();
-		if (!registryIdentifier.getNamespace().equals(ModIds.MINECRAFT_ID)) {
-			stringBuilder.append(registryIdentifier.getNamespace()).append(".");
-		}
+        String registryId = ResourceLocationUtil.sanitizePath(registryIdentifier.getPath());
+        String tagId = ResourceLocationUtil.sanitizePath(tagIdentifier.getPath());
 
-		String registryId = ResourceLocationUtil.sanitizePath(registryIdentifier.getPath());
-		String tagId = ResourceLocationUtil.sanitizePath(tagIdentifier.getPath());
-
-		stringBuilder.append(registryId)
-			.append(".")
-			.append(tagIdentifier.getNamespace())
-			.append(".")
-			.append(tagId);
-		return stringBuilder.toString();
-	}
+        stringBuilder.append(registryId)
+                .append(".")
+                .append(tagIdentifier.getNamespace())
+                .append(".")
+                .append(tagId);
+        return stringBuilder.toString();
+    }
 }
