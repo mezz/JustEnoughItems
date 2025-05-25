ec7db7e5ab517e8c79ed1ba111536e84aeee0d6f
mezz <tehgeek@gmail.com>
2025-05-25 18:59:35 +0900

    fix return type and warnings
    Move removal warning suppressions onto the deprecated ClickableIngredient methods that need them on 1.20.1.
    
    Target adaptation: 1.20.1 does not have the newer IGlobalGuiHandler overload changed by the source commit.
    
    (cherry picked from commit cc2e44a5dba2f16ccfbdfdf41d7a81cff48087b6)

--- Common/src/main/java/mezz/jei/common/input/ClickableIngredient.java
+++ Common/src/main/java/mezz/jei/common/input/ClickableIngredient.java
@@ -23,11 +23,13 @@ public class ClickableIngredient<V> implements IClickableIngredient<V> {
 		return value;
 	}
 
+	@SuppressWarnings("removal")
 	@Override
 	public IIngredientType<V> getIngredientType() {
 		return value.getType();
 	}
 
+	@SuppressWarnings("removal")
 	@Override
 	public V getIngredient() {
 		return value.getIngredient();
