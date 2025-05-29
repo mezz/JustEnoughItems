eb5bd8dc2026006e31abbeb167db2ab8734d3f2e
mezz <tehgeek@gmail.com>
2025-05-29 18:13:48 +0900

    spotless cleanup
    (cherry picked from commit 502ae82c3d5f06930b90c26c3d3c556d844ad6a6)

--- Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/ItemStackListFactory.java
+++ Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/ItemStackListFactory.java
@@ -16,7 +16,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Marker;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
