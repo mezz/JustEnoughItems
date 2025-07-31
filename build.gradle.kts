f61efdf5f6604d0d3a55a67cc5d28ec340f189aa
mezz <tehgeek@gmail.com>
2025-07-31 22:58:36 +0900

    Update to Minecraft 1.21.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("fabric-loom") version("1.11.4") apply(false)
 
     // https://projects.neoforged.net/neoforged/moddevgradle
-    id("net.neoforged.moddev") version("2.0.99") apply(false)
+    id("net.neoforged.moddev") version("2.0.105") apply(false)
 
     // https://plugins.gradle.org/plugin/me.modmuss50.mod-publish-plugin
     id("me.modmuss50.mod-publish-plugin") version("0.8.4") apply(false)
