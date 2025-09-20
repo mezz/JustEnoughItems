9bad62233035d9d8d7f8f950ab9b89606f4618bc
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-09-20 02:13:13 -0400

    [1.20.1] Backport ingredient removal optimization (#4067)

--- Library/src/main/java/mezz/jei/library/config/EditModeConfig.java
+++ Library/src/main/java/mezz/jei/library/config/EditModeConfig.java
@@ -64,18 +64,6 @@ public class EditModeConfig implements IEditModeConfig {
 		return false;
 	}
 
-	public <V> void removeIngredientFromConfigBlacklist(
-		ITypedIngredient<V> typedIngredient,
-		HideMode blacklistType,
-		IIngredientHelper<V> ingredientHelper
-	) {
-		final String uid = getIngredientUid(typedIngredient, blacklistType, ingredientHelper);
-		if (blacklist.remove(uid)) {
-			serializer.save(this);
-			notifyListenersOfVisibilityChange(typedIngredient, true);
-		}
-	}
-
 	public <V> boolean isIngredientOnConfigBlacklist(ITypedIngredient<V> typedIngredient, IIngredientHelper<V> ingredientHelper) {
 		for (HideMode hideMode : HideMode.values()) {
 			if (isIngredientOnConfigBlacklist(typedIngredient, hideMode, ingredientHelper)) {
@@ -144,7 +132,11 @@ public class EditModeConfig implements IEditModeConfig {
 	public <V> void showIngredientUsingConfigFile(ITypedIngredient<V> ingredient, HideMode hideMode) {
 		IIngredientType<V> type = ingredient.getType();
 		IIngredientHelper<V> ingredientHelper = ingredientManager.getIngredientHelper(type);
-		removeIngredientFromConfigBlacklist(ingredient, hideMode, ingredientHelper);
+		final Object blacklistUid = getIngredientUid(ingredient, hideMode, ingredientHelper);
+		if (blacklist.remove(blacklistUid)) {
+			serializer.save(this);
+			notifyListenersOfVisibilityChange(ingredient, true);
+		}
 	}
 
 	public void registerListener(IngredientVisibility ingredientVisibility) {
