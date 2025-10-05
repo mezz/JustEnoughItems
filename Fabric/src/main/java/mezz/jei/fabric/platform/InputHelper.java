bdfdb4c09026c4fb488805ff729c66ae48ede875
mezz <tehgeek@gmail.com>
2025-10-05 21:51:47 +0900

    Fix build

--- Fabric/src/main/java/mezz/jei/fabric/platform/InputHelper.java
+++ Fabric/src/main/java/mezz/jei/fabric/platform/InputHelper.java
@@ -2,20 +2,20 @@ package mezz.jei.fabric.platform;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.datafixers.util.Either;
+import mezz.jei.common.input.MouseButtonEventData;
 import mezz.jei.common.input.keys.IJeiKeyMappingCategoryBuilder;
 import mezz.jei.common.platform.IPlatformInputHelper;
 import mezz.jei.fabric.input.FabricJeiKeyMappingCategoryBuilder;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.input.KeyEvent;
-import net.minecraft.client.input.MouseButtonEvent;
 
 public class InputHelper implements IPlatformInputHelper {
 	@Override
-	public boolean isActiveAndMatches(KeyMapping keyMapping, InputConstants.Key key, Either<MouseButtonEvent, KeyEvent> event) {
+	public boolean isActiveAndMatches(KeyMapping keyMapping, InputConstants.Key key, Either<MouseButtonEventData, KeyEvent> event) {
 		if (keyMapping.isUnbound()) {
 			return false;
 		}
-		return event.map(keyMapping::matchesMouse, keyMapping::matches);
+		return event.map(e -> keyMapping.matchesMouse(e.event()), keyMapping::matches);
 	}
 
 	@Override
