597a0f69dbe897cfc25a57012a5c800a9c04639c
Patbox <39821509+Patbox@users.noreply.github.com>
2025-11-03 11:58:31 +0100

    Use fabric recipe sync. (#4134)
    Co-authored-by: mezz

--- Fabric/src/main/java/mezz/jei/fabric/JustEnoughItems.java
+++ Fabric/src/main/java/mezz/jei/fabric/JustEnoughItems.java
@@ -9,11 +9,18 @@ import mezz.jei.fabric.network.ServerNetworkHandler;
 import mezz.jei.library.plugins.vanilla.crafting.JeiShapedRecipe;
 import mezz.jei.library.recipes.RecipeSerializers;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.recipe.v1.sync.RecipeSynchronization;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class JustEnoughItems implements ModInitializer {
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	@Override
 	public void onInitialize() {
 		IServerConfig serverConfig = ServerConfig.getInstance();
@@ -24,5 +31,18 @@ public class JustEnoughItems implements ModInitializer {
 		var recipeSerializer = new JeiShapedRecipe.Serializer();
 		var registered = Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, resourceLocation, recipeSerializer);
 		RecipeSerializers.register(() -> registered);
+
+		// Run through vanilla recipe serializers and sync them
+		for (var entry : BuiltInRegistries.RECIPE_SERIALIZER.entrySet()) {
+			ResourceKey<RecipeSerializer<?>> resourceKey = entry.getKey();
+			if (resourceKey.location().getNamespace().equals(ModIds.MINECRAFT_ID)) {
+				RecipeSerializer<?> serializer = entry.getValue();
+				try {
+					RecipeSynchronization.synchronizeRecipeSerializer(serializer);
+				} catch (RuntimeException e) {
+					LOGGER.warn("Failed to synchronize recipe serializer {}", resourceKey, e);
+				}
+			}
+		}
 	}
 }
--- Fabric/src/main/java/mezz/jei/fabric/mixin/ClientPacketListenerMixin.java
+++ Fabric/src/main/java/mezz/jei/fabric/mixin/ClientPacketListenerMixin.java
@@ -3,7 +3,6 @@ package mezz.jei.fabric.mixin;
 import mezz.jei.fabric.events.JeiLifecycleEvents;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.game.ClientboundLoginPacket;
-import net.minecraft.network.protocol.game.ClientboundUpdateRecipesPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
-	@Inject(method = "handleUpdateRecipes", at = @At("RETURN"))
-	private void handleUpdateRecipes(ClientboundUpdateRecipesPacket packet, CallbackInfo ci) {
-		JeiLifecycleEvents.AFTER_RECIPE_SYNC.invoker().run();
-	}
-
 	@Inject(
 		method = "handleLogin(Lnet/minecraft/network/protocol/game/ClientboundLoginPacket;)V",
 		at = @At(
