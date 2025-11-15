cff3b3a410d695d3bafafe0c9814d18a7a31d7a8
mezz <tehgeek@gmail.com>
2025-11-16 00:30:02 +0900

    Optimize creation of grindstone recipes
    (cherry picked from commit 8f9bf1c42deabaf030988167970a5939c81e7f49)

--- Library/src/main/java/mezz/jei/library/plugins/vanilla/grindstone/GrindstoneRecipe.java
+++ Library/src/main/java/mezz/jei/library/plugins/vanilla/grindstone/GrindstoneRecipe.java
@@ -1,32 +1,40 @@
 package mezz.jei.library.plugins.vanilla.grindstone;
 
 import mezz.jei.api.recipe.vanilla.IJeiGrindstoneRecipe;
+import mezz.jei.common.util.MathUtil;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.List;
+import java.util.Map;
 
-public record GrindstoneRecipe(
+public final class GrindstoneRecipe implements IJeiGrindstoneRecipe {
+	private final List<ItemStack> topInputs;
+	private final List<ItemStack> bottomInputs;
+	private final List<ItemStack> outputs;
+	private int minXpReward;
+	private final int maxXpReward;
+	private final @Nullable ResourceLocation uid;
+
+	public GrindstoneRecipe(
 		List<ItemStack> topInputs,
 		List<ItemStack> bottomInputs,
 		List<ItemStack> outputs,
 		int minXpReward,
 		int maxXpReward,
-		@Nullable ResourceLocation uid,
-		boolean isOutputRenderOnly
-) implements IJeiGrindstoneRecipe {
-
-	public GrindstoneRecipe(
-			List<ItemStack> topInputs,
-			List<ItemStack> bottomInputs,
-			List<ItemStack> outputs,
-			int minXpReward,
-			int maxXpReward,
-			@Nullable ResourceLocation uid) {
-		this(topInputs, bottomInputs, outputs, minXpReward, maxXpReward, uid, true);
+		@Nullable ResourceLocation uid
+	) {
+		this.topInputs = topInputs;
+		this.bottomInputs = bottomInputs;
+		this.outputs = outputs;
+		this.minXpReward = minXpReward;
+		this.maxXpReward = maxXpReward;
+		this.uid = uid;
 	}
 
 	@Override
@@ -52,11 +60,17 @@ public record GrindstoneRecipe(
 
 	@Override
 	public int getMinXpReward() {
+		if (minXpReward < 0) {
+			minXpReward = getMinXp(topInputs.get(0), bottomInputs.get(0));
+		}
 		return minXpReward;
 	}
 
 	@Override
 	public int getMaxXpReward() {
+		if (maxXpReward < 0) {
+			return getMinXpReward() * 2;
+		}
 		return maxXpReward;
 	}
 
@@ -69,6 +83,27 @@ public record GrindstoneRecipe(
 	@Override
 	@Unmodifiable
 	public boolean isOutputRenderOnly() {
-		return isOutputRenderOnly;
+		return true;
+	}
+
+	private static int getMinXp(ItemStack topItem, ItemStack bottomItem) {
+		int topXp = getExperienceFromItem(topItem);
+		int bottomXp = getExperienceFromItem(bottomItem);
+		return MathUtil.divideCeil(topXp + bottomXp, 2);
+	}
+
+	private static int getExperienceFromItem(ItemStack stack) {
+		int i = 0;
+		Map<Enchantment, Integer> itemEnchantments = EnchantmentHelper.getEnchantments(stack);
+
+		for (Map.Entry<Enchantment, Integer> entry : itemEnchantments.entrySet()) {
+			Enchantment enchantment = entry.getKey();
+			int j = entry.getValue();
+			if (!enchantment.isCurse()) {
+				i += enchantment.getMinCost(j);
+			}
+		}
+
+		return i;
 	}
 }
