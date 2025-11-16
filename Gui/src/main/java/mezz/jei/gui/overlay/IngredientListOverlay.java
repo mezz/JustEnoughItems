00019938377a9b7bce02c09c0094c82b85b9bb2c
vfyjxf_ <2331007009@qq.com>
2025-11-16 22:12:57 +0800

    fix spacing

--- Gui/src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
+++ Gui/src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
@@ -235,7 +235,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 		if (isListDisplayed()) {
 			return Stream.concat(this.contents.getIngredientUnderMouse(mouseX, mouseY), this.lookupHistoryOverlay.getIngredientUnderMouse(mouseX, mouseY));
 		}
-		if(this.lookupHistoryOverlay.isListDisplayed()){
+		if (this.lookupHistoryOverlay.isListDisplayed()) {
 			return this.lookupHistoryOverlay.getIngredientUnderMouse(mouseX, mouseY);
 		}
 		return Stream.empty();
@@ -246,7 +246,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 		if (isListDisplayed()) {
 			return Stream.concat(this.contents.getDraggableIngredientUnderMouse(mouseX, mouseY), this.lookupHistoryOverlay.getDraggableIngredientUnderMouse(mouseX, mouseY));
 		}
-		if(this.lookupHistoryOverlay.isListDisplayed()){
+		if (this.lookupHistoryOverlay.isListDisplayed()) {
 			return this.lookupHistoryOverlay.getDraggableIngredientUnderMouse(mouseX, mouseY);
 		}
 		return Stream.empty();
