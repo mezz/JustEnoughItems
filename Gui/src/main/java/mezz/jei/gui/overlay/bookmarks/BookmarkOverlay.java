4729eb8542767bd8ad4c7c9b998783bdc627766c
vfyjxf_ <2331007009@qq.com>
2025-11-16 22:18:36 +0800

    fix spacing

--- Gui/src/main/java/mezz/jei/gui/overlay/bookmarks/BookmarkOverlay.java
+++ Gui/src/main/java/mezz/jei/gui/overlay/bookmarks/BookmarkOverlay.java
@@ -217,7 +217,7 @@ public class BookmarkOverlay implements IRecipeFocusSource, IBookmarkOverlay {
 		if (isListDisplayed()) {
 			return Stream.concat(this.contents.getIngredientUnderMouse(mouseX, mouseY), this.lookupHistoryOverlay.getIngredientUnderMouse(mouseX, mouseY));
 		}
-		if(this.lookupHistoryOverlay.isListDisplayed()){
+		if (this.lookupHistoryOverlay.isListDisplayed()) {
 			return this.lookupHistoryOverlay.getIngredientUnderMouse(mouseX, mouseY);
 		}
 		return Stream.empty();
@@ -228,7 +228,7 @@ public class BookmarkOverlay implements IRecipeFocusSource, IBookmarkOverlay {
 		if (isListDisplayed()) {
 			return Stream.concat(this.contents.getDraggableIngredientUnderMouse(mouseX, mouseY), this.lookupHistoryOverlay.getDraggableIngredientUnderMouse(mouseX, mouseY));
 		}
-		if(this.lookupHistoryOverlay.isListDisplayed()){
+		if (this.lookupHistoryOverlay.isListDisplayed()) {
 			return this.lookupHistoryOverlay.getDraggableIngredientUnderMouse(mouseX, mouseY);
 		}
 		return Stream.empty();
