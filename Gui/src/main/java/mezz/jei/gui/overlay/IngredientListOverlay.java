63ae09ea3b06986c94b1482d5be6df7f354acf71
vfyjxf <42764413+vfyjxf@users.noreply.github.com>
2025-11-16 22:30:36 +0800

    [1.21.10] fix #4152 The query history function will not function properly when there are no bookmarks (#4156)

--- Gui/src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
+++ Gui/src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
@@ -228,6 +228,9 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 		if (isListDisplayed()) {
 			return Stream.concat(this.contents.getIngredientUnderMouse(mouseX, mouseY), this.lookupHistoryOverlay.getIngredientUnderMouse(mouseX, mouseY));
 		}
+		if (this.lookupHistoryOverlay.isListDisplayed()) {
+			return this.lookupHistoryOverlay.getIngredientUnderMouse(mouseX, mouseY);
+		}
 		return Stream.empty();
 	}
 
@@ -235,6 +238,9 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 	public Stream<IDraggableIngredientInternal<?>> getDraggableIngredientUnderMouse(double mouseX, double mouseY) {
 		if (isListDisplayed()) {
 			return Stream.concat(this.contents.getDraggableIngredientUnderMouse(mouseX, mouseY),this.lookupHistoryOverlay.getDraggableIngredientUnderMouse(mouseX, mouseY));
+		}
+		if (this.lookupHistoryOverlay.isListDisplayed()) {
+			return this.lookupHistoryOverlay.getDraggableIngredientUnderMouse(mouseX, mouseY);
 		}
 		return Stream.empty();
 	}
