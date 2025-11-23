3f10c7833ca143e222bba0616997031255869595
mezz <tehgeek@gmail.com>
2025-11-23 21:21:30 +0900

    Fix #4177 JEI handles some inputs when it is not visible

--- Gui/src/main/java/mezz/jei/gui/input/ClientInputHandler.java
+++ Gui/src/main/java/mezz/jei/gui/input/ClientInputHandler.java
@@ -1,9 +1,10 @@
 package mezz.jei.gui.input;
 
+import mezz.jei.api.runtime.IScreenHelper;
 import mezz.jei.common.input.IInternalKeyMappings;
+import mezz.jei.core.util.ReflectionUtil;
 import mezz.jei.gui.input.handlers.DragRouter;
 import mezz.jei.gui.input.handlers.UserInputRouter;
-import mezz.jei.core.util.ReflectionUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
@@ -15,18 +16,21 @@ public class ClientInputHandler {
 	private final UserInputRouter inputRouter;
 	private final DragRouter dragRouter;
 	private final IInternalKeyMappings keybindings;
+	private final IScreenHelper screenHelper;
 	private final ReflectionUtil reflectionUtil = new ReflectionUtil();
 
 	public ClientInputHandler(
 		List<ICharTypedHandler> charTypedHandlers,
 		UserInputRouter inputRouter,
 		DragRouter dragRouter,
-		IInternalKeyMappings keybindings
+		IInternalKeyMappings keybindings,
+		IScreenHelper screenHelper
 	) {
 		this.charTypedHandlers = charTypedHandlers;
 		this.inputRouter = inputRouter;
 		this.dragRouter = dragRouter;
 		this.keybindings = keybindings;
+		this.screenHelper = screenHelper;
 	}
 
 	public void onInitGui() {
@@ -39,7 +43,9 @@ public class ClientInputHandler {
 	 */
 	public boolean onKeyboardKeyPressedPre(Screen screen, UserInput input) {
 		if (!isContainerTextFieldFocused(screen)) {
-			return this.inputRouter.handleUserInput(screen, input, keybindings);
+			if (screenHelper.getGuiProperties(screen).isPresent()) {
+				return this.inputRouter.handleUserInput(screen, input, keybindings);
+			}
 		}
 		return false;
 	}
@@ -49,7 +55,9 @@ public class ClientInputHandler {
 	 */
 	public boolean onKeyboardKeyPressedPost(Screen screen, UserInput input) {
 		if (isContainerTextFieldFocused(screen)) {
-			return this.inputRouter.handleUserInput(screen, input, keybindings);
+			if (screenHelper.getGuiProperties(screen).isPresent()) {
+				return this.inputRouter.handleUserInput(screen, input, keybindings);
+			}
 		}
 		return false;
 	}
@@ -74,6 +82,10 @@ public class ClientInputHandler {
 	}
 
 	public boolean onGuiMouseClicked(Screen screen, UserInput input) {
+		if (screenHelper.getGuiProperties(screen).isEmpty()) {
+			return false;
+		}
+
 		boolean handled = this.inputRouter.handleUserInput(screen, input, keybindings);
 
 		if (Minecraft.getInstance().screen == screen && input.is(keybindings.getLeftClick())) {
@@ -83,6 +95,10 @@ public class ClientInputHandler {
 	}
 
 	public boolean onGuiMouseReleased(Screen screen, UserInput input) {
+		if (screenHelper.getGuiProperties(screen).isEmpty()) {
+			return false;
+		}
+
 		boolean handled = this.inputRouter.handleUserInput(screen, input, keybindings);
 
 		if (input.is(keybindings.getLeftClick())) {
--- Gui/src/main/java/mezz/jei/gui/startup/JeiGuiStarter.java
+++ Gui/src/main/java/mezz/jei/gui/startup/JeiGuiStarter.java
@@ -237,7 +237,8 @@ public class JeiGuiStarter {
 			charTypedHandlers,
 			userInputRouter,
 			dragRouter,
-			keyMappings
+			keyMappings,
+			screenHelper
 		);
 		ResourceReloadHandler resourceReloadHandler = new ResourceReloadHandler(
 			ingredientListOverlay,
