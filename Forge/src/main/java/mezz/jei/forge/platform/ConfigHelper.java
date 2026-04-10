3c1212264e07cb82c6d024e5e4518ba719b1de57
vfyjxf <2331007009@qq.com>
2026-04-10 10:56:32 +0800

    add fabric & forge config gui helper

--- Forge/src/main/java/mezz/jei/forge/platform/ConfigHelper.java
+++ Forge/src/main/java/mezz/jei/forge/platform/ConfigHelper.java
@@ -1,12 +1,8 @@
 package mezz.jei.forge.platform;
 
-import mezz.jei.api.constants.ModIds;
 import mezz.jei.common.platform.IPlatformConfigHelper;
-import net.minecraft.client.Minecraft;
+import mezz.jei.gui.config.screen.JeiConfigScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.fml.ModContainer;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.nio.file.Path;
@@ -21,11 +17,6 @@ public class ConfigHelper implements IPlatformConfigHelper {
 
 	@Override
 	public Optional<Screen> getConfigScreen() {
-		Minecraft minecraft = Minecraft.getInstance();
-		return ModList.get()
-			.getModContainerById(ModIds.JEI_ID)
-			.map(ModContainer::getModInfo)
-			.flatMap(ConfigScreenHandler::getScreenFactoryFor)
-			.map(f -> f.apply(minecraft, minecraft.screen));
+		return Optional.of(JeiConfigScreen.create());
 	}
 }
