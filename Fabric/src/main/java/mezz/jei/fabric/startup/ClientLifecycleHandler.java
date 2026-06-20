ef2c9530b980badd9696680c0d6bd1e9c3e417e8
mezz <tehgeek@gmail.com>
2026-06-20 10:39:42 +0900

    Add support for NeoForge's key mapping category event registration
    see https://github.com/neoforged/NeoForge/pull/2642
    
    (cherry picked from commit fce4157fa47a65d7fb3ae5af703c8e7c423f17fb)
    (cherry picked from commit 93351ef2379de1d9db78a6df6d2134e6dee36de5)

--- Fabric/src/main/java/mezz/jei/fabric/startup/ClientLifecycleHandler.java
+++ Fabric/src/main/java/mezz/jei/fabric/startup/ClientLifecycleHandler.java
@@ -11,6 +11,7 @@ import mezz.jei.library.startup.JeiStarter;
 import mezz.jei.library.startup.StartData;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
@@ -29,7 +30,7 @@ public class ClientLifecycleHandler {
 		IConnectionToServer serverConnection = new ConnectionToServer();
 		Internal.setServerConnection(serverConnection);
 
-		InternalKeyMappings keyMappings = new InternalKeyMappings(KeyBindingHelper::registerKeyBinding);
+		InternalKeyMappings keyMappings = new InternalKeyMappings(KeyBindingHelper::registerKeyBinding, KeyMapping.Category::register);
 		Internal.setKeyMappings(keyMappings);
 
 		ClientNetworkHandler.registerClientPacketHandler(serverConnection);
