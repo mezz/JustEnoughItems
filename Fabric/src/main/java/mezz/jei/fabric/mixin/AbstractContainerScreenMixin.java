858a24b820c9ca7f3a4380c42d334c253bbf6e8c
mezz <tehgeek@gmail.com>
2026-06-21 19:02:16 +0900

    Rename mixin parameters to match real parameter names
    (cherry picked from commit 1336e3c152ae05782df50801b96401bd9e36537c)

--- Fabric/src/main/java/mezz/jei/fabric/mixin/AbstractContainerScreenMixin.java
+++ Fabric/src/main/java/mezz/jei/fabric/mixin/AbstractContainerScreenMixin.java
@@ -19,9 +19,9 @@ public class AbstractContainerScreenMixin {
 			shift = At.Shift.AFTER
 		)
 	)
-	private void drawForeground(GuiGraphicsExtractor guiGraphics, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+	private void drawForeground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a, CallbackInfo ci) {
 		@SuppressWarnings("DataFlowIssue")
 		AbstractContainerScreen<?> screen = (AbstractContainerScreen<?>) (Object) this;
-		JeiScreenEvents.DRAW_FOREGROUND.invoker().drawForeground(screen, guiGraphics, mouseX, mouseY);
+		JeiScreenEvents.DRAW_FOREGROUND.invoker().drawForeground(screen, graphics, mouseX, mouseY);
 	}
 }
