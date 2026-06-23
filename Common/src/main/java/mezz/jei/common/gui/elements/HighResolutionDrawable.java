f7dca9976b07b4f887fc9c084ac347a8ceed67a9
mezz <tehgeek@gmail.com>
2026-06-23 17:33:24 +0900

    Add methods to IGuiHelper for creating drawables from sprites
    (cherry picked from commit 07cf55dc10371e3ba62828d52414aae14d65bf06)

--- Common/src/main/java/mezz/jei/common/gui/elements/HighResolutionDrawable.java
+++ Common/src/main/java/mezz/jei/common/gui/elements/HighResolutionDrawable.java
@@ -1,6 +1,5 @@
 package mezz.jei.common.gui.elements;
 
-import com.google.common.base.Preconditions;
 import mezz.jei.api.gui.drawable.IDrawable;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 
@@ -12,29 +11,20 @@ public class HighResolutionDrawable implements IDrawable {
 	private final int scale;
 
 	public HighResolutionDrawable(IDrawable drawable, int scale) {
-		int width = drawable.getWidth();
-		int height = drawable.getHeight();
-		Preconditions.checkArgument(
-			width % scale == 0,
-			String.format("drawable width %s must be divisible by the scale %s", width, scale)
-		);
-		Preconditions.checkArgument(
-			height % scale == 0,
-			String.format("drawable height %s must be divisible by the scale %s", height, scale)
-		);
-
 		this.drawable = drawable;
 		this.scale = scale;
 	}
 
 	@Override
 	public int getWidth() {
-		return drawable.getWidth() / scale;
+		int width = drawable.getWidth();
+		return width / scale;
 	}
 
 	@Override
 	public int getHeight() {
-		return drawable.getHeight() / scale;
+		int height = drawable.getHeight();
+		return height / scale;
 	}
 
 	@Override
