89fbbad5e265fa9d73b999882f85cfe22a48c8ef
mezz <tehgeek@gmail.com>
2026-06-30 17:37:21 +0900

    Try exporting game test results in a way Jenkins can read

--- NeoForge/src/gameTest/java/mezz/jei/neoforge/tests/JeiTests.java
+++ NeoForge/src/gameTest/java/mezz/jei/neoforge/tests/JeiTests.java
@@ -1,18 +1,25 @@
 package mezz.jei.neoforge.tests;
 
+import java.nio.file.Path;
+
 import net.minecraft.resources.Identifier;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.testframework.conf.FrameworkConfiguration;
 import net.neoforged.testframework.impl.MutableTestFramework;
+import net.neoforged.testframework.summary.JUnitSummaryDumper;
 
 @Mod(JeiTests.MOD_ID)
 public final class JeiTests {
 	public static final String MOD_ID = "jeitests";
+	private static final String JUNIT_OUTPUT_DIR_PROPERTY = "jei.gameTest.junitDir";
 
 	public JeiTests(IEventBus modEventBus, ModContainer modContainer) {
 		MutableTestFramework framework = FrameworkConfiguration.builder(Identifier.fromNamespaceAndPath(MOD_ID, "tests"))
+			.dumpers(new JUnitSummaryDumper(Path.of(
+				System.getProperty(JUNIT_OUTPUT_DIR_PROPERTY, "../../build/test-results/gameTest")
+			)))
 			.build()
 			.create();
 		framework.init(modEventBus, modContainer);
