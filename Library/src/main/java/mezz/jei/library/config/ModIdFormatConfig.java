959be65bf5b86770104ad0e00d90683564ebf5d2
0196071 <leandre.p@live.ca>
2026-07-01 10:21:36 -0400

    Improve mod-name formatting detection
    Backport style-preserving mod-name replacement and its tooltip-list detection tests to 1.19.
    
    Keep the target tooltip-list helper and Java 17-compatible test implementation.
    
    (cherry picked from commit 2f3695c7b497a2e5dd91cbe8e586b09e18dab4b8)

--- Library/src/main/java/mezz/jei/library/config/ModIdFormatConfig.java
+++ Library/src/main/java/mezz/jei/library/config/ModIdFormatConfig.java
@@ -8,7 +8,6 @@ import mezz.jei.core.util.function.CachedSupplierTransformer;
 import mezz.jei.library.config.serializers.ChatFormattingSerializer;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -72,7 +71,7 @@ public class ModIdFormatConfig implements IModIdFormatConfig {
 			if (lineString.contains(ModIds.MINECRAFT_NAME)) {
 				String withoutFormatting = StringUtil.removeChatFormatting(lineString);
 				if (withoutFormatting.contains(ModIds.MINECRAFT_NAME)) {
-					return StringUtils.replaceOnce(lineString, ModIds.MINECRAFT_NAME, MOD_NAME_FORMAT_CODE);
+					return StyledTextHelper.replaceFirst(line, ModIds.MINECRAFT_NAME, MOD_NAME_FORMAT_CODE);
 				}
 			}
 		}
---
+++ Library/src/test/java/mezz/jei/test/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package mezz.jei.test;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
