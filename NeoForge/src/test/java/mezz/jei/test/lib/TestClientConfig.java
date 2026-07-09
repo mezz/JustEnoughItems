80b6c6f17ca6e3a93edb42c17898e11836899e90
mezz <tehgeek@gmail.com>
2026-07-09 21:44:21 +0900

    Implement toast reflow in test config

--- NeoForge/src/test/java/mezz/jei/test/lib/TestClientConfig.java
+++ NeoForge/src/test/java/mezz/jei/test/lib/TestClientConfig.java
@@ -149,6 +149,11 @@ public class TestClientConfig implements IClientConfig {
 	}
 
 	@Override
+	public boolean isToastReflowEnabled() {
+		return true;
+	}
+
+	@Override
 	public int getMaxRecipeGuiHeight() {
 		return 500;
 	}
