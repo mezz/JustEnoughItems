2aa645a574180580d70af979cc92ac4ad75e4f8a
mezz <tehgeek@gmail.com>
2026-07-10 16:24:32 +0900

    Add support for getting clickable ingredients from screen handlers
    (cherry picked from commit b32d1bd0dc1db6c7854ec5a8dba322c28060900b)

--- CommonApi/src/main/java/mezz/jei/api/gui/handlers/IScreenHandler.java
+++ CommonApi/src/main/java/mezz/jei/api/gui/handlers/IScreenHandler.java
@@ -1,10 +1,14 @@
 package mezz.jei.api.gui.handlers;
 
+import mezz.jei.api.gui.builder.IClickableIngredientFactory;
+import mezz.jei.api.registration.IModIngredientRegistration;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.runtime.IClickableIngredient;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import org.jspecify.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
@@ -17,4 +21,26 @@ public interface IScreenHandler<T extends Screen> extends Function<T, IGuiProper
 	@Override
 	@Nullable
 	IGuiProperties apply(T guiScreen);
+
+	/**
+	 * Return a clickable ingredient under the mouse that JEI could not normally detect, used for JEI recipe lookups.
+	 *
+	 * This is useful for screens that don't have normal slots (which is how JEI normally detects items under the mouse).
+	 *
+	 * This can also be used to let JEI look up liquids in tanks directly, by returning a FluidStack.
+	 * Works with any ingredient type that has been registered with {@link IModIngredientRegistration}.
+	 *
+	 * @param mouseX the current X position of the mouse in screen coordinates.
+	 * @param mouseY the current Y position of the mouse in screen coordinates.
+	 *
+	 * @since 29.15.0
+	 */
+	default Optional<? extends IClickableIngredient<?>> getClickableIngredientUnderMouse(
+		IClickableIngredientFactory factory,
+		T screen,
+		double mouseX,
+		double mouseY
+	) {
+		return Optional.empty();
+	}
 }
