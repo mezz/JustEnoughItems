Mysticpasta1/JEI-Async

Fix #2752 Crash on gui slots that are to the left or above the gui

src/main/java/mezz/jei/input/GuiContainerWrapper.java
@@ -38,14 +38,19 @@ public class GuiContainerWrapper implements IRecipeFocusSource {
 
 	private Optional<IClickedIngredient<?>> getSlotIngredientUnderMouse(AbstractContainerScreen<?> guiContainer) {
 		return Optional.ofNullable(guiContainer.getSlotUnderMouse())
-			.flatMap(this::getClickedIngredient);
+			.flatMap(slot -> getClickedIngredient(slot, guiContainer));
 	}
 
-	private Optional<IClickedIngredient<?>> getClickedIngredient(Slot slot) {
+	private Optional<IClickedIngredient<?>> getClickedIngredient(Slot slot, AbstractContainerScreen<?> guiContainer) {
 		ItemStack stack = slot.getItem();
 		return TypedIngredient.createTyped(this.registeredIngredients, VanillaTypes.ITEM, stack)
 			.map(typedIngredient -> {
-				ImmutableRect2i slotArea = new ImmutableRect2i(slot.x, slot.y, 16, 16);
+				ImmutableRect2i slotArea = new ImmutableRect2i(
+						guiContainer.getGuiLeft() + slot.x,
+						guiContainer.getGuiTop() + slot.y,
+						16,
+						16
+				);
 				return new ClickedIngredient<>(typedIngredient, slotArea, false, false);
 			});
 	}

mezz/JustEnoughItems

(cherry picked from commit 98e5ead592a85f084a9e840fd5e4125e3c8df4cc)

src/main/java/mezz/jei/input/GuiContainerWrapper.java
@@ -30,7 +30,12 @@ public class GuiContainerWrapper implements IShowsRecipeFocuses {
 		if (slotUnderMouse != null) {
 			ItemStack stack = slotUnderMouse.getStack();
 			if (!stack.isEmpty()) {
-				Rectangle slotArea = new Rectangle(slotUnderMouse.xPos, slotUnderMouse.yPos, 16, 16);
+				Rectangle slotArea = new Rectangle(
+					guiContainer.getGuiLeft() + slotUnderMouse.xPos,
+					guiContainer.getGuiTop() + slotUnderMouse.yPos,
+					16,
+					16
+				);
 				return ClickedIngredient.create(stack, slotArea);
 			}
 		}