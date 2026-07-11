82bcb6864c5bfe1884fade2fb135de49a39d0368
mezz <tehgeek@gmail.com>
2026-07-11 21:20:44 +0900

    Test recipe button input priority
    (cherry picked from commit 41571e2818224db42dade5b20d36a8c2813f9470)
    (cherry picked from commit 7777627541b34e85cd66c68ad48fcddfc00d31e1)

--- Gui/src/main/java/mezz/jei/gui/input/handlers/CombinedInputHandler.java
+++ Gui/src/main/java/mezz/jei/gui/input/handlers/CombinedInputHandler.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.screens.Screen;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CombinedInputHandler implements IUserInputHandler {
@@ -40,10 +41,17 @@ public class CombinedInputHandler implements IUserInputHandler {
 	 * 2. every mouse handler that never got a chance to handleClick because something else handled it first.
 	 */
 	private Optional<IUserInputHandler> handleClickInternal(Screen screen, IGuiProperties guiProperties, UserInput input, IInternalKeyMappings keyBindings) {
+		return handleClickInternal(this.inputHandlers, inputHandler -> inputHandler.handleUserInput(screen, guiProperties, input, keyBindings));
+	}
+
+	static Optional<IUserInputHandler> handleClickInternal(
+		List<IUserInputHandler> inputHandlers,
+		Function<IUserInputHandler, Optional<IUserInputHandler>> handleInput
+	) {
 		Optional<IUserInputHandler> firstHandled = Optional.empty();
-		for (IUserInputHandler inputHandler : this.inputHandlers) {
+		for (IUserInputHandler inputHandler : inputHandlers) {
 			if (firstHandled.isEmpty()) {
-				firstHandled = inputHandler.handleUserInput(screen, guiProperties, input, keyBindings);
+				firstHandled = handleInput.apply(inputHandler);
 				if (firstHandled.isEmpty()) {
 					inputHandler.unfocus();
 				}
