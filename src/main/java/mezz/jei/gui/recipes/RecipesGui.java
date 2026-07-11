Mysticpasta1/JEI-Async

Close the recipe screen even if parent screen is null (#2298)

src/main/java/mezz/jei/gui/recipes/RecipesGui.java
@@ -396,10 +396,8 @@ public class RecipesGui extends Screen implements IRecipesGui, IShowsRecipeFocus
 	@Override
 	public void closeScreen() {
 		if (isOpen() && minecraft != null) {
-			if (parentScreen != null) {
-				minecraft.displayGuiScreen(parentScreen);
-				parentScreen = null;
-			}
+			minecraft.displayGuiScreen(parentScreen);
+			parentScreen = null;
 			logic.clearHistory();
 			return;
 		}

mezz/JustEnoughItems

(cherry picked from commit 3cf55c21e0a8e199bf46de56b2fe0e6001c5f74d)

src/main/java/mezz/jei/gui/recipes/RecipesGui.java
@@ -387,10 +387,8 @@ public class RecipesGui extends GuiScreen implements IRecipesGui, IShowsRecipeFo
 			return;
 		}
 		if (isOpen()) {
-			if (parentScreen != null) {
-				mc.displayGuiScreen(parentScreen);
-				parentScreen = null;
-			}
+			mc.displayGuiScreen(parentScreen);
+			parentScreen = null;
 			logic.clearHistory();
 		}
 	}