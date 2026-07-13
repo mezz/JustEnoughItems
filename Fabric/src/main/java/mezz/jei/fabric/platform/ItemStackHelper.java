a1a7c1b92c72f674ddf759c51cd3632fc7975067
mezz <tehgeek@gmail.com>
2026-07-13 19:46:13 +0900

    Use vanilla tooltip lines for mod name format detection
    (cherry picked from commit a84da2e9b765f3365280643137053c4a429eae39)

--- Fabric/src/main/java/mezz/jei/fabric/platform/ItemStackHelper.java
+++ Fabric/src/main/java/mezz/jei/fabric/platform/ItemStackHelper.java
@@ -3,24 +3,14 @@ package mezz.jei.fabric.platform;
 import mezz.jei.common.platform.IPlatformItemStackHelper;
 import net.fabricmc.fabric.api.item.v1.EnchantingContext;
 import net.minecraft.core.Holder;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.entity.FuelValues;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.jspecify.annotations.Nullable;
 
-import java.util.List;
 import java.util.Optional;
 
 public class ItemStackHelper implements IPlatformItemStackHelper {
-	private static final Logger LOGGER = LogManager.getLogger();
-
 	@Override
 	public int getBurnTime(ItemStack itemStack, RecipeType<?> recipeType, FuelValues fuelValues) {
 		return fuelValues.burnDuration(itemStack);
@@ -29,16 +19,6 @@ public class ItemStackHelper implements IPlatformItemStackHelper {
 	@Override
 	public Optional<String> getCreatorModId(ItemStack stack) {
 		return Optional.empty();
-	}
-
-	@Override
-	public List<Component> getTestTooltip(@Nullable Player player, ItemStack itemStack) {
-		try {
-			return itemStack.getTooltipLines(Item.TooltipContext.EMPTY, player, TooltipFlag.Default.NORMAL);
-		} catch (LinkageError | RuntimeException e) {
-			LOGGER.error("Error while Testing for mod name formatting", e);
-		}
-		return List.of();
 	}
 
 	@Override
--- NeoForge/src/main/java/mezz/jei/neoforge/platform/ItemStackHelper.java
+++ NeoForge/src/main/java/mezz/jei/neoforge/platform/ItemStackHelper.java
@@ -6,23 +6,15 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.entity.FuelValues;
-import net.neoforged.neoforge.event.EventHooks;
-import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jspecify.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class ItemStackHelper implements IPlatformItemStackHelper {
@@ -48,19 +40,6 @@ public class ItemStackHelper implements IPlatformItemStackHelper {
 		Item item = stack.getItem();
 		String creatorModId = item.getCreatorModId(registryAccess, stack);
 		return Optional.ofNullable(creatorModId);
-	}
-
-	@Override
-	public List<Component> getTestTooltip(@Nullable Player player, ItemStack itemStack) {
-		try {
-			List<Component> tooltip = new ArrayList<>();
-			tooltip.add(Component.literal("JEI Tooltip Testing for mod name formatting"));
-			ItemTooltipEvent tooltipEvent = EventHooks.onItemTooltip(itemStack, player, tooltip, TooltipFlag.Default.NORMAL, Item.TooltipContext.EMPTY);
-			return tooltipEvent.getToolTip();
-		} catch (LinkageError | RuntimeException e) {
-			LOGGER.error("Error while Testing for mod name formatting", e);
-		}
-		return List.of();
 	}
 
 	@Override
