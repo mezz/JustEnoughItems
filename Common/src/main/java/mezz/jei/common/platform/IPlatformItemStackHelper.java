eb86b799247090a6fc5312c24b8ef31a2f82859d
mezz <tehgeek@gmail.com>
2026-07-13 19:46:13 +0900

    Use vanilla tooltip lines for mod name format detection
    (cherry picked from commit a84da2e9b765f3365280643137053c4a429eae39)
    (cherry picked from commit a1a7c1b92c72f674ddf759c51cd3632fc7975067)

--- Common/src/main/java/mezz/jei/common/platform/IPlatformItemStackHelper.java
+++ Common/src/main/java/mezz/jei/common/platform/IPlatformItemStackHelper.java
@@ -2,24 +2,18 @@ package mezz.jei.common.platform;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.entity.FuelValues;
-import org.jspecify.annotations.Nullable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface IPlatformItemStackHelper {
 	int getBurnTime(ItemStack itemStack, RecipeType<?> recipeType, FuelValues fuelValues);
 
 	Optional<String> getCreatorModId(ItemStack stack);
-
-	List<Component> getTestTooltip(@Nullable Player player, ItemStack itemStack);
 
 	default ItemAttributeModifiers getItemAttributeModifiers(ItemStack stack) {
 		return stack.getOrDefault(DataComponents.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.EMPTY);
