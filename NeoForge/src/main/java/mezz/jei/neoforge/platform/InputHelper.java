df12729dffe82fd5b0d16d1ffa27d9f7cebc9e5c
mezz <tehgeek@gmail.com>
2026-07-16 00:02:02 +0900

    Use NeoForge's new TooltipFlag#shouldDisplayAllInformation for indexing tooltips

--- NeoForge/src/main/java/mezz/jei/neoforge/platform/InputHelper.java
+++ NeoForge/src/main/java/mezz/jei/neoforge/platform/InputHelper.java
@@ -11,6 +11,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.neoforge.client.ClientTooltipFlag;
+import net.neoforged.neoforge.common.extensions.TooltipFlagExtension;
 
 public class InputHelper implements IPlatformInputHelper {
 	@Override
@@ -30,5 +31,27 @@ public class InputHelper implements IPlatformInputHelper {
 			return tooltipFlag;
 		}
 		return ClientTooltipFlag.of(tooltipFlag);
+	}
+
+	@Override
+	public TooltipFlag getSearchTooltipFlag(TooltipFlag tooltipFlag) {
+		return new SearchTooltipFlag(tooltipFlag.isAdvanced(), tooltipFlag.isCreative());
+	}
+
+	private record SearchTooltipFlag(boolean advanced, boolean creative) implements TooltipFlag, TooltipFlagExtension {
+		@Override
+		public boolean isAdvanced() {
+			return advanced;
+		}
+
+		@Override
+		public boolean isCreative() {
+			return creative;
+		}
+
+		@Override
+		public boolean shouldDisplayAllInformation() {
+			return true;
+		}
 	}
 }
