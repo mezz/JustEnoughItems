13352626e397a2869e1c328b931eae1bfee4bfdf
mezz <tehgeek@gmail.com>
2026-07-16 00:19:29 +0900

    Reopen recipes gui after sizing config changes
    Backport to 1.20.1 using the target listener methods added in the previous config-foundation backport instead of the newer source branch IJeiConfigValue-returning IClientConfig accessors.
    
    (cherry picked from commit 4041c0442111a8e4c67406f611e136e712e557a1)

--- Gui/src/main/java/mezz/jei/gui/recipes/RecipesGui.java
+++ Gui/src/main/java/mezz/jei/gui/recipes/RecipesGui.java
@@ -139,8 +139,8 @@ public class RecipesGui extends Screen implements IRecipesGui, IRecipeFocusSourc
 		this.minecraft = Minecraft.getInstance();
 		this.layouts = new RecipeGuiLayouts();
 		IClientConfig clientConfig = Internal.getJeiClientConfigs().getClientConfig();
-		clientConfig.addCenterSearchBarEnabledListener(v -> closeIfOpen());
-		clientConfig.addMaxRecipeGuiHeightListener(v -> closeIfOpen());
+		clientConfig.addCenterSearchBarEnabledListener(v -> reopenIfOpen());
+		clientConfig.addMaxRecipeGuiHeightListener(v -> reopenIfOpen());
 
 		Textures textures = Internal.getTextures();
 		IDrawableStatic arrowNext = textures.getArrowNext();
@@ -445,9 +445,12 @@ public class RecipesGui extends Screen implements IRecipesGui, IRecipeFocusSourc
 		return minecraft != null && minecraft.screen == this;
 	}
 
-	private void closeIfOpen() {
-		if (isOpen()) {
-			onClose();
+	private void reopenIfOpen() {
+		if (isOpen() && minecraft != null) {
+			Screen currentParentScreen = parentScreen;
+			minecraft.setScreen(currentParentScreen);
+			parentScreen = currentParentScreen;
+			open();
 		}
 	}
 
