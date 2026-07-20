fd48e59f6254fddb7f751068b2622486c3dd5d71
mezz <tehgeek@gmail.com>
2026-07-20 17:34:37 +0900

    Validate NeoForge access transformers
    (cherry picked from commit 7c9816b2b571a5fa3c0c182949604797ab61cfa6)
    (cherry picked from commit 4e0101b61337924cb8ecf392f924523afcee7c58)

--- NeoForge/build.gradle.kts
+++ NeoForge/build.gradle.kts
@@ -168,6 +168,7 @@ neoForge {
 	version = neoforgeVersion
 	// MDG already defaults to this, but override it for clarity.
 	setAccessTransformers("src/main/resources/META-INF/accesstransformer.cfg")
+	validateAccessTransformers = true
 
 	addModdingDependenciesTo(sourceSets.test.get())
 	addModdingDependenciesTo(sourceSets.named("gameTest").get())
