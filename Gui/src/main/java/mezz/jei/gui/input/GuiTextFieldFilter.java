a65367b3b732760be166ae6b8384db22377a63c4
mezz <tehgeek@gmail.com>
2026-07-23 19:17:46 +0900

    Add search field narration
    (cherry picked from commit 2d43c20290e47a8d4f7224fd788ff656611ddacf)
    (cherry picked from commit 946712c4c8fdce2286f894b49f7bdc1d4d1fee9c)
    (cherry picked from commit 156989dde9f2e727fe5da19e9690e3d44a603244)

--- Gui/src/main/java/mezz/jei/gui/input/GuiTextFieldFilter.java
+++ Gui/src/main/java/mezz/jei/gui/input/GuiTextFieldFilter.java
@@ -13,7 +13,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
@@ -31,8 +31,7 @@ public class GuiTextFieldFilter extends EditBox implements ISearchField {
 	private @Nullable ScreenFocusHandler screenUnfocusHandler;
 
 	public GuiTextFieldFilter(BooleanSupplier filterEmpty) {
-		// TODO narrator string
-		super(Minecraft.getInstance().font, 0, 0, 0, 0, CommonComponents.EMPTY);
+		super(Minecraft.getInstance().font, 0, 0, 0, 0, Component.translatable("gui.jei.search"));
 		this.filterEmpty = filterEmpty;
 
 		setMaxLength(maxSearchLength);
