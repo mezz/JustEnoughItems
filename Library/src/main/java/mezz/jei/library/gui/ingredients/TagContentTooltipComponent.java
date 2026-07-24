ce3c51ef365833944cfc013a9f4050754b0187b6
mezz <tehgeek@gmail.com>
2026-07-24 22:08:47 +0900

    Fix missing count for tooltips on items with many tags

--- Library/src/main/java/mezz/jei/library/gui/ingredients/TagContentTooltipComponent.java
+++ Library/src/main/java/mezz/jei/library/gui/ingredients/TagContentTooltipComponent.java
@@ -38,7 +38,7 @@ public class TagContentTooltipComponent<T> implements ClientTooltipComponent, To
 				final int textWidth = font.width(countString);
 				final int textCenterX = x + (MAX_PER_LINE - 1) * INGREDIENT_SIZE  + ((INGREDIENT_SIZE - textWidth) / 2);
 				final int textCenterY = y + (MAX_LINES - 1) * INGREDIENT_SIZE + ((INGREDIENT_SIZE - textHeight) / 2);
-				guiGraphics.text(font, countString, textCenterX, textCenterY, 0xAAAAAA);
+				guiGraphics.text(font, countString, textCenterX, textCenterY, 0xFFAAAAAA);
 			}
 		}
 	}
