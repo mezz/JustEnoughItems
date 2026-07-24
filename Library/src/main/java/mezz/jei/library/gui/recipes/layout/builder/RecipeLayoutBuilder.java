Mysticpasta1/JEI-Async

Fix #3767 Recipe ID should be shown for all recipes when advanced tooltips are enabled

Library/src/main/java/mezz/jei/library/gui/recipes/layout/builder/RecipeLayoutBuilder.java
@@ -17,6 +17,7 @@ import mezz.jei.api.gui.widgets.ISlottedWidgetFactory;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.IRecipeCategoryDecorator;
 import mezz.jei.api.runtime.IIngredientManager;
@@ -97,11 +98,9 @@ public class RecipeLayoutBuilder<T> implements IRecipeLayoutBuilder, IRecipeExtr
 	private void addOutputSlotTooltipCallback(RecipeSlotBuilder slot) {
 		ResourceLocation recipeName = recipeCategory.getRegistryName(recipe);
 		if (recipeName != null) {
-			ResourceLocation recipeTypeUid = recipeCategory.getRecipeType().getUid();
-			if (!recipeTypeUid.getNamespace().equals(recipeName.getNamespace())) {
-				OutputSlotTooltipCallback callback = new OutputSlotTooltipCallback(recipeName);
-				slot.addRichTooltipCallback(callback);
-			}
+			RecipeType<T> recipeType = recipeCategory.getRecipeType();
+			OutputSlotTooltipCallback callback = new OutputSlotTooltipCallback(recipeName, recipeType);
+			slot.addRichTooltipCallback(callback);
 		}
 	}
 

mezz/JustEnoughItems

(cherry picked from commit 2e8c54db52e0612a0886cde59a146bb1ea0fc1a4)

Library/src/main/java/mezz/jei/library/gui/recipes/layout/builder/RecipeLayoutBuilder.java
@@ -11,6 +11,7 @@ import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.common.Internal;
@@ -70,11 +71,9 @@ public class RecipeLayoutBuilder<T> implements IRecipeLayoutBuilder {
 	private void addOutputSlotTooltipCallback(RecipeSlotBuilder slot) {
 		ResourceLocation recipeName = recipeCategory.getRegistryName(recipe);
 		if (recipeName != null) {
-			ResourceLocation recipeTypeUid = recipeCategory.getRecipeType().getUid();
-			if (!recipeTypeUid.getNamespace().equals(recipeName.getNamespace())) {
-				OutputSlotTooltipCallback callback = new OutputSlotTooltipCallback(recipeName);
-				slot.addTooltipCallback(callback);
-			}
+			RecipeType<T> recipeType = recipeCategory.getRecipeType();
+			OutputSlotTooltipCallback callback = new OutputSlotTooltipCallback(recipeName, recipeType);
+			slot.addTooltipCallback(callback);
 		}
 	}
 