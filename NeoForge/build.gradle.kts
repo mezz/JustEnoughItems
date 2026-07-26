b7ce46ed612ae851bf8d907c8f728fe331ebe1a6
mezz <tehgeek@gmail.com>
2026-07-26 19:38:07 +0900

    Use ModShade for embedded libraries
    (cherry picked from commit 0e6b09b13c3db5f2e36a155d3465ce7a52344c8f)
    (cherry picked from commit ff5e6d1e33c07313bc786b9fcff54717c6c5317e)

--- NeoForge/build.gradle.kts
+++ NeoForge/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 	id("maven-publish")
 	id("me.modmuss50.mod-publish-plugin")
 	id("net.neoforged.moddev")
+	id("net.mezzdev.modshade")
 }
 
 // gradle.properties
@@ -70,13 +71,6 @@ dependencyProjects.forEach {
 }
 project.evaluationDependsOn(debugProject.path)
 
-val embeddedLibraries: Configuration by configurations.creating {
-	isCanBeConsumed = false
-	isCanBeResolved = true
-}
-configurations.implementation {
-	extendsFrom(embeddedLibraries)
-}
 configurations.named("gameTestImplementation") {
 	extendsFrom(configurations.implementation.get())
 }
@@ -152,10 +146,10 @@ dependencies {
 	dependencyProjects.forEach {
 		implementation(it)
 	}
-	embeddedLibraries("net.mezzdev:baked-substring-index:${bakedSubstringIndexVersion}") {
+	modShadeImplementation("net.mezzdev:baked-substring-index:${bakedSubstringIndexVersion}") {
 		isTransitive = false
 	}
-	embeddedLibraries("net.mezzdev:suffixtree:${suffixtreeVersion}") {
+	modShadeImplementation("net.mezzdev:suffixtree:${suffixtreeVersion}") {
 		isTransitive = false
 	}
 	"gameTestImplementation"("net.neoforged:testframework:${neoforgeVersion}") {
@@ -338,12 +332,10 @@ clientRecipeSyncRuns.forEach { (runName, _) ->
 }
 
 tasks.jar {
-	dependsOn(embeddedLibraries)
 	from(sourceSets.main.get().output)
 	for (p in dependencyProjects) {
 		from(p.sourceSets.main.get().output)
 	}
-	from(embeddedLibraries.map(::zipTree))
 
 	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
@@ -357,8 +349,11 @@ val sourcesJarTask = tasks.named<Jar>("sourcesJar") {
 	archiveClassifier.set("sources")
 }
 
+val shadedJar = modShade.shadeJar()
+val shadedSourcesJar = modShade.shadeSourcesJar()
+
 publishMods {
-	file.set(tasks.jar.get().archiveFile)
+	file.set(shadedJar.flatMap { it.archiveFile })
 	type = BETA
 	modLoaders.add("neoforge")
 	displayName.set("${project.version} for NeoForge $minecraftVersion")
@@ -413,8 +408,8 @@ publishing {
 	publications {
 		register<MavenPublication>("neoforgeJar") {
 			artifactId = baseArchivesName
-			artifact(tasks.jar.get())
-			artifact(sourcesJarTask.get())
+			artifact(shadedJar)
+			artifact(shadedSourcesJar)
 		}
 	}
 	repositories {
