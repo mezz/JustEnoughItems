f6d9c158060c7e73262dcb5dbc16e1ebeef6bab2
mezz <tehgeek@gmail.com>
2026-07-26 21:05:22 +0900

    Fix changelog generation for publishing

--- Changelog/build.gradle.kts
+++ Changelog/build.gradle.kts
@@ -1,17 +1,17 @@
 import se.bjurr.gitchangelog.plugin.gradle.GitChangelogTask
 
 plugins {
-	id("se.bjurr.gitchangelog.git-changelog-gradle-plugin") version("1.77.2")
+	id("se.bjurr.gitchangelog.git-changelog-gradle-plugin") version("3.1.2")
 }
 
 // gradle.properties
 val specificationVersion: String by extra
 
 tasks.register<GitChangelogTask>("makeChangelog") {
-	fromRepo = projectDir.absolutePath.toString()
-	file = file("changelog.html")
-	untaggedName = "Current release $specificationVersion"
-	fromCommit = "e72e49fa7a072755e7f96cad65388205f6a010dc"
-	toRef = "HEAD"
-	templateContent = file("changelog.mustache").readText()
+	fromRepo.set(projectDir.absolutePath.toString())
+	file.set(file("changelog.html"))
+	untaggedName.set("Current release $specificationVersion")
+	fromRevision.set("e72e49fa7a072755e7f96cad65388205f6a010dc")
+	toRevision.set("HEAD")
+	templateContent.set(file("changelog.mustache").readText())
 }
