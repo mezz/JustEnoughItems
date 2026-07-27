8d545d7abbd6cc5f80c5d3ac835a2fc6d21ff229
mezz <tehgeek@gmail.com>
2026-07-27 20:26:55 +0900

    Fix server loading of mod name format config
    Backport to 1.20.1 by moving client-only tooltip probing into a helper while preserving this branch's String-based mod-name format API and older ItemStack tooltip signature.
    
    (cherry picked from commit d6c6e87097fff7d5285a64a386d2f0450424c33d)

--- Library/src/main/java/mezz/jei/library/config/ModIdFormatConfig.java
+++ Library/src/main/java/mezz/jei/library/config/ModIdFormatConfig.java
@@ -6,16 +6,8 @@ import mezz.jei.common.config.file.IConfigSchemaBuilder;
 import mezz.jei.core.util.function.CachedSupplierTransformer;
 import mezz.jei.library.config.serializers.ChatFormattingSerializer;
 import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.TooltipFlag;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -23,7 +15,6 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class ModIdFormatConfig implements IModIdFormatConfig {
-	private static final Logger LOGGER = LogManager.getLogger();
 	protected static final List<ChatFormatting> defaultModNameFormat = List.of(ChatFormatting.BLUE, ChatFormatting.ITALIC);
 	public static final String MOD_NAME_FORMAT_CODE = "%MODNAME%";
 
@@ -50,7 +41,7 @@ public class ModIdFormatConfig implements IModIdFormatConfig {
 
 	private String getOverride() {
 		if (cachedOverride == null) {
-			cachedOverride = detectModNameTooltipFormatting();
+			cachedOverride = ModIdFormatDetectionHelper.detectModNameTooltipFormatting();
 		}
 		return cachedOverride;
 	}
@@ -67,22 +58,6 @@ public class ModIdFormatConfig implements IModIdFormatConfig {
 	@Override
 	public final boolean isModNameFormatOverrideActive() {
 		return !getOverride().isEmpty();
-	}
-
-	private static String detectModNameTooltipFormatting() {
-		Minecraft minecraft = Minecraft.getInstance();
-		LocalPlayer player = minecraft.player;
-		List<Component> tooltip = getTestTooltip(player, new ItemStack(Items.APPLE));
-		return detectModNameTooltipFormatting(tooltip);
-	}
-
-	private static List<Component> getTestTooltip(@Nullable Player player, ItemStack itemStack) {
-		try {
-			return itemStack.getTooltipLines(player, TooltipFlag.Default.NORMAL);
-		} catch (LinkageError | RuntimeException e) {
-			LOGGER.error("Error while Testing for mod name formatting", e);
-		}
-		return List.of();
 	}
 
 	public static String detectModNameTooltipFormatting(List<Component> tooltip) {
