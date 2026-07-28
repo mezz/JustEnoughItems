31b5f30368d10625f1dea6974416f2ce7aea622f
mezz <tehgeek@gmail.com>
2026-07-28 22:28:38 +0900

    Add Jenkins Discord build notifications

--- NeoForge/build.gradle.kts
+++ NeoForge/build.gradle.kts
@@ -361,6 +361,7 @@ publishMods {
 
 	curseforge {
 		projectId = curseProjectId
+		projectSlug = curseHomepageUrl.substringAfterLast("/")
 		accessToken.set(curseforgeApikey ?: "0")
 		changelog.set(changelogHtml.singleFileContents())
 		changelogType = "html"
