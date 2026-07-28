1c7b98a4532f3239f4f7d4938606fb4403fdd8e5
mezz <tehgeek@gmail.com>
2026-07-28 22:28:38 +0900

    Add Jenkins Discord build notifications
    (cherry picked from commit c536ee0c0ed0b4c50650de58feec92368a8057ee)
    (cherry picked from commit 9a41b42f3589f5c4f1bcaec19e35430651f431d8)
    (cherry picked from commit e834167f912ee49fdbb1630bc7838c6e5b1c7c7c)
    (cherry picked from commit b87144e41db60d190fa703d7c36bc42059344085)

--- Forge/build.gradle.kts
+++ Forge/build.gradle.kts
@@ -159,6 +159,7 @@ publishMods {
 
 	curseforge {
 		projectId = curseProjectId
+		projectSlug = curseHomepageUrl.substringAfterLast("/")
 		accessToken.set(curseforgeApikey ?: "0")
 		changelog.set(provider { file("../Changelog/changelog.html").readText() })
 		changelogType = "html"
