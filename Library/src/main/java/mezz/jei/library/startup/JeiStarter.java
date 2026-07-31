0b9dde91b99c47e8d50a6e2f2841af4bd3dafb7e
mezz <tehgeek@gmail.com>
2026-07-31 18:11:56 +0900

    Fix #4400 stop crash on shutdown when recipes gui is open
    (cherry picked from commit a00c952cc0992f9eccb31007ab20f9002156f1db)
    (cherry picked from commit a36f2122d73c41ac89d5a0070a383217178bc4ce)

--- Library/src/main/java/mezz/jei/library/startup/JeiStarter.java
+++ Library/src/main/java/mezz/jei/library/startup/JeiStarter.java
@@ -72,6 +72,7 @@ public final class JeiStarter {
 	private final ConfigManager configManager;
 	private final JeiClientConfigs jeiClientConfigs;
 	private final List<IStopCallback> stopCallbacks = new ArrayList<>();
+	private boolean running = false;
 
 	public JeiStarter(StartData data) {
 		ErrorUtil.checkNotEmpty(data.plugins(), "plugins");
@@ -112,6 +113,11 @@ public final class JeiStarter {
 	}
 
 	public void start() {
+		if (running) {
+			LOGGER.error("Failed to start JEI, it is already running.");
+			return;
+		}
+
 		Minecraft minecraft = Minecraft.getInstance();
 		ClientLevel level = minecraft.level;
 		if (level == null) {
@@ -217,6 +223,7 @@ public final class JeiStarter {
 
 		PluginCaller.callOnPlugins("Sending Runtime", plugins, p -> p.onRuntimeAvailable(jeiRuntime));
 		Internal.setRuntime(jeiRuntime);
+		this.running = true;
 
 		totalTime.stop();
 
@@ -260,6 +267,11 @@ public final class JeiStarter {
 	}
 
 	public void stop() {
+		if (!running) {
+			return;
+		}
+		this.running = false;
+
 		LOGGER.info("Stopping JEI");
 
 		List<IModPlugin> plugins = data.plugins();
