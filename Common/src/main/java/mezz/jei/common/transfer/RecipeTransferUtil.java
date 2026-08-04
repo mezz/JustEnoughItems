Mysticpasta1/JEI-Async

Fix #4190 replace UidContext.Ingredient with UidContext.Recipe in RecipeTransferUtil (#4206)

Common/src/main/java/mezz/jei/common/transfer/RecipeTransferUtil.java
@@ -189,7 +189,7 @@ public final class RecipeTransferUtil {
 
 		for (Map.Entry<Slot, ItemStack> slotTuple : availableItemStacks.entrySet()) {
 			ItemStack slotItemStack = slotTuple.getValue();
-			Object slotItemStackUid = stackhelper.getUidForStack(slotItemStack, UidContext.Ingredient);
+			Object slotItemStackUid = stackhelper.getUidForStack(slotItemStack, UidContext.Recipe);
 
 			for (IRecipeSlotView ingredient : nonEmptyRequiredStacks) {
 				Set<Object> ingredientUids = slotUidCache.computeIfAbsent(ingredient, s -> calculateUids(s, stackhelper));
@@ -297,7 +297,7 @@ public final class RecipeTransferUtil {
 			}
 			ITypedIngredient<ItemStack> typedItemStack = typedIngredient.castToItemStackType();
 			if (typedItemStack != null) {
-				Object uid = stackhelper.getUidForStack(typedItemStack, UidContext.Ingredient);
+				Object uid = stackhelper.getUidForStack(typedItemStack, UidContext.Recipe);
 				uids.add(uid);
 			}
 		}
@@ -336,7 +336,7 @@ public final class RecipeTransferUtil {
 
 		@Override
 		public boolean equals(ItemStack a, ItemStack b) {
-			return stackhelper.isEquivalent(a, b, UidContext.Ingredient);
+			return stackhelper.isEquivalent(a, b, UidContext.Recipe);
 		}
 	}
 }

mezz/JustEnoughItems

(cherry picked from commit 4343f38de92e582d1392f7a2cf6fbb610ae9f7ae)

Common/src/main/java/mezz/jei/common/transfer/RecipeTransferUtil.java
@@ -186,12 +186,12 @@ public final class RecipeTransferUtil {
 
 		for (Map.Entry<Slot, ItemStack> slotTuple : availableItemStacks.entrySet()) {
 			ItemStack slotItemStack = slotTuple.getValue();
-			String slotItemStackUid = stackhelper.getUniqueIdentifierForStack(slotItemStack, UidContext.Ingredient);
+			String slotItemStackUid = stackhelper.getUniqueIdentifierForStack(slotItemStack, UidContext.Recipe);
 
 			for (IRecipeSlotView ingredient : nonEmptyRequiredStacks) {
 				Set<String> ingredientUids = slotUidCache.computeIfAbsent(ingredient, s ->
 					s.getItemStacks()
-					.map(i -> stackhelper.getUniqueIdentifierForStack(i, UidContext.Ingredient))
+					.map(i -> stackhelper.getUniqueIdentifierForStack(i, UidContext.Recipe))
 					.collect(Collectors.toSet())
 				);
 
@@ -205,7 +205,7 @@ public final class RecipeTransferUtil {
 
 							@Override
 							public boolean equals(ItemStack a, ItemStack b) {
-								return stackhelper.isEquivalent(a, b, UidContext.Ingredient);
+								return stackhelper.isEquivalent(a, b, UidContext.Recipe);
 							}
 						}))
 						.computeIfAbsent(slotItemStack, it -> new ArrayList<>())