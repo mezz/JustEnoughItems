Mysticpasta1/JEI-Async

Add a getGuiSlotIndexToIngredientMap helper for recipe transfer

Library/src/main/java/mezz/jei/library/plugins/vanilla/crafting/CraftingRecipeCategory.java
@@ -20,6 +20,7 @@ import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategor
 import mezz.jei.api.recipe.category.extensions.vanilla.crafting.IExtendableCraftingRecipeCategory;
 import mezz.jei.common.Constants;
 import mezz.jei.common.util.ErrorUtil;
+import mezz.jei.common.util.ImmutableSize2i;
 import mezz.jei.library.recipes.CraftingExtensionHelper;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
@@ -140,5 +141,16 @@ public class CraftingRecipeCategory implements IExtendableCraftingRecipeCategory
 	@Override
 	public Codec<RecipeHolder<CraftingRecipe>> getCodec(ICodecHelper codecHelper, IRecipeManager recipeManager) {
 		return codecHelper.getRecipeHolderCodec();
+	}
+
+	public ImmutableSize2i getRecipeSize(RecipeHolder<CraftingRecipe> recipeHolder) {
+		ErrorUtil.checkNotNull(recipeHolder, "recipeHolder");
+		return this.extendableHelper.getOptionalRecipeExtension(recipeHolder)
+			.map(extension -> {
+				int width = extension.getWidth(recipeHolder);
+				int height = extension.getHeight(recipeHolder);
+				return new ImmutableSize2i(width, height);
+			})
+			.orElse(ImmutableSize2i.EMPTY);
 	}
 }

mezz/JustEnoughItems

(cherry picked from commit 7fea3d94a3b2ca128a6c2ab358a07c992ed5613e)

Library/src/main/java/mezz/jei/library/plugins/vanilla/crafting/CraftingRecipeCategory.java
@@ -15,6 +15,7 @@ import mezz.jei.api.recipe.category.AbstractRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.IExtendableRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
 import mezz.jei.common.util.ErrorUtil;
+import mezz.jei.common.util.ImmutableSize2i;
 import mezz.jei.library.recipes.ExtendableRecipeCategoryHelper;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
@@ -117,5 +118,16 @@ public class CraftingRecipeCategory extends AbstractRecipeCategory<CraftingRecip
 		return this.extendableHelper.getOptionalRecipeExtension(recipe)
 			.flatMap(extension -> Optional.ofNullable(extension.getRegistryName()))
 			.orElseGet(recipe::getId);
+	}
+
+	public ImmutableSize2i getRecipeSize(CraftingRecipe recipe) {
+		ErrorUtil.checkNotNull(recipe, "recipe");
+		return this.extendableHelper.getOptionalRecipeExtension(recipe)
+			.map(extension -> {
+				int width = extension.getWidth();
+				int height = extension.getHeight();
+				return new ImmutableSize2i(width, height);
+			})
+			.orElse(ImmutableSize2i.EMPTY);
 	}
 }