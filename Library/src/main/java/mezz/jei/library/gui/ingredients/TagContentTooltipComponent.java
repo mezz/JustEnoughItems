Mysticpasta1/JEI-Async

Add tooltip to show tag ingredient details (#3591)
Co-authored-by: mezz <tehgeek@gmail.com>

Library/src/main/java/mezz/jei/library/gui/ingredients/TagContentTooltipComponent.java
@@ -1,0 +1,86 @@
+package mezz.jei.library.gui.ingredients;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.common.util.MathUtil;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+
+import java.util.List;
+
+public class TagContentTooltipComponent<T> implements ClientTooltipComponent {
+    private static final int MAX_PER_LINE = 10;
+    private static final int MAX_LINES = 3;
+    private static final int MAX_INGREDIENTS = MAX_PER_LINE * MAX_LINES;
+    private static final int INGREDIENT_SIZE = 18;
+    private static final int INGREDIENT_PADDING = 1;
+
+    private final IIngredientRenderer<T> renderer;
+    private final List<T> ingredients;
+
+    public TagContentTooltipComponent(IIngredientRenderer<T> renderer, List<T> ingredients) {
+        this.renderer = renderer;
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public void renderImage(Font font, int x, int y, GuiGraphics guiGraphics) {
+        if (ingredients.size() <= MAX_INGREDIENTS) {
+            drawIngredients(guiGraphics, x, y, ingredients.size());
+        } else {
+            final int drawCount = MAX_INGREDIENTS - 1;
+            drawIngredients(guiGraphics, x, y, drawCount);
+            if (ingredients.size() > MAX_INGREDIENTS) {
+                final int remainingCount = Math.min(ingredients.size() - drawCount, 99);
+                String countString = "+" + remainingCount;
+                final int textHeight = font.lineHeight - 1;
+                final int textWidth = font.width(countString);
+                final int textCenterX = x + (MAX_PER_LINE - 1) * INGREDIENT_SIZE  + ((INGREDIENT_SIZE - textWidth) / 2);
+                final int textCenterY = y + (MAX_LINES - 1) * INGREDIENT_SIZE + ((INGREDIENT_SIZE - textHeight) / 2);
+                guiGraphics.drawString(font, countString, textCenterX, textCenterY, 0xAAAAAA);
+            }
+        }
+    }
+
+    private void drawIngredients(GuiGraphics guiGraphics, int x, int y, int maxIngredients) {
+        final int maxPerLine = MathUtil.divideCeil(maxIngredients, getLineCount());
+
+        for (int i = 0; i < ingredients.size() && i < maxIngredients; i++) {
+            int column = i % maxPerLine;
+            int row = i / maxPerLine;
+            PoseStack poseStack = guiGraphics.pose();
+            poseStack.pushPose();
+            {
+                poseStack.translate(
+                    x + column * INGREDIENT_SIZE + INGREDIENT_PADDING,
+                    y + row * INGREDIENT_SIZE + INGREDIENT_PADDING,
+                    0.0D
+                );
+                renderer.render(guiGraphics, ingredients.get(i));
+            }
+            poseStack.popPose();
+        }
+    }
+
+    private int getLineCount() {
+        int lineCount = MathUtil.divideCeil(ingredients.size(), MAX_PER_LINE);
+        return Math.min(lineCount, MAX_LINES);
+    }
+
+    private int getMaxPerLine() {
+        int perLine = MathUtil.divideCeil(ingredients.size(), getLineCount());
+        return Math.min(perLine, MAX_PER_LINE);
+    }
+
+    @Override
+    public int getHeight() {
+        return getLineCount() * INGREDIENT_SIZE + (2 * INGREDIENT_PADDING);
+    }
+
+    @Override
+    public int getWidth(Font font) {
+        return getMaxPerLine() * INGREDIENT_SIZE + (2 * INGREDIENT_PADDING);
+    }
+}
+

Library/src/main/java/mezz/jei/library/gui/recipes/RecipeLayout.java
@@ -7,7 +7,9 @@ import mezz.jei.api.gui.drawable.IScalableDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IModIdHelper;
+import mezz.jei.api.ingredients.IIngredientRenderer;
 import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.category.IRecipeCategory;
@@ -15,13 +17,17 @@ import mezz.jei.api.recipe.category.extensions.IRecipeCategoryDecorator;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IJeiRuntime;
 import mezz.jei.common.Internal;
+import mezz.jei.common.config.IClientConfig;
+import mezz.jei.common.gui.TooltipHelper;
 import mezz.jei.common.gui.TooltipRenderer;
 import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
 import mezz.jei.common.util.ImmutableRect2i;
 import mezz.jei.common.util.MathUtil;
 import mezz.jei.library.gui.ingredients.RecipeSlot;
 import mezz.jei.library.gui.ingredients.RecipeSlots;
+import mezz.jei.library.gui.ingredients.TagContentTooltipComponent;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -29,9 +35,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -237,9 +245,14 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 			IModIdHelper modIdHelper = jeiRuntime.getJeiHelpers().getModIdHelper();
 			hoveredSlot.getDisplayedIngredient()
 				.ifPresent(i -> {
-					List<Component> tooltip = hoveredSlot.getTooltip();
-					tooltip = modIdHelper.addModNameToIngredientTooltip(tooltip, i);
-					TooltipRenderer.drawHoveringText(guiGraphics, tooltip, mouseX, mouseY, i);
+					List<ClientTooltipComponent> components = hoveredSlot.getTooltip().stream()
+							.map(TooltipHelper::toTooltipComponent)
+							.collect(Collectors.toCollection(ArrayList::new));
+					addTagContentTooltip(components, i, hoveredSlot);
+                    List<Component> modIdTooltip = new ArrayList<>();
+                    modIdTooltip = modIdHelper.addModNameToIngredientTooltip(modIdTooltip, i);
+                    addAllTooltipComponents(components, modIdTooltip);
+					TooltipRenderer.drawHoveringTooltip(guiGraphics, components, mouseX, mouseY, i);
 				});
 		} else if (isMouseOver(mouseX, mouseY)) {
 			List<Component> tooltipStrings = recipeCategory.getTooltipStrings(recipe, recipeSlots.getView(), recipeMouseX, recipeMouseY);
@@ -252,6 +265,26 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 			}
 			if (!tooltipStrings.isEmpty()) {
 				TooltipRenderer.drawHoveringText(guiGraphics, tooltipStrings, mouseX, mouseY);
+			}
+		}
+	}
+
+    private void addAllTooltipComponents(List<ClientTooltipComponent> tooltipComponents, List<Component> components) {
+		tooltipComponents.addAll(TooltipHelper.toTooltipComponents(components));
+    }
+
+	private <T> void addTagContentTooltip(List<ClientTooltipComponent> tooltipComponents, ITypedIngredient<T> displayed, IRecipeSlotDrawable slotDrawable) {
+		IClientConfig clientConfig = Internal.getJeiClientConfigs().getClientConfig();
+		if (clientConfig.isTagContentTooltipEnabled()) {
+			IIngredientType<T> type = displayed.getType();
+
+			IJeiRuntime jeiRuntime = Internal.getJeiRuntime();
+			IIngredientManager ingredientManager = jeiRuntime.getIngredientManager();
+			IIngredientRenderer<T> renderer = ingredientManager.getIngredientRenderer(type);
+
+			List<T> ingredients = slotDrawable.getIngredients(type).toList();
+			if (ingredients.size() > 1) {
+				tooltipComponents.add(new TagContentTooltipComponent<>(renderer, ingredients));
 			}
 		}
 	}

mezz/JustEnoughItems

(cherry picked from commit 2c2083d4fc2785e18c9b4ea03083aec1af95e603)

Library/src/main/java/mezz/jei/library/gui/ingredients/TagContentTooltipComponent.java
@@ -1,0 +1,84 @@
+package mezz.jei.library.gui.ingredients;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.common.util.MathUtil;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+
+import java.util.List;
+
+public class TagContentTooltipComponent<T> implements ClientTooltipComponent {
+	private static final int MAX_PER_LINE = 10;
+	private static final int MAX_LINES = 3;
+	private static final int MAX_INGREDIENTS = MAX_PER_LINE * MAX_LINES;
+	private static final int INGREDIENT_SIZE = 18;
+	private static final int INGREDIENT_PADDING = 1;
+
+	private final IIngredientRenderer<T> renderer;
+	private final List<T> ingredients;
+
+	public TagContentTooltipComponent(IIngredientRenderer<T> renderer, List<T> ingredients) {
+		this.renderer = renderer;
+		this.ingredients = ingredients;
+	}
+
+	@Override
+	public void renderImage(Font font, int x, int y, PoseStack poseStack, ItemRenderer itemRenderer, int z) {
+		if (ingredients.size() <= MAX_INGREDIENTS) {
+			drawIngredients(poseStack, x, y, ingredients.size());
+		} else {
+			final int drawCount = MAX_INGREDIENTS - 1;
+			drawIngredients(poseStack, x, y, drawCount);
+			if (ingredients.size() > MAX_INGREDIENTS) {
+				final int remainingCount = Math.min(ingredients.size() - drawCount, 99);
+				String countString = "+" + remainingCount;
+				final int textHeight = font.lineHeight - 1;
+				final int textWidth = font.width(countString);
+				final int textCenterX = x + (MAX_PER_LINE - 1) * INGREDIENT_SIZE  + ((INGREDIENT_SIZE - textWidth) / 2);
+				final int textCenterY = y + (MAX_LINES - 1) * INGREDIENT_SIZE + ((INGREDIENT_SIZE - textHeight) / 2);
+				font.draw(poseStack, countString, textCenterX, textCenterY, 0xAAAAAA);
+			}
+		}
+	}
+
+	private void drawIngredients(PoseStack poseStack, int x, int y, int maxIngredients) {
+		final int maxPerLine = MathUtil.divideCeil(maxIngredients, getLineCount());
+
+		for (int i = 0; i < ingredients.size() && i < maxIngredients; i++) {
+			int column = i % maxPerLine;
+			int row = i / maxPerLine;
+			poseStack.pushPose();
+			{
+				poseStack.translate(
+					x + column * INGREDIENT_SIZE + INGREDIENT_PADDING,
+					y + row * INGREDIENT_SIZE + INGREDIENT_PADDING,
+					0.0D
+				);
+				renderer.render(poseStack, ingredients.get(i));
+			}
+			poseStack.popPose();
+		}
+	}
+
+	private int getLineCount() {
+		int lineCount = MathUtil.divideCeil(ingredients.size(), MAX_PER_LINE);
+		return Math.min(lineCount, MAX_LINES);
+	}
+
+	private int getMaxPerLine() {
+		int perLine = MathUtil.divideCeil(ingredients.size(), getLineCount());
+		return Math.min(perLine, MAX_PER_LINE);
+	}
+
+	@Override
+	public int getHeight() {
+		return getLineCount() * INGREDIENT_SIZE + (2 * INGREDIENT_PADDING);
+	}
+
+	@Override
+	public int getWidth(Font font) {
+		return getMaxPerLine() * INGREDIENT_SIZE + (2 * INGREDIENT_PADDING);
+	}
+}

Library/src/main/java/mezz/jei/library/gui/recipes/RecipeLayout.java
@@ -9,18 +9,22 @@ import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.gui.inputs.IJeiInputHandler;
 import mezz.jei.api.gui.inputs.RecipeSlotUnderMouse;
+import mezz.jei.api.ingredients.IIngredientRenderer;
 import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.IRecipeCategoryDecorator;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.common.Internal;
+import mezz.jei.common.config.IClientConfig;
 import mezz.jei.common.gui.JeiTooltip;
 import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
 import mezz.jei.common.util.ImmutablePoint2i;
 import mezz.jei.common.util.ImmutableRect2i;
 import mezz.jei.core.util.LimitedLogger;
 import mezz.jei.library.gui.ingredients.CycleTicker;
+import mezz.jei.library.gui.ingredients.TagContentTooltipComponent;
 import mezz.jei.library.gui.recipes.layout.builder.RecipeLayoutBuilder;
 import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.network.chat.Component;
@@ -212,9 +216,8 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		if (hoveredSlotResult != null) {
 			IRecipeSlotDrawable hoveredSlot = hoveredSlotResult.slot();
 
-			JeiTooltip tooltip = new JeiTooltip();
-			tooltip.addAll(hoveredSlot.getTooltip());
-			tooltip.draw(poseStack, mouseX, mouseY);
+			hoveredSlot.getDisplayedIngredient()
+				.ifPresent(i -> drawSlotTooltip(poseStack, mouseX, mouseY, hoveredSlot, i));
 		} else if (isMouseOver(mouseX, mouseY)) {
 			JeiTooltip tooltip = new JeiTooltip();
 			try {
@@ -237,6 +240,28 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 				}
 			}
 			tooltip.draw(poseStack, mouseX, mouseY);
+		}
+	}
+
+	private <T> void drawSlotTooltip(PoseStack poseStack, int mouseX, int mouseY, IRecipeSlotDrawable hoveredSlot, ITypedIngredient<T> displayed) {
+		IIngredientType<T> type = displayed.getType();
+		IIngredientManager ingredientManager = Internal.getJeiRuntime().getIngredientManager();
+		IIngredientRenderer<T> renderer = ingredientManager.getIngredientRenderer(type);
+
+		JeiTooltip tooltip = new JeiTooltip();
+		tooltip.addAll(hoveredSlot.getTooltip());
+		addTagContentTooltip(tooltip, displayed, renderer, hoveredSlot);
+		tooltip.draw(poseStack, mouseX, mouseY, displayed, renderer, ingredientManager);
+	}
+
+	private <T> void addTagContentTooltip(JeiTooltip tooltip, ITypedIngredient<T> displayed, IIngredientRenderer<T> renderer, IRecipeSlotDrawable slotDrawable) {
+		IClientConfig clientConfig = Internal.getJeiClientConfigs().getClientConfig();
+		if (clientConfig.isTagContentTooltipEnabled()) {
+			IIngredientType<T> type = displayed.getType();
+			List<T> ingredients = slotDrawable.getIngredients(type).toList();
+			if (ingredients.size() > 1) {
+				tooltip.addClientTooltipComponent(new TagContentTooltipComponent<>(renderer, ingredients));
+			}
 		}
 	}
 