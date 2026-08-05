Mysticpasta1/JEI-Async

Remove one pose stack translation from rendering offset ingredients

Gui/src/main/java/mezz/jei/gui/recipes/RecipeCategoryTab.java
@@ -63,8 +63,8 @@ public class RecipeCategoryTab extends RecipeGuiTab {
 		super.draw(selected, guiGraphics, mouseX, mouseY);
 
 		IDrawable icon = RecipeCategoryIconUtil.create(category, recipeManager, guiHelper);
-		int iconX = x + (TAB_WIDTH - icon.getWidth()) / 2;
-		int iconY = y + (TAB_HEIGHT - icon.getHeight()) / 2;
+		int iconX = area.x() + (TAB_WIDTH - icon.getWidth()) / 2;
+		int iconY = area.y() + (TAB_HEIGHT - icon.getHeight()) / 2;
 		icon.draw(guiGraphics, iconX, iconY);
 	}
 

Gui/src/main/java/mezz/jei/gui/recipes/RecipeGuiTab.java
@@ -14,13 +14,9 @@ public abstract class RecipeGuiTab implements IUserInputHandler {
 	public static final int TAB_HEIGHT = 24;
 	public static final int TAB_WIDTH = 24;
 
-	protected final int x;
-	protected final int y;
-	private final ImmutableRect2i area;
+	protected final ImmutableRect2i area;
 
 	public RecipeGuiTab(int x, int y) {
-		this.x = x;
-		this.y = y;
 		this.area = new ImmutableRect2i(x, y, TAB_WIDTH, TAB_HEIGHT);
 	}
 
@@ -34,7 +30,7 @@ public abstract class RecipeGuiTab implements IUserInputHandler {
 		Textures textures = Internal.getTextures();
 		IDrawable tab = selected ? textures.getTabSelected() : textures.getTabUnselected();
 
-		tab.draw(guiGraphics, x, y);
+		tab.draw(guiGraphics, area.x(), area.y());
 	}
 
 	public abstract JeiTooltip getTooltip();

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -20,13 +20,18 @@ import java.util.List;
 public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 	@Override
 	public void render(GuiGraphics guiGraphics, @Nullable ItemStack ingredient) {
+		render(guiGraphics, ingredient, 0, 0);
+	}
+
+	@Override
+	public void render(GuiGraphics guiGraphics, @Nullable ItemStack ingredient, int posX, int posY) {
 		if (ingredient != null) {
 			RenderSystem.enableDepthTest();
 
 			Minecraft minecraft = Minecraft.getInstance();
 			Font font = getFontRenderer(minecraft, ingredient);
-			guiGraphics.renderFakeItem(ingredient, 0, 0);
-			guiGraphics.renderItemDecorations(font, ingredient, 0, 0);
+			guiGraphics.renderFakeItem(ingredient, posX, posY);
+			guiGraphics.renderItemDecorations(font, ingredient, posX, posY);
 			RenderSystem.disableBlend();
 		}
 	}

mezz/JustEnoughItems

(cherry picked from commit 8eaa9c2239905b9474c62130d31bfb239c724388)

Gui/src/main/java/mezz/jei/gui/recipes/RecipeCategoryTab.java
@@ -63,8 +63,8 @@ public class RecipeCategoryTab extends RecipeGuiTab {
 		super.draw(selected, poseStack, mouseX, mouseY);
 
 		IDrawable icon = RecipeCategoryIconUtil.create(category, recipeManager, guiHelper);
-		int iconX = x + (TAB_WIDTH - icon.getWidth()) / 2;
-		int iconY = y + (TAB_HEIGHT - icon.getHeight()) / 2;
+		int iconX = area.getX() + (TAB_WIDTH - icon.getWidth()) / 2;
+		int iconY = area.getY() + (TAB_HEIGHT - icon.getHeight()) / 2;
 		icon.draw(poseStack, iconX, iconY);
 	}
 

Gui/src/main/java/mezz/jei/gui/recipes/RecipeGuiTab.java
@@ -13,13 +13,9 @@ public abstract class RecipeGuiTab implements IUserInputHandler {
 	public static final int TAB_HEIGHT = 24;
 	public static final int TAB_WIDTH = 24;
 
-	protected final int x;
-	protected final int y;
-	private final ImmutableRect2i area;
+	protected final ImmutableRect2i area;
 
 	public RecipeGuiTab(int x, int y) {
-		this.x = x;
-		this.y = y;
 		this.area = new ImmutableRect2i(x, y, TAB_WIDTH, TAB_HEIGHT);
 	}
 
@@ -33,7 +29,7 @@ public abstract class RecipeGuiTab implements IUserInputHandler {
 		Textures textures = Internal.getTextures();
 		IDrawable tab = selected ? textures.getTabSelected() : textures.getTabUnselected();
 
-		tab.draw(poseStack, x, y);
+		tab.draw(poseStack, area.getX(), area.getY());
 	}
 
 	public abstract JeiTooltip getTooltip();

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -28,6 +28,11 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 
 	@Override
 	public void render(PoseStack poseStack, @Nullable ItemStack ingredient) {
+		render(poseStack, ingredient, 0, 0);
+	}
+
+	@Override
+	public void render(PoseStack poseStack, @Nullable ItemStack ingredient, int posX, int posY) {
 		if (ingredient != null) {
 			PoseStack modelViewStack = RenderSystem.getModelViewStack();
 			modelViewStack.pushPose();
@@ -39,8 +44,8 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 				Minecraft minecraft = Minecraft.getInstance();
 				Font font = getFontRenderer(minecraft, ingredient);
 				ItemRenderer itemRenderer = minecraft.getItemRenderer();
-				itemRenderer.renderAndDecorateFakeItem(ingredient, 0, 0);
-				itemRenderer.renderGuiItemDecorations(font, ingredient, 0, 0);
+				itemRenderer.renderAndDecorateFakeItem(ingredient, posX, posY);
+				itemRenderer.renderGuiItemDecorations(font, ingredient, posX, posY);
 				RenderSystem.disableBlend();
 			}
 			modelViewStack.popPose();