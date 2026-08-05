Mysticpasta1/JEI-Async

Implement an extension point for custom modded smithing recipes

Library/src/main/java/mezz/jei/library/plugins/vanilla/VanillaPlugin.java
@@ -14,6 +14,7 @@ import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
 import mezz.jei.api.ingredients.subtypes.ISubtypeManager;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.vanilla.crafting.IExtendableCraftingRecipeCategory;
+import mezz.jei.api.recipe.category.extensions.vanilla.smithing.IExtendableSmithingRecipeCategory;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
 import mezz.jei.api.recipe.vanilla.IJeiBrewingRecipe;
 import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
@@ -37,6 +38,7 @@ import mezz.jei.common.util.RegistryUtil;
 import mezz.jei.common.util.StackHelper;
 import mezz.jei.library.plugins.vanilla.anvil.AnvilRecipeCategory;
 import mezz.jei.library.plugins.vanilla.anvil.AnvilRecipeMaker;
+import mezz.jei.library.plugins.vanilla.anvil.SmithingCategoryExtension;
 import mezz.jei.library.plugins.vanilla.anvil.SmithingRecipeCategory;
 import mezz.jei.library.plugins.vanilla.brewing.BrewingRecipeCategory;
 import mezz.jei.library.plugins.vanilla.brewing.PotionSubtypeInterpreter;
@@ -139,7 +141,7 @@ public class VanillaPlugin implements IModPlugin {
 	@Nullable
 	private IRecipeCategory<RecipeHolder<CampfireCookingRecipe>> campfireCategory;
 	@Nullable
-	private IRecipeCategory<RecipeHolder<SmithingRecipe>> smithingCategory;
+	private SmithingRecipeCategory smithingCategory;
 
 	@Override
 	public ResourceLocation getPluginUid() {
@@ -231,6 +233,13 @@ public class VanillaPlugin implements IModPlugin {
 	public void registerVanillaCategoryExtensions(IVanillaCategoryExtensionRegistration registration) {
 		IExtendableCraftingRecipeCategory craftingCategory = registration.getCraftingCategory();
 		craftingCategory.addExtension(CraftingRecipe.class, new CraftingCategoryExtension());
+
+		IExtendableSmithingRecipeCategory smithingCategory = registration.getSmithingCategory();
+		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
+		recipeHelper.getSupportedSmithingRecipeClasses();
+		for (Class<? extends SmithingRecipe> recipeClass : recipeHelper.getSupportedSmithingRecipeClasses()) {
+			smithingCategory.addExtension(recipeClass, new SmithingCategoryExtension<>(recipeHelper));
+		}
 	}
 
 	@Override
@@ -330,6 +339,10 @@ public class VanillaPlugin implements IModPlugin {
 
 	public Optional<CraftingRecipeCategory> getCraftingCategory() {
 		return Optional.ofNullable(craftingCategory);
+	}
+
+	public Optional<SmithingRecipeCategory> getSmithingCategory() {
+		return Optional.ofNullable(smithingCategory);
 	}
 
 	/**

Library/src/main/java/mezz/jei/library/plugins/vanilla/anvil/SmithingRecipeCategory.java
@@ -2,6 +2,7 @@ package mezz.jei.library.plugins.vanilla.anvil;
 
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
@@ -9,10 +10,11 @@ import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.recipe.category.extensions.vanilla.smithing.IExtendableSmithingRecipeCategory;
+import mezz.jei.api.recipe.category.extensions.vanilla.smithing.ISmithingCategoryExtension;
 import mezz.jei.common.Internal;
 import mezz.jei.common.gui.textures.Textures;
-import mezz.jei.common.platform.IPlatformRecipeHelper;
-import mezz.jei.common.platform.Services;
+import mezz.jei.common.util.ErrorUtil;
 import mezz.jei.library.util.RecipeUtil;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
@@ -21,12 +23,17 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.SmithingRecipe;
 import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.Nullable;
 
-public class SmithingRecipeCategory implements IRecipeCategory<RecipeHolder<SmithingRecipe>> {
+import java.util.HashMap;
+import java.util.Map;
+
+public class SmithingRecipeCategory implements IRecipeCategory<RecipeHolder<SmithingRecipe>>, IExtendableSmithingRecipeCategory {
 	private final IDrawable background;
 	private final IDrawable icon;
 	private final IDrawable slot;
 	private final IDrawable recipeArrow;
+	private final Map<Class<? extends SmithingRecipe>, ISmithingCategoryExtension<?>> extensions = new HashMap<>();
 
 	public SmithingRecipeCategory(IGuiHelper guiHelper) {
 		background = guiHelper.createBlankDrawable(108, 28);
@@ -60,19 +67,23 @@ public class SmithingRecipeCategory implements IRecipeCategory<RecipeHolder<Smit
 	public void setRecipe(IRecipeLayoutBuilder builder, RecipeHolder<SmithingRecipe> recipeHolder, IFocusGroup focuses) {
 		SmithingRecipe recipe = recipeHolder.value();
 
-		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
+		ISmithingCategoryExtension<? super SmithingRecipe> extension = getExtension(recipe);
+		if (extension == null) {
+			return;
+		}
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 1, 6)
-			.addIngredients(recipeHelper.getTemplate(recipe))
+		IRecipeSlotBuilder template = builder.addSlot(RecipeIngredientRole.INPUT, 1, 6)
 			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 19, 6)
-			.addIngredients(recipeHelper.getBase(recipe))
+		IRecipeSlotBuilder base = builder.addSlot(RecipeIngredientRole.INPUT, 19, 6)
 			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 37, 6)
-			.addIngredients(recipeHelper.getAddition(recipe))
+		IRecipeSlotBuilder addition = builder.addSlot(RecipeIngredientRole.INPUT, 37, 6)
 			.setBackground(slot, -1, -1);
+
+		extension.setTemplate(recipe, template);
+		extension.setBase(recipe, base);
+		extension.setAddition(recipe, addition);
 
 		builder.addSlot(RecipeIngredientRole.OUTPUT, 91, 6)
 			.addItemStack(RecipeUtil.getResultItem(recipe))
@@ -87,12 +98,40 @@ public class SmithingRecipeCategory implements IRecipeCategory<RecipeHolder<Smit
 	@Override
 	public boolean isHandled(RecipeHolder<SmithingRecipe> recipeHolder) {
 		SmithingRecipe recipe = recipeHolder.value();
-		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
-		return recipeHelper.isHandled(recipe);
+		var extension = getExtension(recipe);
+		return extension != null;
 	}
 
 	@Override
 	public ResourceLocation getRegistryName(RecipeHolder<SmithingRecipe> recipe) {
 		return recipe.id();
+	}
+
+	@Override
+	public <R extends SmithingRecipe> void addExtension(Class<? extends R> recipeClass, ISmithingCategoryExtension<R> extension) {
+		ErrorUtil.checkNotNull(recipeClass, "recipeClass");
+		ErrorUtil.checkNotNull(extension, "extension");
+		if (extensions.containsKey(recipeClass)) {
+			throw new IllegalArgumentException("An extension has already been registered for: " + recipeClass);
+		}
+		extensions.put(recipeClass, extension);
+	}
+
+	@Nullable
+	private <R extends SmithingRecipe> ISmithingCategoryExtension<? super R> getExtension(SmithingRecipe recipe) {
+		{
+			ISmithingCategoryExtension<?> extension = extensions.get(recipe.getClass());
+			if (extension != null) {
+				//noinspection unchecked
+				return (ISmithingCategoryExtension<? super R>) extension;
+			}
+		}
+		for (Map.Entry<Class<? extends SmithingRecipe>, ISmithingCategoryExtension<?>> e : extensions.entrySet()) {
+			if (e.getKey().isInstance(recipe)) {
+				//noinspection unchecked
+				return (ISmithingCategoryExtension<? super R>) e.getValue();
+			}
+		}
+		return null;
 	}
 }

mezz/JustEnoughItems

(cherry picked from commit a695e5ee5ebb31e76d32e8956e43020b83e51285)

Library/src/main/java/mezz/jei/library/plugins/vanilla/VanillaPlugin.java
@@ -14,6 +14,7 @@ import mezz.jei.api.ingredients.subtypes.ISubtypeManager;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.IExtendableRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
+import mezz.jei.api.recipe.category.extensions.vanilla.smithing.IExtendableSmithingRecipeCategory;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
 import mezz.jei.api.recipe.vanilla.IJeiBrewingRecipe;
 import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
@@ -37,6 +38,7 @@ import mezz.jei.common.platform.IPlatformRegistry;
 import mezz.jei.common.platform.Services;
 import mezz.jei.library.plugins.vanilla.anvil.AnvilRecipeCategory;
 import mezz.jei.library.plugins.vanilla.anvil.AnvilRecipeMaker;
+import mezz.jei.library.plugins.vanilla.anvil.SmithingCategoryExtension;
 import mezz.jei.library.plugins.vanilla.anvil.SmithingRecipeCategory;
 import mezz.jei.library.plugins.vanilla.brewing.BrewingRecipeCategory;
 import mezz.jei.library.plugins.vanilla.compostable.CompostableRecipeCategory;
@@ -137,7 +139,7 @@ public class VanillaPlugin implements IModPlugin {
 	@Nullable
 	private IRecipeCategory<CampfireCookingRecipe> campfireCategory;
 	@Nullable
-	private IRecipeCategory<UpgradeRecipe> smithingCategory;
+	private SmithingRecipeCategory smithingCategory;
 
 	@Override
 	public ResourceLocation getPluginUid() {
@@ -215,6 +217,12 @@ public class VanillaPlugin implements IModPlugin {
 	public void registerVanillaCategoryExtensions(IVanillaCategoryExtensionRegistration registration) {
 		IExtendableRecipeCategory<CraftingRecipe, ICraftingCategoryExtension> craftingCategory = registration.getCraftingCategory();
 		craftingCategory.addCategoryExtension(CraftingRecipe.class, r -> !r.isSpecial(), CraftingCategoryExtension::new);
+
+		IExtendableSmithingRecipeCategory smithingCategory = registration.getSmithingCategory();
+		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
+		for (Class<? extends UpgradeRecipe> recipeClass : recipeHelper.getSupportedSmithingRecipeClasses()) {
+			smithingCategory.addExtension(recipeClass, new SmithingCategoryExtension<>(recipeHelper));
+		}
 	}
 
 	@Override
@@ -314,6 +322,10 @@ public class VanillaPlugin implements IModPlugin {
 
 	public Optional<CraftingRecipeCategory> getCraftingCategory() {
 		return Optional.ofNullable(craftingCategory);
+	}
+
+	public Optional<SmithingRecipeCategory> getSmithingCategory() {
+		return Optional.ofNullable(smithingCategory);
 	}
 
 	/**

Library/src/main/java/mezz/jei/library/plugins/vanilla/anvil/SmithingRecipeCategory.java
@@ -3,6 +3,7 @@ package mezz.jei.library.plugins.vanilla.anvil;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
@@ -10,21 +11,27 @@ import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.recipe.category.extensions.vanilla.smithing.IExtendableSmithingRecipeCategory;
+import mezz.jei.api.recipe.category.extensions.vanilla.smithing.ISmithingCategoryExtension;
 import mezz.jei.common.Internal;
 import mezz.jei.common.gui.textures.Textures;
-import mezz.jei.common.platform.IPlatformRecipeHelper;
-import mezz.jei.common.platform.Services;
+import mezz.jei.common.util.ErrorUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.UpgradeRecipe;
 import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.Nullable;
 
-public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe> {
+import java.util.HashMap;
+import java.util.Map;
+
+public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe>, IExtendableSmithingRecipeCategory {
 	private final IDrawable background;
 	private final IDrawable icon;
 	private final IDrawable slot;
 	private final IDrawable recipeArrow;
+	private final Map<Class<? extends UpgradeRecipe>, ISmithingCategoryExtension<?>> extensions = new HashMap<>();
 
 	public SmithingRecipeCategory(IGuiHelper guiHelper) {
 		background = guiHelper.createBlankDrawable(125, 28);
@@ -56,15 +63,19 @@ public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe> {
 
 	@Override
 	public void setRecipe(IRecipeLayoutBuilder builder, UpgradeRecipe recipe, IFocusGroup focuses) {
-		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
+		ISmithingCategoryExtension<? super UpgradeRecipe> extension = getExtension(recipe);
+		if (extension == null) {
+			return;
+		}
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 1, 6)
-			.addIngredients(recipeHelper.getBase(recipe))
+		IRecipeSlotBuilder base = builder.addSlot(RecipeIngredientRole.INPUT, 1, 6)
 			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 50, 6)
-			.addIngredients(recipeHelper.getAddition(recipe))
+		IRecipeSlotBuilder addition = builder.addSlot(RecipeIngredientRole.INPUT, 50, 6)
 			.setBackground(slot, -1, -1);
+
+		extension.setBase(recipe, base);
+		extension.setAddition(recipe, addition);
 
 		builder.addSlot(RecipeIngredientRole.OUTPUT, 108, 6)
 			.addItemStack(recipe.getResultItem())
@@ -78,11 +89,39 @@ public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe> {
 
 	@Override
 	public boolean isHandled(UpgradeRecipe recipe) {
-		return !recipe.isSpecial();
+		return getExtension(recipe) != null;
 	}
 
 	@Override
 	public ResourceLocation getRegistryName(UpgradeRecipe recipe) {
 		return recipe.getId();
+	}
+
+	@Override
+	public <R extends UpgradeRecipe> void addExtension(Class<? extends R> recipeClass, ISmithingCategoryExtension<R> extension) {
+		ErrorUtil.checkNotNull(recipeClass, "recipeClass");
+		ErrorUtil.checkNotNull(extension, "extension");
+		if (extensions.containsKey(recipeClass)) {
+			throw new IllegalArgumentException("An extension has already been registered for: " + recipeClass);
+		}
+		extensions.put(recipeClass, extension);
+	}
+
+	@Nullable
+	private <R extends UpgradeRecipe> ISmithingCategoryExtension<? super R> getExtension(UpgradeRecipe recipe) {
+		{
+			ISmithingCategoryExtension<?> extension = extensions.get(recipe.getClass());
+			if (extension != null) {
+				//noinspection unchecked
+				return (ISmithingCategoryExtension<? super R>) extension;
+			}
+		}
+		for (Map.Entry<Class<? extends UpgradeRecipe>, ISmithingCategoryExtension<?>> e : extensions.entrySet()) {
+			if (e.getKey().isInstance(recipe)) {
+				//noinspection unchecked
+				return (ISmithingCategoryExtension<? super R>) e.getValue();
+			}
+		}
+		return null;
 	}
 }