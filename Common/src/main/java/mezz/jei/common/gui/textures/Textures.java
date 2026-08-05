Mysticpasta1/JEI-Async

Show recipe previews on bookmark tooltips (#3582)

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -26,6 +26,7 @@ public class Textures {
 	private final DrawableNineSliceTexture ingredientListBackground;
 	private final DrawableNineSliceTexture bookmarkListBackground;
 	private final DrawableNineSliceTexture recipeBackground;
+	private final DrawableNineSliceTexture recipePreviewBackground;
 	private final DrawableNineSliceTexture searchBackground;
 
 	private final HighResolutionDrawable shapelessIcon;
@@ -63,6 +64,7 @@ public class Textures {
 		this.ingredientListBackground = createNineSliceGuiSprite("ingredient_list_background", 64, 64, 16, 16, 16, 16);
 		this.bookmarkListBackground = createNineSliceGuiSprite("bookmark_list_background", 64, 64, 16, 16, 16, 16);
 		this.recipeBackground = createNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
+		this.recipePreviewBackground = createNineSliceGuiSprite("recipe_preview_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = createNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
 		this.catalystTab = createNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeOptionsTab = createNineSliceGuiSprite("recipe_options_tab", 28, 28, 8, 9, 8, 8);
@@ -184,6 +186,10 @@ public class Textures {
 
 	public DrawableNineSliceTexture getRecipeBackground() {
 		return recipeBackground;
+	}
+
+	public DrawableNineSliceTexture getRecipePreviewBackground() {
+		return recipePreviewBackground;
 	}
 
 	public DrawableNineSliceTexture getSearchBackground() {

Gui/src/main/java/mezz/jei/gui/overlay/bookmarks/IngredientsTooltipComponent.java
@@ -1,0 +1,149 @@
+package mezz.jei.gui.overlay.bookmarks;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.IRecipeLayoutDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotView;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.ingredients.IIngredientHelper;
+import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.ingredients.subtypes.UidContext;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.common.Internal;
+import mezz.jei.common.util.MathUtil;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class IngredientsTooltipComponent implements ClientTooltipComponent {
+	private static final int MAX_INGREDIENTS_PER_ROW = 16;
+	private static final int INGREDIENT_SIZE = 18;
+	private final List<RenderElement<?>> ingredients;
+
+	public IngredientsTooltipComponent(IRecipeLayoutDrawable<?> layout) {
+		IIngredientManager ingredientManager = Internal.getJeiRuntime().getIngredientManager();
+		IRecipeSlotsView recipeSlotsView = layout.getRecipeSlotsView();
+		Map<String, SummaryElement<?>> summary = new HashMap<>();
+		recipeSlotsView.getSlotViews(RecipeIngredientRole.INPUT)
+			.stream()
+			.map(IRecipeSlotView::getDisplayedIngredient)
+			.filter(Optional::isPresent)
+			.<ITypedIngredient<?>>map(Optional::get)
+			.forEach(ingredient -> {
+				addToSummary(ingredient, ingredientManager, summary);
+			});
+
+		Comparator<SummaryElement<?>> comparator = Comparator.comparingLong(SummaryElement::getAmount);
+		this.ingredients = summary.values().stream()
+			.sorted(comparator.reversed())
+			.<RenderElement<?>>map(e -> RenderElement.create(e, ingredientManager))
+			.toList();
+	}
+
+	private static <T> void addToSummary(ITypedIngredient<T> typedIngredient, IIngredientManager ingredientManager, Map<String, SummaryElement<?>> summary) {
+		IIngredientType<T> type = typedIngredient.getType();
+		IIngredientHelper<T> helper = ingredientManager.getIngredientHelper(type);
+		T ingredient = typedIngredient.getIngredient();
+		long ingredientAmount = helper.getAmount(ingredient);
+		if (ingredientAmount == -1) {
+			return;
+		}
+		String uid = getUid(typedIngredient, ingredientManager);
+		summary.compute(uid, (k, v) -> {
+			if (v == null) {
+				return SummaryElement.create(typedIngredient, ingredientAmount);
+			}
+			long newAmount = v.getAmount() + ingredientAmount;
+			v.setAmount(newAmount);
+			return v;
+		});
+	}
+
+	private static <T> String getUid(ITypedIngredient<T> typedIngredient, IIngredientManager ingredientManager) {
+		IIngredientHelper<T> ingredientHelper = ingredientManager.getIngredientHelper(typedIngredient.getType());
+		return ingredientHelper.getUniqueId(typedIngredient.getIngredient(), UidContext.Recipe);
+	}
+
+	@Override
+	public int getHeight() {
+		return INGREDIENT_SIZE * MathUtil.divideCeil(ingredients.size(), MAX_INGREDIENTS_PER_ROW);
+	}
+
+	@Override
+	public int getWidth(Font font) {
+		return INGREDIENT_SIZE * Math.min(ingredients.size(), MAX_INGREDIENTS_PER_ROW);
+	}
+
+	@Override
+	public void renderImage(Font font, int x, int y, GuiGraphics guiGraphics) {
+		for (int i = 0; i < ingredients.size(); i++) {
+			int elementX = 1 + x + ((i % MAX_INGREDIENTS_PER_ROW) * INGREDIENT_SIZE);
+			int elementY = 1 + y + ((i / MAX_INGREDIENTS_PER_ROW) * INGREDIENT_SIZE);
+			RenderElement<?> renderElement = ingredients.get(i);
+			PoseStack pose = guiGraphics.pose();
+			pose.pushPose();
+			{
+				pose.translate(elementX, elementY, 0);
+				renderElement.render(guiGraphics);
+			}
+			pose.popPose();
+		}
+	}
+
+	private static class SummaryElement<T> {
+		public static <T> SummaryElement<T> create(ITypedIngredient<T> ingredient, long amount) {
+			return new SummaryElement<>(ingredient, amount);
+		}
+
+		private final ITypedIngredient<T> ingredient;
+		private long amount;
+
+		private SummaryElement(ITypedIngredient<T> ingredient, long amount) {
+			this.ingredient = ingredient;
+			this.amount = amount;
+		}
+
+		public ITypedIngredient<T> getIngredient() {
+			return ingredient;
+		}
+
+		public long getAmount() {
+			return amount;
+		}
+
+		public void setAmount(long amount) {
+			this.amount = amount;
+		}
+	}
+
+	private static class RenderElement<T> {
+		public static <T> RenderElement<T> create(SummaryElement<T> summaryElement, IIngredientManager ingredientManager) {
+			ITypedIngredient<T> typedIngredient = summaryElement.getIngredient();
+			IIngredientType<T> type = typedIngredient.getType();
+			IIngredientHelper<T> helper = ingredientManager.getIngredientHelper(type);
+			IIngredientRenderer<T> renderer = ingredientManager.getIngredientRenderer(type);
+			T ingredient = helper.copyWithAmount(typedIngredient.getIngredient(), summaryElement.getAmount());
+			return new RenderElement<>(renderer, ingredient);
+		}
+
+		private final IIngredientRenderer<T> renderer;
+		private final T ingredient;
+
+		private RenderElement(IIngredientRenderer<T> renderer, T ingredient) {
+			this.renderer = renderer;
+			this.ingredient = ingredient;
+		}
+
+		public void render(GuiGraphics guiGraphics) {
+			renderer.render(guiGraphics, ingredient);
+		}
+	}
+}

Gui/src/main/java/mezz/jei/gui/overlay/bookmarks/PreviewTooltipComponent.java
@@ -1,0 +1,80 @@
+package mezz.jei.gui.overlay.bookmarks;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.IRecipeLayoutDrawable;
+import mezz.jei.api.recipe.transfer.IRecipeTransferError;
+import mezz.jei.api.recipe.transfer.IRecipeTransferManager;
+import mezz.jei.common.Internal;
+import mezz.jei.common.transfer.RecipeTransferUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import org.jetbrains.annotations.Nullable;
+
+public class PreviewTooltipComponent<R> implements ClientTooltipComponent {
+	private static final int UPDATE_INTERVAL_MS = 2000;
+
+	private final IRecipeLayoutDrawable<R> drawable;
+	private @Nullable IRecipeTransferError transferError;
+	private long lastUpdateTime = 0;
+
+	public PreviewTooltipComponent(IRecipeLayoutDrawable<R> drawable) {
+		this.drawable = drawable;
+	}
+
+	@Override
+	public int getHeight() {
+		return drawable.getRect().getHeight() + 10;
+	}
+
+	@Override
+	public int getWidth(Font font) {
+		return drawable.getRect().getWidth() + 4;
+	}
+
+	@Override
+	public void renderImage(Font font, int x, int y, GuiGraphics guiGraphics) {
+		PoseStack pose = guiGraphics.pose();
+		pose.pushPose();
+		{
+			pose.translate(x + 2, y + 5, 0);
+			drawable.drawRecipe(guiGraphics, 0, 0);
+			updateTransferError();
+			if (transferError != null) {
+				Rect2i recipeRect = drawable.getRect();
+				transferError.showError(guiGraphics, x, y, drawable.getRecipeSlotsView(), recipeRect.getX(), recipeRect.getY());
+			}
+		}
+		pose.popPose();
+	}
+
+	private void updateTransferError() {
+		long currentTime = System.currentTimeMillis();
+		if (currentTime - lastUpdateTime < UPDATE_INTERVAL_MS) {
+			return;
+		}
+		lastUpdateTime = currentTime;
+
+		Minecraft minecraft = Minecraft.getInstance();
+		LocalPlayer player = minecraft.player;
+		if (player == null) {
+			transferError = null;
+			return;
+		}
+		Screen screen = Minecraft.getInstance().screen;
+		if (screen instanceof AbstractContainerScreen<?> containerScreen) {
+			AbstractContainerMenu container = containerScreen.getMenu();
+			IRecipeTransferManager recipeTransferManager = Internal.getJeiRuntime().getRecipeTransferManager();
+			transferError = RecipeTransferUtil.getTransferRecipeError(recipeTransferManager, container, drawable, player)
+				.orElse(null);
+		} else {
+			transferError = null;
+		}
+	}
+}

mezz/JustEnoughItems

(cherry picked from commit 3ffa31ba86003bb3da0514a39f29f60cebb93c6a)

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -26,6 +26,7 @@ public class Textures {
 	private final DrawableNineSliceTexture ingredientListBackground;
 	private final DrawableNineSliceTexture bookmarkListBackground;
 	private final DrawableNineSliceTexture recipeBackground;
+	private final DrawableNineSliceTexture recipePreviewBackground;
 	private final DrawableNineSliceTexture searchBackground;
 
 	private final HighResolutionDrawable shapelessIcon;
@@ -63,6 +64,7 @@ public class Textures {
 		this.ingredientListBackground = registerNineSliceGuiSprite("ingredient_list_background", 64, 64, 16, 16, 16, 16);
 		this.bookmarkListBackground = registerNineSliceGuiSprite("bookmark_list_background", 64, 64, 16, 16, 16, 16);
 		this.recipeBackground = registerNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
+		this.recipePreviewBackground = registerNineSliceGuiSprite("recipe_preview_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = registerNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
 		this.catalystTab = registerNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeOptionsTab = registerNineSliceGuiSprite("recipe_options_tab", 28, 28, 8, 9, 8, 8);
@@ -189,6 +191,10 @@ public class Textures {
 
 	public DrawableNineSliceTexture getRecipeBackground() {
 		return recipeBackground;
+	}
+
+	public DrawableNineSliceTexture getRecipePreviewBackground() {
+		return recipePreviewBackground;
 	}
 
 	public DrawableNineSliceTexture getSearchBackground() {

Gui/src/main/java/mezz/jei/gui/overlay/bookmarks/IngredientsTooltipComponent.java
@@ -1,0 +1,148 @@
+package mezz.jei.gui.overlay.bookmarks;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.IRecipeLayoutDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotView;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.ingredients.IIngredientHelper;
+import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.ingredients.subtypes.UidContext;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.common.Internal;
+import mezz.jei.common.util.MathUtil;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class IngredientsTooltipComponent implements ClientTooltipComponent {
+	private static final int MAX_INGREDIENTS_PER_ROW = 16;
+	private static final int INGREDIENT_SIZE = 18;
+	private final List<RenderElement<?>> ingredients;
+
+	public IngredientsTooltipComponent(IRecipeLayoutDrawable<?> layout) {
+		IIngredientManager ingredientManager = Internal.getJeiRuntime().getIngredientManager();
+		IRecipeSlotsView recipeSlotsView = layout.getRecipeSlotsView();
+		Map<String, SummaryElement<?>> summary = new HashMap<>();
+		recipeSlotsView.getSlotViews(RecipeIngredientRole.INPUT)
+			.stream()
+			.map(IRecipeSlotView::getDisplayedIngredient)
+			.filter(Optional::isPresent)
+			.<ITypedIngredient<?>>map(Optional::get)
+			.forEach(ingredient -> {
+				addToSummary(ingredient, ingredientManager, summary);
+			});
+
+		Comparator<SummaryElement<?>> comparator = Comparator.comparingLong(SummaryElement::getAmount);
+		this.ingredients = summary.values().stream()
+			.sorted(comparator.reversed())
+			.<RenderElement<?>>map(e -> RenderElement.create(e, ingredientManager))
+			.toList();
+	}
+
+	private static <T> void addToSummary(ITypedIngredient<T> typedIngredient, IIngredientManager ingredientManager, Map<String, SummaryElement<?>> summary) {
+		IIngredientType<T> type = typedIngredient.getType();
+		IIngredientHelper<T> helper = ingredientManager.getIngredientHelper(type);
+		T ingredient = typedIngredient.getIngredient();
+		long ingredientAmount = helper.getAmount(ingredient);
+		if (ingredientAmount == -1) {
+			return;
+		}
+		String uid = getUid(typedIngredient, ingredientManager);
+		summary.compute(uid, (k, v) -> {
+			if (v == null) {
+				return SummaryElement.create(typedIngredient, ingredientAmount);
+			}
+			long newAmount = v.getAmount() + ingredientAmount;
+			v.setAmount(newAmount);
+			return v;
+		});
+	}
+
+	private static <T> String getUid(ITypedIngredient<T> typedIngredient, IIngredientManager ingredientManager) {
+		IIngredientHelper<T> ingredientHelper = ingredientManager.getIngredientHelper(typedIngredient.getType());
+		return ingredientHelper.getUniqueId(typedIngredient.getIngredient(), UidContext.Recipe);
+	}
+
+	@Override
+	public int getHeight() {
+		return INGREDIENT_SIZE * MathUtil.divideCeil(ingredients.size(), MAX_INGREDIENTS_PER_ROW);
+	}
+
+	@Override
+	public int getWidth(Font font) {
+		return INGREDIENT_SIZE * Math.min(ingredients.size(), MAX_INGREDIENTS_PER_ROW);
+	}
+
+	@Override
+	public void renderImage(Font font, int x, int y, PoseStack poseStack, ItemRenderer itemRenderer, int z) {
+		for (int i = 0; i < ingredients.size(); i++) {
+			int elementX = 1 + x + ((i % MAX_INGREDIENTS_PER_ROW) * INGREDIENT_SIZE);
+			int elementY = 1 + y + ((i / MAX_INGREDIENTS_PER_ROW) * INGREDIENT_SIZE);
+			RenderElement<?> renderElement = ingredients.get(i);
+			poseStack.pushPose();
+			{
+				poseStack.translate(elementX, elementY, 0);
+				renderElement.render(poseStack);
+			}
+			poseStack.popPose();
+		}
+	}
+
+	private static class SummaryElement<T> {
+		public static <T> SummaryElement<T> create(ITypedIngredient<T> ingredient, long amount) {
+			return new SummaryElement<>(ingredient, amount);
+		}
+
+		private final ITypedIngredient<T> ingredient;
+		private long amount;
+
+		private SummaryElement(ITypedIngredient<T> ingredient, long amount) {
+			this.ingredient = ingredient;
+			this.amount = amount;
+		}
+
+		public ITypedIngredient<T> getIngredient() {
+			return ingredient;
+		}
+
+		public long getAmount() {
+			return amount;
+		}
+
+		public void setAmount(long amount) {
+			this.amount = amount;
+		}
+	}
+
+	private static class RenderElement<T> {
+		public static <T> RenderElement<T> create(SummaryElement<T> summaryElement, IIngredientManager ingredientManager) {
+			ITypedIngredient<T> typedIngredient = summaryElement.getIngredient();
+			IIngredientType<T> type = typedIngredient.getType();
+			IIngredientHelper<T> helper = ingredientManager.getIngredientHelper(type);
+			IIngredientRenderer<T> renderer = ingredientManager.getIngredientRenderer(type);
+			T ingredient = helper.copyWithAmount(typedIngredient.getIngredient(), summaryElement.getAmount());
+			return new RenderElement<>(renderer, ingredient);
+		}
+
+		private final IIngredientRenderer<T> renderer;
+		private final T ingredient;
+
+		private RenderElement(IIngredientRenderer<T> renderer, T ingredient) {
+			this.renderer = renderer;
+			this.ingredient = ingredient;
+		}
+
+		public void render(PoseStack poseStack) {
+			renderer.render(poseStack, ingredient);
+		}
+	}
+}

Gui/src/main/java/mezz/jei/gui/overlay/bookmarks/PreviewTooltipComponent.java
@@ -1,0 +1,79 @@
+package mezz.jei.gui.overlay.bookmarks;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.IRecipeLayoutDrawable;
+import mezz.jei.api.recipe.transfer.IRecipeTransferError;
+import mezz.jei.api.recipe.transfer.IRecipeTransferManager;
+import mezz.jei.common.Internal;
+import mezz.jei.common.transfer.RecipeTransferUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import org.jetbrains.annotations.Nullable;
+
+public class PreviewTooltipComponent<R> implements ClientTooltipComponent {
+	private static final int UPDATE_INTERVAL_MS = 2000;
+
+	private final IRecipeLayoutDrawable<R> drawable;
+	private @Nullable IRecipeTransferError transferError;
+	private long lastUpdateTime = 0;
+
+	public PreviewTooltipComponent(IRecipeLayoutDrawable<R> drawable) {
+		this.drawable = drawable;
+	}
+
+	@Override
+	public int getHeight() {
+		return drawable.getRect().getHeight() + 10;
+	}
+
+	@Override
+	public int getWidth(Font font) {
+		return drawable.getRect().getWidth() + 4;
+	}
+
+	@Override
+	public void renderImage(Font font, int x, int y, PoseStack poseStack, ItemRenderer itemRenderer, int z) {
+		poseStack.pushPose();
+		{
+			poseStack.translate(x + 2, y + 5, 0);
+			drawable.drawRecipe(poseStack, 0, 0);
+			updateTransferError();
+			if (transferError != null) {
+				Rect2i recipeRect = drawable.getRect();
+				transferError.showError(poseStack, x, y, drawable.getRecipeSlotsView(), recipeRect.getX(), recipeRect.getY());
+			}
+		}
+		poseStack.popPose();
+	}
+
+	private void updateTransferError() {
+		long currentTime = System.currentTimeMillis();
+		if (currentTime - lastUpdateTime < UPDATE_INTERVAL_MS) {
+			return;
+		}
+		lastUpdateTime = currentTime;
+
+		Minecraft minecraft = Minecraft.getInstance();
+		LocalPlayer player = minecraft.player;
+		if (player == null) {
+			transferError = null;
+			return;
+		}
+		Screen screen = Minecraft.getInstance().screen;
+		if (screen instanceof AbstractContainerScreen<?> containerScreen) {
+			AbstractContainerMenu container = containerScreen.getMenu();
+			IRecipeTransferManager recipeTransferManager = Internal.getJeiRuntime().getRecipeTransferManager();
+			transferError = RecipeTransferUtil.getTransferRecipeError(recipeTransferManager, container, drawable, player)
+				.orElse(null);
+		} else {
+			transferError = null;
+		}
+	}
+}