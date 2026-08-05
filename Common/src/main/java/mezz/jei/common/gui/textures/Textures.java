Mysticpasta1/JEI-Async

Support sorting recipes so that bookmarked and craftable recipes are shown first

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -20,6 +20,8 @@ public class Textures {
 	private final DrawableNineSliceTexture buttonDisabled;
 	private final DrawableNineSliceTexture buttonEnabled;
 	private final DrawableNineSliceTexture buttonHighlight;
+	private final DrawableNineSliceTexture buttonPressed;
+	private final DrawableNineSliceTexture buttonPressedHighlight;
 	private final DrawableNineSliceTexture recipeGuiBackground;
 	private final DrawableNineSliceTexture ingredientListBackground;
 	private final DrawableNineSliceTexture bookmarkListBackground;
@@ -37,8 +39,11 @@ public class Textures {
 	private final IDrawableStatic bookmarkButtonEnabledIcon;
 	private final IDrawableStatic infoIcon;
 	private final DrawableNineSliceTexture catalystTab;
+	private final DrawableNineSliceTexture recipeOptionsTab;
 	private final IDrawableStatic flameIcon;
 	private final IDrawableStatic recipeArrow;
+	private final IDrawableStatic bookmarksFirst;
+	private final IDrawableStatic craftableFirst;
 
 	public Textures(JeiSpriteUploader spriteUploader) {
 		this.spriteUploader = spriteUploader;
@@ -52,12 +57,15 @@ public class Textures {
 		this.buttonDisabled = createNineSliceGuiSprite("button_disabled", 20, 20, 6, 6, 6, 6);
 		this.buttonEnabled = createNineSliceGuiSprite("button_enabled", 20, 20, 6, 6, 6, 6);
 		this.buttonHighlight = createNineSliceGuiSprite("button_highlight", 20, 20, 6, 6, 6, 6);
+		this.buttonPressed = createNineSliceGuiSprite("button_pressed", 20, 20, 6, 6, 6, 6);
+		this.buttonPressedHighlight = createNineSliceGuiSprite("button_pressed_highlight", 20, 20, 6, 6, 6, 6);
 		this.recipeGuiBackground = createNineSliceGuiSprite("gui_background", 64, 64, 16, 16, 16, 16);
 		this.ingredientListBackground = createNineSliceGuiSprite("ingredient_list_background", 64, 64, 16, 16, 16, 16);
 		this.bookmarkListBackground = createNineSliceGuiSprite("bookmark_list_background", 64, 64, 16, 16, 16, 16);
 		this.recipeBackground = createNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = createNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
 		this.catalystTab = createNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
+		this.recipeOptionsTab = createNineSliceGuiSprite("recipe_options_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeArrow = createGuiSprite("recipe_arrow", 22, 15);
 
 		DrawableSprite rawShapelessIcon = createGuiSprite("icons/shapeless_icon", 36, 36)
@@ -76,6 +84,8 @@ public class Textures {
 		this.bookmarkButtonEnabledIcon = createGuiSprite("icons/bookmark_button_enabled", 16, 16);
 		this.infoIcon = createGuiSprite("icons/info", 16, 16);
 		this.flameIcon = createGuiSprite("icons/flame", 14, 14);
+		this.bookmarksFirst = createGuiSprite("icons/bookmarks_first", 16, 16);
+		this.craftableFirst = createGuiSprite("icons/craftable_first", 16, 16);
 	}
 
 	private ResourceLocation createSprite(String name) {
@@ -124,6 +134,14 @@ public class Textures {
 		return recipeBookmark;
 	}
 
+	public IDrawableStatic getBookmarksFirst() {
+		return bookmarksFirst;
+	}
+
+	public IDrawableStatic getCraftableFirst() {
+		return craftableFirst;
+	}
+
 	public IDrawableStatic getConfigButtonIcon() {
 		return configButtonIcon;
 	}
@@ -140,13 +158,15 @@ public class Textures {
 		return bookmarkButtonEnabledIcon;
 	}
 
-	public DrawableNineSliceTexture getButtonForState(boolean enabled, boolean hovered) {
+	public DrawableNineSliceTexture getButtonForState(boolean pressed, boolean enabled, boolean hovered) {
 		if (!enabled) {
 			return buttonDisabled;
-		} else if (hovered) {
-			return buttonHighlight;
+		}
+
+		if (hovered) {
+			return pressed ? buttonPressedHighlight : buttonHighlight;
 		} else {
-			return buttonEnabled;
+			return pressed ? buttonPressed : buttonEnabled;
 		}
 	}
 
@@ -176,6 +196,10 @@ public class Textures {
 
 	public DrawableNineSliceTexture getCatalystTab() {
 		return catalystTab;
+	}
+
+	public DrawableNineSliceTexture getRecipeOptionsTab() {
+		return recipeOptionsTab;
 	}
 
 	public IDrawableStatic getRecipeArrow() {

Gui/src/main/java/mezz/jei/gui/recipes/RecipeOptionButtons.java
@@ -1,0 +1,114 @@
+package mezz.jei.gui.recipes;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import mezz.jei.common.Internal;
+import mezz.jei.common.config.RecipeSorterStage;
+import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
+import mezz.jei.common.gui.textures.Textures;
+import mezz.jei.common.util.ImmutableRect2i;
+import mezz.jei.gui.elements.GuiIconToggleButton;
+import mezz.jei.gui.input.IUserInputHandler;
+import mezz.jei.gui.input.handlers.CombinedInputHandler;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+
+import javax.annotation.Nonnegative;
+import java.util.List;
+
+public class RecipeOptionButtons {
+	private static final int buttonSize = 16;
+	private static final int buttonBorderSize = 1;
+	private static final int borderSize = 5;
+	private static final int overlapSize = 6;
+
+	private final List<GuiIconToggleButton> buttons;
+
+	private final DrawableNineSliceTexture backgroundTab;
+	private ImmutableRect2i area;
+
+	public RecipeOptionButtons(Runnable onValueChanged) {
+		Textures textures = Internal.getTextures();
+		GuiIconToggleButton bookmarksFirstButton = new RecipeSortStateButton(
+			RecipeSorterStage.BOOKMARKED,
+			textures.getBookmarksFirst(),
+			textures.getBookmarksFirst(),
+			Component.translatable("jei.tooltip.recipe.sort.bookmarks.first.disabled"),
+			Component.translatable("jei.tooltip.recipe.sort.bookmarks.first.enabled"),
+			onValueChanged
+		);
+		GuiIconToggleButton craftableFirstButton = new RecipeSortStateButton(
+			RecipeSorterStage.CRAFTABLE,
+			textures.getCraftableFirst(),
+			textures.getCraftableFirst(),
+			Component.translatable("jei.tooltip.recipe.sort.craftable.first.disabled"),
+			Component.translatable("jei.tooltip.recipe.sort.craftable.first.enabled"),
+			onValueChanged
+		);
+
+		buttons = List.of(bookmarksFirstButton, craftableFirstButton);
+		backgroundTab = textures.getRecipeOptionsTab();
+	}
+
+	public void tick() {
+		for (GuiIconToggleButton button : buttons) {
+			button.tick();
+		}
+	}
+
+	public void updateLayout(ImmutableRect2i recipeArea) {
+		int width = (2 * buttonBorderSize) + (borderSize * 2) + buttonSize;
+		int height = (2 * buttonBorderSize) + (borderSize * 2) + (buttons.size() * buttonSize);
+		int y = recipeArea.getY() + recipeArea.getHeight() - height;
+		int x = recipeArea.getX() - width + overlapSize; // overlaps the recipe gui slightly
+
+		this.area = new ImmutableRect2i(
+			x,
+			y,
+			width,
+			height
+		);
+
+		final int buttonX = x + borderSize + buttonBorderSize;
+		for (int i = 0; i < buttons.size(); i++) {
+			GuiIconToggleButton button = buttons.get(i);
+			int buttonY = y + borderSize + (i * buttonSize) + buttonBorderSize;
+			button.updateBounds(new ImmutableRect2i(buttonX, buttonY, buttonSize, buttonSize));
+		}
+	}
+
+	public ImmutableRect2i getArea() {
+		return area;
+	}
+
+	public void draw(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+		RenderSystem.disableDepthTest();
+		{
+			backgroundTab.draw(guiGraphics, this.area);
+		}
+		RenderSystem.enableDepthTest();
+
+		for (GuiIconToggleButton button : buttons) {
+			button.draw(guiGraphics, mouseX, mouseY, partialTicks);
+		}
+	}
+
+	@Nonnegative
+	public int getWidth() {
+		return Math.max(0, area.getWidth() - overlapSize);
+	}
+
+	public IUserInputHandler createInputHandler() {
+		List<IUserInputHandler> handlers = buttons.stream()
+			.map(GuiIconToggleButton::createInputHandler)
+			.toList();
+		return new CombinedInputHandler(handlers);
+	}
+
+	public void drawTooltips(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+		for (GuiIconToggleButton button : buttons) {
+			button.drawTooltips(guiGraphics, mouseX, mouseY);
+		}
+	}
+}

mezz/JustEnoughItems

(cherry picked from commit ab9a6b926be6e1ba71143522e993c956a509e8d1)

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -20,6 +20,8 @@ public class Textures {
 	private final DrawableNineSliceTexture buttonDisabled;
 	private final DrawableNineSliceTexture buttonEnabled;
 	private final DrawableNineSliceTexture buttonHighlight;
+	private final DrawableNineSliceTexture buttonPressed;
+	private final DrawableNineSliceTexture buttonPressedHighlight;
 	private final DrawableNineSliceTexture recipeGuiBackground;
 	private final DrawableNineSliceTexture ingredientListBackground;
 	private final DrawableNineSliceTexture bookmarkListBackground;
@@ -37,8 +39,11 @@ public class Textures {
 	private final IDrawableStatic bookmarkButtonEnabledIcon;
 	private final IDrawableStatic infoIcon;
 	private final DrawableNineSliceTexture catalystTab;
+	private final DrawableNineSliceTexture recipeOptionsTab;
 	private final IDrawableStatic flameIcon;
 	private final IDrawableStatic recipeArrow;
+	private final IDrawableStatic bookmarksFirst;
+	private final IDrawableStatic craftableFirst;
 
 	public Textures(JeiSpriteUploader spriteUploader) {
 		this.spriteUploader = spriteUploader;
@@ -52,12 +57,15 @@ public class Textures {
 		this.buttonDisabled = registerNineSliceGuiSprite("button_disabled", 20, 20, 6, 6, 6, 6);
 		this.buttonEnabled = registerNineSliceGuiSprite("button_enabled", 20, 20, 6, 6, 6, 6);
 		this.buttonHighlight = registerNineSliceGuiSprite("button_highlight", 20, 20, 6, 6, 6, 6);
+		this.buttonPressed = registerNineSliceGuiSprite("button_pressed", 20, 20, 6, 6, 6, 6);
+		this.buttonPressedHighlight = registerNineSliceGuiSprite("button_pressed_highlight", 20, 20, 6, 6, 6, 6);
 		this.recipeGuiBackground = registerNineSliceGuiSprite("gui_background", 64, 64, 16, 16, 16, 16);
 		this.ingredientListBackground = registerNineSliceGuiSprite("ingredient_list_background", 64, 64, 16, 16, 16, 16);
 		this.bookmarkListBackground = registerNineSliceGuiSprite("bookmark_list_background", 64, 64, 16, 16, 16, 16);
 		this.recipeBackground = registerNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = registerNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
 		this.catalystTab = registerNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
+		this.recipeOptionsTab = registerNineSliceGuiSprite("recipe_options_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeArrow = registerGuiSprite("recipe_arrow", 22, 15);
 
 		DrawableSprite rawShapelessIcon = registerGuiSprite("icons/shapeless_icon", 36, 36)
@@ -76,6 +84,8 @@ public class Textures {
 		this.bookmarkButtonEnabledIcon = registerGuiSprite("icons/bookmark_button_enabled", 16, 16);
 		this.infoIcon = registerGuiSprite("icons/info", 16, 16);
 		this.flameIcon = registerGuiSprite("icons/flame", 14, 14);
+		this.bookmarksFirst = registerGuiSprite("icons/bookmarks_first", 16, 16);
+		this.craftableFirst = registerGuiSprite("icons/craftable_first", 16, 16);
 	}
 
 	private ResourceLocation registerSprite(String name) {
@@ -126,6 +136,14 @@ public class Textures {
 		return recipeBookmark;
 	}
 
+	public IDrawableStatic getBookmarksFirst() {
+		return bookmarksFirst;
+	}
+
+	public IDrawableStatic getCraftableFirst() {
+		return craftableFirst;
+	}
+
 	public IDrawableStatic getConfigButtonIcon() {
 		return configButtonIcon;
 	}
@@ -143,13 +161,18 @@ public class Textures {
 	}
 
 	public DrawableNineSliceTexture getButtonForState(boolean enabled, boolean hovered) {
+		return getButtonForState(false, enabled, hovered);
+	}
+
+	public DrawableNineSliceTexture getButtonForState(boolean pressed, boolean enabled, boolean hovered) {
 		if (!enabled) {
 			return buttonDisabled;
-		} else if (hovered) {
-			return buttonHighlight;
-		} else {
-			return buttonEnabled;
 		}
+
+		if (hovered) {
+			return pressed ? buttonPressedHighlight : buttonHighlight;
+		}
+		return pressed ? buttonPressed : buttonEnabled;
 	}
 
 	public DrawableNineSliceTexture getRecipeGuiBackground() {
@@ -178,6 +201,10 @@ public class Textures {
 
 	public DrawableNineSliceTexture getCatalystTab() {
 		return catalystTab;
+	}
+
+	public DrawableNineSliceTexture getRecipeOptionsTab() {
+		return recipeOptionsTab;
 	}
 
 	public IDrawableStatic getRecipeArrow() {

Gui/src/main/java/mezz/jei/gui/recipes/RecipeOptionButtons.java
@@ -1,0 +1,114 @@
+package mezz.jei.gui.recipes;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.common.Internal;
+import mezz.jei.common.config.RecipeSorterStage;
+import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
+import mezz.jei.common.gui.textures.Textures;
+import mezz.jei.common.util.ImmutableRect2i;
+import mezz.jei.gui.elements.GuiIconToggleButton;
+import mezz.jei.gui.input.IUserInputHandler;
+import mezz.jei.gui.input.handlers.CombinedInputHandler;
+import net.minecraft.network.chat.Component;
+
+import javax.annotation.Nonnegative;
+import java.util.List;
+
+public class RecipeOptionButtons {
+	private static final int buttonSize = 16;
+	private static final int buttonBorderSize = 1;
+	private static final int borderSize = 5;
+	private static final int overlapSize = 6;
+
+	private final List<GuiIconToggleButton> buttons;
+
+	private final DrawableNineSliceTexture backgroundTab;
+	private ImmutableRect2i area = ImmutableRect2i.EMPTY;
+
+	public RecipeOptionButtons(Runnable onValueChanged) {
+		Textures textures = Internal.getTextures();
+		GuiIconToggleButton bookmarksFirstButton = new RecipeSortStateButton(
+			RecipeSorterStage.BOOKMARKED,
+			textures.getBookmarksFirst(),
+			textures.getBookmarksFirst(),
+			Component.translatable("jei.tooltip.recipe.sort.bookmarks.first.disabled"),
+			Component.translatable("jei.tooltip.recipe.sort.bookmarks.first.enabled"),
+			onValueChanged
+		);
+		GuiIconToggleButton craftableFirstButton = new RecipeSortStateButton(
+			RecipeSorterStage.CRAFTABLE,
+			textures.getCraftableFirst(),
+			textures.getCraftableFirst(),
+			Component.translatable("jei.tooltip.recipe.sort.craftable.first.disabled"),
+			Component.translatable("jei.tooltip.recipe.sort.craftable.first.enabled"),
+			onValueChanged
+		);
+
+		buttons = List.of(bookmarksFirstButton, craftableFirstButton);
+		backgroundTab = textures.getRecipeOptionsTab();
+	}
+
+	public void tick() {
+		for (GuiIconToggleButton button : buttons) {
+			button.tick();
+		}
+	}
+
+	public void updateLayout(ImmutableRect2i recipeArea) {
+		int width = (2 * buttonBorderSize) + (borderSize * 2) + buttonSize;
+		int height = (2 * buttonBorderSize) + (borderSize * 2) + (buttons.size() * buttonSize);
+		int y = recipeArea.getY() + recipeArea.getHeight() - height;
+		int x = recipeArea.getX() - width + overlapSize; // overlaps the recipe gui slightly
+
+		this.area = new ImmutableRect2i(
+			x,
+			y,
+			width,
+			height
+		);
+
+		final int buttonX = x + borderSize + buttonBorderSize;
+		for (int i = 0; i < buttons.size(); i++) {
+			GuiIconToggleButton button = buttons.get(i);
+			int buttonY = y + borderSize + (i * buttonSize) + buttonBorderSize;
+			button.updateBounds(new ImmutableRect2i(buttonX, buttonY, buttonSize, buttonSize));
+		}
+	}
+
+	public ImmutableRect2i getArea() {
+		return area;
+	}
+
+	public void draw(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+		RenderSystem.disableDepthTest();
+		{
+			backgroundTab.draw(poseStack, this.area);
+		}
+		RenderSystem.enableDepthTest();
+
+		for (GuiIconToggleButton button : buttons) {
+			button.draw(poseStack, mouseX, mouseY, partialTicks);
+		}
+	}
+
+	@Nonnegative
+	public int getWidth() {
+		return Math.max(0, area.getWidth() - overlapSize);
+	}
+
+	public IUserInputHandler createInputHandler() {
+		List<IUserInputHandler> handlers = buttons.stream()
+			.map(GuiIconToggleButton::createInputHandler)
+			.toList();
+		return new CombinedInputHandler("RecipeOptionButtons", handlers);
+	}
+
+	public void drawTooltips(PoseStack poseStack, int mouseX, int mouseY) {
+		for (GuiIconToggleButton button : buttons) {
+			button.drawTooltips(poseStack, mouseX, mouseY);
+		}
+	}
+}