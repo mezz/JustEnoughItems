Mysticpasta1/JEI-Async

Improve smithing table recipe category assets

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -30,6 +30,7 @@ public class Textures {
 	private final IDrawableStatic arrowPrevious;
 	private final IDrawableStatic arrowNext;
 	private final IDrawableStatic recipeTransfer;
+	private final IDrawableStatic recipeBookmark;
 	private final IDrawableStatic configButtonIcon;
 	private final IDrawableStatic configButtonCheatIcon;
 	private final IDrawableStatic bookmarkButtonDisabledIcon;
@@ -37,6 +38,7 @@ public class Textures {
 	private final IDrawableStatic infoIcon;
 	private final DrawableNineSliceTexture catalystTab;
 	private final IDrawableStatic flameIcon;
+	private final IDrawableStatic recipeArrow;
 
 	public Textures(JeiSpriteUploader spriteUploader) {
 		this.spriteUploader = spriteUploader;
@@ -56,6 +58,7 @@ public class Textures {
 		this.recipeBackground = createNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = createNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
 		this.catalystTab = createNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
+		this.recipeArrow = createGuiSprite("recipe_arrow", 22, 15);
 
 		DrawableSprite rawShapelessIcon = createGuiSprite("icons/shapeless_icon", 36, 36)
 			.trim(1, 2, 1, 1);
@@ -66,6 +69,7 @@ public class Textures {
 		this.arrowNext = createGuiSprite("icons/arrow_next", 9, 9)
 			.trim(0, 0, 1, 1);
 		this.recipeTransfer = createGuiSprite("icons/recipe_transfer", 7, 7);
+		this.recipeBookmark = createGuiSprite("icons/recipe_bookmark", 9, 9);
 		this.configButtonIcon = createGuiSprite("icons/config_button", 16, 16);
 		this.configButtonCheatIcon = createGuiSprite("icons/config_button_cheat", 16, 16);
 		this.bookmarkButtonDisabledIcon = createGuiSprite("icons/bookmark_button_disabled", 16, 16);
@@ -114,6 +118,10 @@ public class Textures {
 
 	public IDrawableStatic getRecipeTransfer() {
 		return recipeTransfer;
+	}
+
+	public IDrawableStatic getRecipeBookmark() {
+		return recipeBookmark;
 	}
 
 	public IDrawableStatic getConfigButtonIcon() {
@@ -168,6 +176,10 @@ public class Textures {
 
 	public DrawableNineSliceTexture getCatalystTab() {
 		return catalystTab;
+	}
+
+	public IDrawableStatic getRecipeArrow() {
+		return recipeArrow;
 	}
 
 	public DrawableNineSliceTexture getRecipeCatalystSlotBackground() {

Library/src/main/java/mezz/jei/library/plugins/vanilla/anvil/SmithingRecipeCategory.java
@@ -3,15 +3,18 @@ package mezz.jei.library.plugins.vanilla.anvil;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
-import mezz.jei.common.Constants;
+import mezz.jei.common.Internal;
+import mezz.jei.common.gui.textures.Textures;
 import mezz.jei.common.platform.IPlatformRecipeHelper;
 import mezz.jei.common.platform.Services;
 import mezz.jei.library.util.RecipeUtil;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
@@ -21,10 +24,15 @@ import net.minecraft.world.level.block.Blocks;
 public class SmithingRecipeCategory implements IRecipeCategory<RecipeHolder<SmithingRecipe>> {
 	private final IDrawable background;
 	private final IDrawable icon;
+	private final IDrawable slot;
+	private final IDrawable recipeArrow;
 
 	public SmithingRecipeCategory(IGuiHelper guiHelper) {
-		background = guiHelper.createDrawable(Constants.RECIPE_GUI_VANILLA, 128, 0, 108, 18);
+		background = guiHelper.createBlankDrawable(108, 28);
+		slot = guiHelper.getSlotDrawable();
 		icon = guiHelper.createDrawableItemStack(new ItemStack(Blocks.SMITHING_TABLE));
+		Textures textures = Internal.getTextures();
+		recipeArrow = textures.getRecipeArrow();
 	}
 
 	@Override
@@ -53,17 +61,26 @@ public class SmithingRecipeCategory implements IRecipeCategory<RecipeHolder<Smit
 
 		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 1, 1)
-				.addIngredients(recipeHelper.getTemplate(recipe));
+		builder.addSlot(RecipeIngredientRole.INPUT, 1, 6)
+			.addIngredients(recipeHelper.getTemplate(recipe))
+			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 19, 1)
-			.addIngredients(recipeHelper.getBase(recipe));
+		builder.addSlot(RecipeIngredientRole.INPUT, 19, 6)
+			.addIngredients(recipeHelper.getBase(recipe))
+			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 37, 1)
-			.addIngredients(recipeHelper.getAddition(recipe));
+		builder.addSlot(RecipeIngredientRole.INPUT, 37, 6)
+			.addIngredients(recipeHelper.getAddition(recipe))
+			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.OUTPUT, 91, 1)
-			.addItemStack(RecipeUtil.getResultItem(recipe));
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 91, 6)
+			.addItemStack(RecipeUtil.getResultItem(recipe))
+			.setBackground(slot, -1, -1);
+	}
+
+	@Override
+	public void draw(RecipeHolder<SmithingRecipe> recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+		recipeArrow.draw(guiGraphics, 61, 7);
 	}
 
 	@Override

mezz/JustEnoughItems

(cherry picked from commit f6623d8cdbfc6433c1e67a12c49a76f8d5da4654)

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -37,6 +37,7 @@ public class Textures {
 	private final IDrawableStatic infoIcon;
 	private final DrawableNineSliceTexture catalystTab;
 	private final IDrawableStatic flameIcon;
+	private final IDrawableStatic recipeArrow;
 
 	public Textures(JeiSpriteUploader spriteUploader) {
 		this.spriteUploader = spriteUploader;
@@ -56,6 +57,7 @@ public class Textures {
 		this.recipeBackground = registerNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = registerNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
 		this.catalystTab = registerNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
+		this.recipeArrow = registerGuiSprite("recipe_arrow", 22, 15);
 
 		DrawableSprite rawShapelessIcon = registerGuiSprite("icons/shapeless_icon", 36, 36)
 			.trim(1, 2, 1, 1);
@@ -170,6 +172,10 @@ public class Textures {
 
 	public DrawableNineSliceTexture getCatalystTab() {
 		return catalystTab;
+	}
+
+	public IDrawableStatic getRecipeArrow() {
+		return recipeArrow;
 	}
 
 	public DrawableNineSliceTexture getRecipeCatalystSlotBackground() {

Library/src/main/java/mezz/jei/library/plugins/vanilla/anvil/SmithingRecipeCategory.java
@@ -1,29 +1,36 @@
 package mezz.jei.library.plugins.vanilla.anvil;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
-import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.RecipeType;
-import mezz.jei.common.Constants;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.common.Internal;
+import mezz.jei.common.gui.textures.Textures;
 import mezz.jei.common.platform.IPlatformRecipeHelper;
 import mezz.jei.common.platform.Services;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.UpgradeRecipe;
-
-import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.Blocks;
 
 public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe> {
 	private final IDrawable background;
 	private final IDrawable icon;
+	private final IDrawable slot;
+	private final IDrawable recipeArrow;
 
 	public SmithingRecipeCategory(IGuiHelper guiHelper) {
-		background = guiHelper.createDrawable(Constants.RECIPE_GUI_VANILLA, 0, 168, 125, 18);
+		background = guiHelper.createBlankDrawable(125, 28);
+		slot = guiHelper.getSlotDrawable();
 		icon = guiHelper.createDrawableItemStack(new ItemStack(Blocks.SMITHING_TABLE));
+		Textures textures = Internal.getTextures();
+		recipeArrow = textures.getRecipeArrow();
 	}
 
 	@Override
@@ -50,14 +57,22 @@ public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe> {
 	public void setRecipe(IRecipeLayoutBuilder builder, UpgradeRecipe recipe, IFocusGroup focuses) {
 		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 1, 1)
-			.addIngredients(recipeHelper.getBase(recipe));
+		builder.addSlot(RecipeIngredientRole.INPUT, 1, 6)
+			.addIngredients(recipeHelper.getBase(recipe))
+			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 50, 1)
-			.addIngredients(recipeHelper.getAddition(recipe));
+		builder.addSlot(RecipeIngredientRole.INPUT, 50, 6)
+			.addIngredients(recipeHelper.getAddition(recipe))
+			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.OUTPUT, 108, 1)
-			.addItemStack(recipe.getResultItem());
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 108, 6)
+			.addItemStack(recipe.getResultItem())
+			.setBackground(slot, -1, -1);
+	}
+
+	@Override
+	public void draw(UpgradeRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+		recipeArrow.draw(stack, 79, 7);
 	}
 
 	@Override
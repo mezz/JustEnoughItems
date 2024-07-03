c8fd715c976b78c1d1f39d97ec695cf22b59368f
mezz <tehgeek@gmail.com>
2024-07-03 17:27:51 +0900

    Improve smithing table recipe category assets
    (cherry picked from commit f6623d8cdbfc6433c1e67a12c49a76f8d5da4654)

--- Library/src/main/java/mezz/jei/library/plugins/vanilla/anvil/SmithingRecipeCategory.java
+++ Library/src/main/java/mezz/jei/library/plugins/vanilla/anvil/SmithingRecipeCategory.java
@@ -1,29 +1,36 @@
 package mezz.jei.library.plugins.vanilla.anvil;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
-import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.RecipeType;
-import mezz.jei.common.Constants;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.common.Internal;
+import mezz.jei.common.gui.textures.Textures;
 import mezz.jei.common.platform.IPlatformRecipeHelper;
 import mezz.jei.common.platform.Services;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.UpgradeRecipe;
-
-import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.Blocks;
 
 public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe> {
 	private final IDrawable background;
 	private final IDrawable icon;
+	private final IDrawable slot;
+	private final IDrawable recipeArrow;
 
 	public SmithingRecipeCategory(IGuiHelper guiHelper) {
-		background = guiHelper.createDrawable(Constants.RECIPE_GUI_VANILLA, 0, 168, 125, 18);
+		background = guiHelper.createBlankDrawable(125, 28);
+		slot = guiHelper.getSlotDrawable();
 		icon = guiHelper.createDrawableItemStack(new ItemStack(Blocks.SMITHING_TABLE));
+		Textures textures = Internal.getTextures();
+		recipeArrow = textures.getRecipeArrow();
 	}
 
 	@Override
@@ -50,14 +57,22 @@ public class SmithingRecipeCategory implements IRecipeCategory<UpgradeRecipe> {
 	public void setRecipe(IRecipeLayoutBuilder builder, UpgradeRecipe recipe, IFocusGroup focuses) {
 		IPlatformRecipeHelper recipeHelper = Services.PLATFORM.getRecipeHelper();
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 1, 1)
-			.addIngredients(recipeHelper.getBase(recipe));
+		builder.addSlot(RecipeIngredientRole.INPUT, 1, 6)
+			.addIngredients(recipeHelper.getBase(recipe))
+			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.INPUT, 50, 1)
-			.addIngredients(recipeHelper.getAddition(recipe));
+		builder.addSlot(RecipeIngredientRole.INPUT, 50, 6)
+			.addIngredients(recipeHelper.getAddition(recipe))
+			.setBackground(slot, -1, -1);
 
-		builder.addSlot(RecipeIngredientRole.OUTPUT, 108, 1)
-			.addItemStack(recipe.getResultItem());
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 108, 6)
+			.addItemStack(recipe.getResultItem())
+			.setBackground(slot, -1, -1);
+	}
+
+	@Override
+	public void draw(UpgradeRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+		recipeArrow.draw(stack, 79, 7);
 	}
 
 	@Override
