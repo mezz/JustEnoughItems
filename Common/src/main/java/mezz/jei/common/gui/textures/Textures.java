Mysticpasta1/JEI-Async

Add support for scrollable and draggable widgets in recipe layouts (#3631)

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -28,6 +28,8 @@ public class Textures {
 	private final DrawableNineSliceTexture recipeBackground;
 	private final DrawableNineSliceTexture recipePreviewBackground;
 	private final DrawableNineSliceTexture searchBackground;
+	private final DrawableNineSliceTexture scrollbarBackground;
+	private final DrawableNineSliceTexture scrollbarMarker;
 
 	private final HighResolutionDrawable shapelessIcon;
 	private final IDrawableStatic arrowPrevious;
@@ -66,6 +68,8 @@ public class Textures {
 		this.recipeBackground = createNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
 		this.recipePreviewBackground = createNineSliceGuiSprite("recipe_preview_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = createNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
+		this.scrollbarBackground = createNineSliceGuiSprite("scrollbar_background", 14, 50, 6, 6, 6, 6);
+		this.scrollbarMarker = createNineSliceGuiSprite("scrollbar_marker", 12, 15, 2, 2, 2, 1);
 		this.catalystTab = createNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeOptionsTab = createNineSliceGuiSprite("recipe_options_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeArrow = createGuiSprite("recipe_arrow", 22, 15);
@@ -226,6 +230,14 @@ public class Textures {
 
 	public IDrawableStatic getFlameIcon() {
 		return flameIcon;
+	}
+
+	public DrawableNineSliceTexture getScrollbarMarker() {
+		return scrollbarMarker;
+	}
+
+	public DrawableNineSliceTexture getScrollbarBackground() {
+		return scrollbarBackground;
 	}
 
 	public JeiSpriteUploader getSpriteUploader() {

Library/src/main/java/mezz/jei/library/gui/recipes/RecipeLayout.java
@@ -9,6 +9,8 @@ import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.gui.inputs.IJeiGuiEventListener;
 import mezz.jei.api.gui.inputs.IJeiInputHandler;
 import mezz.jei.api.gui.inputs.RecipeSlotUnderMouse;
+import mezz.jei.api.gui.widgets.IRecipeWidget;
+import mezz.jei.api.gui.widgets.ISlottedRecipeWidget;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.category.IRecipeCategory;
@@ -29,8 +31,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -44,6 +48,12 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 	 * Slots handled by the recipe category directly.
 	 */
 	private final List<IRecipeSlotDrawable> recipeCategorySlots;
+	/**
+	 * All slots, including slots handled by the recipe category and widgets.
+	 */
+	private final List<IRecipeSlotDrawable> allSlots;
+	private final List<ISlottedRecipeWidget> slottedWidgets;
+	private final List<IRecipeWidget> allWidgets;
 	private final R recipe;
 	private final IScalableDrawable recipeBackground;
 	private final int recipeBorderPadding;
@@ -109,14 +119,23 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		@Nullable ShapelessIcon shapelessIcon,
 		ImmutablePoint2i recipeTransferButtonPos,
 		List<IRecipeSlotDrawable> recipeCategorySlots,
+		List<IRecipeSlotDrawable> allSlots,
+		List<ISlottedRecipeWidget> slottedWidgets,
+		List<IRecipeWidget> widgets,
 		List<IJeiInputHandler> inputHandlers,
 		List<IJeiGuiEventListener> guiEventListeners
 	) {
 		this.recipeCategory = recipeCategory;
 		this.recipeCategoryDecorators = recipeCategoryDecorators;
+		this.slottedWidgets = Collections.unmodifiableList(slottedWidgets);
 		this.inputHandler = new RecipeLayoutInputHandler<>(this, inputHandlers, guiEventListeners);
 
+		Set<IRecipeWidget> allWidgets = new HashSet<>(widgets);
+		allWidgets.addAll(slottedWidgets);
+		this.allWidgets = List.copyOf(allWidgets);
+
 		this.recipeCategorySlots = recipeCategorySlots;
+		this.allSlots = Collections.unmodifiableList(allSlots);
 		this.recipeBorderPadding = recipeBorderPadding;
 		this.area = new ImmutableRect2i(
 			0,
@@ -167,6 +186,15 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 					slot.draw(guiGraphics);
 				}
 				recipeCategory.draw(recipe, recipeCategorySlotsView, guiGraphics, recipeMouseX, recipeMouseY);
+				for (IRecipeWidget widget : allWidgets) {
+					ScreenPosition position = widget.getPosition();
+					poseStack.pushPose();
+					{
+						poseStack.translate(position.x(), position.y(), 0);
+						widget.draw(guiGraphics, recipeMouseX, recipeMouseY);
+					}
+					poseStack.popPose();
+				}
 
 				// drawExtras and drawInfo often render text which messes with the color, this clears it
 				RenderSystem.setShaderColor(1, 1, 1, 1);
@@ -270,6 +298,16 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		final double recipeMouseX = mouseX - area.getX();
 		final double recipeMouseY = mouseY - area.getY();
 
+		for (ISlottedRecipeWidget widget : slottedWidgets) {
+			ScreenPosition position = widget.getPosition();
+			double relativeMouseX = recipeMouseX - position.x();
+			double relativeMouseY = recipeMouseY - position.y();
+			Optional<RecipeSlotUnderMouse> slotResult = widget.getSlotUnderMouse(relativeMouseX, relativeMouseY);
+			if (slotResult.isPresent()) {
+				return slotResult
+					.map(slot -> slot.addOffset(area.x(), area.y()));
+			}
+		}
 		for (IRecipeSlotDrawable slot : recipeCategorySlots) {
 			if (slot.isMouseOver(recipeMouseX, recipeMouseY)) {
 				return Optional.of(new RecipeSlotUnderMouse(slot, area.getScreenPosition()));
@@ -295,9 +333,10 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		return area;
 	}
 
+	@SuppressWarnings("RedundantUnmodifiable")
 	@Override
 	public IRecipeSlotsView getRecipeSlotsView() {
-		return () -> Collections.unmodifiableList(recipeCategorySlots);
+		return () -> Collections.unmodifiableList(allSlots);
 	}
 
 	@Override
@@ -308,5 +347,12 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 	@Override
 	public IJeiInputHandler getInputHandler() {
 		return inputHandler;
+	}
+
+	@Override
+	public void tick() {
+		for (IRecipeWidget widget : allWidgets) {
+			widget.tick();
+		}
 	}
 }

Library/src/main/java/mezz/jei/library/plugins/vanilla/cooking/AbstractCookingCategory.java
@@ -1,19 +1,19 @@
 package mezz.jei.library.plugins.vanilla.cooking;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import net.minecraft.client.gui.GuiGraphics;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.widgets.IRecipeWidget;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.common.Constants;
 import mezz.jei.library.util.RecipeUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -26,10 +26,10 @@ import static mezz.jei.api.recipe.RecipeIngredientRole.OUTPUT;
 
 public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> extends FurnaceVariantCategory<RecipeHolder<T>> {
 	private final IDrawable background;
-	private final int regularCookTime;
 	private final IDrawable icon;
 	private final Component localizedName;
-	private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+	protected final IGuiHelper guiHelper;
+	protected final int regularCookTime;
 
 	public AbstractCookingCategory(IGuiHelper guiHelper, Block icon, String translationKey, int regularCookTime) {
 		super(guiHelper);
@@ -37,24 +37,7 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 		this.regularCookTime = regularCookTime;
 		this.icon = guiHelper.createDrawableItemStack(new ItemStack(icon));
 		this.localizedName = Component.translatable(translationKey);
-		this.cachedArrows = CacheBuilder.newBuilder()
-			.maximumSize(25)
-			.build(new CacheLoader<>() {
-				@Override
-				public IDrawableAnimated load(Integer cookTime) {
-					return guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
-						.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
-				}
-			});
-	}
-
-	protected IDrawableAnimated getArrow(RecipeHolder<T> recipeHolder) {
-		T recipe = recipeHolder.value();
-		int cookTime = recipe.getCookingTime();
-		if (cookTime <= 0) {
-			cookTime = regularCookTime;
-		}
-		return this.cachedArrows.getUnchecked(cookTime);
+		this.guiHelper = guiHelper;
 	}
 
 	@Override
@@ -70,9 +53,6 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	@Override
 	public void draw(RecipeHolder<T> recipeHolder, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
 		animatedFlame.draw(guiGraphics, 1, 20);
-
-		IDrawableAnimated arrow = getArrow(recipeHolder);
-		arrow.draw(guiGraphics, 24, 18);
 
 		drawExperience(recipeHolder, guiGraphics, 0);
 		drawCookTime(recipeHolder, guiGraphics, 45);
@@ -120,6 +100,11 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	}
 
 	@Override
+	public void createRecipeExtras(IRecipeExtrasBuilder acceptor, RecipeHolder<T> recipeHolder, IFocusGroup focuses) {
+		acceptor.addWidget(createCookingArrowWidget(recipeHolder, new ScreenPosition(24, 18)));
+	}
+
+	@Override
 	public boolean isHandled(RecipeHolder<T> recipeHolder) {
 		T recipe = recipeHolder.value();
 		return !recipe.isSpecial();
@@ -128,5 +113,35 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	@Override
 	public ResourceLocation getRegistryName(RecipeHolder<T> recipe) {
 		return recipe.id();
+	}
+
+	protected IRecipeWidget createCookingArrowWidget(RecipeHolder<T> recipeHolder, ScreenPosition position) {
+		return new CookingArrowRecipeWidget<>(guiHelper, recipeHolder, regularCookTime, position);
+	}
+
+	private static class CookingArrowRecipeWidget<T extends AbstractCookingRecipe> implements IRecipeWidget {
+		private final IDrawableAnimated arrow;
+		private final ScreenPosition position;
+
+		public CookingArrowRecipeWidget(IGuiHelper guiHelper, RecipeHolder<T> recipeHolder, int regularCookTime, ScreenPosition position) {
+			T recipe = recipeHolder.value();
+			int cookTime = recipe.getCookingTime();
+			if (cookTime <= 0) {
+				cookTime = regularCookTime;
+			}
+			this.arrow = guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
+				.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
+			this.position = position;
+		}
+
+		@Override
+		public ScreenPosition getPosition() {
+			return position;
+		}
+
+		@Override
+		public void draw(GuiGraphics guiGraphics, double mouseX, double mouseY) {
+			arrow.draw(guiGraphics);
+		}
 	}
 }

mezz/JustEnoughItems

(cherry picked from commit 21f1598bc4903c4e4474a5c785f503ff9c0c21cc)

Common/src/main/java/mezz/jei/common/gui/textures/Textures.java
@@ -28,6 +28,8 @@ public class Textures {
 	private final DrawableNineSliceTexture recipeBackground;
 	private final DrawableNineSliceTexture recipePreviewBackground;
 	private final DrawableNineSliceTexture searchBackground;
+	private final DrawableNineSliceTexture scrollbarBackground;
+	private final DrawableNineSliceTexture scrollbarMarker;
 
 	private final HighResolutionDrawable shapelessIcon;
 	private final IDrawableStatic arrowPrevious;
@@ -66,6 +68,8 @@ public class Textures {
 		this.recipeBackground = registerNineSliceGuiSprite("single_recipe_background", 64, 64, 16, 16, 16, 16);
 		this.recipePreviewBackground = registerNineSliceGuiSprite("recipe_preview_background", 64, 64, 16, 16, 16, 16);
 		this.searchBackground = registerNineSliceGuiSprite("search_background", 20, 20, 6, 6, 6, 6);
+		this.scrollbarBackground = registerNineSliceGuiSprite("scrollbar_background", 14, 50, 6, 6, 6, 6);
+		this.scrollbarMarker = registerNineSliceGuiSprite("scrollbar_marker", 12, 15, 2, 2, 2, 1);
 		this.catalystTab = registerNineSliceGuiSprite("catalyst_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeOptionsTab = registerNineSliceGuiSprite("recipe_options_tab", 28, 28, 8, 9, 8, 8);
 		this.recipeArrow = registerGuiSprite("recipe_arrow", 22, 15);
@@ -231,5 +235,17 @@ public class Textures {
 
 	public IDrawableStatic getFlameIcon() {
 		return flameIcon;
+	}
+
+	public DrawableNineSliceTexture getScrollbarMarker() {
+		return scrollbarMarker;
+	}
+
+	public DrawableNineSliceTexture getScrollbarBackground() {
+		return scrollbarBackground;
+	}
+
+	public JeiSpriteUploader getSpriteUploader() {
+		return spriteUploader;
 	}
 }

Library/src/main/java/mezz/jei/library/gui/recipes/RecipeLayout.java
@@ -10,6 +10,8 @@ import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.gui.inputs.IJeiGuiEventListener;
 import mezz.jei.api.gui.inputs.IJeiInputHandler;
 import mezz.jei.api.gui.inputs.RecipeSlotUnderMouse;
+import mezz.jei.api.gui.widgets.IRecipeWidget;
+import mezz.jei.api.gui.widgets.ISlottedRecipeWidget;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.category.IRecipeCategory;
@@ -33,8 +35,10 @@ import org.jetbrains.annotations.Nullable;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -49,6 +53,12 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 	 * Slots handled by the recipe category directly.
 	 */
 	private final List<IRecipeSlotDrawable> recipeCategorySlots;
+	/**
+	 * All slots, including slots handled by the recipe category and widgets.
+	 */
+	private final List<IRecipeSlotDrawable> allSlots;
+	private final List<ISlottedRecipeWidget> slottedWidgets;
+	private final List<IRecipeWidget> allWidgets;
 	private final R recipe;
 	private final IScalableDrawable recipeBackground;
 	private final int recipeBorderPadding;
@@ -115,16 +125,25 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		@Nullable ShapelessIcon shapelessIcon,
 		ImmutablePoint2i recipeTransferButtonPos,
 		List<IRecipeSlotDrawable> recipeCategorySlots,
+		List<IRecipeSlotDrawable> allSlots,
+		List<ISlottedRecipeWidget> slottedWidgets,
+		List<IRecipeWidget> widgets,
 		List<IJeiInputHandler> inputHandlers,
 		List<IJeiGuiEventListener> guiEventListeners,
 		CycleTicker cycleTicker
 	) {
 		this.recipeCategory = recipeCategory;
 		this.recipeCategoryDecorators = recipeCategoryDecorators;
+		this.slottedWidgets = Collections.unmodifiableList(slottedWidgets);
 		this.inputHandler = new RecipeLayoutInputHandler<>(this, inputHandlers, guiEventListeners);
 		this.cycleTicker = cycleTicker;
 
+		Set<IRecipeWidget> allWidgets = new HashSet<>(widgets);
+		allWidgets.addAll(slottedWidgets);
+		this.allWidgets = List.copyOf(allWidgets);
+
 		this.recipeCategorySlots = recipeCategorySlots;
+		this.allSlots = Collections.unmodifiableList(allSlots);
 		this.recipeBorderPadding = recipeBorderPadding;
 		this.area = new ImmutableRect2i(
 			0,
@@ -161,7 +180,6 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		final double recipeMouseY = mouseY - area.getY();
 
 		IRecipeSlotsView recipeCategorySlotsView = () -> Collections.unmodifiableList(recipeCategorySlots);
-		RecipeSlotUnderMouse hoveredSlotResult = getSlotUnderMouse(mouseX, mouseY).orElse(null);
 
 		poseStack.pushPose();
 		{
@@ -173,8 +191,16 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 			{
 				recipeCategory.draw(recipe, recipeCategorySlotsView, poseStack, recipeMouseX, recipeMouseY);
 				for (IRecipeSlotDrawable slot : recipeCategorySlots) {
-					boolean hovered = hoveredSlotResult != null && hoveredSlotResult.slot() == slot;
-					slot.draw(poseStack, hovered);
+					slot.draw(poseStack, false);
+				}
+				for (IRecipeWidget widget : allWidgets) {
+					Rect2i widgetArea = widget.getArea();
+					poseStack.pushPose();
+					{
+						poseStack.translate(widgetArea.getX(), widgetArea.getY(), 0);
+						widget.draw(poseStack, recipeMouseX, recipeMouseY);
+					}
+					poseStack.popPose();
 				}
 
 				// drawExtras and drawInfo often render text which messes with the color, this clears it
@@ -216,8 +242,7 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		RecipeSlotUnderMouse hoveredSlotResult = getSlotUnderMouse(mouseX, mouseY).orElse(null);
 
 		if (hoveredSlotResult != null) {
-			IRecipeSlotDrawable hoveredSlot = hoveredSlotResult.slot();
-			drawSlotTooltip(poseStack, mouseX, mouseY, hoveredSlot);
+			drawSlotTooltip(poseStack, mouseX, mouseY, hoveredSlotResult);
 		} else if (isMouseOver(mouseX, mouseY)) {
 			JeiTooltip tooltip = new JeiTooltip();
 			try {
@@ -242,7 +267,17 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		}
 	}
 
-	private void drawSlotTooltip(PoseStack poseStack, int mouseX, int mouseY, IRecipeSlotDrawable hoveredSlot) {
+	@SuppressWarnings("removal")
+	private void drawSlotTooltip(PoseStack poseStack, int mouseX, int mouseY, RecipeSlotUnderMouse hoveredSlotResult) {
+		IRecipeSlotDrawable hoveredSlot = hoveredSlotResult.slot();
+
+		poseStack.pushPose();
+		{
+			poseStack.translate(hoveredSlotResult.x(), hoveredSlotResult.y(), 0);
+			hoveredSlot.drawHoverOverlays(poseStack);
+		}
+		poseStack.popPose();
+
 		JeiTooltip tooltip = new JeiTooltip();
 		hoveredSlot.getTooltip(tooltip);
 		tooltip.draw(poseStack, mouseX, mouseY);
@@ -281,6 +316,16 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		final double recipeMouseX = mouseX - area.getX();
 		final double recipeMouseY = mouseY - area.getY();
 
+		for (ISlottedRecipeWidget widget : slottedWidgets) {
+			Rect2i widgetArea = widget.getArea();
+			double relativeMouseX = recipeMouseX - widgetArea.getX();
+			double relativeMouseY = recipeMouseY - widgetArea.getY();
+			Optional<RecipeSlotUnderMouse> slotResult = widget.getSlotUnderMouse(relativeMouseX, relativeMouseY);
+			if (slotResult.isPresent()) {
+				return slotResult
+					.map(slot -> slot.addOffset(area.getX(), area.getY()));
+			}
+		}
 		for (IRecipeSlotDrawable slot : recipeCategorySlots) {
 			if (slot.isMouseOver(recipeMouseX, recipeMouseY)) {
 				return Optional.of(new RecipeSlotUnderMouse(slot, area.getX(), area.getY()));
@@ -306,9 +351,10 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 		return area;
 	}
 
+	@SuppressWarnings("RedundantUnmodifiable")
 	@Override
 	public IRecipeSlotsView getRecipeSlotsView() {
-		return () -> Collections.unmodifiableList(recipeCategorySlots);
+		return () -> Collections.unmodifiableList(allSlots);
 	}
 
 	@Override
@@ -324,5 +370,8 @@ public class RecipeLayout<R> implements IRecipeLayoutDrawable<R> {
 	@Override
 	public void tick() {
 		cycleTicker.tick();
+		for (IRecipeWidget widget : allWidgets) {
+			widget.tick();
+		}
 	}
 }

Library/src/main/java/mezz/jei/library/plugins/vanilla/cooking/AbstractCookingCategory.java
@@ -1,18 +1,18 @@
 package mezz.jei.library.plugins.vanilla.cooking;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.widgets.IRecipeWidget;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.common.Constants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -24,10 +24,10 @@ import static mezz.jei.api.recipe.RecipeIngredientRole.OUTPUT;
 
 public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> extends FurnaceVariantCategory<T> {
 	private final IDrawable background;
-	private final int regularCookTime;
 	private final IDrawable icon;
 	private final Component localizedName;
-	private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+	protected final IGuiHelper guiHelper;
+	protected final int regularCookTime;
 
 	public AbstractCookingCategory(IGuiHelper guiHelper, Block icon, String translationKey, int regularCookTime) {
 		super(guiHelper);
@@ -35,23 +35,7 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 		this.regularCookTime = regularCookTime;
 		this.icon = guiHelper.createDrawableItemStack(new ItemStack(icon));
 		this.localizedName = Component.translatable(translationKey);
-		this.cachedArrows = CacheBuilder.newBuilder()
-			.maximumSize(25)
-			.build(new CacheLoader<>() {
-				@Override
-				public IDrawableAnimated load(Integer cookTime) {
-					return guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
-						.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
-				}
-			});
-	}
-
-	protected IDrawableAnimated getArrow(T recipe) {
-		int cookTime = recipe.getCookingTime();
-		if (cookTime <= 0) {
-			cookTime = regularCookTime;
-		}
-		return this.cachedArrows.getUnchecked(cookTime);
+		this.guiHelper = guiHelper;
 	}
 
 	@Override
@@ -67,9 +51,6 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	@Override
 	public void draw(T recipe, IRecipeSlotsView recipeSlotsView, PoseStack poseStack, double mouseX, double mouseY) {
 		animatedFlame.draw(poseStack, 1, 20);
-
-		IDrawableAnimated arrow = getArrow(recipe);
-		arrow.draw(poseStack, 24, 18);
 
 		drawExperience(recipe, poseStack, 0);
 		drawCookTime(recipe, poseStack, 45);
@@ -118,7 +99,41 @@ public abstract class AbstractCookingCategory<T extends AbstractCookingRecipe> e
 	}
 
 	@Override
+	public void createRecipeExtras(IRecipeExtrasBuilder acceptor, T recipe, IFocusGroup focuses) {
+		acceptor.addWidget(createCookingArrowWidget(recipe, 24, 18));
+	}
+
+	@Override
 	public ResourceLocation getRegistryName(T recipe) {
 		return recipe.getId();
+	}
+
+	protected IRecipeWidget createCookingArrowWidget(T recipe, int x, int y) {
+		return new CookingArrowRecipeWidget<>(guiHelper, recipe, regularCookTime, x, y);
+	}
+
+	private static class CookingArrowRecipeWidget<T extends AbstractCookingRecipe> implements IRecipeWidget {
+		private final IDrawableAnimated arrow;
+		private final Rect2i area;
+
+		public CookingArrowRecipeWidget(IGuiHelper guiHelper, T recipe, int regularCookTime, int x, int y) {
+			int cookTime = recipe.getCookingTime();
+			if (cookTime <= 0) {
+				cookTime = regularCookTime;
+			}
+			this.arrow = guiHelper.drawableBuilder(Constants.RECIPE_GUI_VANILLA, 82, 128, 24, 17)
+				.buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
+			this.area = new Rect2i(x, y, arrow.getWidth(), arrow.getHeight());
+		}
+
+		@Override
+		public Rect2i getArea() {
+			return area;
+		}
+
+		@Override
+		public void draw(PoseStack poseStack, double mouseX, double mouseY) {
+			arrow.draw(poseStack);
+		}
 	}
 }