Mysticpasta1/JEI-Async

Fix #3145 JEI fails to load on Forge server

Forge/src/main/java/mezz/jei/forge/JustEnoughItemsClientSafeRunner.java
@@ -1,0 +1,26 @@
+package mezz.jei.forge;
+
+import mezz.jei.common.config.IServerConfig;
+import mezz.jei.forge.events.PermanentEventSubscriptions;
+import mezz.jei.forge.network.NetworkHandler;
+
+public class JustEnoughItemsClientSafeRunner {
+	private final NetworkHandler networkHandler;
+	private final PermanentEventSubscriptions subscriptions;
+	private final IServerConfig serverConfig;
+
+	public JustEnoughItemsClientSafeRunner(
+		NetworkHandler networkHandler,
+		PermanentEventSubscriptions subscriptions,
+		IServerConfig serverConfig
+	) {
+		this.networkHandler = networkHandler;
+		this.subscriptions = subscriptions;
+		this.serverConfig = serverConfig;
+	}
+
+	public void registerClient() {
+		JustEnoughItemsClient jeiClient = new JustEnoughItemsClient(networkHandler, subscriptions, serverConfig);
+		jeiClient.register();
+	}
+}

mezz/JustEnoughItems

(cherry picked from commit 8d7aaa149bfb330af91cc46ac70a23950e4834fb)

Forge/src/main/java/mezz/jei/forge/JustEnoughItemsClientSafeRunner.java
@@ -1,0 +1,26 @@
+package mezz.jei.forge;
+
+import mezz.jei.core.config.IServerConfig;
+import mezz.jei.forge.events.PermanentEventSubscriptions;
+import mezz.jei.forge.network.NetworkHandler;
+
+public class JustEnoughItemsClientSafeRunner {
+	private final NetworkHandler networkHandler;
+	private final PermanentEventSubscriptions subscriptions;
+	private final IServerConfig serverConfig;
+
+	public JustEnoughItemsClientSafeRunner(
+		NetworkHandler networkHandler,
+		PermanentEventSubscriptions subscriptions,
+		IServerConfig serverConfig
+	) {
+		this.networkHandler = networkHandler;
+		this.subscriptions = subscriptions;
+		this.serverConfig = serverConfig;
+	}
+
+	public void registerClient() {
+		JustEnoughItemsClient jeiClient = new JustEnoughItemsClient(networkHandler, subscriptions, serverConfig);
+		jeiClient.register();
+	}
+}