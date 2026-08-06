Mysticpasta1/JEI-Async

Clean up item stack batch rendering a little

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -42,7 +42,7 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 
 	@Override
 	public void renderBatch(GuiGraphics guiGraphics, List<BatchRenderElement<ItemStack>> batchRenderElements) {
-		batchRenderer.renderBatch(guiGraphics, batchRenderElements);
+		batchRenderer.renderBatch(guiGraphics, this, batchRenderElements);
 	}
 
 	@SuppressWarnings("removal")

Library/src/main/java/mezz/jei/library/render/batch/ItemStackBatchRendererCache.java
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import mezz.jei.api.ingredients.rendering.BatchRenderElement;
+import mezz.jei.library.render.ItemStackRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.entity.ItemRenderer;
@@ -24,12 +25,12 @@ public class ItemStackBatchRendererCache {
 				}
 			});
 
-	public void renderBatch(GuiGraphics guiGraphics, List<BatchRenderElement<ItemStack>> elements) {
+	public void renderBatch(GuiGraphics guiGraphics, ItemStackRenderer itemStackRenderer, List<BatchRenderElement<ItemStack>> elements) {
 		ItemStackBatchRenderer batchData = cache.getUnchecked(elements);
 
 		Minecraft minecraft = Minecraft.getInstance();
 		ItemRenderer itemRenderer = minecraft.getItemRenderer();
-		batchData.render(guiGraphics, minecraft, itemRenderer);
+		batchData.render(guiGraphics, minecraft, itemRenderer, itemStackRenderer);
 	}
 
 }

mezz/JustEnoughItems

(cherry picked from commit ac7c600f40b5242dcbb0ae38d98c26981cd9744e)

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -59,7 +59,7 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 
 	@Override
 	public void renderBatch(PoseStack poseStack, List<BatchRenderElement<ItemStack>> batchRenderElements) {
-		batchRenderer.renderBatch(poseStack, batchRenderElements);
+		batchRenderer.renderBatch(poseStack, this, batchRenderElements);
 	}
 
 	@SuppressWarnings("removal")

Library/src/main/java/mezz/jei/library/render/batch/ItemStackBatchRendererCache.java
@@ -5,6 +5,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.ingredients.rendering.BatchRenderElement;
+import mezz.jei.library.render.ItemStackRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.world.item.ItemStack;
@@ -23,12 +24,12 @@ public class ItemStackBatchRendererCache {
 				}
 			});
 
-	public void renderBatch(PoseStack poseStack, List<BatchRenderElement<ItemStack>> elements) {
+	public void renderBatch(PoseStack poseStack, ItemStackRenderer itemStackRenderer, List<BatchRenderElement<ItemStack>> elements) {
 		ItemStackBatchRenderer batchData = cache.getUnchecked(elements);
 
 		Minecraft minecraft = Minecraft.getInstance();
 		ItemRenderer itemRenderer = minecraft.getItemRenderer();
-		batchData.render(poseStack, minecraft, itemRenderer);
+		batchData.render(poseStack, minecraft, itemRenderer, itemStackRenderer);
 	}
 
 }