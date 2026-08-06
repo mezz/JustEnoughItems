Mysticpasta1/JEI-Async

Add API support for batch-rendering ingredients (#3711)
Add basic batch-rendering for ItemStacks

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -3,8 +3,10 @@ package mezz.jei.library.render;
 import com.mojang.blaze3d.systems.RenderSystem;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.rendering.BatchRenderElement;
 import mezz.jei.common.platform.IPlatformRenderHelper;
 import mezz.jei.common.platform.Services;
+import mezz.jei.library.render.batch.ItemStackBatchRendererCache;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
@@ -18,6 +20,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
+	private final ItemStackBatchRendererCache batchRenderer = new ItemStackBatchRendererCache();
+
 	@Override
 	public void render(GuiGraphics guiGraphics, @Nullable ItemStack ingredient) {
 		render(guiGraphics, ingredient, 0, 0);
@@ -34,6 +38,11 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 			guiGraphics.renderItemDecorations(font, ingredient, posX, posY);
 			RenderSystem.disableBlend();
 		}
+	}
+
+	@Override
+	public void renderBatch(GuiGraphics guiGraphics, List<BatchRenderElement<ItemStack>> batchRenderElements) {
+		batchRenderer.renderBatch(guiGraphics, batchRenderElements);
 	}
 
 	@SuppressWarnings("removal")

Library/src/main/java/mezz/jei/library/render/batch/ItemStackBatchRendererCache.java
@@ -1,0 +1,35 @@
+package mezz.jei.library.render.batch;
+
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import mezz.jei.api.ingredients.rendering.BatchRenderElement;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+public class ItemStackBatchRendererCache {
+	private final LoadingCache<List<BatchRenderElement<ItemStack>>, ItemStackBatchRenderer> cache =
+		CacheBuilder.newBuilder()
+			.maximumSize(6)
+			.build(new CacheLoader<>() {
+				@Override
+				public ItemStackBatchRenderer load(List<BatchRenderElement<ItemStack>> elements) {
+					Minecraft minecraft = Minecraft.getInstance();
+					return new ItemStackBatchRenderer(minecraft, elements);
+				}
+			});
+
+	public void renderBatch(GuiGraphics guiGraphics, List<BatchRenderElement<ItemStack>> elements) {
+		ItemStackBatchRenderer batchData = cache.getUnchecked(elements);
+
+		Minecraft minecraft = Minecraft.getInstance();
+		ItemRenderer itemRenderer = minecraft.getItemRenderer();
+		batchData.render(guiGraphics, minecraft, itemRenderer);
+	}
+
+}

mezz/JustEnoughItems

Adapted for 1.19.2 by keeping the existing PoseStack-based API and overlay code.
(cherry picked from commit f4d357714e3146e032848cafd4e67155fcd4139f)

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -4,9 +4,11 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.rendering.BatchRenderElement;
 import mezz.jei.common.platform.IPlatformRenderHelper;
 import mezz.jei.common.platform.Services;
 import mezz.jei.common.util.ErrorUtil;
+import mezz.jei.library.render.batch.ItemStackBatchRendererCache;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -25,6 +27,7 @@ import java.util.List;
 
 public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 	private static final Logger LOGGER = LogManager.getLogger();
+	private final ItemStackBatchRendererCache batchRenderer = new ItemStackBatchRendererCache();
 
 	@Override
 	public void render(PoseStack poseStack, @Nullable ItemStack ingredient) {
@@ -52,6 +55,11 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 			// Restore model-view matrix now that the item has been rendered
 			RenderSystem.applyModelViewMatrix();
 		}
+	}
+
+	@Override
+	public void renderBatch(PoseStack poseStack, List<BatchRenderElement<ItemStack>> batchRenderElements) {
+		batchRenderer.renderBatch(poseStack, batchRenderElements);
 	}
 
 	@SuppressWarnings("removal")

Library/src/main/java/mezz/jei/library/render/batch/ItemStackBatchRendererCache.java
@@ -1,0 +1,34 @@
+package mezz.jei.library.render.batch;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.ingredients.rendering.BatchRenderElement;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+public class ItemStackBatchRendererCache {
+	private final LoadingCache<List<BatchRenderElement<ItemStack>>, ItemStackBatchRenderer> cache =
+		CacheBuilder.newBuilder()
+			.maximumSize(6)
+			.build(new CacheLoader<>() {
+				@Override
+				public ItemStackBatchRenderer load(List<BatchRenderElement<ItemStack>> elements) {
+					Minecraft minecraft = Minecraft.getInstance();
+					return new ItemStackBatchRenderer(minecraft, elements);
+				}
+			});
+
+	public void renderBatch(PoseStack poseStack, List<BatchRenderElement<ItemStack>> elements) {
+		ItemStackBatchRenderer batchData = cache.getUnchecked(elements);
+
+		Minecraft minecraft = Minecraft.getInstance();
+		ItemRenderer itemRenderer = minecraft.getItemRenderer();
+		batchData.render(poseStack, minecraft, itemRenderer);
+	}
+
+}