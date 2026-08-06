Mysticpasta1/JEI-Async

add an IPlaceable interface for GUI elements that can have their position set

Common/src/main/java/mezz/jei/common/gui/elements/TextWidget.java
@@ -1,13 +1,13 @@
 package mezz.jei.common.gui.elements;
 
 import mezz.jei.api.gui.builder.ITooltipBuilder;
+import mezz.jei.api.gui.placement.HorizontalAlignment;
+import mezz.jei.api.gui.placement.VerticalAlignment;
 import mezz.jei.api.gui.widgets.IRecipeWidget;
 import mezz.jei.api.gui.widgets.ITextWidget;
 import mezz.jei.common.config.DebugConfig;
-import mezz.jei.common.util.HorizontalAlignment;
 import mezz.jei.common.util.ImmutableRect2i;
 import mezz.jei.common.util.StringUtil;
-import mezz.jei.common.util.VerticalAlignment;
 import mezz.jei.core.util.Pair;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -22,7 +22,7 @@ import java.util.List;
 
 public class TextWidget implements ITextWidget, IRecipeWidget {
 	private final List<FormattedText> text;
-	private final ImmutableRect2i area;
+	private ImmutableRect2i availableArea;
 
 	private HorizontalAlignment horizontalAlignment;
 	private VerticalAlignment verticalAlignment;
@@ -30,11 +30,12 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 	private int color;
 	private boolean shadow;
 	private int lineSpacing;
-	private List<FormattedText> tooltipText = List.of();
+
 	private @Nullable List<FormattedText> wrappedText;
+	private boolean truncated = false;
 
 	public TextWidget(List<FormattedText> text, int xPos, int yPos, int maxWidth, int maxHeight) {
-		this.area = new ImmutableRect2i(xPos, yPos, maxWidth, maxHeight);
+		this.availableArea = new ImmutableRect2i(xPos, yPos, maxWidth, maxHeight);
 		Minecraft minecraft = Minecraft.getInstance();
 		this.font = minecraft.font;
 		this.color = 0xFF000000;
@@ -44,69 +45,79 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 		this.verticalAlignment = VerticalAlignment.TOP;
 	}
 
+	private void invalidateCachedValues() {
+		wrappedText = null;
+		truncated = false;
+	}
+
 	@Override
-	public ITextWidget alignHorizontalLeft() {
-		this.horizontalAlignment = HorizontalAlignment.LEFT;
+	public int getWidth() {
+		return availableArea.width();
+	}
+
+	@Override
+	public int getHeight() {
+		return availableArea.height();
+	}
+
+	@Override
+	public TextWidget setPosition(int xPos, int yPos) {
+		this.availableArea = this.availableArea.setPosition(xPos, yPos);
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
-	public ITextWidget alignHorizontalRight() {
-		this.horizontalAlignment = HorizontalAlignment.RIGHT;
+	public TextWidget setTextAlignment(HorizontalAlignment horizontalAlignment) {
+		if (this.horizontalAlignment.equals(horizontalAlignment)) {
+			return this;
+		}
+		this.horizontalAlignment = horizontalAlignment;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
-	public ITextWidget alignHorizontalCenter() {
-		this.horizontalAlignment = HorizontalAlignment.CENTER;
-		return this;
-	}
-
-	@Override
-	public ITextWidget alignVerticalTop() {
-		this.verticalAlignment = VerticalAlignment.TOP;
-		return this;
-	}
-
-	@Override
-	public ITextWidget alignVerticalCenter() {
-		this.verticalAlignment = VerticalAlignment.CENTER;
-		return this;
-	}
-
-	@Override
-	public ITextWidget alignVerticalBottom() {
-		this.verticalAlignment = VerticalAlignment.BOTTOM;
+	public TextWidget setTextAlignment(VerticalAlignment verticalAlignment) {
+		if (this.verticalAlignment.equals(verticalAlignment)) {
+			return this;
+		}
+		this.verticalAlignment = verticalAlignment;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setFont(Font font) {
 		this.font = font;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setColor(int color) {
 		this.color = color;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setLineSpacing(int lineSpacing) {
 		this.lineSpacing = lineSpacing;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setShadow(boolean shadow) {
 		this.shadow = shadow;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ScreenPosition getPosition() {
-		return area.getScreenPosition();
+		return availableArea.getScreenPosition();
 	}
 
 	private List<FormattedText> calculateWrappedText() {
@@ -114,18 +125,13 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 			return wrappedText;
 		}
 		int lineHeight = getLineHeight();
-		int maxLines = area.height() / lineHeight;
-		if (maxLines * lineHeight + font.lineHeight <= area.height()) {
+		int maxLines = availableArea.height() / lineHeight;
+		if (maxLines * lineHeight + font.lineHeight <= availableArea.height()) {
 			maxLines++;
 		}
-		Pair<List<FormattedText>, Boolean> result = StringUtil.splitLines(font, text, area.width(), maxLines);
+		Pair<List<FormattedText>, Boolean> result = StringUtil.splitLines(font, text, availableArea.width(), maxLines);
 		this.wrappedText = result.first();
-		boolean truncated = result.second();
-		if (truncated) {
-			this.tooltipText = text;
-		} else {
-			this.tooltipText = List.of();
-		}
+		this.truncated = result.second();
 		return wrappedText;
 	}
 
@@ -148,38 +154,30 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 		}
 
 		if (DebugConfig.isDebugGuisEnabled()) {
-			guiGraphics.fill(0,0, area.width(), area.height(), 0xAAAAAA00);
+			guiGraphics.fill(0,0, availableArea.width(), availableArea.height(), 0xAAAAAA00);
 		}
 	}
 
 	@Override
 	public void getTooltip(ITooltipBuilder tooltip, double mouseX, double mouseY) {
-		if (mouseX >= 0 && mouseX < area.width() && mouseY >= 0 && mouseY < area.height()) {
+		if (mouseX >= 0 && mouseX < availableArea.width() && mouseY >= 0 && mouseY < availableArea.height()) {
 			calculateWrappedText();
-			tooltip.addAll(tooltipText);
+			if (truncated) {
+				tooltip.addAll(text);
+			}
 		}
 	}
 
 	private int getXPos(FormattedCharSequence text) {
-		return switch (horizontalAlignment) {
-			case LEFT -> 0;
-			case RIGHT -> this.area.width() - font.width(text);
-			case CENTER -> Math.round((this.area.width() - font.width(text)) / 2f);
-		};
+		return getXPos(font.width(text));
+	}
+
+	private int getXPos(int lineWidth) {
+		return horizontalAlignment.getXPos(this.availableArea.width(), lineWidth);
 	}
 
 	private int getYPosStart(int lineHeight, List<FormattedText> text) {
-		if (verticalAlignment == VerticalAlignment.TOP) {
-			return 0;
-		}
-
 		int linesHeight = (lineHeight * text.size()) - lineSpacing - 1;
-		if (verticalAlignment == VerticalAlignment.BOTTOM) {
-			return area.height() - linesHeight;
-		} else if (verticalAlignment == VerticalAlignment.CENTER) {
-			return Math.round((area.height() - linesHeight) / 2f);
-		} else {
-			throw new IllegalArgumentException("Unknown verticalAlignment " + verticalAlignment);
-		}
+		return verticalAlignment.getYPos(this.availableArea.height(), linesHeight);
 	}
 }

mezz/JustEnoughItems

(cherry picked from commit 3735d8df8990bcb799c24f35c74181ff697a0d0e)

Common/src/main/java/mezz/jei/common/gui/elements/TextWidget.java
@@ -2,13 +2,13 @@ package mezz.jei.common.gui.elements;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
+import mezz.jei.api.gui.placement.HorizontalAlignment;
+import mezz.jei.api.gui.placement.VerticalAlignment;
 import mezz.jei.api.gui.widgets.IRecipeWidget;
 import mezz.jei.api.gui.widgets.ITextWidget;
 import mezz.jei.common.config.DebugConfig;
-import mezz.jei.common.util.HorizontalAlignment;
 import mezz.jei.common.util.ImmutableRect2i;
 import mezz.jei.common.util.StringUtil;
-import mezz.jei.common.util.VerticalAlignment;
 import mezz.jei.core.util.Pair;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -23,7 +23,7 @@ import java.util.List;
 
 public class TextWidget implements ITextWidget, IRecipeWidget {
 	private final List<FormattedText> text;
-	private final ImmutableRect2i area;
+	private ImmutableRect2i availableArea;
 
 	private HorizontalAlignment horizontalAlignment;
 	private VerticalAlignment verticalAlignment;
@@ -31,11 +31,12 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 	private int color;
 	private boolean shadow;
 	private int lineSpacing;
-	private List<FormattedText> tooltipText = List.of();
+
 	private @Nullable List<FormattedText> wrappedText;
+	private boolean truncated = false;
 
 	public TextWidget(List<FormattedText> text, int xPos, int yPos, int maxWidth, int maxHeight) {
-		this.area = new ImmutableRect2i(xPos, yPos, maxWidth, maxHeight);
+		this.availableArea = new ImmutableRect2i(xPos, yPos, maxWidth, maxHeight);
 		Minecraft minecraft = Minecraft.getInstance();
 		this.font = minecraft.font;
 		this.color = 0xFF000000;
@@ -45,69 +46,79 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 		this.verticalAlignment = VerticalAlignment.TOP;
 	}
 
+	private void invalidateCachedValues() {
+		wrappedText = null;
+		truncated = false;
+	}
+
 	@Override
-	public ITextWidget alignHorizontalLeft() {
-		this.horizontalAlignment = HorizontalAlignment.LEFT;
+	public int getWidth() {
+		return availableArea.width();
+	}
+
+	@Override
+	public int getHeight() {
+		return availableArea.height();
+	}
+
+	@Override
+	public TextWidget setPosition(int xPos, int yPos) {
+		this.availableArea = this.availableArea.setPosition(xPos, yPos);
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
-	public ITextWidget alignHorizontalRight() {
-		this.horizontalAlignment = HorizontalAlignment.RIGHT;
+	public TextWidget setTextAlignment(HorizontalAlignment horizontalAlignment) {
+		if (this.horizontalAlignment.equals(horizontalAlignment)) {
+			return this;
+		}
+		this.horizontalAlignment = horizontalAlignment;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
-	public ITextWidget alignHorizontalCenter() {
-		this.horizontalAlignment = HorizontalAlignment.CENTER;
-		return this;
-	}
-
-	@Override
-	public ITextWidget alignVerticalTop() {
-		this.verticalAlignment = VerticalAlignment.TOP;
-		return this;
-	}
-
-	@Override
-	public ITextWidget alignVerticalCenter() {
-		this.verticalAlignment = VerticalAlignment.CENTER;
-		return this;
-	}
-
-	@Override
-	public ITextWidget alignVerticalBottom() {
-		this.verticalAlignment = VerticalAlignment.BOTTOM;
+	public TextWidget setTextAlignment(VerticalAlignment verticalAlignment) {
+		if (this.verticalAlignment.equals(verticalAlignment)) {
+			return this;
+		}
+		this.verticalAlignment = verticalAlignment;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setFont(Font font) {
 		this.font = font;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setColor(int color) {
 		this.color = color;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setLineSpacing(int lineSpacing) {
 		this.lineSpacing = lineSpacing;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public ITextWidget setShadow(boolean shadow) {
 		this.shadow = shadow;
+		invalidateCachedValues();
 		return this;
 	}
 
 	@Override
 	public Rect2i getArea() {
-		return area.toMutable();
+		return availableArea.toMutable();
 	}
 
 	private List<FormattedText> calculateWrappedText() {
@@ -115,18 +126,13 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 			return wrappedText;
 		}
 		int lineHeight = getLineHeight();
-		int maxLines = area.height() / lineHeight;
-		if (maxLines * lineHeight + font.lineHeight <= area.height()) {
+		int maxLines = availableArea.height() / lineHeight;
+		if (maxLines * lineHeight + font.lineHeight <= availableArea.height()) {
 			maxLines++;
 		}
-		Pair<List<FormattedText>, Boolean> result = StringUtil.splitLines(font, text, area.width(), maxLines);
+		Pair<List<FormattedText>, Boolean> result = StringUtil.splitLines(font, text, availableArea.width(), maxLines);
 		this.wrappedText = result.first();
-		boolean truncated = result.second();
-		if (truncated) {
-			this.tooltipText = text;
-		} else {
-			this.tooltipText = List.of();
-		}
+		this.truncated = result.second();
 		return wrappedText;
 	}
 
@@ -153,38 +159,30 @@ public class TextWidget implements ITextWidget, IRecipeWidget {
 		}
 
 		if (DebugConfig.isDebugGuisEnabled()) {
-			Screen.fill(poseStack, 0, 0, area.width(), area.height(), 0xAAAAAA00);
+			Screen.fill(poseStack, 0, 0, availableArea.width(), availableArea.height(), 0xAAAAAA00);
 		}
 	}
 
 	@Override
 	public void getTooltip(ITooltipBuilder tooltip, double mouseX, double mouseY) {
-		if (mouseX >= 0 && mouseX < area.width() && mouseY >= 0 && mouseY < area.height()) {
+		if (mouseX >= 0 && mouseX < availableArea.width() && mouseY >= 0 && mouseY < availableArea.height()) {
 			calculateWrappedText();
-			tooltipText.forEach(tooltip::add);
+			if (truncated) {
+				text.forEach(tooltip::add);
+			}
 		}
 	}
 
 	private int getXPos(FormattedCharSequence text) {
-		return switch (horizontalAlignment) {
-			case LEFT -> 0;
-			case RIGHT -> this.area.width() - font.width(text);
-			case CENTER -> Math.round((this.area.width() - font.width(text)) / 2f);
-		};
+		return getXPos(font.width(text));
+	}
+
+	private int getXPos(int lineWidth) {
+		return horizontalAlignment.getXPos(this.availableArea.width(), lineWidth);
 	}
 
 	private int getYPosStart(int lineHeight, List<FormattedText> text) {
-		if (verticalAlignment == VerticalAlignment.TOP) {
-			return 0;
-		}
-
 		int linesHeight = (lineHeight * text.size()) - lineSpacing - 1;
-		if (verticalAlignment == VerticalAlignment.BOTTOM) {
-			return area.height() - linesHeight;
-		} else if (verticalAlignment == VerticalAlignment.CENTER) {
-			return Math.round((area.height() - linesHeight) / 2f);
-		} else {
-			throw new IllegalArgumentException("Unknown verticalAlignment " + verticalAlignment);
-		}
+		return verticalAlignment.getYPos(this.availableArea.height(), linesHeight);
 	}
 }