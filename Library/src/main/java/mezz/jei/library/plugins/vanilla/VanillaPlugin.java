Mysticpasta1/JEI-Async

Add convenience methods for adding multiple catalysts to one recipe category

Library/src/main/java/mezz/jei/library/plugins/vanilla/VanillaPlugin.java
@@ -322,15 +322,23 @@ public class VanillaPlugin implements IModPlugin {
 
 	@Override
 	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
-		registration.addRecipeCatalyst(Blocks.CRAFTING_TABLE, RecipeTypes.CRAFTING);
-		registration.addRecipeCatalyst(Blocks.CRAFTER, RecipeTypes.CRAFTING);
+		registration.addRecipeCatalysts(RecipeTypes.CRAFTING,
+			Blocks.CRAFTING_TABLE,
+			Blocks.CRAFTER
+		);
+		registration.addRecipeCatalysts(RecipeTypes.FUELING,
+			Blocks.FURNACE,
+			Blocks.SMOKER,
+			Blocks.BLAST_FURNACE
+		);
+		registration.addRecipeCatalysts(RecipeTypes.CAMPFIRE_COOKING,
+			Blocks.CAMPFIRE,
+			Blocks.SOUL_CAMPFIRE
+		);
 		registration.addRecipeCatalyst(Blocks.STONECUTTER, RecipeTypes.STONECUTTING);
-		registration.addRecipeCatalyst(Blocks.FURNACE, RecipeTypes.SMELTING, RecipeTypes.FUELING);
-
-		registration.addRecipeCatalyst(Blocks.SMOKER, RecipeTypes.SMOKING, RecipeTypes.FUELING);
-		registration.addRecipeCatalyst(Blocks.BLAST_FURNACE, RecipeTypes.BLASTING, RecipeTypes.FUELING);
-		registration.addRecipeCatalyst(Blocks.CAMPFIRE, RecipeTypes.CAMPFIRE_COOKING);
-		registration.addRecipeCatalyst(Blocks.SOUL_CAMPFIRE, RecipeTypes.CAMPFIRE_COOKING);
+		registration.addRecipeCatalyst(Blocks.FURNACE, RecipeTypes.SMELTING);
+		registration.addRecipeCatalyst(Blocks.SMOKER, RecipeTypes.SMOKING);
+		registration.addRecipeCatalyst(Blocks.BLAST_FURNACE, RecipeTypes.BLASTING);
 		registration.addRecipeCatalyst(Blocks.BREWING_STAND, RecipeTypes.BREWING);
 		registration.addRecipeCatalyst(Blocks.ANVIL, RecipeTypes.ANVIL);
 		registration.addRecipeCatalyst(Blocks.SMITHING_TABLE, RecipeTypes.SMITHING);

mezz/JustEnoughItems

(cherry picked from commit 7f75d829f2855ef894a7fc793bb3d33652b2920d)

Library/src/main/java/mezz/jei/library/plugins/vanilla/VanillaPlugin.java
@@ -305,19 +305,27 @@ public class VanillaPlugin implements IModPlugin {
 
 	@Override
 	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
-		registration.addRecipeCatalyst(new ItemStack(Blocks.CRAFTING_TABLE), RecipeTypes.CRAFTING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.STONECUTTER), RecipeTypes.STONECUTTING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.FURNACE), RecipeTypes.SMELTING, RecipeTypes.FUELING);
-
-		registration.addRecipeCatalyst(new ItemStack(Blocks.SMOKER), RecipeTypes.SMOKING, RecipeTypes.FUELING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.BLAST_FURNACE), RecipeTypes.BLASTING, RecipeTypes.FUELING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.CAMPFIRE), RecipeTypes.CAMPFIRE_COOKING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.SOUL_CAMPFIRE), RecipeTypes.CAMPFIRE_COOKING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.BREWING_STAND), RecipeTypes.BREWING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.ANVIL), RecipeTypes.ANVIL);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.GRINDSTONE), RecipeTypes.GRINDSTONE);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.SMITHING_TABLE), RecipeTypes.SMITHING);
-		registration.addRecipeCatalyst(new ItemStack(Blocks.COMPOSTER), RecipeTypes.COMPOSTING);
+		registration.addRecipeCatalysts(RecipeTypes.CRAFTING,
+			Blocks.CRAFTING_TABLE
+		);
+		registration.addRecipeCatalysts(RecipeTypes.FUELING,
+			Blocks.FURNACE,
+			Blocks.SMOKER,
+			Blocks.BLAST_FURNACE
+		);
+		registration.addRecipeCatalysts(RecipeTypes.CAMPFIRE_COOKING,
+			Blocks.CAMPFIRE,
+			Blocks.SOUL_CAMPFIRE
+		);
+		registration.addRecipeCatalyst(Blocks.STONECUTTER, RecipeTypes.STONECUTTING);
+		registration.addRecipeCatalyst(Blocks.FURNACE, RecipeTypes.SMELTING);
+		registration.addRecipeCatalyst(Blocks.SMOKER, RecipeTypes.SMOKING);
+		registration.addRecipeCatalyst(Blocks.BLAST_FURNACE, RecipeTypes.BLASTING);
+		registration.addRecipeCatalyst(Blocks.BREWING_STAND, RecipeTypes.BREWING);
+		registration.addRecipeCatalyst(Blocks.ANVIL, RecipeTypes.ANVIL);
+		registration.addRecipeCatalyst(Blocks.GRINDSTONE, RecipeTypes.GRINDSTONE);
+		registration.addRecipeCatalyst(Blocks.SMITHING_TABLE, RecipeTypes.SMITHING);
+		registration.addRecipeCatalyst(Blocks.COMPOSTER, RecipeTypes.COMPOSTING);
 	}
 
 	public Optional<CraftingRecipeCategory> getCraftingCategory() {