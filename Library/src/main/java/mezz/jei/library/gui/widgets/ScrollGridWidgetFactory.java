Mysticpasta1/JEI-Async

Add convenience methods and make scrolling grids of ingredients much easier to create

Library/src/main/java/mezz/jei/library/gui/widgets/ScrollGridWidgetFactory.java
@@ -1,45 +1,41 @@
 package mezz.jei.library.gui.widgets;
 
-import mezz.jei.api.gui.drawable.IDrawableStatic;
 import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
-import mezz.jei.api.gui.widgets.IScrollGridWidgetFactory;
-import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.common.util.ImmutablePoint2i;
 import mezz.jei.common.util.ImmutableSize2i;
 import net.minecraft.client.gui.navigation.ScreenRectangle;
 
 import java.util.List;
 
-public class ScrollGridWidgetFactory<R> implements IScrollGridWidgetFactory<R> {
+@SuppressWarnings("removal")
+public class ScrollGridWidgetFactory<R> implements mezz.jei.api.gui.widgets.IScrollGridWidgetFactory<R> {
 	private final int columns;
 	private final int visibleRows;
-	private final IGuiHelper guiHelper;
-	private ScreenRectangle area;
+	private ImmutablePoint2i position;
 
-	public ScrollGridWidgetFactory(IGuiHelper guiHelper, int columns, int visibleRows) {
-		this.guiHelper = guiHelper;
-		IDrawableStatic slotBackground = guiHelper.getSlotDrawable();
-
+	public ScrollGridWidgetFactory(int columns, int visibleRows) {
 		this.columns = columns;
 		this.visibleRows = visibleRows;
-		ImmutableSize2i size = ScrollGridRecipeWidget.calculateSize(slotBackground, columns, visibleRows);
-		this.area = new ScreenRectangle(0, 0, size.width(), size.height());
+		this.position = ImmutablePoint2i.ORIGIN;
 	}
 
 	@Override
 	public void setPosition(int x, int y) {
-		area = new ScreenRectangle(x, y, area.width(), area.height());
+		position = new ImmutablePoint2i(x, y);
 	}
 
 	@Override
 	public ScreenRectangle getArea() {
-		return area;
+		ImmutableSize2i size = ScrollGridRecipeWidget.calculateSize(columns, visibleRows);
+		return new ScreenRectangle(position.x(), position.y(), size.width(), size.height());
 	}
 
 	@Override
 	public void createWidgetForSlots(IRecipeExtrasBuilder builder, R recipe, List<IRecipeSlotDrawable> slots) {
-		ScrollGridRecipeWidget widget = new ScrollGridRecipeWidget(guiHelper, area, columns, visibleRows, slots);
-		builder.addWidget(widget);
+		ScrollGridRecipeWidget widget = ScrollGridRecipeWidget.create(slots, columns, visibleRows);
+		widget.setPosition(position.x(), position.y());
+		builder.addSlottedWidget(widget, slots);
 		builder.addInputHandler(widget);
 	}
 }

mezz/JustEnoughItems

(cherry picked from commit 2d5e129ec8c2a27f760edc24855ba32dc31a16ce)

Library/src/main/java/mezz/jei/library/gui/widgets/ScrollGridWidgetFactory.java
@@ -1,45 +1,41 @@
 package mezz.jei.library.gui.widgets;
 
-import mezz.jei.api.gui.drawable.IDrawableStatic;
 import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
-import mezz.jei.api.gui.widgets.IScrollGridWidgetFactory;
-import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.common.util.ImmutablePoint2i;
 import mezz.jei.common.util.ImmutableSize2i;
 import net.minecraft.client.renderer.Rect2i;
 
 import java.util.List;
 
-public class ScrollGridWidgetFactory<R> implements IScrollGridWidgetFactory<R> {
+@SuppressWarnings("removal")
+public class ScrollGridWidgetFactory<R> implements mezz.jei.api.gui.widgets.IScrollGridWidgetFactory<R> {
 	private final int columns;
 	private final int visibleRows;
-	private final IGuiHelper guiHelper;
-	private Rect2i area;
+	private ImmutablePoint2i position;
 
-	public ScrollGridWidgetFactory(IGuiHelper guiHelper, int columns, int visibleRows) {
-		this.guiHelper = guiHelper;
-		IDrawableStatic slotBackground = guiHelper.getSlotDrawable();
-
+	public ScrollGridWidgetFactory(int columns, int visibleRows) {
 		this.columns = columns;
 		this.visibleRows = visibleRows;
-		ImmutableSize2i size = ScrollGridRecipeWidget.calculateSize(slotBackground, columns, visibleRows);
-		this.area = new Rect2i(0, 0, size.width(), size.height());
+		this.position = ImmutablePoint2i.ORIGIN;
 	}
 
 	@Override
 	public void setPosition(int x, int y) {
-		area = new Rect2i(x, y, area.getWidth(), area.getHeight());
+		position = new ImmutablePoint2i(x, y);
 	}
 
 	@Override
 	public Rect2i getArea() {
-		return area;
+		ImmutableSize2i size = ScrollGridRecipeWidget.calculateSize(columns, visibleRows);
+		return new Rect2i(position.x(), position.y(), size.width(), size.height());
 	}
 
 	@Override
 	public void createWidgetForSlots(IRecipeExtrasBuilder builder, R recipe, List<IRecipeSlotDrawable> slots) {
-		ScrollGridRecipeWidget widget = new ScrollGridRecipeWidget(guiHelper, area, columns, visibleRows, slots);
-		builder.addWidget(widget);
+		ScrollGridRecipeWidget widget = ScrollGridRecipeWidget.create(slots, columns, visibleRows);
+		widget.setPosition(position.x(), position.y());
+		builder.addSlottedWidget(widget, slots);
 		builder.addInputHandler(widget);
 	}
 }