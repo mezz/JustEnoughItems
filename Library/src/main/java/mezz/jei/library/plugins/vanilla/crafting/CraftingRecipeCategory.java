Mysticpasta1/JEI-Async

Add support for onDisplayedIngredientsUpdate to ICraftingCategoryExtension

Library/src/main/java/mezz/jei/library/plugins/vanilla/crafting/CraftingRecipeCategory.java
@@ -7,6 +7,7 @@ import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
+import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
 import mezz.jei.api.helpers.ICodecHelper;
@@ -72,6 +73,12 @@ public class CraftingRecipeCategory implements IExtendableCraftingRecipeCategory
 	public void setRecipe(IRecipeLayoutBuilder builder, RecipeHolder<CraftingRecipe> recipeHolder, IFocusGroup focuses) {
 		var recipeExtension = this.extendableHelper.getRecipeExtension(recipeHolder);
 		recipeExtension.setRecipe(recipeHolder, builder, craftingGridHelper, focuses);
+	}
+
+	@Override
+	public void onDisplayedIngredientsUpdate(RecipeHolder<CraftingRecipe> recipeHolder, List<IRecipeSlotDrawable> recipeSlots, IFocusGroup focuses) {
+		var recipeExtension = this.extendableHelper.getRecipeExtension(recipeHolder);
+		recipeExtension.onDisplayedIngredientsUpdate(recipeHolder, recipeSlots, focuses);
 	}
 
 	@Override

mezz/JustEnoughItems

(cherry picked from commit 92259ffceb2853a74ce1010e193d3dfc6f5451ba)

Library/src/main/java/mezz/jei/library/plugins/vanilla/crafting/CraftingRecipeCategory.java
@@ -7,6 +7,7 @@ import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
+import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
 import mezz.jei.api.helpers.IGuiHelper;
@@ -71,6 +72,12 @@ public class CraftingRecipeCategory implements IExtendableRecipeCategory<Craftin
 	public void setRecipe(IRecipeLayoutBuilder builder, CraftingRecipe recipe, IFocusGroup focuses) {
 		ICraftingCategoryExtension recipeExtension = this.extendableHelper.getRecipeExtension(recipe);
 		recipeExtension.setRecipe(builder, craftingGridHelper, focuses);
+	}
+
+	@Override
+	public void onDisplayedIngredientsUpdate(CraftingRecipe recipe, List<IRecipeSlotDrawable> recipeSlots, IFocusGroup focuses) {
+		var recipeExtension = this.extendableHelper.getRecipeExtension(recipe);
+		recipeExtension.onDisplayedIngredientsUpdate(recipeSlots, focuses);
 	}
 
 	@Override