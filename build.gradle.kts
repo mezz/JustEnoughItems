afc2f79999e568a5bbeb5a707896a46077909cae
mezz <tehgeek@gmail.com>
2026-08-08 11:19:30 +0900

    enable isolated projects with Loom alpha 13

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     id("net.mezzdev.java-formatting") version("0.4.0")
 
     // https://maven.fabricmc.net/fabric-loom/fabric-loom.gradle.plugin/maven-metadata.xml
-    id("net.fabricmc.fabric-loom") version("1.18.0-alpha.10") apply(false)
+    id("net.fabricmc.fabric-loom") version("1.18.0-alpha.13") apply(false)
 
     // https://maven.fabricmc.net/net/fabricmc/fabric-loom-companion/net.fabricmc.fabric-loom-companion.gradle.plugin/maven-metadata.xml
     // applying this to all projects allows loom projects to access the required data in a manner that follows Gradle's best practices.
-    id("net.fabricmc.fabric-loom-companion") version("1.18.0-alpha.12")
+    id("net.fabricmc.fabric-loom-companion") version("1.18.0-alpha.13")
 
     id("net.mezzdev.modshade") version("0.3.0") apply(false)
 
@@ -16,7 +16,20 @@ plugins {
     id("mezz.jei.api-compatibility")
 }
 
+val javaSourceTrees = listOf(
+    "Common", "CommonApi",
+    "Debug",
+    "Fabric", "FabricApi",
+    "Gui", "Library",
+    "NeoForge", "NeoForgeApi"
+).map { moduleName ->
+    layout.projectDirectory.dir("$moduleName/src").asFileTree.matching {
+        include("*/java/**/*.java")
+    }
+}
+
 javaFormatting {
+    target(javaSourceTrees)
     all()
 }
 
