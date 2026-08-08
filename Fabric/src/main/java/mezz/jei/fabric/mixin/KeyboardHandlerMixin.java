ddffc02fa11cf2d49505a3891f6fe2e1599078bc
mezz <tehgeek@gmail.com>
2026-08-08 13:16:26 +0900

    Fix #4176 Consume Fabric focus hotkey character
    (cherry picked from commit c7d9330920c650b3d99645d6f76c0b1cc3c0f00b)

--- Fabric/src/main/java/mezz/jei/fabric/mixin/KeyboardHandlerMixin.java
+++ Fabric/src/main/java/mezz/jei/fabric/mixin/KeyboardHandlerMixin.java
@@ -1,6 +1,7 @@
 package mezz.jei.fabric.mixin;
 
 import mezz.jei.fabric.events.JeiCharTypedEvents;
+import mezz.jei.fabric.input.KeyboardHandlerExtension;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.input.CharacterEvent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,7 +11,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardHandler.class)
-public class KeyboardHandlerMixin {
+public class KeyboardHandlerMixin implements KeyboardHandlerExtension {
+	@Unique
+	private boolean consumeNextCharTyped;
+
+	@Override
+	public void jei$setConsumeNextCharTyped(boolean consumeNextCharTyped) {
+		this.consumeNextCharTyped = consumeNextCharTyped;
+	}
 
 	@Inject(
 		method = "charTyped",
@@ -18,6 +26,11 @@ public class KeyboardHandlerMixin {
 		cancellable = true
 	)
 	private void beforeCharTypedEvent(long handle, CharacterEvent event, CallbackInfo ci) {
+		if (this.consumeNextCharTyped) {
+			this.consumeNextCharTyped = false;
+			ci.cancel();
+			return;
+		}
 		beforeCharTypedEventInternal(handle, event, ci);
 	}
 
