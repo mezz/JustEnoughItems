44557bd8de8ab44b53fed4f8cce381185980c823
mezz <tehgeek@gmail.com>
2026-08-09 16:22:26 +0900

    Fix #4311 Remove plugins that crash on getPluginUid
    (cherry picked from commit ffaca473e2735873da68ab30c605e76d9018a1d5)
    (cherry picked from commit 1215b17763274c5409b62373732761a4409c5af8)
    (cherry picked from commit 776028c7a323a346705c0d00e55bde569bd29f08)
    (cherry picked from commit ba7a8c610674545af313ee7ab6a2e33b4efe5413)
    (cherry picked from commit 40aa2224e35cfef49722e21cee1f03cfcd9083e5)

--- Library/src/main/java/mezz/jei/library/load/PluginHelper.java
+++ Library/src/main/java/mezz/jei/library/load/PluginHelper.java
@@ -1,14 +1,30 @@
 package mezz.jei.library.load;
 
+import mezz.jei.api.IModPlugin;
 import mezz.jei.library.plugins.jei.JeiInternalPlugin;
+import mezz.jei.library.plugins.vanilla.VanillaPlugin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
+
 import java.util.List;
 import java.util.Optional;
 
-import mezz.jei.api.IModPlugin;
-import mezz.jei.library.plugins.vanilla.VanillaPlugin;
-
 public class PluginHelper {
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public static void removePluginsWithCrashingUids(List<IModPlugin> plugins) {
+		plugins.removeIf(plugin -> {
+			try {
+				plugin.getPluginUid();
+				return false;
+			} catch (RuntimeException | LinkageError e) {
+				LOGGER.error("Failed to get plugin UID, removing plugin from JEI: {}", plugin.getClass(), e);
+				return true;
+			}
+		});
+	}
+
 	public static void sortPlugins(List<IModPlugin> plugins, VanillaPlugin vanillaPlugin, @Nullable JeiInternalPlugin jeiInternalPlugin) {
 		plugins.remove(vanillaPlugin);
 		plugins.add(0, vanillaPlugin);
