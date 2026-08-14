a598841ba13a3f0cc5a2272c1a240e3cb0504f9f
Seungyeop Lee <lsy020206@outlook.kr>
2026-08-15 00:09:15 +0900

    fix ime preedit rendering in the ingredient search field

--- Gui/src/main/java/mezz/jei/gui/input/GuiTextFieldFilter.java
+++ Gui/src/main/java/mezz/jei/gui/input/GuiTextFieldFilter.java
@@ -86,15 +86,21 @@ public class GuiTextFieldFilter extends EditBox implements ISearchField {
 
 		if (previousFocus != keyboardFocus) {
 			Minecraft minecraft = Minecraft.getInstance();
+			Screen screen = minecraft.gui.screen();
 			if (keyboardFocus) {
-				Screen screen = minecraft.gui.screen();
 				if (screen != null) {
 					screenUnfocusHandler = ScreenFocusHandler.create(screen);
 					if (screenUnfocusHandler != null) {
 						screenUnfocusHandler.unFocus();
 					}
+					screen.setFocused(this);
 				}
+				minecraft.onTextInputFocusChange(this, true);
 			} else {
+				minecraft.onTextInputFocusChange(this, false);
+				if (screen != null && screen.getFocused() == this) {
+					screen.setFocused(null);
+				}
 				if (screenUnfocusHandler != null) {
 					screenUnfocusHandler.focus();
 					screenUnfocusHandler = null;
