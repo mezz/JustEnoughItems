13b67ea6e71c9e104f078924b709b927cc0528bd
mezz <tehgeek@gmail.com>
2026-08-20 00:52:24 +0900

    update dev dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     id("net.mezzdev.java-formatting") version("0.4.0")
 
     // https://maven.fabricmc.net/fabric-loom/fabric-loom.gradle.plugin/maven-metadata.xml
-    id("net.fabricmc.fabric-loom") version("1.18.0-alpha.13") apply(false)
+    id("net.fabricmc.fabric-loom") version("1.18.0-alpha.16") apply(false)
 
     // https://maven.fabricmc.net/net/fabricmc/fabric-loom-companion/net.fabricmc.fabric-loom-companion.gradle.plugin/maven-metadata.xml
     // applying this to all projects allows loom projects to access the required data in a manner that follows Gradle's best practices.
-    id("net.fabricmc.fabric-loom-companion") version("1.18.0-alpha.13")
+    id("net.fabricmc.fabric-loom-companion") version("1.18.0-alpha.16")
 
     id("net.mezzdev.modshade") version("0.3.0") apply(false)
 
