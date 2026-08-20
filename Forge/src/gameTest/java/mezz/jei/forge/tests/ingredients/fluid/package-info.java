091705cdc65b96c9021f17cedd58965150a9459d
mezz <tehgeek@gmail.com>
2026-08-20 23:38:17 +0900

    Reject unobtainable flowing fluid ingredients
    (cherry picked from commit 104ddfdc11a152fcb87d8f5ffa14572f4e26c972)

---
+++ Forge/src/gameTest/java/mezz/jei/forge/tests/ingredients/fluid/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package mezz.jei.forge.tests.ingredients.fluid;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
