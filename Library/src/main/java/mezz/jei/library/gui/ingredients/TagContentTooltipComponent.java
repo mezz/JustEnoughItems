cc534dd9dba4986ec028c92b3595254627f4db37
mezz <tehgeek@gmail.com>
2026-08-20 23:53:04 +0900

    Improve recipe slot ingredient tooltips
    (cherry picked from commit 99418d63a5e29cd062bb8e680c2a643f850b983f)

--- Library/src/main/java/mezz/jei/library/gui/ingredients/TagContentTooltipComponent.java
+++ Library/src/main/java/mezz/jei/library/gui/ingredients/TagContentTooltipComponent.java
@@ -2,25 +2,42 @@ package mezz.jei.library.gui.ingredients;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.common.gui.IngredientGridTooltipComponent;
+import mezz.jei.common.util.SafeIngredientUtil;
 
 import java.util.List;
 
-public class TagContentTooltipComponent<T> extends IngredientGridTooltipComponent<T> {
-	private final IIngredientRenderer<T> renderer;
+public class TagContentTooltipComponent extends IngredientGridTooltipComponent<ITypedIngredient<?>> {
+	private final IIngredientManager ingredientManager;
 
-	public TagContentTooltipComponent(IIngredientRenderer<T> renderer, List<T> ingredients) {
+	public TagContentTooltipComponent(IIngredientManager ingredientManager, List<ITypedIngredient<?>> ingredients) {
 		super(ingredients);
-		this.renderer = renderer;
+		this.ingredientManager = ingredientManager;
 	}
 
 	@Override
-	protected void drawIngredient(PoseStack poseStack, T ingredient, int index, int x, int y, boolean hovered) {
-		poseStack.pushPose();
-		{
-			poseStack.translate(x, y, 0);
-			this.renderer.render(poseStack, ingredient);
-		}
-		poseStack.popPose();
+	protected void drawIngredient(
+		PoseStack poseStack,
+		ITypedIngredient<?> ingredient,
+		int index,
+		int x,
+		int y,
+		boolean hovered
+	) {
+		drawIngredient(poseStack, ingredient, x, y);
+	}
+
+	private <T> void drawIngredient(
+		PoseStack poseStack,
+		ITypedIngredient<T> ingredient,
+		int x,
+		int y
+	) {
+		IIngredientType<T> ingredientType = ingredient.getType();
+		IIngredientRenderer<T> renderer = this.ingredientManager.getIngredientRenderer(ingredientType);
+		SafeIngredientUtil.render(poseStack, renderer, ingredient, x, y);
 	}
 }
