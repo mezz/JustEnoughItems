4aa22719fd403f901c2e69322cfb2c1afa0ad7e8
mezz <tehgeek@gmail.com>
2026-08-26 00:18:33 +0900

    Fix #4445 Prioritize cheat input handling

--- Gui/src/main/java/mezz/jei/gui/input/handlers/FocusInputHandler.java
+++ Gui/src/main/java/mezz/jei/gui/input/handlers/FocusInputHandler.java
@@ -6,23 +6,16 @@ import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IRecipesGui;
 import mezz.jei.common.chat.JeiChatItemLinks;
-import mezz.jei.common.config.IClientConfig;
-import mezz.jei.common.config.IClientToggleState;
 import mezz.jei.common.input.IInternalKeyMappings;
-import mezz.jei.common.network.IConnectionToServer;
 import mezz.jei.gui.input.CombinedRecipeFocusSource;
 import mezz.jei.gui.input.IClickableIngredientInternal;
 import mezz.jei.gui.input.IUserInputHandler;
 import mezz.jei.gui.input.UserInput;
 import mezz.jei.gui.overlay.elements.IElement;
-import mezz.jei.gui.util.CommandUtil;
 import mezz.jei.gui.util.FocusUtil;
-import mezz.jei.gui.util.GiveAmount;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,24 +25,17 @@ public class FocusInputHandler implements IUserInputHandler {
 	private final IRecipesGui recipesGui;
 	private final FocusUtil focusUtil;
 	private final IIngredientManager ingredientManager;
-	private final IClientToggleState toggleState;
-	private final CommandUtil commandUtil;
 
 	public FocusInputHandler(
 		CombinedRecipeFocusSource focusSource,
 		IRecipesGui recipesGui,
 		FocusUtil focusUtil,
-		IClientConfig clientConfig,
-		IIngredientManager ingredientManager,
-		IClientToggleState toggleState,
-		IConnectionToServer serverConnection
+		IIngredientManager ingredientManager
 	) {
 		this.focusSource = focusSource;
 		this.recipesGui = recipesGui;
 		this.focusUtil = focusUtil;
 		this.ingredientManager = ingredientManager;
-		this.toggleState = toggleState;
-		this.commandUtil = new CommandUtil(clientConfig, serverConnection);
 	}
 
 	@Override
@@ -57,24 +43,6 @@ public class FocusInputHandler implements IUserInputHandler {
 		Optional<IUserInputHandler> handledClick = handleClick(input, keyBindings);
 		if (handledClick.isPresent()) {
 			return handledClick;
-		}
-
-		if (toggleState.isCheatItemsEnabled()) {
-			if (screen instanceof AbstractContainerScreen) {
-				if (input.is(keyBindings.getCheatItemStack())) {
-					Optional<IUserInputHandler> handler = handleGive(input, keyBindings, GiveAmount.MAX);
-					if (handler.isPresent()) {
-						return handler;
-					}
-				}
-
-				if (input.is(keyBindings.getCheatOneItem())) {
-					Optional<IUserInputHandler> handler = handleGive(input, keyBindings, GiveAmount.ONE);
-					if (handler.isPresent()) {
-						return handler;
-					}
-				}
-			}
 		}
 
 		if (input.is(keyBindings.getShowRecipe())) {
@@ -134,20 +102,5 @@ public class FocusInputHandler implements IUserInputHandler {
 				}
 				return new SameElementInputHandler(this, clicked::isMouseOver);
 			});
-	}
-
-	private Optional<IUserInputHandler> handleGive(UserInput input, IInternalKeyMappings keyBindings, GiveAmount giveAmount) {
-		return focusSource.getIngredientUnderMouse(input, keyBindings)
-			.<IUserInputHandler>mapMulti((clicked, consumer) -> {
-				ItemStack itemStack = clicked.getCheatItemStack(ingredientManager);
-				if (!itemStack.isEmpty()) {
-					if (!input.isSimulate()) {
-						commandUtil.giveStack(itemStack, giveAmount);
-					}
-					IUserInputHandler handler = new SameElementInputHandler(this, clicked::isMouseOver);
-					consumer.accept(handler);
-				}
-			})
-			.findFirst();
 	}
 }
