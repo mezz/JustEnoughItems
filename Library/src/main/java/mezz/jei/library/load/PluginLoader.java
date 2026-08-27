27cd0840c79e1c01e424767c742642a0cf08607b
mezz <tehgeek@gmail.com>
2026-08-28 01:39:04 +0900

    Close #4238 Add context-aware ingredient visibility
    (cherry picked from commit dcaa9d2220f94bf5af5351499489f78f4b78bf00)

--- Library/src/main/java/mezz/jei/library/load/PluginLoader.java
+++ Library/src/main/java/mezz/jei/library/load/PluginLoader.java
@@ -148,7 +148,7 @@ public final class PluginLoader {
 		);
 
 		IClientToggleState toggleState = Internal.getClientToggleState();
-		IngredientBlacklistInternal blacklist = new IngredientBlacklistInternal();
+		IngredientBlacklistInternal blacklist = new IngredientBlacklistInternal(ingredientManager);
 		ingredientManager.registerIngredientListener(blacklist);
 
 		IngredientVisibility ingredientVisibility = new IngredientVisibility(
