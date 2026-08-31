eff864d54e645d44f96ab71544fd451b47069814
mezz <tehgeek@gmail.com>
2026-08-31 22:20:55 +0900

    Disable unbound bookmark preview pinning
    (cherry picked from commit c6c3d52f67227a0ac6b9bfbca30d7251a56519ea)

--- Gui/src/main/java/mezz/jei/gui/overlay/elements/RecipeBookmarkElement.java
+++ Gui/src/main/java/mezz/jei/gui/overlay/elements/RecipeBookmarkElement.java
@@ -138,7 +138,10 @@ public class RecipeBookmarkElement<T, R> implements IElement<R> {
 	}
 
 	public Optional<PreviewTooltipComponent> getInteractivePreview() {
-		if (!getBookmarkTooltipFeatures().contains(BookmarkTooltipFeature.PREVIEW)) {
+		IJeiKeyMappingInternal pauseRecipeCycling = Internal.getKeyMappings().getPauseRecipeCycling();
+		if (pauseRecipeCycling.isUnbound() ||
+			!getBookmarkTooltipFeatures().contains(BookmarkTooltipFeature.PREVIEW)
+		) {
 			return Optional.empty();
 		}
 		TooltipComponent component = getOrCreateTooltipComponent(BookmarkTooltipFeature.PREVIEW);
@@ -184,7 +187,9 @@ public class RecipeBookmarkElement<T, R> implements IElement<R> {
 
 		if (previewAdded && !pinned) {
 			IJeiKeyMappingInternal pauseRecipeCycling = Internal.getKeyMappings().getPauseRecipeCycling();
-			tooltip.addKeyUsageComponent("jei.tooltip.bookmarks.preview.pin.usage", pauseRecipeCycling);
+			if (!pauseRecipeCycling.isUnbound()) {
+				tooltip.addKeyUsageComponent("jei.tooltip.bookmarks.preview.pin.usage", pauseRecipeCycling);
+			}
 		}
 	}
 
