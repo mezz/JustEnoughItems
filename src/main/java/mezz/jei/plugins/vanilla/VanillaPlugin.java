Mysticpasta1/JEI-Async

Fix #2744 Workaround for Optifine bug

src/main/java/mezz/jei/plugins/vanilla/VanillaPlugin.java
@@ -292,7 +292,7 @@ public class VanillaPlugin implements IModPlugin {
 	 * we do not replace it.
 	 */
 	private static List<CraftingRecipe> replaceSpecialCraftingRecipes(List<CraftingRecipe> unhandledCraftingRecipes) {
-		Map<Class<? extends CraftingRecipe>, Supplier<Stream<CraftingRecipe>>> replacers = new IdentityHashMap<>();
+		Map<Class<? extends CraftingRecipe>, Supplier<List<CraftingRecipe>>> replacers = new IdentityHashMap<>();
 		replacers.put(TippedArrowRecipe.class, TippedArrowRecipeMaker::createRecipes);
 		replacers.put(ShulkerBoxColoring.class, ShulkerBoxColoringRecipeMaker::createRecipes);
 		replacers.put(SuspiciousStewRecipe.class, SuspiciousStewRecipeMaker::createRecipes);
@@ -304,9 +304,10 @@ public class VanillaPlugin implements IModPlugin {
 			// distinct + this limit will ensure we stop iterating early if we find all the recipes we're looking for.
 			.limit(replacers.size())
 			.flatMap(recipeClass -> {
-				Supplier<Stream<CraftingRecipe>> supplier = replacers.get(recipeClass);
+				Supplier<List<CraftingRecipe>> supplier = replacers.get(recipeClass);
 				try {
-					return supplier.get();
+					List<CraftingRecipe> results = supplier.get();
+					return results.stream();
 				} catch (RuntimeException e) {
 					LOGGER.error("Failed to create JEI recipes for {}", recipeClass, e);
 					return Stream.of();

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/ShulkerBoxColoringRecipeMaker.java
@@ -16,16 +16,18 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public final class ShulkerBoxColoringRecipeMaker {
 	private static final String group = "jei.shulker.color";
 
-	public static Stream<CraftingRecipe> createRecipes() {
+	public static List<CraftingRecipe> createRecipes() {
 		ItemStack baseShulkerStack = new ItemStack(Blocks.SHULKER_BOX);
 		Ingredient baseShulkerIngredient = Ingredient.of(baseShulkerStack);
 		return Arrays.stream(DyeColor.values())
-			.map(color -> createRecipe(color, baseShulkerIngredient));
+			.map(color -> createRecipe(color, baseShulkerIngredient))
+			.toList();
 	}
 
 	private static CraftingRecipe createRecipe(DyeColor color, Ingredient baseShulkerIngredient) {

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/SuspiciousStewRecipeMaker.java
@@ -18,11 +18,12 @@ import net.minecraft.world.item.crafting.ShapelessRecipe;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerBlock;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public final class SuspiciousStewRecipeMaker {
 
-	public static Stream<CraftingRecipe> createRecipes() {
+	public static List<CraftingRecipe> createRecipes() {
 		String group = "jei.suspicious.stew";
 		Ingredient brownMushroom = Ingredient.of(Blocks.BROWN_MUSHROOM.asItem());
 		Ingredient redMushroom = Ingredient.of(Blocks.RED_MUSHROOM.asItem());
@@ -36,7 +37,7 @@ public final class SuspiciousStewRecipeMaker {
 			.map(item -> ((BlockItem) item).getBlock())
 			.filter(FlowerBlock.class::isInstance)
 			.map(FlowerBlock.class::cast)
-			.map(flowerBlock -> {
+			.<CraftingRecipe>map(flowerBlock -> {
 				Ingredient flower = Ingredient.of(flowerBlock.asItem());
 				NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, brownMushroom, redMushroom, bowl, flower);
 				ItemStack output = new ItemStack(Items.SUSPICIOUS_STEW, 1);
@@ -44,7 +45,8 @@ public final class SuspiciousStewRecipeMaker {
 				SuspiciousStewItem.saveMobEffect(output, mobeffect, flowerBlock.getEffectDuration());
 				ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, "jei.suspicious.stew." + flowerBlock.getDescriptionId());
 				return new ShapelessRecipe(id, group, output, inputs);
-			});
+			})
+			.toList();
 	}
 
 	private SuspiciousStewRecipeMaker() {

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/TippedArrowRecipeMaker.java
@@ -12,17 +12,18 @@ import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.minecraftforge.common.crafting.NBTIngredient;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public final class TippedArrowRecipeMaker {
 
-	public static Stream<CraftingRecipe> createRecipes() {
+	public static List<CraftingRecipe> createRecipes() {
 		String group = "jei.tipped.arrow";
 		ItemStack arrowStack = new ItemStack(Items.ARROW);
 		Ingredient arrowIngredient = Ingredient.of(arrowStack);
 
 		return ForgeRegistries.POTIONS.getValues().stream()
-			.map(potion -> {
+			.<CraftingRecipe>map(potion -> {
 				ItemStack input = PotionUtils.setPotion(new ItemStack(Items.LINGERING_POTION), potion);
 				ItemStack output = PotionUtils.setPotion(new ItemStack(Items.TIPPED_ARROW, 8), potion);
 
@@ -34,7 +35,8 @@ public final class TippedArrowRecipeMaker {
 				);
 				ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, "jei.tipped.arrow." + output.getDescriptionId());
 				return new ShapedRecipe(id, group, 3, 3, inputs, output);
-			});
+			})
+			.toList();
 	}
 
 	private TippedArrowRecipeMaker() {

mezz/JustEnoughItems

(cherry picked from commit dcfab2e4f61c1834971f4a34718f7f426199aded)

src/main/java/mezz/jei/plugins/vanilla/VanillaPlugin.java
@@ -295,7 +295,7 @@ public class VanillaPlugin implements IModPlugin {
 	 * we do not replace it.
 	 */
 	private static List<ICraftingRecipe> replaceSpecialCraftingRecipes(List<ICraftingRecipe> validRecipes) {
-		Map<Class<? extends ICraftingRecipe>, Supplier<Stream<ICraftingRecipe>>> replacers = new IdentityHashMap<>();
+		Map<Class<? extends ICraftingRecipe>, Supplier<List<ICraftingRecipe>>> replacers = new IdentityHashMap<>();
 		replacers.put(TippedArrowRecipe.class, TippedArrowRecipeMaker::createRecipes);
 		replacers.put(ShulkerBoxColoringRecipe.class, ShulkerBoxColoringRecipeMaker::createRecipes);
 		replacers.put(SuspiciousStewRecipe.class, SuspiciousStewRecipeMaker::createRecipes);
@@ -307,9 +307,10 @@ public class VanillaPlugin implements IModPlugin {
 			// distinct + this limit will ensure we stop iterating early if we find all the recipes we're looking for.
 			.limit(replacers.size())
 			.flatMap(recipeClass -> {
-				Supplier<Stream<ICraftingRecipe>> supplier = replacers.get(recipeClass);
+				Supplier<List<ICraftingRecipe>> supplier = replacers.get(recipeClass);
 				try {
-					return supplier.get();
+					List<ICraftingRecipe> results = supplier.get();
+					return results.stream();
 				} catch (RuntimeException e) {
 					LOGGER.error("Failed to create JEI recipes for {}", recipeClass, e);
 					return Stream.of();

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/ShulkerBoxColoringRecipeMaker.java
@@ -16,16 +16,19 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class ShulkerBoxColoringRecipeMaker {
 	private static final String GROUP = "jei.shulker.color";
 
-	public static Stream<ICraftingRecipe> createRecipes() {
+	public static List<ICraftingRecipe> createRecipes() {
 		ItemStack baseShulkerStack = new ItemStack(Blocks.SHULKER_BOX);
 		Ingredient baseShulkerIngredient = Ingredient.of(baseShulkerStack);
 		return Arrays.stream(DyeColor.values())
-			.map(color -> createRecipe(color, baseShulkerIngredient));
+			.map(color -> createRecipe(color, baseShulkerIngredient))
+			.collect(Collectors.toList());
 	}
 
 	private static ICraftingRecipe createRecipe(DyeColor color, Ingredient baseShulkerIngredient) {

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/SuspiciousStewRecipeMaker.java
@@ -14,11 +14,12 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class SuspiciousStewRecipeMaker {
 
-	public static Stream<ICraftingRecipe> createRecipes() {
+	public static List<ICraftingRecipe> createRecipes() {
 		String group = "jei.suspicious.stew";
 		Ingredient brownMushroom = Ingredient.of(Blocks.BROWN_MUSHROOM.asItem());
 		Ingredient redMushroom = Ingredient.of(Blocks.RED_MUSHROOM.asItem());
@@ -27,7 +28,7 @@ public final class SuspiciousStewRecipeMaker {
 		return BlockTags.SMALL_FLOWERS.getValues().stream()
 			.filter(FlowerBlock.class::isInstance)
 			.map(FlowerBlock.class::cast)
-			.map(flowerBlock -> {
+			.<ICraftingRecipe>map(flowerBlock -> {
 				Ingredient flower = Ingredient.of(flowerBlock.asItem());
 				NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, brownMushroom, redMushroom, bowl, flower);
 				ItemStack output = new ItemStack(Items.SUSPICIOUS_STEW, 1);
@@ -35,7 +36,8 @@ public final class SuspiciousStewRecipeMaker {
 				SuspiciousStewItem.saveMobEffect(output, mobeffect, flowerBlock.getEffectDuration());
 				ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, "jei.suspicious.stew." + flowerBlock.getDescriptionId());
 				return new ShapelessRecipe(id, group, output, inputs);
-			});
+			})
+			.collect(Collectors.toList());
 	}
 
 	private SuspiciousStewRecipeMaker() {

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/TippedArrowRecipeMaker.java
@@ -12,14 +12,15 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.crafting.NBTIngredient;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class TippedArrowRecipeMaker {
 
-	public static Stream<ICraftingRecipe> createRecipes() {
+	public static List<ICraftingRecipe> createRecipes() {
 		String group = "jei.tipped.arrow";
 		return ForgeRegistries.POTION_TYPES.getValues().stream()
-			.map(potion -> {
+			.<ICraftingRecipe>map(potion -> {
 				ItemStack arrowStack = new ItemStack(Items.ARROW);
 				ItemStack lingeringPotion = PotionUtils.setPotion(new ItemStack(Items.LINGERING_POTION), potion);
 				Ingredient arrowIngredient = Ingredient.of(arrowStack);
@@ -34,7 +35,8 @@ public final class TippedArrowRecipeMaker {
 				PotionUtils.setPotion(output, potion);
 				ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, "jei.tipped.arrow." + output.getDescriptionId());
 				return new ShapedRecipe(id, group, 3, 3, inputs, output);
-			});
+			})
+			.collect(Collectors.toList());
 	}
 
 	private TippedArrowRecipeMaker() {