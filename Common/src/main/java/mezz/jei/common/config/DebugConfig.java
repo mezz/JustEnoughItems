Mysticpasta1/JEI-Async

Localize Configs

Common/src/main/java/mezz/jei/common/config/DebugConfig.java
@@ -23,36 +23,12 @@ public final class DebugConfig {
 
 	private DebugConfig(IConfigSchemaBuilder schema) {
 		IConfigCategoryBuilder advanced = schema.addCategory("debug");
-		debugModeEnabled = advanced.addBoolean(
-			"DebugMode",
-			false,
-			"Debug mode enabled."
-		);
-		debugGuisEnabled = advanced.addBoolean(
-			"DebugGuis",
-			false,
-			"Debug GUIs enabled."
-		);
-		debugInputsEnabled = advanced.addBoolean(
-			"DebugInputs",
-			false,
-			"Debug inputs enabled."
-		);
-		debugInfoTooltipsEnabled = advanced.addBoolean(
-			"debugInfoTooltipsEnabled",
-			false,
-			"Add debug information to ingredient tooltips when advanced tooltips are enabled."
-		);
-		crashingTestIngredientsEnabled = advanced.addBoolean(
-			"CrashingTestItemsEnabled",
-			false,
-			"Adds ingredients to JEI that intentionally crash, to help debug JEI."
-		);
-		logSuffixTreeStats = advanced.addBoolean(
-			"logSuffixTreeStats",
-			false,
-			"Log information about the suffix trees used for searching, to help debug JEI."
-		);
+		debugModeEnabled = advanced.addBoolean("debugMode", false);
+		debugGuisEnabled = advanced.addBoolean("debugGuis", false);
+		debugInputsEnabled = advanced.addBoolean("debugInputs", false);
+		debugInfoTooltipsEnabled = advanced.addBoolean("debugInfoTooltipsEnabled", false);
+		crashingTestIngredientsEnabled = advanced.addBoolean("crashingTestItemsEnabled", false);
+		logSuffixTreeStats = advanced.addBoolean("logSuffixTreeStats", false);
 	}
 
 	public static boolean isDebugModeEnabled() {

mezz/JustEnoughItems

Adapt the localized schema to preserve 1.18 lookup history, grid layout and navigation, bookmark tooltip, and debug configuration options.
(cherry picked from commit 719c00ecc104fa53d46db0fc7dc6f02f246385cc)

Common/src/main/java/mezz/jei/common/config/DebugConfig.java
@@ -22,31 +22,11 @@ public final class DebugConfig {
 
 	private DebugConfig(IConfigSchemaBuilder schema) {
 		IConfigCategoryBuilder advanced = schema.addCategory("debug");
-		debugIngredientsEnabled = advanced.addBoolean(
-			"debugIngredientsEnabled",
-			false,
-			"Log added and updated ingredients in JEI's ingredient filter."
-		);
-		debugGuisEnabled = advanced.addBoolean(
-			"DebugGuis",
-			false,
-			"Debug GUIs enabled."
-		);
-		debugInputsEnabled = advanced.addBoolean(
-			"DebugInputs",
-			false,
-			"Debug inputs enabled."
-		);
-		debugInfoTooltipsEnabled = advanced.addBoolean(
-			"debugInfoTooltipsEnabled",
-			false,
-			"Add debug information to ingredient tooltips when advanced tooltips are enabled."
-		);
-		logSuffixTreeStats = advanced.addBoolean(
-			"logSuffixTreeStats",
-			false,
-			"Log information about the suffix trees used for searching, to help debug JEI."
-		);
+		debugIngredientsEnabled = advanced.addBoolean("debugIngredientsEnabled", false);
+		debugGuisEnabled = advanced.addBoolean("debugGuis", false);
+		debugInputsEnabled = advanced.addBoolean("debugInputs", false);
+		debugInfoTooltipsEnabled = advanced.addBoolean("debugInfoTooltipsEnabled", false);
+		logSuffixTreeStats = advanced.addBoolean("logSuffixTreeStats", false);
 	}
 
 	public static boolean isDebugIngredientsEnabled() {