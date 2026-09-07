Mysticpasta1/JEI-Async

#3656 Prevent mods from adding null lines to tooltips

Common/src/main/java/mezz/jei/common/gui/JeiTooltip.java
@@ -39,12 +39,24 @@ public class JeiTooltip implements ITooltipBuilder {
 	private @Nullable ITypedIngredient<?> typedIngredient;
 
 	@Override
-	public void add(FormattedText formattedText) {
+	public void add(@Nullable FormattedText formattedText) {
+		if (formattedText == null) {
+			if (Services.PLATFORM.getModHelper().isInDev()) {
+				throw new NullPointerException("Tried to add null tooltip text");
+			}
+			return;
+		}
 		lines.add(Either.left(formattedText));
 	}
 
 	@Override
-	public void add(TooltipComponent component) {
+	public void add(@Nullable TooltipComponent component) {
+		if (component == null) {
+			if (Services.PLATFORM.getModHelper().isInDev()) {
+				throw new NullPointerException("Tried to add null tooltip component");
+			}
+			return;
+		}
 		lines.add(Either.right(component));
 	}
 

mezz/JustEnoughItems

(cherry picked from commit 774bc6d3a0abb3e80b7caace671bc932f2d717d6)

Common/src/main/java/mezz/jei/common/gui/JeiTooltip.java
@@ -70,11 +70,23 @@ public class JeiTooltip implements ITooltipBuilder {
 	}
 
 	@Override
-	public void add(TooltipComponent component) {
+	public void add(@Nullable TooltipComponent component) {
+		if (component == null) {
+			if (Services.PLATFORM.getModHelper().isInDev()) {
+				throw new NullPointerException("Tried to add null tooltip component");
+			}
+			return;
+		}
 		elements.add(Either.right(component));
 	}
 
-	public void addClientTooltipComponent(ClientTooltipComponent component) {
+	public void addClientTooltipComponent(@Nullable ClientTooltipComponent component) {
+		if (component == null) {
+			if (Services.PLATFORM.getModHelper().isInDev()) {
+				throw new NullPointerException("Tried to add null tooltip component");
+			}
+			return;
+		}
 		if (component instanceof TooltipComponent tooltipComponent) {
 			add(tooltipComponent);
 			return;