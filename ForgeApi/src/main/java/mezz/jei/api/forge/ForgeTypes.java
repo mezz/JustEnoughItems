Mysticpasta1/JEI-Async

Add IIngredientManager#getIngredientTypeWithSubtypesFromBase
also add IIngredientTypeWithSubtypes#getDefaultIngredient
These will allow looking up default ingredients from item and block registries

ForgeApi/src/main/java/mezz/jei/api/forge/ForgeTypes.java
@@ -4,6 +4,7 @@ import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.IIngredientTypeWithSubtypes;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidType;
 
 /**
  * Built-in {@link IIngredientType} for Forge Minecraft.
@@ -33,6 +34,11 @@ public final class ForgeTypes {
 		@Override
 		public Fluid getBase(FluidStack ingredient) {
 			return ingredient.getFluid();
+		}
+
+		@Override
+		public FluidStack getDefaultIngredient(Fluid base) {
+			return new FluidStack(base, FluidType.BUCKET_VOLUME);
 		}
 	};
 

mezz/JustEnoughItems

(cherry picked from commit 63ed2ca2851b20ce1f8b2bcb537eca95f8b27f7f)

ForgeApi/src/main/java/mezz/jei/api/forge/ForgeTypes.java
@@ -3,6 +3,7 @@ package mezz.jei.api.forge;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.IIngredientTypeWithSubtypes;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.fluids.FluidAttributes;
 import net.minecraftforge.fluids.FluidStack;
 
 /**
@@ -33,6 +34,11 @@ public final class ForgeTypes {
 		@Override
 		public Fluid getBase(FluidStack ingredient) {
 			return ingredient.getFluid();
+		}
+
+		@Override
+		public FluidStack getDefaultIngredient(Fluid base) {
+			return new FluidStack(base, FluidAttributes.BUCKET_VOLUME);
 		}
 	};
 