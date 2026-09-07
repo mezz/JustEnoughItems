Mysticpasta1/JEI-Async

Fix crash from "Show Recipes" tooltip

src/main/java/mezz/jei/gui/ingredients/GuiIngredient.java
@@ -226,7 +226,7 @@ public class GuiIngredient<T> extends GuiComponent implements IGuiIngredient<T> 
 					tooltip.add(acceptsAny.withStyle(ChatFormatting.GRAY));
 				}
 			}
-			TooltipRenderer.drawHoveringText(poseStack, tooltip, xOffset + mouseX, yOffset + mouseY, value);
+			TooltipRenderer.drawHoveringText(poseStack, tooltip, xOffset + mouseX, yOffset + mouseY, value, ingredientRenderer);
 
 			RenderSystem.enableDepthTest();
 		} catch (RuntimeException e) {

src/main/java/mezz/jei/render/IngredientListElementRenderer.java
@@ -96,7 +96,7 @@ public class IngredientListElementRenderer<T> {
 	public void drawTooltip(PoseStack poseStack, int mouseX, int mouseY, IIngredientFilterConfig ingredientFilterConfig, IWorldConfig worldConfig) {
 		T ingredient = element.getIngredient();
 		List<FormattedText> tooltip = getTooltip(ingredientFilterConfig, worldConfig);
-		TooltipRenderer.drawHoveringText(poseStack, tooltip, mouseX, mouseY, ingredient);
+		TooltipRenderer.drawHoveringText(poseStack, tooltip, mouseX, mouseY, ingredient, ingredientRenderer);
 	}
 
 	protected void renderEditMode(PoseStack poseStack, Rect2i area, int padding, IEditModeConfig editModeConfig) {

mezz/JustEnoughItems

(cherry picked from commit dcd7d2bb9a65ea4204a0a790a05f26fc63106ef1)

src/main/java/mezz/jei/gui/ingredients/GuiIngredient.java
@@ -227,7 +227,7 @@ public class GuiIngredient<T> extends AbstractGui implements IGuiIngredient<T> {
 					tooltip.add(acceptsAny.withStyle(TextFormatting.GRAY));
 				}
 			}
-			TooltipRenderer.drawHoveringText(value, tooltip, xOffset + mouseX, yOffset + mouseY, matrixStack);
+			TooltipRenderer.drawHoveringText(value, tooltip, xOffset + mouseX, yOffset + mouseY, matrixStack, ingredientRenderer);
 
 			RenderSystem.enableDepthTest();
 		} catch (RuntimeException e) {

src/main/java/mezz/jei/render/IngredientListElementRenderer.java
@@ -101,7 +101,7 @@ public class IngredientListElementRenderer<T> {
 	public void drawTooltip(MatrixStack matrixStack, int mouseX, int mouseY, IIngredientFilterConfig ingredientFilterConfig, IWorldConfig worldConfig) {
 		T ingredient = element.getIngredient();
 		List<ITextProperties> tooltip = getTooltip(ingredientFilterConfig, worldConfig);
-		TooltipRenderer.drawHoveringText(ingredient, tooltip, mouseX, mouseY, matrixStack);
+		TooltipRenderer.drawHoveringText(ingredient, tooltip, mouseX, mouseY, matrixStack, ingredientRenderer);
 	}
 
 	protected void renderEditMode(MatrixStack matrixStack, Rectangle2d area, int padding, IEditModeConfig editModeConfig) {