Mysticpasta1/JEI-Async

Add smooth scroll box widget and use it for info recipes

Library/src/main/java/mezz/jei/library/plugins/jei/info/IngredientInfoRecipeCategory.java
@@ -5,7 +5,8 @@ import mezz.jei.api.gui.builder.IIngredientAcceptor;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.widgets.IScrollBoxWidget;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocusGroup;
@@ -14,23 +15,33 @@ import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.vanilla.IJeiIngredientInfoRecipe;
 import mezz.jei.common.gui.textures.Textures;
-import mezz.jei.library.ingredients.IngredientInfoRecipe;
+import mezz.jei.common.util.StringUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FormattedText;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 public class IngredientInfoRecipeCategory implements IRecipeCategory<IJeiIngredientInfoRecipe> {
+	private static final int recipeWidth = 170;
+	private static final int recipeHeight = 125;
+	private static final int lineSpacing = 2;
+
 	private final IDrawable background;
+	private final IGuiHelper guiHelper;
 	private final IDrawable icon;
 	private final IDrawable slotBackground;
 	private final Component localizedName;
 
 	public IngredientInfoRecipeCategory(IGuiHelper guiHelper, Textures textures) {
-		this.background = guiHelper.createBlankDrawable(IngredientInfoRecipe.recipeWidth, IngredientInfoRecipe.recipeHeight);
+		this.background = guiHelper.createBlankDrawable(recipeWidth, recipeHeight);
+		this.guiHelper = guiHelper;
 		this.icon = textures.getInfoIcon();
 		this.slotBackground = guiHelper.getSlotDrawable();
 		this.localizedName = Component.translatable("gui.jei.category.itemInformation");
@@ -57,20 +68,23 @@ public class IngredientInfoRecipeCategory implements IRecipeCategory<IJeiIngredi
 	}
 
 	@Override
-	public void draw(IJeiIngredientInfoRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-		int xPos = 0;
+	public void createRecipeExtras(IRecipeExtrasBuilder builder, IJeiIngredientInfoRecipe recipe, IFocusGroup focuses) {
 		int yPos = slotBackground.getHeight() + 4;
-
-		Minecraft minecraft = Minecraft.getInstance();
-		for (FormattedText descriptionLine : recipe.getDescription()) {
-			guiGraphics.drawString(minecraft.font, Language.getInstance().getVisualOrder(descriptionLine), xPos, yPos, 0xFF000000, false);
-			yPos += minecraft.font.lineHeight + IngredientInfoRecipe.lineSpacing;
-		}
+		int height = recipeHeight - yPos;
+		int width = recipeWidth - guiHelper.getScrollBoxScrollbarExtraWidth();
+		IScrollBoxWidget scrollBoxWidget = guiHelper.createScrollBoxWidget(
+			new Contents(recipe, width),
+			height,
+			0,
+			yPos
+		);
+		builder.addWidget(scrollBoxWidget);
+		builder.addInputHandler(scrollBoxWidget);
 	}
 
 	@Override
 	public void setRecipe(IRecipeLayoutBuilder builder, IJeiIngredientInfoRecipe recipe, IFocusGroup focuses) {
-		int xPos = (IngredientInfoRecipe.recipeWidth - 16) / 2;
+		int xPos = (recipeWidth - 16) / 2;
 
 		IRecipeSlotBuilder inputSlotBuilder = builder.addSlot(RecipeIngredientRole.INPUT, xPos, 1)
 			.setBackground(slotBackground, -1, -1);
@@ -90,5 +104,44 @@ public class IngredientInfoRecipeCategory implements IRecipeCategory<IJeiIngredi
 
 	private static <T> void addIngredient(ITypedIngredient<T> typedIngredient, IIngredientAcceptor<?> slotBuilder) {
 		slotBuilder.addIngredient(typedIngredient.getType(), typedIngredient.getIngredient());
+	}
+
+	private static class Contents implements IDrawable {
+		private final List<FormattedText> descriptionLines;
+		private final int lineHeight;
+		private final int width;
+		private final int height;
+
+		public Contents(IJeiIngredientInfoRecipe recipe, int width) {
+			Minecraft minecraft = Minecraft.getInstance();
+			this.lineHeight = minecraft.font.lineHeight + lineSpacing;
+			this.descriptionLines = StringUtil.splitLines(recipe.getDescription(), width);
+			this.width = width;
+			this.height = lineHeight * descriptionLines.size() - lineSpacing;
+		}
+
+		@Override
+		public int getWidth() {
+			return width;
+		}
+
+		@Override
+		public int getHeight() {
+			return height;
+		}
+
+		@Override
+		public void draw(GuiGraphics guiGraphics, int xOffset, int yOffset) {
+			Language language = Language.getInstance();
+			Minecraft minecraft = Minecraft.getInstance();
+			Font font = minecraft.font;
+
+			int yPos = 0;
+			for (FormattedText descriptionLine : descriptionLines) {
+				FormattedCharSequence charSequence = language.getVisualOrder(descriptionLine);
+				guiGraphics.drawString(font, charSequence, 0, yPos, 0xFF000000, false);
+				yPos += lineHeight;
+			}
+		}
 	}
 }

mezz/JustEnoughItems

(cherry picked from commit 324937f62cc383f3d442cede454b8b72a1af3c7c)

Library/src/main/java/mezz/jei/library/plugins/jei/info/IngredientInfoRecipeCategory.java
@@ -7,7 +7,8 @@ import mezz.jei.api.gui.builder.IIngredientAcceptor;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.widgets.IScrollBoxWidget;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocusGroup;
@@ -16,22 +17,31 @@ import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.library.plugins.RecipeCategoryWithType;
 import mezz.jei.api.recipe.vanilla.IJeiIngredientInfoRecipe;
 import mezz.jei.common.gui.textures.Textures;
-import mezz.jei.library.ingredients.IngredientInfoRecipe;
+import mezz.jei.common.util.StringUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FormattedText;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 public class IngredientInfoRecipeCategory implements RecipeCategoryWithType<IJeiIngredientInfoRecipe> {
+	private static final int recipeWidth = 170;
+	private static final int recipeHeight = 125;
+	private static final int lineSpacing = 2;
 	private final IDrawable background;
+	private final IGuiHelper guiHelper;
 	private final IDrawable icon;
 	private final IDrawable slotBackground;
 	private final Component localizedName;
 
 	public IngredientInfoRecipeCategory(IGuiHelper guiHelper, Textures textures) {
-		this.background = guiHelper.createBlankDrawable(IngredientInfoRecipe.recipeWidth, IngredientInfoRecipe.recipeHeight);
+		this.background = guiHelper.createBlankDrawable(recipeWidth, recipeHeight);
+		this.guiHelper = guiHelper;
 		this.icon = textures.getInfoIcon();
 		this.slotBackground = guiHelper.getSlotDrawable();
 		this.localizedName = new TranslatableComponent("gui.jei.category.itemInformation");
@@ -58,20 +68,23 @@ public class IngredientInfoRecipeCategory implements RecipeCategoryWithType<IJei
 	}
 
 	@Override
-	public void draw(IJeiIngredientInfoRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack poseStack, double mouseX, double mouseY) {
-		int xPos = 0;
+	public void createRecipeExtras(IRecipeExtrasBuilder builder, IJeiIngredientInfoRecipe recipe, IFocusGroup focuses) {
 		int yPos = slotBackground.getHeight() + 4;
-
-		Minecraft minecraft = Minecraft.getInstance();
-		for (FormattedText descriptionLine : recipe.getDescription()) {
-			minecraft.font.draw(poseStack, Language.getInstance().getVisualOrder(descriptionLine), xPos, yPos, 0xFF000000);
-			yPos += minecraft.font.lineHeight + IngredientInfoRecipe.lineSpacing;
-		}
+		int height = recipeHeight - yPos;
+		int width = recipeWidth - guiHelper.getScrollBoxScrollbarExtraWidth();
+		IScrollBoxWidget scrollBoxWidget = guiHelper.createScrollBoxWidget(
+			new Contents(recipe, width),
+			height,
+			0,
+			yPos
+		);
+		builder.addWidget(scrollBoxWidget);
+		builder.addInputHandler(scrollBoxWidget);
 	}
 
 	@Override
 	public void setRecipe(IRecipeLayoutBuilder builder, IJeiIngredientInfoRecipe recipe, IFocusGroup focuses) {
-		int xPos = (IngredientInfoRecipe.recipeWidth - 16) / 2;
+		int xPos = (recipeWidth - 16) / 2;
 
 		IRecipeSlotBuilder inputSlotBuilder = builder.addSlot(RecipeIngredientRole.INPUT, xPos, 1)
 			.setBackground(slotBackground, -1, -1);
@@ -91,5 +104,44 @@ public class IngredientInfoRecipeCategory implements RecipeCategoryWithType<IJei
 
 	private static <T> void addIngredient(ITypedIngredient<T> typedIngredient, IIngredientAcceptor<?> slotBuilder) {
 		slotBuilder.addIngredient(typedIngredient.getType(), typedIngredient.getIngredient());
+	}
+
+	private static class Contents implements IDrawable {
+		private final List<FormattedText> descriptionLines;
+		private final int lineHeight;
+		private final int width;
+		private final int height;
+
+		public Contents(IJeiIngredientInfoRecipe recipe, int width) {
+			Minecraft minecraft = Minecraft.getInstance();
+			this.lineHeight = minecraft.font.lineHeight + lineSpacing;
+			this.descriptionLines = StringUtil.splitLines(recipe.getDescription(), width);
+			this.width = width;
+			this.height = lineHeight * descriptionLines.size() - lineSpacing;
+		}
+
+		@Override
+		public int getWidth() {
+			return width;
+		}
+
+		@Override
+		public int getHeight() {
+			return height;
+		}
+
+		@Override
+		public void draw(PoseStack poseStack, int xOffset, int yOffset) {
+			Language language = Language.getInstance();
+			Minecraft minecraft = Minecraft.getInstance();
+			Font font = minecraft.font;
+
+			int yPos = 0;
+			for (FormattedText descriptionLine : descriptionLines) {
+				FormattedCharSequence charSequence = language.getVisualOrder(descriptionLine);
+				font.draw(poseStack, charSequence, xOffset, yOffset + yPos, 0xFF000000);
+				yPos += lineHeight;
+			}
+		}
 	}
 }