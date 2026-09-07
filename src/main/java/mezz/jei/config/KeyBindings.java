Mysticpasta1/JEI-Async

Fix #2812 Add a keybind for the key to exit a recipe screen to the inventory

Forge/src/main/java/mezz/jei/config/KeyBindings.java
@@ -35,6 +35,7 @@ public final class KeyBindings {
 	public static final KeyMapping nextCategory;
 	public static final KeyMapping previousRecipePage;
 	public static final KeyMapping nextRecipePage;
+	public static final KeyMapping closeRecipeGui;
 
 	public static final KeyMapping previousPage;
 	public static final KeyMapping nextPage;
@@ -123,6 +124,7 @@ public final class KeyBindings {
 			nextRecipePage = new KeyMapping("key.jei.nextRecipePage", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_PAGE_DOWN), recipeCategoryName),
 			previousCategory = new KeyMapping("key.jei.previousCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_UP), recipeCategoryName),
 			nextCategory = new KeyMapping("key.jei.nextCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_DOWN), recipeCategoryName),
+			closeRecipeGui = new KeyMapping("key.jei.closeRecipeGui", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_ESCAPE), recipeCategoryName),
 
 			// Dev Tools
 			copyRecipeId = new KeyMapping("key.jei.copy.recipe.id", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_UNKNOWN), devToolsCategoryName)

mezz/JustEnoughItems

(cherry picked from commit 3d1852a1f713fb79260a6670a0551437f8a536db)

src/main/java/mezz/jei/config/KeyBindings.java
@@ -27,6 +27,7 @@ public final class KeyBindings {
 	public static final KeyBinding nextPage;
 	public static final KeyBinding previousCategory;
 	public static final KeyBinding nextCategory;
+	public static final KeyBinding closeRecipeGui;
 	public static final KeyBinding bookmark;
 	public static final KeyBinding toggleBookmarkOverlay;
 	public static final KeyBinding copyRecipeId;
@@ -49,6 +50,7 @@ public final class KeyBindings {
 			nextPage = new KeyBinding("key.jei.nextPage", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_PAGE_DOWN), categoryName),
 			previousCategory = new KeyBinding("key.jei.previousCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_UP), categoryName),
 			nextCategory = new KeyBinding("key.jei.nextCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_DOWN), categoryName),
+			closeRecipeGui = new KeyBinding("key.jei.closeRecipeGui", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_ESCAPE), categoryName),
 			bookmark = new KeyBinding("key.jei.bookmark", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_A), categoryName),
 			toggleBookmarkOverlay = new KeyBinding("key.jei.toggleBookmarkOverlay", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_UNKNOWN), categoryName),
 			copyRecipeId = new KeyBinding("key.jei.copy.recipe.id", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_UNKNOWN), categoryName)