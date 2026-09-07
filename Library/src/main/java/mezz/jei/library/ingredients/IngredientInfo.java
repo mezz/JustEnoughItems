Mysticpasta1/JEI-Async

Fix #4068 Make itemstack data in the ingredient manager immutable

Library/src/main/java/mezz/jei/library/ingredients/IngredientInfo.java
@@ -1,5 +1,6 @@
 package mezz.jei.library.ingredients;
 
+import com.google.common.collect.Collections2;
 import com.mojang.serialization.Codec;
 import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientRenderer;
@@ -20,12 +21,12 @@ public class IngredientInfo<T> {
 	private final IIngredientHelper<T> ingredientHelper;
 	private final IIngredientRenderer<T> ingredientRenderer;
 	private final Codec<T> ingredientCodec;
-	private final IngredientSet<T> ingredientSet;
+	private final TypedIngredientSet<T> ingredientSet;
 	private final ListMultiMap<Object, String> aliases;
 
 	public IngredientInfo(
 		IIngredientType<T> ingredientType,
-		Collection<T> ingredients,
+		Collection<ITypedIngredient<T>> ingredients,
 		IIngredientHelper<T> ingredientHelper,
 		IIngredientRenderer<T> ingredientRenderer,
 		@Nullable Codec<T> ingredientCodec
@@ -40,7 +41,7 @@ public class IngredientInfo<T> {
 		this.ingredientRenderer = ingredientRenderer;
 		this.ingredientCodec = ingredientCodec;
 
-		this.ingredientSet = new IngredientSet<>(ingredientHelper, UidContext.Ingredient);
+		this.ingredientSet = new TypedIngredientSet<>(ingredientHelper, UidContext.Ingredient);
 		this.ingredientSet.addAll(ingredients);
 
 		this.aliases = new ListMultiMap<>();
@@ -63,22 +64,29 @@ public class IngredientInfo<T> {
 	}
 
 	@Unmodifiable
-	public Collection<T> getAllIngredients() {
+	public Collection<ITypedIngredient<T>> getAllTypedIngredients() {
 		return Collections.unmodifiableCollection(ingredientSet);
 	}
 
-	public void addIngredients(Collection<T> ingredients) {
+	@Unmodifiable
+	public Collection<T> getAllIngredients() {
+		Collection<T> transform = Collections2.transform(ingredientSet, ITypedIngredient::getIngredient);
+		return Collections.unmodifiableCollection(transform);
+	}
+
+	public void addIngredients(Collection<ITypedIngredient<T>> ingredients) {
 		this.ingredientSet.addAll(ingredients);
 	}
 
-	public void removeIngredients(Collection<T> ingredients) {
+	public void removeIngredients(Collection<ITypedIngredient<T>> ingredients) {
 		this.ingredientSet.removeAll(ingredients);
 	}
 
 	@SuppressWarnings({"removal"})
 	@Deprecated(forRemoval = true)
 	public Optional<T> getIngredientByLegacyUid(String uid) {
-		return ingredientSet.getByLegacyUid(uid);
+		return ingredientSet.getByLegacyUid(uid)
+			.map(ITypedIngredient::getIngredient);
 	}
 
 	@Unmodifiable

Library/src/main/java/mezz/jei/library/ingredients/TypedIngredientSet.java
@@ -1,0 +1,132 @@
+package mezz.jei.library.ingredients;
+
+import mezz.jei.api.ingredients.IIngredientHelper;
+import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.ingredients.subtypes.UidContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class TypedIngredientSet<T> extends AbstractSet<ITypedIngredient<T>> {
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	private final IIngredientHelper<T> ingredientHelper;
+	private final UidContext context;
+	private final Map<Object, ITypedIngredient<T>> ingredients;
+
+	public TypedIngredientSet(IIngredientHelper<T> ingredientHelper, UidContext context) {
+		this.ingredientHelper = ingredientHelper;
+		this.context = context;
+		this.ingredients = new LinkedHashMap<>();
+	}
+
+	@Nullable
+	private Object getUid(ITypedIngredient<T> typedIngredient) {
+		try {
+			return ingredientHelper.getUid(typedIngredient, context);
+		} catch (RuntimeException e) {
+			try {
+				String ingredientInfo = ingredientHelper.getErrorInfo(typedIngredient.getIngredient());
+				LOGGER.warn("Found a broken ingredient {}", ingredientInfo, e);
+			} catch (RuntimeException e2) {
+				LOGGER.warn("Found a broken ingredient.", e2);
+			}
+			return null;
+		}
+	}
+
+	@Override
+	public boolean add(ITypedIngredient<T> value) {
+		Object uid = getUid(value);
+		return uid != null && ingredients.put(uid, value) == null;
+	}
+
+	@Override
+	public boolean remove(Object value) {
+		if (value instanceof ITypedIngredient<?> typedIngredient) {
+			IIngredientType<?> type = typedIngredient.getType();
+			if (this.ingredientHelper.getIngredientType().equals(type)) {
+				@SuppressWarnings("unchecked")
+				ITypedIngredient<T> cast = (ITypedIngredient<T>) typedIngredient;
+				Object uid = getUid(cast);
+				return uid != null && ingredients.remove(uid) != null;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		Objects.requireNonNull(c);
+		boolean modified = false;
+		for (Object value : c) {
+			modified |= remove(value);
+		}
+		return modified;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends ITypedIngredient<T>> c) {
+		Objects.requireNonNull(c);
+		boolean modified = false;
+		for (ITypedIngredient<T> value : c) {
+			modified |= add(value);
+		}
+		return modified;
+	}
+
+	@Override
+	public boolean contains(Object value) {
+		if (value instanceof ITypedIngredient<?> typedIngredient) {
+			IIngredientType<?> type = typedIngredient.getType();
+			if (this.ingredientHelper.getIngredientType().equals(type)) {
+				@SuppressWarnings("unchecked")
+				ITypedIngredient<T> cast = (ITypedIngredient<T>) typedIngredient;
+				Object uid = getUid(cast);
+				return uid != null && ingredients.containsKey(uid);
+			}
+		}
+		return false;
+	}
+
+	@SuppressWarnings("removal")
+	@Deprecated(forRemoval = true)
+	public Optional<ITypedIngredient<T>> getByLegacyUid(String uid) {
+		ITypedIngredient<T> v = ingredients.get(uid);
+		if (v != null) {
+			return Optional.of(v);
+		}
+
+		for (ITypedIngredient<T> typedIngredient : ingredients.values()) {
+			String legacyUid = ingredientHelper.getUniqueId(typedIngredient.getIngredient(), context);
+			if (uid.equals(legacyUid)) {
+				return Optional.of(typedIngredient);
+			}
+		}
+		return Optional.empty();
+	}
+
+	@Override
+	public void clear() {
+		ingredients.clear();
+	}
+
+	@Override
+	public Iterator<ITypedIngredient<T>> iterator() {
+		return ingredients.values().iterator();
+	}
+
+	@Override
+	public int size() {
+		return ingredients.size();
+	}
+}

mezz/JustEnoughItems

Backport the API additions as version 10.8.0 by storing typed ingredients while retaining the default getAllTypedIngredients compatibility bridge. Copy 1.18 NBT when typed stacks are created and materialized, and bootstrap registries in the target tests.
(cherry picked from commit 0b45f9cda174cbec2e7d729366ebf7cac790851d)

Library/src/main/java/mezz/jei/library/ingredients/IngredientInfo.java
@@ -1,5 +1,6 @@
 package mezz.jei.library.ingredients;
 
+import com.google.common.collect.Collections2;
 import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientRenderer;
 import mezz.jei.api.ingredients.IIngredientType;
@@ -20,16 +21,21 @@ public class IngredientInfo<T> {
 	private final IIngredientType<T> ingredientType;
 	private final IIngredientHelper<T> ingredientHelper;
 	private final IIngredientRenderer<T> ingredientRenderer;
-	private final IngredientSet<T> ingredientSet;
+	private final TypedIngredientSet<T> ingredientSet;
 	private final ListMultiMap<String, String> aliases;
 	private final ListMultiMap<Object, String> baseAliases;
 
-	public IngredientInfo(IIngredientType<T> ingredientType, Collection<T> ingredients, IIngredientHelper<T> ingredientHelper, IIngredientRenderer<T> ingredientRenderer) {
+	public IngredientInfo(
+		IIngredientType<T> ingredientType,
+		Collection<ITypedIngredient<T>> ingredients,
+		IIngredientHelper<T> ingredientHelper,
+		IIngredientRenderer<T> ingredientRenderer
+	) {
 		this.ingredientType = ingredientType;
 		this.ingredientHelper = ingredientHelper;
 		this.ingredientRenderer = ingredientRenderer;
 
-		this.ingredientSet = new IngredientSet<>(ingredientHelper, UidContext.Ingredient);
+		this.ingredientSet = new TypedIngredientSet<>(ingredientHelper, UidContext.Ingredient);
 		this.ingredientSet.addAll(ingredients);
 
 		this.aliases = new ListMultiMap<>();
@@ -49,20 +55,27 @@ public class IngredientInfo<T> {
 	}
 
 	@Unmodifiable
-	public Collection<T> getAllIngredients() {
+	public Collection<ITypedIngredient<T>> getAllTypedIngredients() {
 		return Collections.unmodifiableCollection(ingredientSet);
 	}
 
-	public void addIngredients(Collection<T> ingredients) {
+	@Unmodifiable
+	public Collection<T> getAllIngredients() {
+		Collection<T> transform = Collections2.transform(ingredientSet, ITypedIngredient::getIngredient);
+		return Collections.unmodifiableCollection(transform);
+	}
+
+	public void addIngredients(Collection<ITypedIngredient<T>> ingredients) {
 		this.ingredientSet.addAll(ingredients);
 	}
 
-	public void removeIngredients(Collection<T> ingredients) {
+	public void removeIngredients(Collection<ITypedIngredient<T>> ingredients) {
 		this.ingredientSet.removeAll(ingredients);
 	}
 
 	public Optional<T> getIngredientByUid(String uid) {
-		return ingredientSet.getByUid(uid);
+		return ingredientSet.getByUid(uid)
+			.map(ITypedIngredient::getIngredient);
 	}
 
 	@Unmodifiable

Library/src/main/java/mezz/jei/library/ingredients/TypedIngredientSet.java
@@ -1,0 +1,120 @@
+package mezz.jei.library.ingredients;
+
+import mezz.jei.api.ingredients.IIngredientHelper;
+import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.ingredients.subtypes.UidContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class TypedIngredientSet<T> extends AbstractSet<ITypedIngredient<T>> {
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	private final IIngredientHelper<T> ingredientHelper;
+	private final UidContext context;
+	private final Map<String, ITypedIngredient<T>> ingredients;
+
+	public TypedIngredientSet(IIngredientHelper<T> ingredientHelper, UidContext context) {
+		this.ingredientHelper = ingredientHelper;
+		this.context = context;
+		this.ingredients = new LinkedHashMap<>();
+	}
+
+	@Nullable
+	private String getUid(ITypedIngredient<T> typedIngredient) {
+		try {
+			return ingredientHelper.getUniqueId(typedIngredient, context);
+		} catch (RuntimeException e) {
+			try {
+				String ingredientInfo = ingredientHelper.getErrorInfo(typedIngredient.getIngredient());
+				LOGGER.warn("Found a broken ingredient {}", ingredientInfo, e);
+			} catch (RuntimeException e2) {
+				LOGGER.warn("Found a broken ingredient.", e2);
+			}
+			return null;
+		}
+	}
+
+	@Override
+	public boolean add(ITypedIngredient<T> value) {
+		String uid = getUid(value);
+		return uid != null && ingredients.put(uid, value) == null;
+	}
+
+	@Override
+	public boolean remove(Object value) {
+		if (value instanceof ITypedIngredient<?> typedIngredient) {
+			IIngredientType<?> type = typedIngredient.getType();
+			if (this.ingredientHelper.getIngredientType().equals(type)) {
+				@SuppressWarnings("unchecked")
+				ITypedIngredient<T> cast = (ITypedIngredient<T>) typedIngredient;
+				String uid = getUid(cast);
+				return uid != null && ingredients.remove(uid) != null;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		Objects.requireNonNull(c);
+		boolean modified = false;
+		for (Object value : c) {
+			modified |= remove(value);
+		}
+		return modified;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends ITypedIngredient<T>> c) {
+		Objects.requireNonNull(c);
+		boolean modified = false;
+		for (ITypedIngredient<T> value : c) {
+			modified |= add(value);
+		}
+		return modified;
+	}
+
+	@Override
+	public boolean contains(Object value) {
+		if (value instanceof ITypedIngredient<?> typedIngredient) {
+			IIngredientType<?> type = typedIngredient.getType();
+			if (this.ingredientHelper.getIngredientType().equals(type)) {
+				@SuppressWarnings("unchecked")
+				ITypedIngredient<T> cast = (ITypedIngredient<T>) typedIngredient;
+				String uid = getUid(cast);
+				return uid != null && ingredients.containsKey(uid);
+			}
+		}
+		return false;
+	}
+
+	public Optional<ITypedIngredient<T>> getByUid(String uid) {
+		ITypedIngredient<T> v = ingredients.get(uid);
+		return Optional.ofNullable(v);
+	}
+
+	@Override
+	public void clear() {
+		ingredients.clear();
+	}
+
+	@Override
+	public Iterator<ITypedIngredient<T>> iterator() {
+		return ingredients.values().iterator();
+	}
+
+	@Override
+	public int size() {
+		return ingredients.size();
+	}
+}