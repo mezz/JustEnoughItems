Mysticpasta1/JEI-Async

Expose control of the configs to the API

Forge/src/test/java/mezz/jei/test/IngredientFilterTest.java
@@ -8,7 +8,7 @@ import mezz.jei.api.runtime.IEditModeConfig;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IIngredientVisibility;
 import mezz.jei.common.util.Translator;
-import mezz.jei.core.config.IWorldConfig;
+import mezz.jei.common.config.IWorldConfig;
 import mezz.jei.gui.config.IClientConfig;
 import mezz.jei.gui.filter.FilterTextSource;
 import mezz.jei.gui.filter.IFilterTextSource;

mezz/JustEnoughItems

(cherry picked from commit 2c830f825626242f927bcd45d021949f9afef670)

Forge/src/test/java/mezz/jei/test/IngredientFilterTest.java
@@ -8,8 +8,8 @@ import mezz.jei.api.runtime.IEditModeConfig;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IIngredientVisibility;
 import mezz.jei.common.config.IClientConfig;
+import mezz.jei.common.config.WorldConfig;
 import mezz.jei.core.config.IWorldConfig;
-import mezz.jei.core.config.WorldConfig;
 import mezz.jei.core.search.BakedSubstringIndexBuilder;
 import mezz.jei.gui.filter.FilterTextSource;
 import mezz.jei.gui.filter.IFilterTextSource;