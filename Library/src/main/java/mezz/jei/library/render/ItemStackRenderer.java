Mysticpasta1/JEI-Async

Add API support for batch-rendering ingredients (#3711)
Add basic batch-rendering for ItemStacks

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -3,8 +3,10 @@ package mezz.jei.library.render;
 import com.mojang.blaze3d.systems.RenderSystem;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.rendering.BatchRenderElement;
 import mezz.jei.common.platform.IPlatformRenderHelper;
 import mezz.jei.common.platform.Services;
+import mezz.jei.library.render.batch.ItemStackBatchRendererCache;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
@@ -18,6 +20,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
+	private final ItemStackBatchRendererCache batchRenderer = new ItemStackBatchRendererCache();
+
 	@Override
 	public void render(GuiGraphics guiGraphics, @Nullable ItemStack ingredient) {
 		render(guiGraphics, ingredient, 0, 0);
@@ -34,6 +38,11 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 			guiGraphics.renderItemDecorations(font, ingredient, posX, posY);
 			RenderSystem.disableBlend();
 		}
+	}
+
+	@Override
+	public void renderBatch(GuiGraphics guiGraphics, List<BatchRenderElement<ItemStack>> batchRenderElements) {
+		batchRenderer.renderBatch(guiGraphics, batchRenderElements);
 	}
 
 	@SuppressWarnings("removal")

Library/src/main/java/mezz/jei/library/render/batch/LimitedQuadItemModel.java
@@ -1,0 +1,71 @@
+package mezz.jei.library.render.batch;
+
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.ItemOverrides;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class LimitedQuadItemModel implements BakedModel {
+	private final BakedModel bakedmodel;
+	private @Nullable List<BakedQuad> quads;
+
+	public LimitedQuadItemModel(BakedModel bakedmodel) {
+		this.bakedmodel = bakedmodel;
+	}
+
+	@Override
+	public List<BakedQuad> getQuads(@Nullable BlockState blockState, @Nullable Direction direction, RandomSource randomSource) {
+		if (direction == null) {
+			if (quads == null) {
+				quads = bakedmodel.getQuads(blockState, null, randomSource)
+					.stream()
+					.filter(q -> q.getDirection() == Direction.SOUTH)
+					.toList();
+			}
+			return quads;
+		}
+		return List.of();
+	}
+
+	@Override
+	public boolean useAmbientOcclusion() {
+		return bakedmodel.useAmbientOcclusion();
+	}
+
+	@Override
+	public boolean isGui3d() {
+		return bakedmodel.isGui3d();
+	}
+
+	@Override
+	public boolean usesBlockLight() {
+		return bakedmodel.usesBlockLight();
+	}
+
+	@Override
+	public boolean isCustomRenderer() {
+		return bakedmodel.isCustomRenderer();
+	}
+
+	@Override
+	public TextureAtlasSprite getParticleIcon() {
+		return bakedmodel.getParticleIcon();
+	}
+
+	@Override
+	public ItemTransforms getTransforms() {
+		return bakedmodel.getTransforms();
+	}
+
+	@Override
+	public ItemOverrides getOverrides() {
+		return bakedmodel.getOverrides();
+	}
+}

mezz/JustEnoughItems

Adapted for 1.18.2 by keeping the existing PoseStack-based API and overlay code.
(cherry picked from commit f4d357714e3146e032848cafd4e67155fcd4139f)

Library/src/main/java/mezz/jei/library/render/ItemStackRenderer.java
@@ -5,9 +5,11 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.ingredients.IIngredientRenderer;
+import mezz.jei.api.ingredients.rendering.BatchRenderElement;
 import mezz.jei.common.platform.IPlatformRenderHelper;
 import mezz.jei.common.platform.Services;
 import mezz.jei.common.util.ErrorUtil;
+import mezz.jei.library.render.batch.ItemStackBatchRendererCache;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -26,6 +28,7 @@ import java.util.List;
 
 public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 	private static final Logger LOGGER = LogManager.getLogger();
+	private final ItemStackBatchRendererCache batchRenderer = new ItemStackBatchRendererCache();
 
 	@Override
 	public void render(PoseStack poseStack, @Nullable ItemStack ingredient) {
@@ -53,6 +56,11 @@ public class ItemStackRenderer implements IIngredientRenderer<ItemStack> {
 			// Restore model-view matrix now that the item has been rendered
 			RenderSystem.applyModelViewMatrix();
 		}
+	}
+
+	@Override
+	public void renderBatch(PoseStack poseStack, List<BatchRenderElement<ItemStack>> batchRenderElements) {
+		batchRenderer.renderBatch(poseStack, batchRenderElements);
 	}
 
 	@SuppressWarnings("removal")

Library/src/main/java/mezz/jei/library/render/batch/LimitedQuadItemModel.java
@@ -1,0 +1,71 @@
+package mezz.jei.library.render.batch;
+
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.ItemOverrides;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Random;
+
+public class LimitedQuadItemModel implements BakedModel {
+	private final BakedModel bakedmodel;
+	private @Nullable List<BakedQuad> quads;
+
+	public LimitedQuadItemModel(BakedModel bakedmodel) {
+		this.bakedmodel = bakedmodel;
+	}
+
+	@Override
+	public List<BakedQuad> getQuads(@Nullable BlockState blockState, @Nullable Direction direction, Random random) {
+		if (direction == null) {
+			if (quads == null) {
+				quads = bakedmodel.getQuads(blockState, null, random)
+					.stream()
+					.filter(q -> q.getDirection() == Direction.SOUTH)
+					.toList();
+			}
+			return quads;
+		}
+		return List.of();
+	}
+
+	@Override
+	public boolean useAmbientOcclusion() {
+		return bakedmodel.useAmbientOcclusion();
+	}
+
+	@Override
+	public boolean isGui3d() {
+		return bakedmodel.isGui3d();
+	}
+
+	@Override
+	public boolean usesBlockLight() {
+		return bakedmodel.usesBlockLight();
+	}
+
+	@Override
+	public boolean isCustomRenderer() {
+		return bakedmodel.isCustomRenderer();
+	}
+
+	@Override
+	public TextureAtlasSprite getParticleIcon() {
+		return bakedmodel.getParticleIcon();
+	}
+
+	@Override
+	public ItemTransforms getTransforms() {
+		return bakedmodel.getTransforms();
+	}
+
+	@Override
+	public ItemOverrides getOverrides() {
+		return bakedmodel.getOverrides();
+	}
+}