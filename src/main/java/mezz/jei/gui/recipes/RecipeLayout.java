Mysticpasta1/JEI-Async

Fix #2718 Recipe Category draw from plugins can corrupt the pose stack

src/main/java/mezz/jei/gui/recipes/RecipeLayout.java
@@ -163,9 +163,17 @@ public class RecipeLayout<R> {
 			int height = categoryBackground.getHeight() + (2 * RECIPE_BORDER_PADDING);
 			recipeBorder.draw(poseStack, -RECIPE_BORDER_PADDING, -RECIPE_BORDER_PADDING, width, height);
 			background.draw(poseStack);
-			recipeCategory.draw(recipe, recipeSlots.getView(), poseStack, recipeMouseX, recipeMouseY);
-			// drawExtras and drawInfo often render text which messes with the color, this clears it
-			RenderSystem.setShaderColor(1, 1, 1, 1);
+
+			// defensive push/pop to protect against recipe categories changing the last pose
+			poseStack.pushPose();
+			{
+				recipeCategory.draw(recipe, recipeSlots.getView(), poseStack, recipeMouseX, recipeMouseY);
+
+				// drawExtras and drawInfo often render text which messes with the color, this clears it
+				RenderSystem.setShaderColor(1, 1, 1, 1);
+			}
+			poseStack.popPose();
+
 			if (shapelessIcon != null) {
 				shapelessIcon.draw(poseStack);
 			}

mezz/JustEnoughItems

(cherry picked from commit 083a575e59bb96e4b8150aedb76b6a815a926624)

src/main/java/mezz/jei/gui/recipes/RecipeLayout.java
@@ -169,9 +169,17 @@ public class RecipeLayout<T> implements IRecipeLayoutDrawable {
 			int height = categoryBackground.getHeight() + (2 * RECIPE_BORDER_PADDING);
 			recipeBorder.draw(matrixStack, -RECIPE_BORDER_PADDING, -RECIPE_BORDER_PADDING, width, height);
 			background.draw(matrixStack);
-			recipeCategory.draw(recipe, matrixStack, recipeMouseX, recipeMouseY);
-			// drawExtras and drawInfo often render text which messes with the color, this clears it
-			RenderSystem.color4f(1, 1, 1, 1);
+
+			// defensive push/pop to protect against recipe categories changing the last pose
+			matrixStack.pushPose();
+			{
+				recipeCategory.draw(recipe, matrixStack, recipeMouseX, recipeMouseY);
+
+				// drawExtras and drawInfo often render text which messes with the color, this clears it
+				RenderSystem.color4f(1, 1, 1, 1);
+			}
+			matrixStack.popPose();
+
 			if (shapelessIcon != null) {
 				shapelessIcon.draw(matrixStack, background.getWidth());
 			}