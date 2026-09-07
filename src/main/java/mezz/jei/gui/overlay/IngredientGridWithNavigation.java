Mysticpasta1/JEI-Async

Close #3190 Add support for ghost ingredient dragging from bookmarks

Gui/src/main/java/mezz/jei/gui/overlay/IngredientGridWithNavigation.java
@@ -3,6 +3,8 @@ package mezz.jei.gui.overlay;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.api.runtime.IScreenHelper;
 import mezz.jei.common.gui.elements.DrawableNineSliceTexture;
 import mezz.jei.common.gui.textures.Textures;
 import mezz.jei.common.input.IClickableIngredientInternal;
@@ -14,6 +16,8 @@ import mezz.jei.common.config.IClientToggleState;
 import mezz.jei.gui.PageNavigation;
 import mezz.jei.common.config.IClientConfig;
 import mezz.jei.common.config.IIngredientGridConfig;
+import mezz.jei.gui.ghost.GhostIngredientDragManager;
+import mezz.jei.gui.input.IDragHandler;
 import mezz.jei.gui.input.IPaged;
 import mezz.jei.gui.input.IRecipeFocusSource;
 import mezz.jei.gui.input.IUserInputHandler;
@@ -57,6 +61,7 @@ public class IngredientGridWithNavigation implements IRecipeFocusSource {
 	private final DrawableNineSliceTexture background;
 	private final DrawableNineSliceTexture slotBackground;
 	private final CommandUtil commandUtil;
+	private final GhostIngredientDragManager ghostIngredientDragManager;
 
 	private ImmutableRect2i backgroundArea = ImmutableRect2i.EMPTY;
 	private ImmutableRect2i slotBackgroundArea = ImmutableRect2i.EMPTY;
@@ -72,7 +77,9 @@ public class IngredientGridWithNavigation implements IRecipeFocusSource {
 		DrawableNineSliceTexture background,
 		DrawableNineSliceTexture slotBackground,
 		Textures textures,
-		CheatUtil cheatUtil
+		CheatUtil cheatUtil,
+		IScreenHelper screenHelper,
+		IIngredientManager ingredientManager
 	) {
 		this.toggleState = toggleState;
 		this.clientConfig = clientConfig;
@@ -85,6 +92,7 @@ public class IngredientGridWithNavigation implements IRecipeFocusSource {
 		this.background = background;
 		this.slotBackground = slotBackground;
 		this.commandUtil = new CommandUtil(clientConfig, serverConnection);
+		this.ghostIngredientDragManager = new GhostIngredientDragManager(this, screenHelper, ingredientManager, toggleState);
 
 		this.ingredientSource.addSourceListChangedListener(() -> updateLayout(true));
 	}
@@ -234,6 +242,7 @@ public class IngredientGridWithNavigation implements IRecipeFocusSource {
 	}
 
 	public void drawTooltips(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY) {
+		this.ghostIngredientDragManager.drawTooltips(minecraft, poseStack, mouseX, mouseY);
 		this.ingredientGrid.drawTooltips(minecraft, poseStack, mouseX, mouseY);
 	}
 
@@ -262,6 +271,18 @@ public class IngredientGridWithNavigation implements IRecipeFocusSource {
 
 	public boolean isEmpty() {
 		return this.ingredientSource.getIngredientList().isEmpty();
+	}
+
+	public void close() {
+		this.ghostIngredientDragManager.stopDrag();
+	}
+
+	public void drawOnForeground(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY) {
+		this.ghostIngredientDragManager.drawOnForeground(minecraft, poseStack, mouseX, mouseY);
+	}
+
+	public IDragHandler createDragHandler() {
+		return this.ghostIngredientDragManager.createDragHandler();
 	}
 
 	private class IngredientGridPaged implements IPaged {

Gui/src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
@@ -5,7 +5,6 @@ import mezz.jei.api.gui.handlers.IGuiProperties;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.runtime.IIngredientListOverlay;
-import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IScreenHelper;
 import mezz.jei.common.gui.textures.Textures;
 import mezz.jei.common.input.IClickableIngredientInternal;
@@ -19,7 +18,6 @@ import mezz.jei.gui.GuiProperties;
 import mezz.jei.common.config.IClientConfig;
 import mezz.jei.gui.elements.GuiIconToggleButton;
 import mezz.jei.gui.filter.IFilterTextSource;
-import mezz.jei.gui.ghost.GhostIngredientDragManager;
 import mezz.jei.gui.input.GuiTextFieldFilter;
 import mezz.jei.gui.input.ICharTypedHandler;
 import mezz.jei.gui.input.IDragHandler;
@@ -58,13 +56,11 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 	private final GuiTextFieldFilter searchField;
 	private final IInternalKeyMappings keyBindings;
 	private final CheatUtil cheatUtil;
-	private final GhostIngredientDragManager ghostIngredientDragManager;
 	private final ScreenPropertiesCache screenPropertiesCache;
 
 	public IngredientListOverlay(
 		IIngredientGridSource ingredientGridSource,
 		IFilterTextSource filterTextSource,
-		IIngredientManager ingredientManager,
 		IScreenHelper screenHelper,
 		IngredientGridWithNavigation contents,
 		IClientConfig clientConfig,
@@ -95,7 +91,6 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 		});
 
 		this.configButton = ConfigButton.create(this::isListDisplayed, toggleState, textures, keyBindings);
-		this.ghostIngredientDragManager = new GhostIngredientDragManager(this.contents, screenHelper, ingredientManager, toggleState);
 	}
 
 	@Override
@@ -123,7 +118,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 				Set<ImmutableRect2i> guiExclusionAreas = screenPropertiesCache.getGuiExclusionAreas();
 				updateBounds(guiProperties, displayArea, guiExclusionAreas);
 			}, () -> {
-				this.ghostIngredientDragManager.stopDrag();
+				this.contents.close();
 				this.searchField.setFocused(false);
 			});
 	}
@@ -187,7 +182,6 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 
 	public void drawTooltips(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY) {
 		if (isListDisplayed()) {
-			this.ghostIngredientDragManager.drawTooltips(minecraft, poseStack, mouseX, mouseY);
 			this.contents.drawTooltips(minecraft, poseStack, mouseX, mouseY);
 		}
 		if (this.screenPropertiesCache.hasValidScreen()) {
@@ -195,15 +189,9 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 		}
 	}
 
-	public void drawOnForeground(Minecraft minecraft, PoseStack poseStack, AbstractContainerScreen<?> gui, int mouseX, int mouseY) {
+	public void drawOnForeground(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY) {
 		if (isListDisplayed()) {
-			poseStack.pushPose();
-			{
-				IPlatformScreenHelper screenHelper = Services.PLATFORM.getScreenHelper();
-				poseStack.translate(-screenHelper.getGuiLeft(gui), -screenHelper.getGuiTop(gui), 0);
-				this.ghostIngredientDragManager.drawOnForeground(minecraft, poseStack, mouseX, mouseY);
-			}
-			poseStack.popPose();
+			this.contents.drawOnForeground(minecraft, poseStack, mouseX, mouseY);
 		}
 	}
 
@@ -243,7 +231,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IRecipeFoc
 	}
 
 	public IDragHandler createDragHandler() {
-		final IDragHandler displayedDragHandler = this.ghostIngredientDragManager.createDragHandler();
+		final IDragHandler displayedDragHandler = this.contents.createDragHandler();
 
 		return new ProxyDragHandler(() -> {
 			if (isListDisplayed()) {

mezz/JustEnoughItems

Also folds follow-up source commit c72473fb1.
(cherry picked from commit d11e1491f6d4945151536edb5e37b57a3e8439aa)

src/main/java/mezz/jei/gui/overlay/IngredientGridWithNavigation.java
@@ -24,10 +24,13 @@ import mezz.jei.config.IWorldConfig;
 import mezz.jei.config.KeyBindings;
 import mezz.jei.gui.GuiScreenHelper;
 import mezz.jei.gui.PageNavigation;
+import mezz.jei.gui.ghost.GhostIngredientDragManager;
 import mezz.jei.gui.ghost.IGhostIngredientDragSource;
 import mezz.jei.gui.ingredients.IIngredientListElement;
 import mezz.jei.gui.recipes.RecipesGui;
+import mezz.jei.ingredients.IngredientManager;
 import mezz.jei.input.IClickedIngredient;
+import mezz.jei.input.IMouseDragHandler;
 import mezz.jei.input.IMouseHandler;
 import mezz.jei.input.IPaged;
 import mezz.jei.input.IShowsRecipeFocuses;
@@ -54,6 +57,7 @@ public class IngredientGridWithNavigation implements IShowsRecipeFocuses, IGhost
 	private final IClientConfig clientConfig;
 	private final IngredientGrid ingredientGrid;
 	private final IIngredientGridSource ingredientSource;
+	private final GhostIngredientDragManager ghostIngredientDragManager;
 	private final IMouseHandler mouseHandler;
 	private Rectangle2d area = new Rectangle2d(0, 0, 0, 0);
 
@@ -62,6 +66,7 @@ public class IngredientGridWithNavigation implements IShowsRecipeFocuses, IGhost
 		IFilterTextSource filterTextSource,
 		GuiScreenHelper guiScreenHelper,
 		IngredientGrid ingredientGrid,
+		IngredientManager ingredientManager,
 		IWorldConfig worldConfig,
 		IClientConfig clientConfig
 	) {
@@ -71,6 +76,7 @@ public class IngredientGridWithNavigation implements IShowsRecipeFocuses, IGhost
 		this.ingredientGrid = ingredientGrid;
 		this.ingredientSource = ingredientSource;
 		this.guiScreenHelper = guiScreenHelper;
+		this.ghostIngredientDragManager = new GhostIngredientDragManager(this, guiScreenHelper, ingredientManager, worldConfig);
 		this.pageDelegate = new IngredientGridPaged();
 		this.navigation = new PageNavigation(this.pageDelegate, false);
 		this.mouseHandler = new CombinedMouseHandler(this.pageDelegate, this.ingredientGrid.getMouseHandler(), this.navigation.getMouseHandler());
@@ -135,7 +141,20 @@ public class IngredientGridWithNavigation implements IShowsRecipeFocuses, IGhost
 	}
 
 	public void drawTooltips(Minecraft minecraft, MatrixStack matrixStack, int mouseX, int mouseY) {
+		this.ghostIngredientDragManager.drawTooltips(minecraft, matrixStack, mouseX, mouseY);
 		this.ingredientGrid.drawTooltips(minecraft, matrixStack, mouseX, mouseY);
+	}
+
+	public void close() {
+		this.ghostIngredientDragManager.stopDrag();
+	}
+
+	public void drawOnForeground(Minecraft minecraft, MatrixStack matrixStack, int mouseX, int mouseY) {
+		this.ghostIngredientDragManager.drawOnForeground(minecraft, matrixStack, mouseX, mouseY);
+	}
+
+	public IMouseDragHandler getMouseDragHandler() {
+		return this.ghostIngredientDragManager.getMouseDragHandler();
 	}
 
 	public boolean isMouseOver(double mouseX, double mouseY) {

src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
@@ -11,9 +11,7 @@ import mezz.jei.config.IWorldConfig;
 import mezz.jei.config.KeyBindings;
 import mezz.jei.gui.GuiScreenHelper;
 import mezz.jei.gui.elements.GuiIconToggleButton;
-import mezz.jei.gui.ghost.GhostIngredientDragManager;
 import mezz.jei.gui.ingredients.IIngredientListElement;
-import mezz.jei.ingredients.IngredientManager;
 import mezz.jei.input.CombinedMouseHandler;
 import mezz.jei.input.GuiTextFieldFilter;
 import mezz.jei.input.IClickedIngredient;
@@ -49,7 +47,6 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 	private final IWorldConfig worldConfig;
 	private final GuiScreenHelper guiScreenHelper;
 	private final GuiTextFieldFilter searchField;
-	private final GhostIngredientDragManager ghostIngredientDragManager;
 	private final IMouseHandler displayedMouseHandler;
 	private final IMouseHandler hiddenMouseHandler;
 	private Rectangle2d displayArea = new Rectangle2d(0, 0, 0, 0);
@@ -61,7 +58,6 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 
 	public IngredientListOverlay(
 		IIngredientGridSource ingredientGridSource,
-		IngredientManager ingredientManager,
 		GuiScreenHelper guiScreenHelper,
 		IngredientGridWithNavigation contents,
 		IClientConfig clientConfig,
@@ -74,7 +70,6 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 		ingredientGridSource.addListener(() -> onSetFilterText(worldConfig.getFilterText()));
 		this.searchField = new GuiTextFieldFilter(ingredientGridSource, worldConfig);
 		this.configButton = ConfigButton.create(this, worldConfig);
-		this.ghostIngredientDragManager = new GhostIngredientDragManager(this.contents, guiScreenHelper, ingredientManager, worldConfig);
 
 		this.displayedMouseHandler = new CombinedMouseHandler(
 				this.configButton.getMouseHandler(),
@@ -108,7 +103,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 			if (this.guiProperties != null) {
 				this.guiProperties = null;
 				clearKeyboardFocus();
-				this.ghostIngredientDragManager.stopDrag();
+				this.contents.close();
 			}
 		} else {
 			final boolean guiPropertiesChanged = this.guiProperties == null || !GuiProperties.areEqual(this.guiProperties, guiProperties);
@@ -126,7 +121,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 		this.guiProperties = guiProperties;
 		this.displayArea = createDisplayArea(guiProperties);
 		if (guiPropertiesChanged) {
-			this.ghostIngredientDragManager.stopDrag();
+			this.contents.close();
 		}
 
 		final boolean searchBarCentered = isSearchBarCentered(this.clientConfig, guiProperties);
@@ -196,7 +191,6 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 
 	public void drawTooltips(Minecraft minecraft, MatrixStack matrixStack, int mouseX, int mouseY) {
 		if (isListDisplayed()) {
-			this.ghostIngredientDragManager.drawTooltips(minecraft, matrixStack, mouseX, mouseY);
 			this.contents.drawTooltips(minecraft, matrixStack, mouseX, mouseY);
 		}
 		if (this.guiProperties != null) {
@@ -208,7 +202,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 		if (isListDisplayed()) {
 			matrixStack.pushPose();
 			matrixStack.translate(-gui.getGuiLeft(), -gui.getGuiTop(), 0);
-			this.ghostIngredientDragManager.drawOnForeground(minecraft, matrixStack, mouseX, mouseY);
+			this.contents.drawOnForeground(minecraft, matrixStack, mouseX, mouseY);
 			matrixStack.popPose();
 		}
 	}
@@ -262,7 +256,7 @@ public class IngredientListOverlay implements IIngredientListOverlay, IShowsReci
 	public IMouseDragHandler getMouseDragHandler() {
 		return new ProxyMouseDragHandler(() -> {
 			if (isListDisplayed()) {
-				return this.ghostIngredientDragManager.getMouseDragHandler();
+				return this.contents.getMouseDragHandler();
 			}
 			return NullMouseDragHandler.INSTANCE;
 		});