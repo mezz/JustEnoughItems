Mysticpasta1/JEI-Async

Fix #2744 Workaround for Optifine bug

src/main/java/mezz/jei/plugins/vanilla/VanillaPlugin.java
@@ -94,6 +94,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.IdentityHashMap;
@@ -104,6 +106,8 @@ import java.util.stream.Stream;
 
 @JeiPlugin
 public class VanillaPlugin implements IModPlugin {
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	@Nullable
 	private CraftingRecipeCategory craftingCategory;
 	@Nullable
@@ -299,8 +303,15 @@ public class VanillaPlugin implements IModPlugin {
 			.filter(replacers::containsKey)
 			// distinct + this limit will ensure we stop iterating early if we find all the recipes we're looking for.
 			.limit(replacers.size())
-			.map(replacers::get)
-			.flatMap(Supplier::get)
+			.flatMap(recipeClass -> {
+				Supplier<Stream<CraftingRecipe>> supplier = replacers.get(recipeClass);
+				try {
+					return supplier.get();
+				} catch (RuntimeException e) {
+					LOGGER.error("Failed to create JEI recipes for {}", recipeClass, e);
+					return Stream.of();
+				}
+			})
 			.toList();
 	}
 }

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/ShulkerBoxColoringRecipeMaker.java
@@ -19,29 +19,31 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public final class ShulkerBoxColoringRecipeMaker {
+	private static final String group = "jei.shulker.color";
 
 	public static Stream<CraftingRecipe> createRecipes() {
-		String group = "jei.shulker.color";
 		ItemStack baseShulkerStack = new ItemStack(Blocks.SHULKER_BOX);
 		Ingredient baseShulkerIngredient = Ingredient.of(baseShulkerStack);
 		return Arrays.stream(DyeColor.values())
-			.map(color -> {
-				DyeItem dye = DyeItem.byColor(color);
-				ItemStack dyeStack = new ItemStack(dye);
-				TagKey<Item> colorTag = color.getTag();
-				Ingredient.Value dyeList = new Ingredient.ItemValue(dyeStack);
-				Ingredient.Value colorList = new Ingredient.TagValue(colorTag);
-				Stream<Ingredient.Value> colorIngredientStream = Stream.of(dyeList, colorList);
-				// Shulker box special recipe allows the matching dye item or any item in the tag.
-				// we need to specify both in case someone removes the dye item from the dye tag
-				// as the item will still be valid for this recipe.
-				Ingredient colorIngredient = Ingredient.fromValues(colorIngredientStream);
-				NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, baseShulkerIngredient, colorIngredient);
-				Block coloredShulkerBox = ShulkerBoxBlock.getBlockByColor(color);
-				ItemStack output = new ItemStack(coloredShulkerBox);
-				ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, "jei.shulker.color." + output.getDescriptionId());
-				return new ShapelessRecipe(id, group, output, inputs);
-			});
+			.map(color -> createRecipe(color, baseShulkerIngredient));
+	}
+
+	private static CraftingRecipe createRecipe(DyeColor color, Ingredient baseShulkerIngredient) {
+		DyeItem dye = DyeItem.byColor(color);
+		ItemStack dyeStack = new ItemStack(dye);
+		TagKey<Item> colorTag = color.getTag();
+		Ingredient.Value dyeList = new Ingredient.ItemValue(dyeStack);
+		Ingredient.Value colorList = new Ingredient.TagValue(colorTag);
+		Stream<Ingredient.Value> colorIngredientStream = Stream.of(dyeList, colorList);
+		// Shulker box special recipe allows the matching dye item or any item in the tag.
+		// we need to specify both in case someone removes the dye item from the dye tag
+		// as the item will still be valid for this recipe.
+		Ingredient colorIngredient = Ingredient.fromValues(colorIngredientStream);
+		NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, baseShulkerIngredient, colorIngredient);
+		Block coloredShulkerBox = ShulkerBoxBlock.getBlockByColor(color);
+		ItemStack output = new ItemStack(coloredShulkerBox);
+		ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, group + "." + output.getDescriptionId());
+		return new ShapelessRecipe(id, group, output, inputs);
 	}
 
 	private ShulkerBoxColoringRecipeMaker() {

mezz/JustEnoughItems

(cherry picked from commit d925b2ef8acf8db3226fcc6369236f90c911b1b0)

src/main/java/mezz/jei/plugins/vanilla/VanillaPlugin.java
@@ -94,6 +94,9 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.IdentityHashMap;
@@ -105,6 +108,8 @@ import java.util.stream.Stream;
 
 @JeiPlugin
 public class VanillaPlugin implements IModPlugin {
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	@Nullable
 	private CraftingRecipeCategory craftingCategory;
 	@Nullable
@@ -301,8 +306,15 @@ public class VanillaPlugin implements IModPlugin {
 			.filter(replacers::containsKey)
 			// distinct + this limit will ensure we stop iterating early if we find all the recipes we're looking for.
 			.limit(replacers.size())
-			.map(replacers::get)
-			.flatMap(Supplier::get)
+			.flatMap(recipeClass -> {
+				Supplier<Stream<ICraftingRecipe>> supplier = replacers.get(recipeClass);
+				try {
+					return supplier.get();
+				} catch (RuntimeException e) {
+					LOGGER.error("Failed to create JEI recipes for {}", recipeClass, e);
+					return Stream.of();
+				}
+			})
 			.collect(Collectors.toList());
 	}
 }

src/main/java/mezz/jei/plugins/vanilla/crafting/replacers/ShulkerBoxColoringRecipeMaker.java
@@ -19,29 +19,31 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public final class ShulkerBoxColoringRecipeMaker {
+	private static final String GROUP = "jei.shulker.color";
 
 	public static Stream<ICraftingRecipe> createRecipes() {
-		String group = "jei.shulker.color";
 		ItemStack baseShulkerStack = new ItemStack(Blocks.SHULKER_BOX);
 		Ingredient baseShulkerIngredient = Ingredient.of(baseShulkerStack);
 		return Arrays.stream(DyeColor.values())
-			.map(color -> {
-				DyeItem dye = DyeItem.byColor(color);
-				ItemStack dyeStack = new ItemStack(dye);
-				ITag<Item> colorTag = color.getTag();
-				Ingredient.IItemList dyeList = new Ingredient.SingleItemList(dyeStack);
-				Ingredient.IItemList colorList = new Ingredient.TagList(colorTag);
-				Stream<Ingredient.IItemList> colorIngredientStream = Stream.of(dyeList, colorList);
-				// Shulker box special recipe allows the matching dye item or any item in the tag.
-				// we need to specify both in case someone removes the dye item from the dye tag
-				// as the item will still be valid for this recipe.
-				Ingredient colorIngredient = Ingredient.fromValues(colorIngredientStream);
-				NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, baseShulkerIngredient, colorIngredient);
-				Block coloredShulkerBox = ShulkerBoxBlock.getBlockByColor(color);
-				ItemStack output = new ItemStack(coloredShulkerBox);
-				ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, "jei.shulker.color." + output.getDescriptionId());
-				return new ShapelessRecipe(id, group, output, inputs);
-			});
+			.map(color -> createRecipe(color, baseShulkerIngredient));
+	}
+
+	private static ICraftingRecipe createRecipe(DyeColor color, Ingredient baseShulkerIngredient) {
+		DyeItem dye = DyeItem.byColor(color);
+		ItemStack dyeStack = new ItemStack(dye);
+		ITag<Item> colorTag = color.getTag();
+		Ingredient.IItemList dyeList = new Ingredient.SingleItemList(dyeStack);
+		Ingredient.IItemList colorList = new Ingredient.TagList(colorTag);
+		Stream<Ingredient.IItemList> colorIngredientStream = Stream.of(dyeList, colorList);
+		// Shulker box special recipe allows the matching dye item or any item in the tag.
+		// we need to specify both in case someone removes the dye item from the dye tag
+		// as the item will still be valid for this recipe.
+		Ingredient colorIngredient = Ingredient.fromValues(colorIngredientStream);
+		NonNullList<Ingredient> inputs = NonNullList.of(Ingredient.EMPTY, baseShulkerIngredient, colorIngredient);
+		Block coloredShulkerBox = ShulkerBoxBlock.getBlockByColor(color);
+		ItemStack output = new ItemStack(coloredShulkerBox);
+		ResourceLocation id = new ResourceLocation(ModIds.MINECRAFT_ID, GROUP + "." + output.getDescriptionId());
+		return new ShapelessRecipe(id, GROUP, output, inputs);
 	}
 
 	private ShulkerBoxColoringRecipeMaker() {