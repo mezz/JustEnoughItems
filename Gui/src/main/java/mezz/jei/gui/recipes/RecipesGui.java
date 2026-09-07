Mysticpasta1/JEI-Async

Fix #3931 Simplify tooltip handling and allow modifying rich tooltips

Gui/src/main/java/mezz/jei/gui/recipes/RecipesGui.java
@@ -272,9 +272,7 @@ public class RecipesGui extends Screen implements IRecipesGui, IRecipeFocusSourc
 		hoveredRecipeCatalyst.ifPresent(h -> h.drawHoverOverlays(guiGraphics));
 
 		hoveredRecipeCatalyst.ifPresent(h -> {
-			JeiTooltip tooltip = new JeiTooltip();
-			h.getTooltip(tooltip);
-			tooltip.draw(guiGraphics, mouseX, mouseY);
+			h.drawTooltip(guiGraphics, mouseX, mouseY);
 		});
 		RenderSystem.enableDepthTest();
 

mezz/JustEnoughItems

Backport to 1.18.2 using PoseStack rendering and the target branch legacy tooltip contracts. Preserve separate legacy component and rich element collections, use plain renderer tooltips for search, and register the chat ingredient preview component on Forge.
(cherry picked from commit 241349009918bb9ed48f341f78bfe0a86d05ed6f)

Gui/src/main/java/mezz/jei/gui/recipes/RecipesGui.java
@@ -288,9 +288,7 @@ public class RecipesGui extends Screen implements IRecipesGui, IRecipeFocusSourc
 		hoveredRecipeLayout.ifPresent(l -> l.drawOverlays(poseStack, mouseX, mouseY));
 
 		hoveredRecipeCatalyst.ifPresent(h -> {
-			JeiTooltip tooltip = new JeiTooltip();
-			h.getTooltip(tooltip);
-			tooltip.draw(poseStack, mouseX, mouseY);
+			h.drawTooltip(poseStack, mouseX, mouseY);
 		});
 		RenderSystem.enableDepthTest();
 