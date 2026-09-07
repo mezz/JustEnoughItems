Mysticpasta1/JEI-Async

Add a hotkey to copy a recipe id to the clipboard

src/main/java/mezz/jei/config/KeyBindings.java
@@ -20,6 +20,7 @@ public final class KeyBindings {
 	private static final String editModeCategoryName = Translator.translateToLocal("jei.key.category.edit.mode");
 	private static final String recipeCategoryName = Translator.translateToLocal("jei.key.category.recipe.gui");
 	private static final String searchCategoryName = Translator.translateToLocal("jei.key.category.search");
+	private static final String devToolsCategoryName = Translator.translateToLocal("jei.key.category.dev.tools");
 	private static final String jeiHiddenInternalCategoryName = "jei.key.category.hidden.internal";
 
 	public static final KeyMapping toggleOverlay;
@@ -53,6 +54,8 @@ public final class KeyBindings {
 	public static final KeyMapping hoveredClearSearchBar;
 	public static final KeyMapping previousSearch;
 	public static final KeyMapping nextSearch;
+
+	public static final KeyMapping copyRecipeId;
 
 	private static final List<KeyMapping> allBindings;
 
@@ -119,7 +122,10 @@ public final class KeyBindings {
 			previousRecipePage = new KeyMapping("key.jei.previousRecipePage", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_PAGE_UP), recipeCategoryName),
 			nextRecipePage = new KeyMapping("key.jei.nextRecipePage", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_PAGE_DOWN), recipeCategoryName),
 			previousCategory = new KeyMapping("key.jei.previousCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_UP), recipeCategoryName),
-			nextCategory = new KeyMapping("key.jei.nextCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_DOWN), recipeCategoryName)
+			nextCategory = new KeyMapping("key.jei.nextCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_DOWN), recipeCategoryName),
+
+			// Dev Tools
+			copyRecipeId = new KeyMapping("key.jei.copy.recipe.id", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_UNKNOWN), devToolsCategoryName)
 		);
 
 		showRecipe = List.of(showRecipe1, showRecipe2);

mezz/JustEnoughItems

(cherry picked from commit 2bdc5dbf3097507d5666ea013a42daa71a294ee8)

src/main/java/mezz/jei/config/KeyBindings.java
@@ -29,6 +29,7 @@ public final class KeyBindings {
 	public static final KeyBinding nextCategory;
 	public static final KeyBinding bookmark;
 	public static final KeyBinding toggleBookmarkOverlay;
+	public static final KeyBinding copyRecipeId;
 	private static final List<KeyBinding> allBindings;
 
 	static InputMappings.Input getKey(int key) {
@@ -49,7 +50,8 @@ public final class KeyBindings {
 			previousCategory = new KeyBinding("key.jei.previousCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_UP), categoryName),
 			nextCategory = new KeyBinding("key.jei.nextCategory", KeyConflictContext.GUI, KeyModifier.SHIFT, getKey(GLFW.GLFW_KEY_PAGE_DOWN), categoryName),
 			bookmark = new KeyBinding("key.jei.bookmark", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_A), categoryName),
-			toggleBookmarkOverlay = new KeyBinding("key.jei.toggleBookmarkOverlay", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_UNKNOWN), categoryName)
+			toggleBookmarkOverlay = new KeyBinding("key.jei.toggleBookmarkOverlay", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_UNKNOWN), categoryName),
+			copyRecipeId = new KeyBinding("key.jei.copy.recipe.id", KeyConflictContext.GUI, getKey(GLFW.GLFW_KEY_UNKNOWN), categoryName)
 		);
 	}
 