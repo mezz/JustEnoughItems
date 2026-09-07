Mysticpasta1/JEI-Async

Simplify creation of scrollbox widgets

Common/src/main/java/mezz/jei/common/gui/elements/DrawableWrappedText.java
@@ -1,0 +1,53 @@
+package mezz.jei.common.gui.elements;
+
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.common.util.StringUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.locale.Language;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.util.FormattedCharSequence;
+
+import java.util.List;
+
+public class DrawableWrappedText implements IDrawable {
+	private static final int lineSpacing = 2;
+
+	private final List<FormattedText> descriptionLines;
+	private final int lineHeight;
+	private final int width;
+	private final int height;
+
+	public DrawableWrappedText(List<FormattedText> text, int maxWidth) {
+		Minecraft minecraft = Minecraft.getInstance();
+		this.lineHeight = minecraft.font.lineHeight + lineSpacing;
+		this.descriptionLines = StringUtil.splitLines(text, maxWidth);
+		this.width = maxWidth;
+		this.height = lineHeight * descriptionLines.size() - lineSpacing;
+	}
+
+	@Override
+	public int getWidth() {
+		return width;
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public void draw(GuiGraphics guiGraphics, int xOffset, int yOffset) {
+		Language language = Language.getInstance();
+		Minecraft minecraft = Minecraft.getInstance();
+		Font font = minecraft.font;
+
+		int yPos = 0;
+		for (FormattedText descriptionLine : descriptionLines) {
+			FormattedCharSequence charSequence = language.getVisualOrder(descriptionLine);
+			guiGraphics.drawString(font, charSequence, 0, yPos, 0xFF000000, false);
+			yPos += lineHeight;
+		}
+	}
+}

Library/src/main/java/mezz/jei/library/gui/widgets/AbstractScrollWidget.java
@@ -31,7 +31,9 @@ public abstract class AbstractScrollWidget implements IRecipeWidget, IJeiInputHa
 		);
 	}
 
-	private final ScreenRectangle area;
+	protected final ScreenRectangle area;
+	protected final ScreenRectangle contentsArea;
+
 	private final ImmutableRect2i scrollArea;
 	private final DrawableNineSliceTexture scrollbarMarker;
 	private final DrawableNineSliceTexture scrollbarBackground;
@@ -50,6 +52,12 @@ public abstract class AbstractScrollWidget implements IRecipeWidget, IJeiInputHa
 		Textures textures = Internal.getTextures();
 		this.scrollbarMarker = textures.getScrollbarMarker();
 		this.scrollbarBackground = textures.getScrollbarBackground();
+		this.contentsArea = new ScreenRectangle(
+			0,
+			0,
+			area.width() - getScrollBoxScrollbarExtraWidth(),
+			area.height()
+		);
 	}
 
 	protected ImmutableRect2i calculateScrollbarMarkerArea() {

mezz/JustEnoughItems

(cherry picked from commit 6fcdbcede2da4cfbb189e28518798987f8a7dd97)

Common/src/main/java/mezz/jei/common/gui/elements/DrawableWrappedText.java
@@ -1,0 +1,53 @@
+package mezz.jei.common.gui.elements;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.common.util.StringUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.locale.Language;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.util.FormattedCharSequence;
+
+import java.util.List;
+
+public class DrawableWrappedText implements IDrawable {
+	private static final int lineSpacing = 2;
+
+	private final List<FormattedText> descriptionLines;
+	private final int lineHeight;
+	private final int width;
+	private final int height;
+
+	public DrawableWrappedText(List<FormattedText> text, int maxWidth) {
+		Minecraft minecraft = Minecraft.getInstance();
+		this.lineHeight = minecraft.font.lineHeight + lineSpacing;
+		this.descriptionLines = StringUtil.splitLines(text, maxWidth);
+		this.width = maxWidth;
+		this.height = lineHeight * descriptionLines.size() - lineSpacing;
+	}
+
+	@Override
+	public int getWidth() {
+		return width;
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public void draw(PoseStack poseStack, int xOffset, int yOffset) {
+		Language language = Language.getInstance();
+		Minecraft minecraft = Minecraft.getInstance();
+		Font font = minecraft.font;
+
+		int yPos = 0;
+		for (FormattedText descriptionLine : descriptionLines) {
+			FormattedCharSequence charSequence = language.getVisualOrder(descriptionLine);
+			font.draw(poseStack, charSequence, xOffset, yOffset + yPos, 0xFF000000);
+			yPos += lineHeight;
+		}
+	}
+}

Library/src/main/java/mezz/jei/library/gui/widgets/AbstractScrollWidget.java
@@ -30,7 +30,9 @@ public abstract class AbstractScrollWidget implements IRecipeWidget, IJeiInputHa
 		);
 	}
 
-	private final Rect2i area;
+	protected final Rect2i area;
+	protected final ImmutableRect2i contentsArea;
+
 	private final ImmutableRect2i scrollArea;
 	private final DrawableNineSliceTexture scrollbarMarker;
 	private final DrawableNineSliceTexture scrollbarBackground;
@@ -49,6 +51,12 @@ public abstract class AbstractScrollWidget implements IRecipeWidget, IJeiInputHa
 		Textures textures = Internal.getTextures();
 		this.scrollbarMarker = textures.getScrollbarMarker();
 		this.scrollbarBackground = textures.getScrollbarBackground();
+		this.contentsArea = new ImmutableRect2i(
+			0,
+			0,
+			area.getWidth() - getScrollBoxScrollbarExtraWidth(),
+			area.getHeight()
+		);
 	}
 
 	protected ImmutableRect2i calculateScrollbarMarkerArea() {