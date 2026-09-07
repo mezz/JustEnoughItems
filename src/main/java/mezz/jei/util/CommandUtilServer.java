Mysticpasta1/JEI-Async

Make the give notification message work for any player that has a server

Forge/src/main/java/mezz/jei/util/CommandUtilServer.java
@@ -149,9 +149,9 @@ public final class CommandUtilServer {
 			giveCount = itemStack.getCount();
 		}
 
-		if (giveCount > 0 && sender instanceof ServerPlayer serverPlayerEntity) {
+		if (giveCount > 0) {
 			itemStackCopy.setCount(giveCount);
-			notifyGive(serverPlayerEntity, itemStackCopy);
+			notifyGive(sender, itemStackCopy);
 			containerMenu.broadcastChanges();
 		}
 	}
@@ -185,11 +185,14 @@ public final class CommandUtilServer {
 		notifyGive(entityplayermp, itemStackCopy);
 	}
 
-	private static void notifyGive(Player entityPlayerMP, ItemStack stack) {
-		CommandSourceStack commandSource = entityPlayerMP.createCommandSourceStack();
+	private static void notifyGive(Player player, ItemStack stack) {
+		if (player.getServer() == null) {
+			return;
+		}
+		CommandSourceStack commandSource = player.createCommandSourceStack();
 		int count = stack.getCount();
 		Component stackTextComponent = stack.getDisplayName();
-		Component displayName = entityPlayerMP.getDisplayName();
+		Component displayName = player.getDisplayName();
 		TranslatableComponent message = new TranslatableComponent("commands.give.success.single", count, stackTextComponent, displayName);
 		commandSource.sendSuccess(message, true);
 	}

mezz/JustEnoughItems

(cherry picked from commit 9db2be0b4ed1dead1c2ea167bc07202b4ffc4dab)

src/main/java/mezz/jei/util/CommandUtilServer.java
@@ -147,11 +147,13 @@ public final class CommandUtilServer {
 			giveCount = itemStack.getCount();
 		}
 
-		if (giveCount > 0 && sender instanceof ServerPlayerEntity) {
-			ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) sender;
+		if (giveCount > 0) {
 			itemStackCopy.setCount(giveCount);
-			notifyGive(serverPlayerEntity, itemStackCopy);
-			serverPlayerEntity.broadcastCarriedItem();
+			notifyGive(sender, itemStackCopy);
+			sender.inventoryMenu.broadcastChanges();
+			if (sender instanceof ServerPlayerEntity) {
+				((ServerPlayerEntity) sender).broadcastCarriedItem();
+			}
 		}
 	}
 
@@ -191,11 +193,14 @@ public final class CommandUtilServer {
 		notifyGive(entityplayermp, itemStackCopy);
 	}
 
-	private static void notifyGive(PlayerEntity entityPlayerMP, ItemStack stack) {
-		CommandSource commandSource = entityPlayerMP.createCommandSourceStack();
+	private static void notifyGive(PlayerEntity player, ItemStack stack) {
+		if (player.getServer() == null) {
+			return;
+		}
+		CommandSource commandSource = player.createCommandSourceStack();
 		int count = stack.getCount();
 		ITextComponent stackTextComponent = stack.getDisplayName();
-		ITextComponent displayName = entityPlayerMP.getDisplayName();
+		ITextComponent displayName = player.getDisplayName();
 		TranslationTextComponent message = new TranslationTextComponent("commands.give.success.single", count, stackTextComponent, displayName);
 		commandSource.sendSuccess(message, true);
 	}