Mysticpasta1/JEI-Async

Show recipe previews on bookmark tooltips (#3582)

Common/src/main/java/mezz/jei/common/gui/TooltipRenderer.java
@@ -14,6 +14,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
@@ -50,6 +51,30 @@ public final class TooltipRenderer {
 			CrashReport crashReport = ErrorUtil.createIngredientCrashReport(e, "Rendering ingredient tooltip", ingredientManager, typedIngredient);
 			throw new ReportedException(crashReport);
 		}
+	}
+
+	public static <T> void drawHoveringTooltip(GuiGraphics guiGraphics, List<ClientTooltipComponent> components, int x, int y, ITypedIngredient<T> typedIngredient, IIngredientRenderer<T> ingredientRenderer, IIngredientManager ingredientManager){
+		Minecraft minecraft = Minecraft.getInstance();
+		T ingredient = typedIngredient.getIngredient();
+		Font font = ingredientRenderer.getFontRenderer(minecraft, ingredient);
+		ItemStack itemStack = ingredient instanceof ItemStack i ? i : ItemStack.EMPTY;
+		try {
+			drawHoveringTooltip(guiGraphics, components, x, y, itemStack, font);
+		} catch (RuntimeException e) {
+			CrashReport crashReport = ErrorUtil.createIngredientCrashReport(e, "Rendering ingredient tooltip", ingredientManager, typedIngredient);
+			throw new ReportedException(crashReport);
+		}
+	}
+
+	private static void drawHoveringTooltip(GuiGraphics guiGraphics, List<ClientTooltipComponent> components, int x, int y, ItemStack itemStack, Font font){
+		Minecraft minecraft = Minecraft.getInstance();
+		Screen screen = minecraft.screen;
+		if (screen == null) {
+			return;
+		}
+
+		IPlatformRenderHelper renderHelper = Services.PLATFORM.getRenderHelper();
+		renderHelper.renderTooltip(screen, guiGraphics, components, x, y, font, itemStack);
 	}
 
 	private static void drawHoveringText(GuiGraphics guiGraphics, List<Component> textLines, int x, int y, ItemStack itemStack, Font font) {

Common/src/main/java/mezz/jei/common/platform/IPlatformRenderHelper.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.item.ItemColors;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.network.chat.Component;
@@ -29,4 +30,7 @@ public interface IPlatformRenderHelper {
 
 	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 	void renderTooltip(Screen screen, GuiGraphics guiGraphics, List<Component> textComponents, Optional<TooltipComponent> tooltipComponent, int x, int y, Font font, ItemStack stack);
+
+	void renderTooltip(Screen screen, GuiGraphics guiGraphics, List<ClientTooltipComponent> components, int x, int y, Font font, ItemStack stack);
+
 }

Gui/src/main/java/mezz/jei/gui/overlay/elements/RecipeBookmarkElement.java
@@ -1,34 +1,58 @@
 package mezz.jei.gui.overlay.elements;
 
+import mezz.jei.api.gui.IRecipeLayoutDrawable;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IScalableDrawable;
+import mezz.jei.api.helpers.IModIdHelper;
 import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientRenderer;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocus;
+import mezz.jei.api.recipe.IFocusFactory;
+import mezz.jei.api.recipe.IRecipeManager;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.api.runtime.IJeiRuntime;
 import mezz.jei.api.runtime.IRecipesGui;
+import mezz.jei.common.Internal;
+import mezz.jei.common.config.BookmarkTooltipFeature;
+import mezz.jei.common.config.IClientConfig;
+import mezz.jei.common.util.SafeIngredientUtil;
 import mezz.jei.gui.bookmarks.IBookmark;
 import mezz.jei.gui.bookmarks.RecipeBookmark;
 import mezz.jei.gui.overlay.IngredientGridTooltipHelper;
+import mezz.jei.gui.overlay.bookmarks.IngredientsTooltipComponent;
+import mezz.jei.gui.overlay.bookmarks.PreviewTooltipComponent;
 import mezz.jei.gui.util.FocusUtil;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Optional;
 
-public class RecipeBookmarkElement<T, R> implements IElement<R> {
-	private final RecipeBookmark<T, R> recipeBookmark;
+public class RecipeBookmarkElement<R, I> implements IElement<I> {
+	private final RecipeBookmark<R, I> recipeBookmark;
 	private final IDrawable icon;
+	private final IClientConfig clientConfig;
+	private final EnumMap<BookmarkTooltipFeature, ClientTooltipComponent> cache = new EnumMap<>(BookmarkTooltipFeature.class);
 
-	public RecipeBookmarkElement(RecipeBookmark<T, R> recipeBookmark, IDrawable icon) {
+	public RecipeBookmarkElement(RecipeBookmark<R, I> recipeBookmark, IDrawable icon) {
 		this.recipeBookmark = recipeBookmark;
 		this.icon = icon;
+		this.clientConfig = Internal.getJeiClientConfigs().getClientConfig();
 	}
 
 	@Override
-	public ITypedIngredient<R> getTypedIngredient() {
+	public ITypedIngredient<I> getTypedIngredient() {
 		return recipeBookmark.getRecipeOutput();
 	}
 
@@ -56,22 +80,123 @@ public class RecipeBookmarkElement<T, R> implements IElement<R> {
 	public void show(IRecipesGui recipesGui, FocusUtil focusUtil, List<RecipeIngredientRole> roles) {
 		// ignore roles, always display the bookmarked recipe if it's clicked
 
-		IRecipeCategory<T> recipeCategory = recipeBookmark.getRecipeCategory();
-		T recipe = recipeBookmark.getRecipe();
+		IRecipeCategory<R> recipeCategory = recipeBookmark.getRecipeCategory();
+		R recipe = recipeBookmark.getRecipe();
 		ITypedIngredient<?> ingredient = getTypedIngredient();
 		List<IFocus<?>> focuses = focusUtil.createFocuses(ingredient, List.of(RecipeIngredientRole.OUTPUT));
 		recipesGui.showRecipes(recipeCategory, List.of(recipe), focuses);
 	}
 
 	@Override
-	public List<Component> getTooltip(IngredientGridTooltipHelper tooltipHelper, IIngredientRenderer<R> ingredientRenderer, IIngredientHelper<R> ingredientHelper) {
-		ITypedIngredient<R> ingredient = recipeBookmark.getRecipeOutput();
-		return tooltipHelper.getRecipeTooltip(
+	public List<ClientTooltipComponent> getTooltipComponents(IngredientGridTooltipHelper tooltipHelper, IIngredientRenderer<I> ingredientRenderer, IIngredientHelper<I> ingredientHelper) {
+		List<ClientTooltipComponent> components = new ArrayList<>();
+
+		ITypedIngredient<I> recipeOutput = recipeBookmark.getRecipeOutput();
+		R recipe = recipeBookmark.getRecipe();
+
+		IRecipeCategory<R> recipeCategory = recipeBookmark.getRecipeCategory();
+		addTooltipComponent(components, Component.translatable("jei.tooltip.bookmarks.recipe", recipeCategory.getTitle()));
+
+		addBookmarkTooltipFeaturesIfEnabled(components);
+
+		IJeiRuntime jeiRuntime = Internal.getJeiRuntime();
+		IIngredientManager ingredientManager = jeiRuntime.getIngredientManager();
+		IModIdHelper modIdHelper = jeiRuntime.getJeiHelpers().getModIdHelper();
+
+		ResourceLocation recipeName = recipeCategory.getRegistryName(recipe);
+		if (recipeName != null) {
+			String recipeModId = recipeName.getNamespace();
+			ResourceLocation ingredientName = ingredientHelper.getResourceLocation(recipeOutput.getIngredient());
+			String ingredientModId = ingredientName.getNamespace();
+			if (!recipeModId.equals(ingredientModId)) {
+				String modName = modIdHelper.getFormattedModNameForModId(recipeModId);
+				MutableComponent recipeBy = Component.translatable("jei.tooltip.recipe.by", modName);
+				addTooltipComponent(components, recipeBy.withStyle(ChatFormatting.GRAY));
+			}
+		}
+
+		addTooltipComponent(components, Component.empty());
+
+		List<Component> outputTooltip = SafeIngredientUtil.getTooltip(ingredientManager, ingredientRenderer, recipeOutput);
+
+		addAllTooltipComponents(components, outputTooltip);
+		List<Component> modIdTooltip = new ArrayList<>();
+		modIdTooltip = modIdHelper.addModNameToIngredientTooltip(modIdTooltip, recipeOutput.getIngredient(), ingredientHelper);
+		addAllTooltipComponents(components, modIdTooltip);
+
+		return components;
+	}
+
+	private void addAllTooltipComponents(List<ClientTooltipComponent> tooltipComponents, List<Component> components) {
+		for (Component component : components) {
+			addTooltipComponent(tooltipComponents, component);
+		}
+	}
+
+	private void addTooltipComponent(List<ClientTooltipComponent> tooltipComponents, Component component) {
+		ClientTooltipComponent clientTooltipComponent = ClientTooltipComponent.create(component.getVisualOrderText());
+		tooltipComponents.add(clientTooltipComponent);
+	}
+
+	private void addBookmarkTooltipFeaturesIfEnabled(List<ClientTooltipComponent> components) {
+		if (clientConfig.getBookmarkTooltipFeatures().isEmpty()) {
+			return;
+		}
+		if (clientConfig.isHoldShiftToShowBookmarkTooltipFeaturesEnabled()) {
+			if (Screen.hasShiftDown()) {
+				addBookmarkTooltipFeatures(components);
+			} else {
+				Component shiftKey = Component.keybind("jei.key.shift")
+					.withStyle(ChatFormatting.BOLD);
+				Component component = Component.translatable("jei.tooltip.bookmarks.tooltips.usage", shiftKey)
+					.withStyle(ChatFormatting.ITALIC)
+					.withStyle(ChatFormatting.GRAY);
+				ClientTooltipComponent tooltipComponent = ClientTooltipComponent.create(component.getVisualOrderText());
+				components.add(tooltipComponent);
+			}
+		} else {
+			addBookmarkTooltipFeatures(components);
+		}
+	}
+
+	private void addBookmarkTooltipFeatures(List<ClientTooltipComponent> components) {
+		@Nullable IRecipeLayoutDrawable<R> layoutDrawable = null;
+
+		for (BookmarkTooltipFeature feature : clientConfig.getBookmarkTooltipFeatures()) {
+			ClientTooltipComponent component = cache.get(feature);
+			if (component == null) {
+				if (layoutDrawable == null) {
+					layoutDrawable = createRecipeLayoutDrawable().orElse(null);
+					if (layoutDrawable == null) {
+						break;
+					}
+				}
+				component = createComponent(feature, layoutDrawable);
+				cache.put(feature, component);
+			}
+			components.add(component);
+		}
+	}
+
+	private ClientTooltipComponent createComponent(BookmarkTooltipFeature feature, IRecipeLayoutDrawable<R> layoutDrawable) {
+		return switch (feature) {
+			case PREVIEW -> new PreviewTooltipComponent<>(layoutDrawable);
+			case INGREDIENTS -> new IngredientsTooltipComponent(layoutDrawable);
+		};
+	}
+
+	private Optional<IRecipeLayoutDrawable<R>> createRecipeLayoutDrawable() {
+		IJeiRuntime jeiRuntime = Internal.getJeiRuntime();
+		IRecipeManager recipeManager = jeiRuntime.getRecipeManager();
+		IFocusFactory focusFactory = jeiRuntime.getJeiHelpers().getFocusFactory();
+		IScalableDrawable recipePreviewBackground = Internal.getTextures().getRecipePreviewBackground();
+
+		return recipeManager.createRecipeLayoutDrawable(
 			recipeBookmark.getRecipeCategory(),
 			recipeBookmark.getRecipe(),
-			ingredient,
-			ingredientRenderer,
-			ingredientHelper
+			focusFactory.getEmptyFocusGroup(),
+			recipePreviewBackground,
+			4
 		);
 	}
 

mezz/JustEnoughItems

(cherry picked from commit 3ffa31ba86003bb3da0514a39f29f60cebb93c6a)
# Conflicts:
#	Common/src/main/java/mezz/jei/common/gui/JeiTooltip.java
#	Library/src/main/java/mezz/jei/library/plugins/vanilla/ingredients/ItemStackHelper.java
#	gradle.properties

Common/src/main/java/mezz/jei/common/gui/TooltipRenderer.java
@@ -10,9 +10,12 @@ import mezz.jei.common.platform.IPlatformRenderHelper;
 import mezz.jei.common.platform.Services;
 import mezz.jei.common.util.ErrorUtil;
 import mezz.jei.core.util.LimitedLogger;
+import net.minecraft.CrashReport;
+import net.minecraft.ReportedException;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
@@ -61,6 +64,38 @@ public final class TooltipRenderer {
 		T ingredient = typedIngredient.getIngredient();
 		Font font = ingredientRenderer.getFontRenderer(minecraft, ingredient);
 		drawHoveringText(poseStack, textLines, x, y, typedIngredient, font);
+	}
+
+	public static <T> void drawHoveringTooltip(
+		PoseStack poseStack,
+		List<ClientTooltipComponent> components,
+		int x,
+		int y,
+		ITypedIngredient<T> typedIngredient,
+		IIngredientRenderer<T> ingredientRenderer,
+		IIngredientManager ingredientManager
+	) {
+		Minecraft minecraft = Minecraft.getInstance();
+		T ingredient = typedIngredient.getIngredient();
+		Font font = ingredientRenderer.getFontRenderer(minecraft, ingredient);
+		ItemStack itemStack = typedIngredient.getItemStack().orElse(ItemStack.EMPTY);
+		try {
+			drawHoveringTooltip(poseStack, components, x, y, itemStack, font);
+		} catch (RuntimeException e) {
+			CrashReport crashReport = ErrorUtil.createIngredientCrashReport(e, "Rendering ingredient tooltip", ingredientManager, typedIngredient);
+			throw new ReportedException(crashReport);
+		}
+	}
+
+	private static void drawHoveringTooltip(PoseStack poseStack, List<ClientTooltipComponent> components, int x, int y, ItemStack itemStack, Font font) {
+		Minecraft minecraft = Minecraft.getInstance();
+		Screen screen = minecraft.screen;
+		if (screen == null) {
+			return;
+		}
+
+		IPlatformRenderHelper renderHelper = Services.PLATFORM.getRenderHelper();
+		renderHelper.renderTooltip(screen, poseStack, components, x, y, font, itemStack);
 	}
 
 	private static <T> void drawHoveringText(PoseStack poseStack, List<Component> textLines, int x, int y, ITypedIngredient<T> typedIngredient, Font font) {

Common/src/main/java/mezz/jei/common/platform/IPlatformRenderHelper.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.item.ItemColors;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.network.chat.Component;
@@ -30,4 +31,6 @@ public interface IPlatformRenderHelper {
 
 	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 	void renderTooltip(Screen screen, PoseStack poseStack, List<Component> textComponents, Optional<TooltipComponent> tooltipComponent, int x, int y, @Nullable Font font, ItemStack stack);
+
+	void renderTooltip(Screen screen, PoseStack poseStack, List<ClientTooltipComponent> components, int x, int y, Font font, ItemStack stack);
 }

Gui/src/main/java/mezz/jei/gui/overlay/elements/RecipeBookmarkElement.java
@@ -1,31 +1,57 @@
 package mezz.jei.gui.overlay.elements;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import mezz.jei.api.gui.IRecipeLayoutDrawable;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IScalableDrawable;
+import mezz.jei.api.helpers.IModIdHelper;
 import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientRenderer;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.recipe.IFocus;
+import mezz.jei.api.recipe.IFocusFactory;
+import mezz.jei.api.recipe.IRecipeManager;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.api.runtime.IJeiRuntime;
 import mezz.jei.api.runtime.IRecipesGui;
+import mezz.jei.common.Internal;
+import mezz.jei.common.config.BookmarkTooltipFeature;
+import mezz.jei.common.config.IClientConfig;
 import mezz.jei.common.gui.JeiTooltip;
+import mezz.jei.common.util.SafeIngredientUtil;
 import mezz.jei.gui.bookmarks.IBookmark;
 import mezz.jei.gui.bookmarks.RecipeBookmark;
+import mezz.jei.gui.overlay.bookmarks.IngredientsTooltipComponent;
+import mezz.jei.gui.overlay.bookmarks.PreviewTooltipComponent;
 import mezz.jei.gui.overlay.ingredients.IngredientGridTooltipHelper;
 import mezz.jei.gui.util.FocusUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.KeybindComponent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Optional;
 
 public class RecipeBookmarkElement<T, R> implements IElement<R> {
 	private final RecipeBookmark<T, R> recipeBookmark;
 	private final IDrawable icon;
+	private final IClientConfig clientConfig;
+	private final EnumMap<BookmarkTooltipFeature, ClientTooltipComponent> cache = new EnumMap<>(BookmarkTooltipFeature.class);
 
 	public RecipeBookmarkElement(RecipeBookmark<T, R> recipeBookmark, IDrawable icon) {
 		this.recipeBookmark = recipeBookmark;
 		this.icon = icon;
+		this.clientConfig = Internal.getJeiClientConfigs().getClientConfig();
 	}
 
 	@Override
@@ -56,14 +82,92 @@ public class RecipeBookmarkElement<T, R> implements IElement<R> {
 
 	@Override
 	public void getTooltip(JeiTooltip tooltip, IngredientGridTooltipHelper tooltipHelper, IIngredientRenderer<R> ingredientRenderer, IIngredientHelper<R> ingredientHelper) {
-		ITypedIngredient<R> ingredient = recipeBookmark.getRecipeOutput();
-		tooltipHelper.getRecipeTooltip(
-			tooltip,
+		ITypedIngredient<R> recipeOutput = recipeBookmark.getRecipeOutput();
+		T recipe = recipeBookmark.getRecipe();
+		IRecipeCategory<T> recipeCategory = recipeBookmark.getRecipeCategory();
+
+		tooltip.add(new TranslatableComponent("jei.tooltip.bookmarks.recipe", recipeCategory.getTitle()));
+		addBookmarkTooltipFeaturesIfEnabled(tooltip);
+
+		IJeiRuntime jeiRuntime = Internal.getJeiRuntime();
+		IIngredientManager ingredientManager = jeiRuntime.getIngredientManager();
+		IModIdHelper modIdHelper = jeiRuntime.getJeiHelpers().getModIdHelper();
+
+		ResourceLocation recipeName = recipeCategory.getRegistryName(recipe);
+		if (recipeName != null) {
+			String recipeModId = recipeName.getNamespace();
+			ResourceLocation ingredientName = ingredientHelper.getResourceLocation(recipeOutput.getIngredient());
+			String ingredientModId = ingredientName.getNamespace();
+			if (!recipeModId.equals(ingredientModId)) {
+				String modName = modIdHelper.getFormattedModNameForModId(recipeModId);
+				MutableComponent recipeBy = new TranslatableComponent("jei.tooltip.recipe.by", modName);
+				tooltip.add(recipeBy.withStyle(ChatFormatting.GRAY));
+			}
+		}
+
+		tooltip.add(new TextComponent(""));
+		SafeIngredientUtil.getTooltip(tooltip, ingredientManager, ingredientRenderer, recipeOutput);
+	}
+
+	private void addBookmarkTooltipFeaturesIfEnabled(JeiTooltip tooltip) {
+		if (clientConfig.getBookmarkTooltipFeatures().isEmpty()) {
+			return;
+		}
+		if (clientConfig.isHoldShiftToShowBookmarkTooltipFeaturesEnabled()) {
+			if (Screen.hasShiftDown()) {
+				addBookmarkTooltipFeatures(tooltip);
+			} else {
+				Component shiftKey = new KeybindComponent("jei.key.shift")
+					.withStyle(ChatFormatting.BOLD);
+				Component component = new TranslatableComponent("jei.tooltip.bookmarks.tooltips.usage", shiftKey)
+					.withStyle(ChatFormatting.ITALIC)
+					.withStyle(ChatFormatting.GRAY);
+				tooltip.add(component);
+			}
+		} else {
+			addBookmarkTooltipFeatures(tooltip);
+		}
+	}
+
+	private void addBookmarkTooltipFeatures(JeiTooltip tooltip) {
+		@Nullable
+		IRecipeLayoutDrawable layoutDrawable = null;
+
+		for (BookmarkTooltipFeature feature : clientConfig.getBookmarkTooltipFeatures()) {
+			ClientTooltipComponent component = cache.get(feature);
+			if (component == null) {
+				if (layoutDrawable == null) {
+					layoutDrawable = createRecipeLayoutDrawable().orElse(null);
+					if (layoutDrawable == null) {
+						break;
+					}
+				}
+				component = createComponent(feature, layoutDrawable);
+				cache.put(feature, component);
+			}
+			tooltip.addClientTooltipComponent(component);
+		}
+	}
+
+	private ClientTooltipComponent createComponent(BookmarkTooltipFeature feature, IRecipeLayoutDrawable layoutDrawable) {
+		return switch (feature) {
+			case PREVIEW -> new PreviewTooltipComponent(layoutDrawable);
+			case INGREDIENTS -> new IngredientsTooltipComponent(layoutDrawable);
+		};
+	}
+
+	private Optional<IRecipeLayoutDrawable> createRecipeLayoutDrawable() {
+		IJeiRuntime jeiRuntime = Internal.getJeiRuntime();
+		IRecipeManager recipeManager = jeiRuntime.getRecipeManager();
+		IFocusFactory focusFactory = jeiRuntime.getJeiHelpers().getFocusFactory();
+		IScalableDrawable recipePreviewBackground = Internal.getTextures().getRecipePreviewBackground();
+
+		return recipeManager.createRecipeLayoutDrawable(
 			recipeBookmark.getRecipeCategory(),
 			recipeBookmark.getRecipe(),
-			ingredient,
-			ingredientRenderer,
-			ingredientHelper
+			focusFactory.getEmptyFocusGroup(),
+			recipePreviewBackground,
+			4
 		);
 	}
 