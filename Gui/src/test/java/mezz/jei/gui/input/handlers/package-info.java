f2f62c3597afdd04dc1da7935b4e894d732f5743
mezz <tehgeek@gmail.com>
2026-09-11 22:01:49 +0900

    Fix test nullness annotations
    (cherry picked from commit 0f69b764799d46ced8738ac62a559bf35dd4656d)

---
+++ Gui/src/test/java/mezz/jei/gui/input/handlers/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@FieldsAndMethodsAreNonnullByDefault
+package mezz.jei.gui.input.handlers;
+
+import mezz.jei.common.FieldsAndMethodsAreNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ Gui/src/test/java/mezz/jei/gui/recipes/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@FieldsAndMethodsAreNonnullByDefault
+package mezz.jei.gui.recipes;
+
+import mezz.jei.common.FieldsAndMethodsAreNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
