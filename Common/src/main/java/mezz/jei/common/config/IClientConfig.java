3fabaa7695ac282a32d8bb20a4ddd8b77b9d480f
mezz <tehgeek@gmail.com>
2026-09-11 22:06:18 +0900

    Cap recipe screen height limit
    (cherry picked from commit 02988dcb8af46452ee9abb6cb9a43914e447dd80)
    (cherry picked from commit 6f86af46858b5d2edb6e7dc84cb738bfef2710aa)

--- Common/src/main/java/mezz/jei/common/config/IClientConfig.java
+++ Common/src/main/java/mezz/jei/common/config/IClientConfig.java
@@ -10,6 +10,7 @@ import java.util.function.Consumer;
 public interface IClientConfig {
 	int minRecipeGuiHeight = 175;
 	int defaultRecipeGuiHeight = 350;
+	int maximumRecipeGuiHeight = 7680;
 	boolean defaultCenterSearchBar = false;
 
 	boolean isCenterSearchBarEnabled();
