90af1a89d96bdac0d3ee20df5a3f13bfc7f0ed61
mezz <tehgeek@gmail.com>
2026-09-12 21:06:05 +0900

    Expose native key mappings internally
    (cherry picked from commit 5f490b0db6a33c296b0a21d0db3a605f5d56296c)

--- Common/src/main/java/mezz/jei/common/input/keys/IJeiKeyMappingInternal.java
+++ Common/src/main/java/mezz/jei/common/input/keys/IJeiKeyMappingInternal.java
@@ -21,6 +21,8 @@ public interface IJeiKeyMappingInternal extends IJeiKeyMappingWithExtraModifiers
 	@Override
 	Component getTranslatedKeyMessage();
 
+	KeyMapping getKeyMapping();
+
 	boolean isDown();
 
 	IJeiKeyMappingInternal register(Consumer<KeyMapping> registerMethod);
--- Gui/src/test/java/mezz/jei/gui/input/PinnedTooltipManagerTest.java
+++ Gui/src/test/java/mezz/jei/gui/input/PinnedTooltipManagerTest.java
@@ -90,6 +90,11 @@ public class PinnedTooltipManagerTest {
 		}
 
 		@Override
+		public KeyMapping getKeyMapping() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
 		public Component getTranslatedKeyMessage() {
 			return Component.empty();
 		}
