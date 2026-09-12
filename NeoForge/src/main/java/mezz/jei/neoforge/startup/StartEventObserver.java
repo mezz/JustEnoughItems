75a43bde63e91793e5728299a15e78bf7cacbaf1
mezz <tehgeek@gmail.com>
2026-09-12 20:34:10 +0900

    Restart JEI for batch config changes
    (cherry picked from commit 94fa23915e81725d7111580911f0bc5cb50e1d87)

--- NeoForge/src/main/java/mezz/jei/neoforge/startup/StartEventObserver.java
+++ NeoForge/src/main/java/mezz/jei/neoforge/startup/StartEventObserver.java
@@ -197,7 +197,7 @@ public class StartEventObserver implements ResourceManagerReloadListener {
 		restart();
 	}
 
-	private void restart() {
+	public void restart() {
 		if (this.state != State.JEI_STARTED) {
 			return;
 		}
