6c573c047b409c70f897d12daa78dc83eeb19712
mezz <tehgeek@gmail.com>
2026-09-12 20:54:20 +0900

    Read mod icons from loader metadata
    (cherry picked from commit e1d9e3ed5f20d81c8b3a82d92fd1a046a43336b7)

--- Common/src/main/java/mezz/jei/common/platform/IPlatformModHelper.java
+++ Common/src/main/java/mezz/jei/common/platform/IPlatformModHelper.java
@@ -1,7 +1,11 @@
 package mezz.jei.common.platform;
 
+import java.util.Optional;
+
 public interface IPlatformModHelper {
 	String getModNameForModId(String modId);
+
+	Optional<byte[]> getModIconBytes(String modId);
 
 	boolean isInDev();
 }
