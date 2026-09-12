ffb9252aee980c5867353859ae965429f4bdd1b1
mezz <tehgeek@gmail.com>
2026-09-12 21:10:55 +0900

    Declare MezzConfig in published Maven metadata
    (cherry picked from commit 0fa9d24f8961a4e848ba5935ce1295a415cf9907)

--- Gui/build.gradle.kts
+++ Gui/build.gradle.kts
@@ -103,7 +103,9 @@ publishing {
             artifact(tasks.jar.get())
             artifact(sourcesJarTask.get())
 
-            val dependencyInfos = dependencyProjects.map {
+            val dependencyInfos = listOf(
+                dependencyInfo(mezzConfigApiDependency)
+            ) + dependencyProjects.map {
                 mapOf(
                     "groupId" to it.group,
                     "artifactId" to it.base.archivesName.get(),
@@ -134,6 +136,15 @@ publishing {
             maven(deployDir)
         }
     }
+}
+
+fun dependencyInfo(notation: String): Map<String, String> {
+    val (groupId, artifactId, version) = notation.split(":")
+    return mapOf(
+        "groupId" to groupId,
+        "artifactId" to artifactId,
+        "version" to version
+    )
 }
 
 idea {
