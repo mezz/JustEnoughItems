e75958cfee6bf2a3a55749f1068492d93878831c
mezz <tehgeek@gmail.com>
2026-09-12 20:16:48 +0900

    Deprecate the JEI config API
    Use target API version 30.33.0 for the deprecation annotations and version
    bump.
    
    (cherry picked from commit 40bfbd878cff3cdcbc87c93af903e11df740b0ee)

--- Common/src/api/java/mezz/jei/api/IModPlugin.java
+++ Common/src/api/java/mezz/jei/api/IModPlugin.java
@@ -27,6 +27,7 @@ import net.minecraft.world.item.crafting.display.SlotDisplay;
  * Everything communicated between a mod and JEI is through this class.
  * IModPlugins must have the {@link JeiPlugin} annotation to get loaded by JEI.
  */
+@SuppressWarnings({"deprecation", "removal"})
 public interface IModPlugin {
 
 	/**
@@ -211,7 +212,9 @@ public interface IModPlugin {
 	 * Called when JEI's configs are available.
 	 * This is called early on, as soon as configs are available.
 	 * @since 12.3.0
+	 * @deprecated use MezzConfig APIs directly
 	 */
+	@Deprecated(since = "30.33.0", forRemoval = true)
 	default void onConfigManagerAvailable(IJeiConfigManager configManager) {
 
 	}
