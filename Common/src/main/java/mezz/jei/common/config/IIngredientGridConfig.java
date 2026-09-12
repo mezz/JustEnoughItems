483b11d78b8e82c74e2ef839f3b07889f703b003
mezz <tehgeek@gmail.com>
2026-09-12 20:02:05 +0900

    Move config enums into the config package
    (cherry picked from commit 32d8d41a72cb55f85bcaa5514a97ea316ff69eeb)

--- Common/src/main/java/mezz/jei/common/config/IIngredientGridConfig.java
+++ Common/src/main/java/mezz/jei/common/config/IIngredientGridConfig.java
@@ -3,7 +3,6 @@ package mezz.jei.common.config;
 import mezz.jei.api.gui.placement.HorizontalAlignment;
 import mezz.jei.api.gui.placement.VerticalAlignment;
 import mezz.jei.api.runtime.config.IJeiConfigValue;
-import mezz.jei.common.util.NavigationVisibility;
 
 public interface IIngredientGridConfig {
 	IJeiConfigValue<Integer> maxColumns();
