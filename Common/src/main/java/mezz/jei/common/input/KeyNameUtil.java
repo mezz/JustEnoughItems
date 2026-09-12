6270e7a19c54d79106f77f09ffe6933a52a901d3
mezz <tehgeek@gmail.com>
2026-09-12 20:51:54 +0900

    Localize modifier key names
    (cherry picked from commit 308bf60b80dd344ef97c109c27a42fd2ce92dc00)

--- Common/src/main/java/mezz/jei/common/input/KeyNameUtil.java
+++ Common/src/main/java/mezz/jei/common/input/KeyNameUtil.java
@@ -1,16 +1,19 @@
 package mezz.jei.common.input;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.blaze3d.platform.MacosUtil;
 import net.minecraft.network.chat.Component;
 
-public class KeyNameUtil {
+public final class KeyNameUtil {
+	private KeyNameUtil() {
+
+	}
+
 	/**
 	 * The vanilla translation for left click is "LEFT BUTTON" and right click is "RIGHT BUTTON".
 	 * We want better names for these in tooltips, and so use our own localization.
 	 */
 	public static Component getKeyDisplayName(InputConstants.Key key) {
-		// The vanilla translation for left click is "LEFT BUTTON" and right click is "RIGHT BUTTON".
-		// We want better names for these in tooltips, and so use our own localization.
 		if (key.getType() == InputConstants.Type.MOUSE) {
 			int value = key.getValue();
 			if (value == InputConstants.MOUSE_BUTTON_LEFT) {
@@ -18,6 +21,18 @@ public class KeyNameUtil {
 			} else if (value == InputConstants.MOUSE_BUTTON_RIGHT) {
 				return Component.translatable("jei.key.mouse.right");
 			}
+		}
+		if (key.getType() == InputConstants.Type.KEYBOARD) {
+			int value = key.getValue();
+			if (MacosUtil.IS_MACOS && (value == InputConstants.KEY_LGUI || value == InputConstants.KEY_RGUI)) {
+				return Component.translatable("jei.key.modifier.command");
+			}
+			return switch (value) {
+				case InputConstants.KEY_LSHIFT, InputConstants.KEY_RSHIFT -> Component.translatable("jei.key.modifier.shift");
+				case InputConstants.KEY_LALT, InputConstants.KEY_RALT -> Component.translatable("jei.key.modifier.alt");
+				case InputConstants.KEY_LCONTROL, InputConstants.KEY_RCONTROL -> Component.translatable("jei.key.modifier.control");
+				default -> key.getDisplayName();
+			};
 		}
 		return key.getDisplayName();
 	}
