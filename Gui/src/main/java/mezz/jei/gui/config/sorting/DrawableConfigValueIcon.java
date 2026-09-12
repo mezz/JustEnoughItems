daa7fd72d45409a555b736d74105bf72f5ac8984
mezz <tehgeek@gmail.com>
2026-09-12 20:59:19 +0900

    Show sorting orders in the config screen
    (cherry picked from commit f2f63d1345d88094e3dbabbe43ba921c7a0c95cb)

---
+++ Gui/src/main/java/mezz/jei/gui/config/sorting/DrawableConfigValueIcon.java
@@ -0,0 +1,15 @@
+package mezz.jei.gui.config.sorting;
+
+import mezz.jei.api.gui.drawable.IDrawable;
+import net.mezzdev.config.gui.api.IConfigValueIcon;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.renderer.Rect2i;
+
+record DrawableConfigValueIcon(IDrawable drawable) implements IConfigValueIcon {
+	@Override
+	public void draw(GuiGraphicsExtractor guiGraphics, Rect2i area) {
+		int x = area.getX() + Math.round((area.getWidth() - drawable.getWidth()) / 2.0f);
+		int y = area.getY() + Math.round((area.getHeight() - drawable.getHeight()) / 2.0f);
+		drawable.draw(guiGraphics, x, y);
+	}
+}
