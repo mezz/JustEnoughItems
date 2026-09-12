04a45b8cacda85cd10d4b37823f9ebca76ebe281
mezz <tehgeek@gmail.com>
2026-09-12 21:06:01 +0900

    Distinguish JEI key mapping conflict contexts
    (cherry picked from commit 9aa0b49857a833c77988e0289ec020762c1c8672)

--- Forge/src/main/java/mezz/jei/forge/input/JeiForgeKeyConflictContexts.java
+++ Forge/src/main/java/mezz/jei/forge/input/JeiForgeKeyConflictContexts.java
@@ -4,51 +4,21 @@ import net.minecraftforge.client.settings.IKeyConflictContext;
 import net.minecraftforge.client.settings.KeyConflictContext;
 
 public enum JeiForgeKeyConflictContexts implements IKeyConflictContext {
-	JEI_GUI_HOVER {
-		@Override
-		public boolean isActive() {
-			return KeyConflictContext.GUI.isActive();
-		}
+	JEI_GUI_HOVER,
+	JEI_GUI_HOVER_BOOKMARK,
+	JEI_GUI_HOVER_CHEAT_MODE,
+	JEI_GUI_HOVER_CONFIG_BUTTON,
+	JEI_GUI_HOVER_INGREDIENT,
+	JEI_GUI_HOVER_SEARCH,
+	JEI_GUI_FOCUSED_SEARCH;
 
-		@Override
-		public boolean conflicts(IKeyConflictContext other) {
-			return this == other;
-		}
-	},
+	@Override
+	public boolean isActive() {
+		return KeyConflictContext.GUI.isActive();
+	}
 
-	JEI_GUI_HOVER_CHEAT_MODE {
-		@Override
-		public boolean isActive() {
-			return KeyConflictContext.GUI.isActive();
-		}
-
-		@Override
-		public boolean conflicts(IKeyConflictContext other) {
-			return this == other;
-		}
-	},
-
-	JEI_GUI_HOVER_CONFIG_BUTTON {
-		@Override
-		public boolean isActive() {
-			return KeyConflictContext.GUI.isActive();
-		}
-
-		@Override
-		public boolean conflicts(IKeyConflictContext other) {
-			return this == other;
-		}
-	},
-
-	JEI_GUI_HOVER_SEARCH {
-		@Override
-		public boolean isActive() {
-			return KeyConflictContext.GUI.isActive();
-		}
-
-		@Override
-		public boolean conflicts(IKeyConflictContext other) {
-			return this == other;
-		}
+	@Override
+	public boolean conflicts(IKeyConflictContext other) {
+		return this == other;
 	}
 }
