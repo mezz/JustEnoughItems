58515ba65208c3a50f58f3e076da1366af15790c
mezz <tehgeek@gmail.com>
2026-09-12 20:54:08 +0900

    Open config screens by mod id
    (cherry picked from commit 82677c12eefe06bded4090aec45f4fb5eb5e6467)

--- Forge/src/main/java/mezz/jei/forge/platform/ConfigHelper.java
+++ Forge/src/main/java/mezz/jei/forge/platform/ConfigHelper.java
@@ -1,13 +1,12 @@
 package mezz.jei.forge.platform;
 
-import mezz.jei.api.constants.ModIds;
 import mezz.jei.common.platform.IPlatformConfigHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLPaths;
+import org.jetbrains.annotations.Nullable;
 
 import java.nio.file.Path;
 import java.util.Optional;
@@ -20,12 +19,11 @@ public class ConfigHelper implements IPlatformConfigHelper {
 	}
 
 	@Override
-	public Optional<Screen> getConfigScreen() {
-		Minecraft minecraft = Minecraft.getInstance();
+	public Optional<Screen> getConfigScreen(String modId, @Nullable Screen parent) {
 		return ModList.get()
-			.getModContainerById(ModIds.JEI_ID)
-			.map(ModContainer::getModInfo)
-			.flatMap(ConfigScreenHandler::getScreenFactoryFor)
-			.map(f -> f.apply(minecraft, minecraft.screen));
+			.getModContainerById(modId)
+			.flatMap(modContainer -> modContainer.getCustomExtension(ConfigScreenHandler.ConfigScreenFactory.class))
+			.map(ConfigScreenHandler.ConfigScreenFactory::screenFunction)
+			.map(screenFactory -> screenFactory.apply(Minecraft.getInstance(), parent));
 	}
 }
