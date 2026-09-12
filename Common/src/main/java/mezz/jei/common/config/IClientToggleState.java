d6dcb1149fbe44d9e8b9f6dd6beea323da740d22
mezz <tehgeek@gmail.com>
2026-09-12 20:53:48 +0900

    Expose runtime toggle state changes
    (cherry picked from commit 07e93173033de83278e0b6325d60f52fdc328e85)

--- Common/src/main/java/mezz/jei/common/config/IClientToggleState.java
+++ Common/src/main/java/mezz/jei/common/config/IClientToggleState.java
@@ -1,13 +1,23 @@
 package mezz.jei.common.config;
 
+import java.util.function.Consumer;
+
 public interface IClientToggleState {
 	boolean isOverlayEnabled();
 
 	void toggleOverlayEnabled();
 
+	void setOverlayEnabled(boolean value);
+
+	Runnable addOverlayEnabledListener(Consumer<Boolean> listener);
+
 	boolean isEditModeEnabled();
 
 	void toggleEditModeEnabled();
+
+	void setEditModeEnabled(boolean value);
+
+	Runnable addEditModeEnabledListener(Consumer<Boolean> listener);
 
 	boolean isCheatItemsEnabled();
 
@@ -15,11 +25,17 @@ public interface IClientToggleState {
 
 	void setCheatItemsEnabled(boolean value);
 
+	Runnable addCheatItemsEnabledListener(Consumer<Boolean> listener);
+
 	boolean isBookmarkOverlayEnabled();
+
+	boolean isBookmarkEnabled();
 
 	void toggleBookmarkEnabled();
 
 	void setBookmarkEnabled(boolean value);
+
+	Runnable addBookmarkEnabledListener(Consumer<Boolean> listener);
 
 	void addEditModeToggleListener(IEditModeListener listener);
 
