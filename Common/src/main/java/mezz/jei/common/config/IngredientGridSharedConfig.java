32c0b08da8840d3797c3e4299da4a1391571bc55
mezz <tehgeek@gmail.com>
2026-09-14 00:13:15 +0900

    Share ingredient and bookmark list controls
    (cherry picked from commit 064d1c0e32044c447ae654a185a6381b71254f4e)

---
+++ Common/src/main/java/mezz/jei/common/config/IngredientGridSharedConfig.java
@@ -0,0 +1,43 @@
+package mezz.jei.common.config;
+
+import net.mezzdev.config.api.schema.builder.IConfigCategoryBuilder;
+import net.mezzdev.config.api.value.editor.ConfigValueEditMode;
+import net.mezzdev.config.api.value.IConfigValue;
+
+final class IngredientGridSharedConfig {
+	private static final boolean defaultDrawBackground = false;
+	private static final IngredientGridNavigationMode defaultNavigationMode = IngredientGridNavigationMode.PAGED;
+	private static final NavigationVisibility defaultNavigationVisibility = NavigationVisibility.ENABLED;
+
+	private final IConfigValue<Boolean> drawBackground;
+	private final IConfigValue<IngredientGridNavigationMode> navigationMode;
+	private final IConfigValue<NavigationVisibility> navigationVisibility;
+
+	IngredientGridSharedConfig(IConfigCategoryBuilder category) {
+		drawBackground = category.addBoolean("drawBackground", defaultDrawBackground)
+			.addLegacyValue("ingredientList", "drawBackground")
+			.setEditMode(ConfigValueEditMode.IMMEDIATE)
+			.build();
+		navigationMode = category.addEnum("navigationMode", defaultNavigationMode)
+			.addLegacyValue("ingredientList", "navigationMode")
+			.setEditMode(ConfigValueEditMode.IMMEDIATE)
+			.build();
+		navigationVisibility = category.addEnum("navigationVisibility", defaultNavigationVisibility)
+			.addLegacyValue("ingredientList", "navigationVisibility")
+			.addLegacyValue("ingredientList", "buttonNavigationVisibility")
+			.setEditMode(ConfigValueEditMode.IMMEDIATE)
+			.build();
+	}
+
+	IConfigValue<Boolean> drawBackground() {
+		return drawBackground;
+	}
+
+	IConfigValue<IngredientGridNavigationMode> navigationMode() {
+		return navigationMode;
+	}
+
+	IConfigValue<NavigationVisibility> navigationVisibility() {
+		return navigationVisibility;
+	}
+}
