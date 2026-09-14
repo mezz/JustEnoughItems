076b170307b5ae72d14e6c3ddb8dbc337c963113
mezz <tehgeek@gmail.com>
2026-09-14 21:10:40 +0900

    Fix #4490 Prioritize recipe bookmark transfer input
    (cherry picked from commit 3f0dad3ea858883fbb1ec9aad997ada87b53b182)

---
+++ Gui/src/main/java/mezz/jei/gui/input/handlers/ElementInputHandler.java
@@ -0,0 +1,36 @@
+package mezz.jei.gui.input.handlers;
+
+import mezz.jei.api.gui.handlers.IGuiProperties;
+import mezz.jei.common.input.IInternalKeyMappings;
+import mezz.jei.gui.input.CombinedRecipeFocusSource;
+import mezz.jei.gui.input.IClickableIngredientInternal;
+import mezz.jei.gui.input.IUserInputHandler;
+import mezz.jei.gui.input.UserInput;
+import mezz.jei.gui.overlay.elements.IElement;
+import net.minecraft.client.gui.screens.Screen;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ElementInputHandler implements IUserInputHandler {
+	private final CombinedRecipeFocusSource focusSource;
+
+	public ElementInputHandler(CombinedRecipeFocusSource focusSource) {
+		this.focusSource = focusSource;
+	}
+
+	@Override
+	public Optional<IUserInputHandler> handleUserInput(Screen screen, IGuiProperties guiProperties, UserInput input, IInternalKeyMappings keyBindings) {
+		List<IClickableIngredientInternal<?>> ingredientUnderMouse = focusSource.getIngredientUnderMouse(input, keyBindings)
+			.toList();
+
+		for (IClickableIngredientInternal<?> clicked : ingredientUnderMouse) {
+			IElement<?> element = clicked.getElement();
+			if (element.handleClick(input, keyBindings)) {
+				IUserInputHandler result = new SameElementInputHandler(this, clicked::isMouseOver);
+				return Optional.of(result);
+			}
+		}
+		return Optional.empty();
+	}
+}
