2634c92cd53bf11fd2f65787d7ac8ac95d9889c3
mezz <tehgeek@gmail.com>
2026-09-14 21:10:40 +0900

    Fix #4490 Prioritize recipe bookmark transfer input
    (cherry picked from commit 3f0dad3ea858883fbb1ec9aad997ada87b53b182)
    (cherry picked from commit afacc57b2c8685e6c771ead4f273e0664752a61d)
    (cherry picked from commit a8f68a26ec7aed61855d94c8eadff408612f3ef4)

--- Gui/src/main/java/mezz/jei/gui/startup/JeiGuiStarter.java
+++ Gui/src/main/java/mezz/jei/gui/startup/JeiGuiStarter.java
@@ -51,6 +51,7 @@ import mezz.jei.gui.input.handlers.ChatLinkInputHandler;
 import mezz.jei.gui.input.handlers.CheatInputHandler;
 import mezz.jei.gui.input.handlers.DragRouter;
 import mezz.jei.gui.input.handlers.EditInputHandler;
+import mezz.jei.gui.input.handlers.ElementInputHandler;
 import mezz.jei.gui.input.handlers.FocusInputHandler;
 import mezz.jei.gui.input.handlers.GlobalInputHandler;
 import mezz.jei.gui.input.handlers.GuiAreaInputHandler;
@@ -271,6 +272,7 @@ public class JeiGuiStarter {
 			ingredientListOverlay.createDeleteItemInputHandler(),
 			bookmarkOverlay.createDeleteItemInputHandler(),
 			new CheatInputHandler(recipeFocusSource, clientConfig, ingredientManager, worldConfig, serverConnection),
+			new ElementInputHandler(recipeFocusSource),
 			ingredientListOverlay.createInputHandler(),
 			bookmarkOverlay.createInputHandler(),
 			new FocusInputHandler(recipeFocusSource, recipesGui, focusUtil, ingredientManager),
