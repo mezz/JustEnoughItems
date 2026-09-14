490b69b433772f19d90a97056b9a8c2117c4e168
mezz <tehgeek@gmail.com>
2026-09-14 22:20:20 +0900

    Fix #4488 Preserve focused ingredient order
    (cherry picked from commit 2058e21cea28b5c56cdda14be2936929b425a930)

--- Library/src/main/java/mezz/jei/library/gui/recipes/layout/builder/RecipeSlotBuilder.java
+++ Library/src/main/java/mezz/jei/library/gui/recipes/layout/builder/RecipeSlotBuilder.java
@@ -345,8 +345,8 @@ public class RecipeSlotBuilder implements IRecipeSlotBuilder {
 		}
 
 		List<@Nullable SlotIngredient<?>> focusedIngredients = new ArrayList<>();
-		for (Integer i : focusMatches) {
-			if (i < allIngredients.size()) {
+		for (int i = 0; i < allIngredients.size(); i++) {
+			if (focusMatches.contains(i)) {
 				focusedIngredients.add(allIngredients.get(i));
 			}
 		}
