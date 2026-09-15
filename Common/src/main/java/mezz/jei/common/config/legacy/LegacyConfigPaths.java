3ced9c90e1c32df9a9361c328eca909bf33d77e7
mezz <tehgeek@gmail.com>
2026-09-15 23:37:25 +0900

    Fix #4491 Fix crash from client config registration during datagen
    (cherry picked from commit bf7b64d24f18b957ffdb806227b6077a2088eaff)

--- Common/src/main/java/mezz/jei/common/config/legacy/LegacyConfigPaths.java
+++ Common/src/main/java/mezz/jei/common/config/legacy/LegacyConfigPaths.java
@@ -5,16 +5,21 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
+import org.jspecify.annotations.Nullable;
+
 public final class LegacyConfigPaths {
 	private LegacyConfigPaths() {}
 
-	public static List<Path> get(Path jeiConfigDirectory, UUID profileId, String fileName) {
+	public static List<Path> get(Path jeiConfigDirectory, @Nullable UUID profileId, String fileName) {
 		jeiConfigDirectory = Objects.requireNonNull(jeiConfigDirectory).toAbsolutePath().normalize();
-		Objects.requireNonNull(profileId);
 		Objects.requireNonNull(fileName);
 
-		Path legacyProfileFile = jeiConfigDirectory.resolve("players").resolve(profileId.toString()).resolve(fileName);
 		Path legacyFile = jeiConfigDirectory.resolve(fileName);
+		if (profileId == null) {
+			return List.of(legacyFile);
+		}
+
+		Path legacyProfileFile = jeiConfigDirectory.resolve("players").resolve(profileId.toString()).resolve(fileName);
 		return List.of(legacyProfileFile, legacyFile);
 	}
 }
--- Gui/src/main/java/mezz/jei/gui/config/JeiGuiSortingConfigRegistration.java
+++ Gui/src/main/java/mezz/jei/gui/config/JeiGuiSortingConfigRegistration.java
@@ -5,6 +5,7 @@ import mezz.jei.common.platform.Services;
 import net.mezzdev.config.api.Configs;
 import net.mezzdev.config.api.IConfigRegistration;
 import net.minecraft.client.Minecraft;
+import org.jspecify.annotations.Nullable;
 
 import java.nio.file.Path;
 import java.util.UUID;
@@ -17,7 +18,12 @@ public final class JeiGuiSortingConfigRegistration {
 	public static JeiGuiSortingConfigData register() {
 		IConfigRegistration registration = Configs.forMod(ModIds.JEI_ID);
 		Path jeiConfigDirectory = Services.PLATFORM.getConfigHelper().createJeiConfigDir();
-		UUID profileId = Minecraft.getInstance().getUser().getProfileId();
+		Minecraft minecraft = Minecraft.getInstance();
+		@Nullable
+		UUID profileId = null;
+		if (minecraft != null) {
+			profileId = minecraft.getUser().getProfileId();
+		}
 		return new JeiGuiSortingConfigData(
 			ModNameSortingConfig.create(registration, jeiConfigDirectory, profileId),
 			IngredientTypeSortingConfig.create(registration, jeiConfigDirectory, profileId)
